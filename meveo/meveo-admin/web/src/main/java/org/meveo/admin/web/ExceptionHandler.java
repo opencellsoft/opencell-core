c400b1710c2a0828f8a215137ad745a1e7e9b1e3
Andrius <andrius@manaty.lt>
2013-03-19 00:32:34 -0300

    Web module changes

---
+++ meveo/meveo-admin/web/src/main/java/org/meveo/admin/web/ExceptionHandler.java
@@ -0,0 +1,76 @@
+package org.meveo.admin.web;
+
+import java.io.IOException;
+
+import javax.enterprise.context.NonexistentConversationException;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import org.jboss.seam.security.AuthorizationException;
+import org.jboss.solder.exception.control.CaughtException;
+import org.jboss.solder.exception.control.Handles;
+import org.jboss.solder.exception.control.HandlesExceptions;
+import org.jboss.solder.servlet.http.ContextPath;
+
+@HandlesExceptions
+public class ExceptionHandler {
+
+    @Inject
+    private FacesContext facesContext;
+
+    // @Inject
+    // private Instance<HttpConversationContext> contextInstance;
+
+    @Inject
+    @ContextPath
+    private String contextPath;
+
+    public void handleAuthorizationException(@Handles CaughtException<AuthorizationException> evt) {
+
+        evt.handled();
+
+        try {
+            facesContext.getExternalContext().redirect("/errors/403.xhtml");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleInvalidConversationException(@Handles CaughtException<NonexistentConversationException> evt) {
+
+        evt.handled();
+
+        try {
+            facesContext.getExternalContext().redirect("/errors/sessionExpired.jsf");
+            // facesContext.getExternalContext().redirect(contextPath + "/errors/sessionExpired.jsf");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleViewExpiredException(@Handles CaughtException<javax.faces.application.ViewExpiredException> evt) {
+
+        evt.handled();
+
+        try {
+            facesContext.getExternalContext().redirect("/errors/sessionExpired.jsf");
+            facesContext.getExternalContext().redirect(contextPath + "/errors/sessionExpired.jsf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // public void handleIllegalStateException(@Handles CaughtException<IllegalStateException> evt) {
+    //
+    // evt.handled();
+    //
+    // try {
+    // facesContext.getExternalContext().redirect(contextPath + "/errors/sessionExpired.jsf");
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // }
+
+}
