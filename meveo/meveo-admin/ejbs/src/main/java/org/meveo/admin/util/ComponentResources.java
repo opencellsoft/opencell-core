a8b8ee618a3af93a942cab660e62a74c904108d6
edward.legaspi <edward.legaspi@manaty.net>
2013-03-26 13:57:18 +0800

    Activate slf4j logger.

--- meveo/meveo-admin/ejbs/src/main/java/org/meveo/admin/util/ComponentResources.java
+++ meveo/meveo-admin/ejbs/src/main/java/org/meveo/admin/util/ComponentResources.java
@@ -11,28 +11,27 @@ import javax.faces.context.FacesContext;
 
 import org.meveo.commons.utils.ParamBean;
 import org.slf4j.Logger;
-//import org.jboss.solder.logging.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ComponentResources implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Produces
-    public ResourceBundle getResourceBundle() {
-        Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
-        ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", locale);
-        return resourceBundle;
-    }
+	@Produces
+	public ResourceBundle getResourceBundle() {
+		Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", locale);
+		return resourceBundle;
+	}
 
-    @Produces
-    @ApplicationScoped
-    public ParamBean getParamBean() {
-        return ParamBean.getInstance("meveo-admin");
-    }
+	@Produces
+	@ApplicationScoped
+	public ParamBean getParamBean() {
+		return ParamBean.getInstance("meveo-admin");
+	}
 
-    @Produces
-    Logger createLogger(InjectionPoint injectionPoint) {
-        return null;
-//        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
-    }
+	@Produces
+	Logger createLogger(InjectionPoint injectionPoint) {
+		return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+	}
 }
