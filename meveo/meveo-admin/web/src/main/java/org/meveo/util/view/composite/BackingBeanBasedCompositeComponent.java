66adc6c225cf57af3cc0381742614eb72c046ffa
edward.legaspi <edward.legaspi@manaty.net>
2013-03-28 20:03:44 +0800

    Recursively look for a field to a class and it's superclass.

--- meveo/meveo-admin/web/src/main/java/org/meveo/util/view/composite/BackingBeanBasedCompositeComponent.java
+++ meveo/meveo-admin/web/src/main/java/org/meveo/util/view/composite/BackingBeanBasedCompositeComponent.java
@@ -20,181 +20,199 @@ import org.meveo.model.IEntity;
  */
 public class BackingBeanBasedCompositeComponent extends UINamingContainer {
 
-    private static final String BOOLEAN_TRUE_STRING = "true";
+	private static final String BOOLEAN_TRUE_STRING = "true";
 
-    private BaseBean<? extends IEntity> backingBean;
+	private BaseBean<? extends IEntity> backingBean;
 
-    private IEntity entity;
-    
-    @Inject
-    ParamBean paramBean;
+	private IEntity entity;
 
-    /**
-     * Get backing bean attribute either from parent component (search panel,
-     * thats where it usually should be defined) or from searchField component
-     * attributes (same with formPanel and formField).
-     */
-    @SuppressWarnings("unchecked")
-    public BaseBean<? extends IEntity> getBackingBeanFromParentOrCurrent() {
-        if (backingBean == null) {
-            UIComponent parent = getCompositeComponentParent(this);
-            if (parent != null) {
-                backingBean = (BaseBean<? extends IEntity>) parent.getAttributes().get("backingBean");
-            }
-            if (backingBean == null) {
-                backingBean = (BaseBean<? extends IEntity>) getAttributes().get("backingBean");
-            }
-            if (backingBean == null) {
-                throw new IllegalStateException("No backing bean was set in parent or current composite component!");
-            }
-        }
-        return backingBean;
-    }
+	@Inject
+	ParamBean paramBean;
 
-    /**
-     * Helper method to get entity from backing bean.
-     */
-    public IEntity getEntityFromBackingBeanOrAttribute() {
-        if (entity == null) {
-            entity = (IEntity) getAttributes().get("entity");
-            if (entity == null) {
-                entity = getBackingBeanFromParentOrCurrent().getEntity();
-            }
-        }
-        return entity;
-    }
+	/**
+	 * Get backing bean attribute either from parent component (search panel,
+	 * thats where it usually should be defined) or from searchField component
+	 * attributes (same with formPanel and formField).
+	 */
+	@SuppressWarnings("unchecked")
+	public BaseBean<? extends IEntity> getBackingBeanFromParentOrCurrent() {
+		if (backingBean == null) {
+			UIComponent parent = getCompositeComponentParent(this);
+			if (parent != null) {
+				backingBean = (BaseBean<? extends IEntity>) parent.getAttributes().get(
+						"backingBean");
+			}
+			if (backingBean == null) {
+				backingBean = (BaseBean<? extends IEntity>) getAttributes().get("backingBean");
+			}
+			if (backingBean == null) {
+				throw new IllegalStateException(
+						"No backing bean was set in parent or current composite component!");
+			}
+		}
+		return backingBean;
+	}
 
-    /**
-     * Return date pattern to use for rendered date/calendar fields. If time
-     * attribute was set to true then this methods returns date/time pattern,
-     * otherwise only date without time pattern.
-     */
-    public String getDatePattern() {
-        if (getAttributes().get("time").equals(BOOLEAN_TRUE_STRING)) {
-            return paramBean.getProperty("meveo.dateTimeFormat");
-        } else {
-            return paramBean.getProperty("meveo.dateFormat");
-        }
-    }
+	/**
+	 * Helper method to get entity from backing bean.
+	 */
+	public IEntity getEntityFromBackingBeanOrAttribute() {
+		if (entity == null) {
+			entity = (IEntity) getAttributes().get("entity");
+			if (entity == null) {
+				entity = getBackingBeanFromParentOrCurrent().getEntity();
+			}
+		}
+		return entity;
+	}
 
-    public boolean isText(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            return field.getType() == String.class;
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	/**
+	 * Return date pattern to use for rendered date/calendar fields. If time
+	 * attribute was set to true then this methods returns date/time pattern,
+	 * otherwise only date without time pattern.
+	 */
+	public String getDatePattern() {
+		if (getAttributes().get("time").equals(BOOLEAN_TRUE_STRING)) {
+			return paramBean.getProperty("meveo.dateTimeFormat");
+		} else {
+			return paramBean.getProperty("meveo.dateFormat");
+		}
+	}
 
-    public boolean isBoolean(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == Boolean.class || (type.isPrimitive() && type.getName().equals("boolean"));
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isText(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			return field.getType() == String.class;
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isDate(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            return field.getType() == Date.class;
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isBoolean(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == Boolean.class
+					|| (type.isPrimitive() && type.getName().equals("boolean"));
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isEnum(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            return field.getType().isEnum();
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isDate(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			return field.getType() == Date.class;
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isInteger(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == Integer.class || (type.isPrimitive() && type.getName().equals("int"));
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isEnum(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			return field.getType().isEnum();
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isLong(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == Long.class || (type.isPrimitive() && type.getName().equals("long"));
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isInteger(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == Integer.class || (type.isPrimitive() && type.getName().equals("int"));
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isByte(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == Byte.class || (type.isPrimitive() && type.getName().equals("byte"));
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isLong(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == Long.class || (type.isPrimitive() && type.getName().equals("long"));
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isShort(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == Short.class || (type.isPrimitive() && type.getName().equals("short"));
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isByte(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == Byte.class || (type.isPrimitive() && type.getName().equals("byte"));
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isDouble(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == Double.class || (type.isPrimitive() && type.getName().equals("double"));
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isShort(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == Short.class || (type.isPrimitive() && type.getName().equals("short"));
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isFloat(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == Float.class || (type.isPrimitive() && type.getName().equals("float"));
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isDouble(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == Double.class || (type.isPrimitive() && type.getName().equals("double"));
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isBigDecimal(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            return field.getType() == BigDecimal.class;
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isFloat(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == Float.class || (type.isPrimitive() && type.getName().equals("float"));
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isEntity(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            return IEntity.class.isAssignableFrom(field.getType());
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isBigDecimal(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			return field.getType() == BigDecimal.class;
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public boolean isList(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null) {
-            Class<?> type = field.getType();
-            return type == List.class || type == Set.class;
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isEntity(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			return IEntity.class.isAssignableFrom(field.getType());
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
-    public Object[] getEnumConstants(String fieldName) throws SecurityException, NoSuchFieldException {
-        Field field = getEntityFromBackingBeanOrAttribute().getClass().getDeclaredField(fieldName);
-        if (field != null && field.getType().isEnum()) {
-            return field.getType().getEnumConstants();
-        }
-        throw new IllegalStateException("No field with name '" + fieldName + "' was found");
-    }
+	public boolean isList(String fieldName) throws SecurityException, NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null) {
+			Class<?> type = field.getType();
+			return type == List.class || type == Set.class;
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
 
+	public Object[] getEnumConstants(String fieldName) throws SecurityException,
+			NoSuchFieldException {
+		Field field = getBeanField(getEntityFromBackingBeanOrAttribute().getClass(), fieldName);
+		if (field != null && field.getType().isEnum()) {
+			return field.getType().getEnumConstants();
+		}
+		throw new IllegalStateException("No field with name '" + fieldName + "' was found");
+	}
+
+	private Field getBeanField(Class<?> c, String fieldName) throws SecurityException,
+			NoSuchFieldException {
+		Field field = null;
+		try {
+			field = c.getDeclaredField(fieldName);
+		} catch (NoSuchFieldException e) {
+			if (field == null
+					&& getEntityFromBackingBeanOrAttribute().getClass().getSuperclass() != null) {
+				return getBeanField(c.getSuperclass(), fieldName);
+			}
+		}
+
+		return field;
+	}
 }
