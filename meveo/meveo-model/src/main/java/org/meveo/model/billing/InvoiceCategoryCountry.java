e7178fcefa171b6827e0d50eb3c8951fd98efdad
mbarek <mbarek.omoh@manaty.net>
2013-04-05 10:21:24 +0000

    add getter and setter for tradingCountry

--- meveo/meveo-model/src/main/java/org/meveo/model/billing/InvoiceCategoryCountry.java
+++ meveo/meveo-model/src/main/java/org/meveo/model/billing/InvoiceCategoryCountry.java
@@ -53,6 +53,16 @@ public class InvoiceCategoryCountry  extends AuditableEntity{
 	    private TradingCountry tradingCountry; 
 	    
 	 
+	public TradingCountry getTradingCountry() {
+		return tradingCountry;
+	}
+
+
+	public void setTradingCountry(TradingCountry tradingCountry) {
+		this.tradingCountry = tradingCountry;
+	}
+
+
 	@Column(name = "DISCOUNT_CODE", length = 20)
 	private String discountCode;
 	
