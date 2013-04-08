65ea66832f5cf7ffc6215a18ef9ce0ba0328c2cc
Andrius <andrius@manaty.lt>
2013-04-08 03:10:11 -0300

    Migrated change password screen

--- meveo/meveo-admin/ejbs/src/main/java/org/meveo/admin/action/admin/ChangePasswordAction.java
+++ meveo/meveo-admin/ejbs/src/main/java/org/meveo/admin/action/admin/ChangePasswordAction.java
@@ -17,15 +17,15 @@ package org.meveo.admin.action.admin;
 
 import java.io.Serializable;
 
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.inject.Model;
 import javax.inject.Inject;
-import javax.inject.Named;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.seam.international.status.Messages;
 import org.jboss.seam.international.status.builder.BundleKey;
 import org.jboss.seam.security.Identity;
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.admin.exception.LoginException;
 import org.meveo.admin.util.security.Sha1Encrypt;
 import org.meveo.model.admin.User;
 import org.meveo.security.MeveoUser;
@@ -37,103 +37,112 @@ import org.slf4j.Logger;
  * @author Gediminas Ubartas
  * @created 2010.12.08
  */
-@Named
-@ConversationScoped
+@Model
 public class ChangePasswordAction implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     protected Logger log;
 
-	@Inject
-	private Identity identity;
+    @Inject
+    private Identity identity;
 
-	@Inject
-	private UserService userService;
+    @Inject
+    private UserService userService;
 
-	@Inject
-	protected Messages messages;
+    @Inject
+    protected Messages messages;
 
-	private User currentUser;
-	private String currentPassword;
+    private String username;
 
-	/*
-	 * //TODO: Conversation. @Begin(join = true)
-	 * 
-	 * @Create
-	 */
-	/*
-	 * public void init() { if (currentUser != null) user =
-	 * userService.findById(currentUser.getId()); else user = new User();
-	 * 
-	 * currentPassword = ""; }
-	 */
+    private String currentPassword;
 
-	@PostConstruct
-	private void init() {
-	    if (identity.isLoggedIn()){
-	        currentUser = ((MeveoUser) identity.getUser()).getUser();
-	    } else {
-	        currentUser=new User();
-	    }
-	}
+    private String newPassword;
 
-	public String update() {
-		String result = null;
-		
-		if (currentUser == null) {
-			User tempUser = userService.findByUsernameAndPassword(currentUser.getUserName(),
-					currentPassword);
-			if (tempUser != null) {
-				tempUser.setNewPassword(currentUser.getNewPassword());
-				tempUser.setNewPasswordConfirmation(currentUser.getNewPasswordConfirmation());
-				currentUser = tempUser;
-			} else {
-				messages.error(new BundleKey("messages", "changePassword.err.badUsernameOrPassword"));
-				return null;
-			}
-		}
-		
-		if (validate()) {
-			try {
-				userService.changePassword(currentUser, currentUser.getNewPassword());
-				messages.error(new BundleKey("messages", "changePassword.err.badUsernameOrPassword"));
-			} catch (BusinessException e) {
-				log.error("Error when update the password of #{currentUser.username} with password="
-						+ currentPassword);
-			}
-			result = "success";
-		}
-		currentPassword = "";
+    private String newPasswordConfirmation;
 
-		return result;
-	}
+    public String update() {
 
-	private boolean validate() {
-		if (!Sha1Encrypt.encodePassword(currentPassword).equals(currentUser.getPassword())) {
-			messages.error(new BundleKey("messages", "changePassword.err.currentPasswordIncorrect"));
-			return false;
-		}
+        User currentUser = null;
+        if (identity.isLoggedIn()) {
+            currentUser = userService.findById(((MeveoUser) identity.getUser()).getUser().getId());
 
-		if (Sha1Encrypt.encodePassword(currentUser.getNewPassword()).equals(
-				currentUser.getPassword())) {
-			messages.error(new BundleKey("messages", "changePassword.err.passwordMustBeDifferent"));
-			return false;
-		}
+        } else {
+            try {
+                currentUser = userService.loginChecks(username, currentPassword, true);
 
-		if (!currentUser.getNewPassword().equals(currentUser.getNewPasswordConfirmation())) {
-			messages.error(new BundleKey("messages", "changePassword.err.confirmationFailed"));
-			return false;
-		}
+            } catch (LoginException e) {
+                messages.error(new BundleKey("messages", "changePassword.err.badUsernameOrPassword"));
+                return null;
+            }
+        }
 
-		return true;
-	}
+        if (validate(currentUser)) {
+            try {
+                userService.changePassword(currentUser, newPassword);
 
-	public String getCurrentPassword() {
-		return currentPassword;
-	}
+            } catch (BusinessException e) {
+                log.error("Error when update the password of #{currentUser.username} with password=" + currentPassword);
+                messages.error(new BundleKey("messages", "changePassword.err.badUsernameOrPassword"));
+                return null;
+            }
+            messages.info(new BundleKey("messages", "changePassword.msg.passwordChanged"));
+            return "/home.xhtml?faces-redirect=true";
 
-	public void setCurrentPassword(String currentPassword) {
-		this.currentPassword = currentPassword;
-	}
+        } else {
+            return null;
+        }
+
+    }
+
+    private boolean validate(User currentUser) {
+        if (!Sha1Encrypt.encodePassword(currentPassword).equals(currentUser.getPassword())) {
+            messages.error(new BundleKey("messages", "changePassword.err.currentPasswordIncorrect"));
+            return false;
+        }
+
+        if (Sha1Encrypt.encodePassword(newPassword).equals(currentUser.getPassword())) {
+            messages.error(new BundleKey("messages", "changePassword.err.passwordMustBeDifferent"));
+            return false;
+        }
+
+        if (!StringUtils.equals(newPassword, newPasswordConfirmation)) {
+            messages.error(new BundleKey("messages", "changePassword.err.confirmationFailed"));
+            return false;
+        }
+
+        return true;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getNewPasswordConfirmation() {
+        return newPasswordConfirmation;
+    }
+
+    public void setNewPasswordConfirmation(String newPasswordConfirmation) {
+        this.newPasswordConfirmation = newPasswordConfirmation;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
