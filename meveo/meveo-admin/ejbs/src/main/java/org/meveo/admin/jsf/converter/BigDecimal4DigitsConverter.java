249a17e1c9ba93bfe6a6cbfeb9364ac5c361d91b
edward.legaspi <edward.legaspi@manaty.net>
2013-05-23 17:45:30 +0800

    Test #240.

--- meveo/meveo-admin/ejbs/src/main/java/org/meveo/admin/jsf/converter/BigDecimal4DigitsConverter.java
+++ meveo/meveo-admin/ejbs/src/main/java/org/meveo/admin/jsf/converter/BigDecimal4DigitsConverter.java
@@ -1,36 +1,35 @@
 /*
-* (C) Copyright 2009-2013 Manaty SARL (http://manaty.net/) and contributors.
-*
-* Licensed under the GNU Public Licence, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.gnu.org/licenses/gpl-2.0.txt
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * (C) Copyright 2009-2013 Manaty SARL (http://manaty.net/) and contributors.
+ *
+ * Licensed under the GNU Public Licence, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.meveo.admin.jsf.converter;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
+import javax.faces.context.FacesContext;
 import javax.faces.convert.FacesConverter;
 
-/**
- * 
- * @author anasseh
- * @created 18.01.2011
- */
 @FacesConverter("bigDecimal4DigitsConverter")
 public class BigDecimal4DigitsConverter extends BigDecimalConverter {
 
-    private DecimalFormat format = new DecimalFormat("#,##0.0000");
+	@Override
+	protected DecimalFormat getDecimalFormat() {
+		DecimalFormatSymbols decimalFormatSymbol = new DecimalFormatSymbols(FacesContext
+				.getCurrentInstance().getViewRoot().getLocale());
+		DecimalFormat format = new DecimalFormat("#,##0.0000", decimalFormatSymbol);
 
-    @Override
-    protected DecimalFormat getDecimalFormat() {
-        return format;
-    }
+		return format;
+	}
 }
