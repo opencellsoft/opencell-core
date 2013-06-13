d076fd045d270214a1ec3ff75f0acaae13345706
smichea <smichea@manaty.net>
2013-06-13 05:05:03 +0200

    forgot to commit this one

---
+++ meveo/meveo-admin/ejbs/src/main/java/org/meveo/services/job/TimerEntityConverter.java
@@ -0,0 +1,32 @@
+package org.meveo.services.job;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+import org.meveo.model.jobs.TimerEntity;
+
+
+@FacesConverter(forClass=TimerEntity.class,value="timerEntityConverter")
+public class TimerEntityConverter implements Converter {
+
+	@Inject
+	TimerEntityService timerEntityService;
+	
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+   	 if (value == null || value.length() == 0) {
+         return null;
+     }
+   	 return timerEntityService.findById(Long.parseLong(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+
+    	TimerEntity timerEntity = (TimerEntity) value;
+        return timerEntity==null?"":timerEntity.getId().toString();
+    }
+}
