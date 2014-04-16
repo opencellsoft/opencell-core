1d5516e896a0e6a2d638ea8a5e57b323757996ad
smichea <sebastien.michea@manaty.net>
2014-04-16 16:32:33 +0200

    fix conversation in properties page

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/action/ParamActionBean.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/action/ParamActionBean.java
@@ -1,22 +1,33 @@
 package org.meveo.admin.action;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.jboss.seam.international.status.Messages;
-import org.jboss.seam.international.status.builder.BundleKey;
 import org.meveo.commons.utils.ParamBean;
 import org.meveo.model.ParamProperty;
+import org.primefaces.component.datatable.DataTable;
+import org.primefaces.event.CellEditEvent;
 
 @Named
-public class ParamActionBean {
+@ConversationScoped
+public class ParamActionBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4570971790276879220L;
 
 	@Inject
-	protected Messages messages;
+	protected Conversation conversation;
 	
 	@Inject
 	private org.slf4j.Logger log;
@@ -25,19 +36,28 @@ public class ParamActionBean {
 	
 	private List<ParamProperty> properties = null;
 	
+	private void beginConversation() {
+		if (conversation.isTransient()) {
+			conversation.begin();
+		}
+	}
+	
+	public void preRenderView(){
+		beginConversation();
+	}
+	
 	public void reset(){
 		log.debug("load properties from paramBean");
 		properties = new ArrayList<ParamProperty>();
 		Set<Object> keys=paramBean.getProperties().keySet();
 		if(keys!=null){
 			for(Object key:keys){
-				ParamProperty paramProp=new ParamProperty();
+				ParamProperty paramProp=new ParamProperty(log);
 				paramProp.setKey(key.toString());
 				paramProp.setValue(paramBean.getProperties().getProperty(key.toString()));
 				properties.add(paramProp);
 			}
 		}
-		messages.info(new BundleKey("messages", "properties.reset.successful"));
 	}
 	
 	public List<ParamProperty> getProperties(){
@@ -47,12 +67,28 @@ public class ParamActionBean {
 		return properties;
 	}
 	
+	public void setProperties(List<ParamProperty> properties){
+		this.properties=properties;
+	}
+	
 	public void save(){
-		log.info("update and save paramBean properties");
+		log.info("update and save paramBean properties "+properties.size());
 		for(ParamProperty property:properties){
+			log.info(property.getKey()+"->"+property.getValue());
 			paramBean.setProperty(property.getKey(), property.getValue());
 		}
 		paramBean.saveProperties();
-		messages.info(new BundleKey("messages", "properties.save.successful"));
+		reset();
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "success", "properties.save.successful");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+	}
+	
+	public void onCellEdit(CellEditEvent event) {
+	  Object oldValue = event.getOldValue();  
+      Object newValue = event.getNewValue();  
+      DataTable o=(DataTable) event.getSource();
+      ParamProperty property=(ParamProperty) o.getRowData();
+      property.setValue(newValue==null?null:newValue.toString());
+      log.debug("Old: " + oldValue + ", New:" + newValue);
 	}
 }
--- meveo-admin/ejbs/src/main/java/org/meveo/model/ParamProperty.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/model/ParamProperty.java
@@ -2,15 +2,22 @@ package org.meveo.model;
 
 public class ParamProperty {
 
+	private org.slf4j.Logger log;
+	
 	private String key;
 	
 	private String value;
+	
+	public ParamProperty(org.slf4j.Logger log){
+		this.log=log;
+	}
 
 	public String getKey() {
 		return key;
 	}
 
 	public void setKey(String key) {
+		log.debug("setKey :"+key);
 		this.key = key;
 	}
 
@@ -19,6 +26,7 @@ public class ParamProperty {
 	}
 
 	public void setValue(String value) {
+		log.debug("setValue :"+key+" -> "+value);
 		this.value = value;
 	}
 	
