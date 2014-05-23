c9bf5db8c780b8138ec50f29d897ee6e6a502bac
smichea <sebastien.michea@manaty.net>
2014-05-23 15:42:51 +0200

    added entity manager in job

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/job/ReccuringRatingJob.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/ReccuringRatingJob.java
@@ -1,6 +1,5 @@
 package org.meveo.admin.job;
 
-import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -16,36 +15,34 @@ import javax.ejb.TimerConfig;
 import javax.ejb.TimerHandle;
 import javax.ejb.TimerService;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import org.meveo.admin.exception.IncorrectChargeTemplateException;
-import org.meveo.model.billing.ApplicationTypeEnum;
 import org.meveo.model.billing.InstanceStatusEnum;
-import org.meveo.model.billing.InvoiceSubCategory;
-import org.meveo.model.billing.InvoiceSubcategoryCountry;
 import org.meveo.model.billing.RecurringChargeInstance;
-import org.meveo.model.billing.Tax;
-import org.meveo.model.billing.TradingCountry;
-import org.meveo.model.billing.TradingCurrency;
-import org.meveo.model.billing.WalletOperation;
 import org.meveo.model.catalog.RecurringChargeTemplate;
 import org.meveo.model.crm.Provider;
 import org.meveo.model.jobs.JobExecutionResult;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.jobs.TimerInfo;
 import org.meveo.model.shared.DateUtils;
-import org.meveo.service.billing.impl.InvoiceSubCategoryCountryService;
-import org.meveo.service.billing.impl.RatingService;
 import org.meveo.service.billing.impl.RecurringChargeInstanceService;
 import org.meveo.service.billing.impl.WalletOperationService;
 import org.meveo.service.crm.impl.ProviderService;
 import org.meveo.services.job.Job;
 import org.meveo.services.job.JobExecutionService;
 import org.meveo.services.job.TimerEntityService;
+import org.meveo.util.MeveoJpaForJobs;
 import org.slf4j.Logger;
 
 @Startup
 @Singleton
 public class ReccuringRatingJob implements Job {
+
+
+	@Inject
+	@MeveoJpaForJobs
+	private EntityManager em;
 
 	@Resource
 	TimerService timerService;
@@ -62,12 +59,6 @@ public class ReccuringRatingJob implements Job {
 	@Inject
 	private WalletOperationService walletOperationService;
 
-	@Inject
-	private RatingService chargeApplicationRatingService;
-
-	@Inject
-	private InvoiceSubCategoryCountryService invoiceSubCategoryCountryService;
-	
     @Inject
     protected Logger log;
     
@@ -108,12 +99,12 @@ public class ReccuringRatingJob implements Job {
 
 					if (!recurringChargeTemplate.getApplyInAdvance()) {
 						walletOperationService
-								.applyNotAppliedinAdvanceReccuringCharge(
+								.applyNotAppliedinAdvanceReccuringCharge(em,
 										activeRecurringChargeInstance, false,
 										recurringChargeTemplate, null);
 						result.registerSucces();
 					} else {
-						walletOperationService.applyReccuringCharge(activeRecurringChargeInstance, false, recurringChargeTemplate, null);
+						walletOperationService.applyReccuringCharge(em,activeRecurringChargeInstance, false, recurringChargeTemplate, null);
 						result.registerSucces();
 					}
 				} catch (Exception e) {
