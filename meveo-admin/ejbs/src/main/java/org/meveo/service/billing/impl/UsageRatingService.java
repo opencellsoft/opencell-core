26984617ed8355197a17d93cda5c2767782882a6
edward.legaspi <czetsuya@gmail.com>
2014-07-11 10:44:33 +0800

    Fixed rating bug.

--- meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/UsageRatingService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/UsageRatingService.java
@@ -304,7 +304,7 @@ public class UsageRatingService {
 	 * @throws BusinessException
 	 */
 	public WalletOperation rateEDRwithMatchingCharge(EDR edr,
-			UsageChargeInstanceCache chargeCache,
+			BigDecimal deducedQuantity, UsageChargeInstanceCache chargeCache,
 			UsageChargeInstance chargeInstance, Provider provider)
 			throws BusinessException {
 		WalletOperation walletOperation = new WalletOperation();
@@ -341,7 +341,11 @@ public class UsageRatingService {
 		walletOperation.setWallet(edr.getSubscription().getUserAccount()
 				.getWallet());
 		walletOperation.setCode(chargeInstance.getCode());
-		walletOperation.setQuantity(edr.getQuantity());
+		if (deducedQuantity != null) {
+			walletOperation.setQuantity(deducedQuantity);
+		} else {
+			walletOperation.setQuantity(edr.getQuantity());
+		}
 		walletOperation.setTaxPercent(tax.getPercent());
 		walletOperation.setStartDate(null);
 		walletOperation.setEndDate(null);
@@ -467,7 +471,7 @@ public class UsageRatingService {
 			UsageChargeInstance chargeInstance = usageChargeInstanceService
 					.findById(charge.getChargeInstanceId());
 			WalletOperation walletOperation = rateEDRwithMatchingCharge(edr,
-					charge, chargeInstance, provider);
+					deducedQuantity, charge, chargeInstance, provider);
 			if (deducedQuantity != null) {
 				edr.setQuantity(edr.getQuantity().subtract(deducedQuantity));
 				walletOperation.setQuantity(deducedQuantity);
