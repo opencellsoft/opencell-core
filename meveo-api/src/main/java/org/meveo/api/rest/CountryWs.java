3d42c3e436e4401d87238113ca14c450af39427a
Edward P. Legaspi <czetsuya@gmail.com>
2014-11-04 22:16:34 +0800

    Add arquillian testing.

--- meveo-api/src/main/java/org/meveo/api/rest/CountryWs.java
+++ meveo-api/src/main/java/org/meveo/api/rest/CountryWs.java
@@ -1,164 +1,26 @@
 package org.meveo.api.rest;
 
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.interceptor.Interceptors;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import org.meveo.api.ActionStatus;
-import org.meveo.api.ActionStatusEnum;
-import org.meveo.api.CountryServiceApi;
-import org.meveo.api.MeveoApiErrorCode;
-import org.meveo.api.dto.CountryDto;
-import org.meveo.api.exception.CountryDoesNotExistsException;
-import org.meveo.api.exception.CurrencyDoesNotExistsException;
-import org.meveo.api.exception.MissingParameterException;
-import org.meveo.api.exception.TradingCountryAlreadyExistsException;
-import org.meveo.api.exception.TradingCountryDoesNotExistsException;
-import org.meveo.api.logging.LoggingInterceptor;
-import org.meveo.api.response.GetCountryResponse;
-import org.meveo.api.rest.security.WSSecured;
-import org.meveo.commons.utils.ParamBean;
+import org.meveo.api.dto.response.GetCountryResponse;
 
 /**
- * Web service for managing {@link org.meveo.model.billing.Country} and {@link
- * org.meveo.model.billing.TradingCountry}.
- * 
  * @author Edward P. Legaspi
  **/
 @Path("/country")
 @RequestScoped
 @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-@Interceptors({ LoggingInterceptor.class })
-//@WSSecured
-public class CountryWs extends BaseWs {
-
-	@Inject
-	private ParamBean paramBean;
-
-	@Inject
-	private CountryServiceApi countryServiceApi;
-
-	/***
-	 * Creates an instance of @see TradingCountry base on @see Country.
-	 * 
-	 * @param countryDto
-	 * @return @see ActionStatus
-	 */
-	@POST
-	@Path("/")
-	public ActionStatus create(CountryDto countryDto) {
-		ActionStatus result = new ActionStatus(ActionStatusEnum.SUCCESS, "");
-
-		try {
-			countryDto.setCurrentUser(currentUser);
-			countryServiceApi.create(countryDto);
-		} catch (MissingParameterException e) {
-			result.setErrorCode(MeveoApiErrorCode.MISSING_PARAMETER);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (CurrencyDoesNotExistsException e) {
-			result.setErrorCode(MeveoApiErrorCode.CURRENCY_DOES_NOT_EXISTS);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (TradingCountryAlreadyExistsException e) {
-			result.setErrorCode(MeveoApiErrorCode.TRADING_COUNTRY_ALREADY_EXISTS);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (Exception e) {
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		}
-
-		return result;
-	}
+public interface CountryWs {
 
 	@GET
 	@Path("/")
-	public GetCountryResponse find(@QueryParam("countryCode") String countryCode) {
-		GetCountryResponse result = new GetCountryResponse();
-		result.getActionStatus().setStatus(ActionStatusEnum.SUCCESS);
-
-		try {
-			result.setCountry(countryServiceApi.find(countryCode));
-		} catch (Exception e) {
-			result.getActionStatus().setStatus(ActionStatusEnum.FAIL);
-			result.getActionStatus().setMessage(e.getMessage());
-		}
-
-		return result;
-	}
-
-	@DELETE
-	@Path("/{countryCode}/{currencyCode}")
-	public ActionStatus remove(@PathParam("countryCode") String countryCode,
-			@PathParam("currencyCode") String currencyCode) {
-		ActionStatus result = new ActionStatus(ActionStatusEnum.SUCCESS, "");
-		Long providerId = Long.valueOf(paramBean.getProperty(
-				"asp.api.providerId", "1"));
-
-		try {
-			countryServiceApi.remove(countryCode, currencyCode, providerId);
-		} catch (MissingParameterException e) {
-			result.setErrorCode(MeveoApiErrorCode.MISSING_PARAMETER);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (TradingCountryDoesNotExistsException e) {
-			result.setErrorCode(MeveoApiErrorCode.TRADING_COUNTRY_DOES_NOT_EXISTS);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (CurrencyDoesNotExistsException e) {
-			result.setErrorCode(MeveoApiErrorCode.CURRENCY_DOES_NOT_EXISTS);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (Exception e) {
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		}
-
-		return result;
-	}
-
-	@PUT
-	@Path("/")
-	public ActionStatus update(CountryDto countryDto) {
-		ActionStatus result = new ActionStatus(ActionStatusEnum.SUCCESS, "");
-
-		try {
-			countryDto.setCurrentUser(currentUser);
-			countryServiceApi.update(countryDto);
-		} catch (MissingParameterException e) {
-			result.setErrorCode(MeveoApiErrorCode.MISSING_PARAMETER);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (CountryDoesNotExistsException e) {
-			result.setErrorCode(MeveoApiErrorCode.COUNTRY_DOES_NOT_EXISTS);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (CurrencyDoesNotExistsException e) {
-			result.setErrorCode(MeveoApiErrorCode.CURRENCY_DOES_NOT_EXISTS);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (TradingCountryDoesNotExistsException e) {
-			result.setErrorCode(MeveoApiErrorCode.TRADING_COUNTRY_DOES_NOT_EXISTS);
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		} catch (Exception e) {
-			result.setStatus(ActionStatusEnum.FAIL);
-			result.setMessage(e.getMessage());
-		}
-
-		return result;
-	}
+	public GetCountryResponse find(@QueryParam("countryCode") String countryCode);
 
 }
--- meveo-api/src/test/java/org/meveo/api/rest/CountryWsTest.java
+++ meveo-api/src/test/java/org/meveo/api/rest/CountryWsTest.java
@@ -1,26 +1,41 @@
 package org.meveo.api.rest;
 
-import java.net.URI;
+import java.io.File;
 import java.net.URL;
-
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriBuilder;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.meveo.admin.util.ComponentResources;
+import org.meveo.admin.util.pagination.PaginationConfiguration;
+import org.meveo.api.BaseApi;
+import org.meveo.api.CountryServiceApi;
+import org.meveo.api.dto.response.GetCountryResponse;
+import org.meveo.commons.utils.ParamBean;
+import org.meveo.commons.utils.QueryBuilder;
+import org.meveo.service.admin.impl.CountryService;
+import org.meveo.service.admin.impl.CurrencyService;
+import org.meveo.service.admin.impl.RoleService;
+import org.meveo.service.admin.impl.TradingCurrencyService;
+import org.meveo.service.admin.impl.UserService;
+import org.meveo.service.base.BaseService;
+import org.meveo.service.base.PersistenceService;
+import org.meveo.service.base.local.IPersistenceService;
+import org.meveo.service.billing.impl.TradingCountryService;
+import org.meveo.service.catalog.impl.TitleService;
+import org.meveo.service.crm.impl.ProviderService;
+import org.meveo.util.Resources;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.http.ContentType;
 
 /**
  * @author Edward P. Legaspi
@@ -28,17 +43,46 @@ import com.jayway.restassured.http.ContentType;
 @RunWith(Arquillian.class)
 public class CountryWsTest {
 
-	private Logger log = LoggerFactory.getLogger(CountryWsTest.class);
+	private static Logger log = LoggerFactory.getLogger(CountryWsTest.class);
+
+	@ArquillianResource
+	private URL deploymentURL;
 
 	@Deployment
 	public static Archive<?> createTestArchive() {
 		WebArchive result = ShrinkWrap.create(WebArchive.class,
-				"rest-country.war");
+				"meveo-api-country-test.war");
 
-		// File[] files = Maven.resolver().loadPomFromFile("pom.xml")
-		// .resolve("org.meveo:meveo-api").withTransitivity().asFile();
+		// add seam security
+		File[] seamDependencies = Maven.resolver()
+				.resolve("org.jboss.seam.security:seam-security:3.1.0.Final")
+				.withTransitivity().asFile();
+		result.addAsLibraries(seamDependencies);
 
-		result.addPackage("org.meveo");
+		// base services
+		result.addClasses(Resources.class, PersistenceService.class,
+				IPersistenceService.class, BaseService.class,
+				PaginationConfiguration.class, QueryBuilder.class);
+
+		// base api
+		result.addClasses(BaseApi.class, ProviderService.class,
+				UserService.class, RoleService.class, ParamBean.class,
+				ComponentResources.class, TitleService.class);
+
+		result.addClasses(CountryWs.class, CountryWsImpl.class,
+				CountryServiceApi.class, CountryService.class,
+				TradingCountryService.class, CurrencyService.class,
+				TradingCurrencyService.class);
+
+		result.addPackages(true, "org/meveo/api/dto");
+
+		// add models
+		result.addPackages(true, "org/meveo/model");
+
+		// add exceptions
+		result.addPackage("org/meveo/api/message/exception");
+		result.addPackage("org/meveo/api/exception");
+		result.addPackage("org/meveo/admin/exception");
 
 		result.addAsResource("META-INF/test-persistence.xml",
 				"META-INF/persistence.xml")
@@ -49,25 +93,12 @@ public class CountryWsTest {
 		return result;
 	}
 
-	URI buildUri(URL deploymentUrl, String... paths) {
-		UriBuilder builder = UriBuilder.fromUri(deploymentUrl.toString());
-		for (String path : paths) {
-			builder.path(path);
-		}
-		return builder.build();
-	}
-
 	@RunAsClient
 	@Test
-	public void testGetVersion(@ArquillianResource URL deploymentUrl) {
-		log.debug("Version test");
-		URI uri = buildUri(deploymentUrl, "api/rest", "country", "version");
+	public void testFind(@ArquillianResteasyResource CountryWs countryWs) {
+		GetCountryResponse result = countryWs.find("PH");
 
-		log.debug("URI={}", uri.toString());
-
-		RestAssured.given().body(new CountryWs()).contentType(ContentType.JSON)
-				.expect().contentType(ContentType.JSON)
-				.statusCode(Status.OK.getStatusCode()).when().post(uri);
+		log.debug("response" + result);
 	}
 
 }
