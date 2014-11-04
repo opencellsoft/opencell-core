54610c81f86012a3048aae7e28740084ec3aa8ad
Edward P. Legaspi <czetsuya@gmail.com>
2014-11-04 09:12:46 +0800

    Recurring rating job merge.

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/job/ReccuringRatingJob.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/ReccuringRatingJob.java
@@ -1,8 +1,6 @@
 package org.meveo.admin.job;
 
 import java.util.Collection;
-import java.util.Date;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -16,22 +14,14 @@ import javax.ejb.TimerHandle;
 import javax.ejb.TimerService;
 import javax.inject.Inject;
 
-import org.meveo.admin.exception.IncorrectChargeTemplateException;
-import org.meveo.model.billing.InstanceStatusEnum;
-import org.meveo.model.billing.RecurringChargeInstance;
-import org.meveo.model.catalog.RecurringChargeTemplate;
 import org.meveo.model.crm.Provider;
 import org.meveo.model.jobs.JobExecutionResult;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.jobs.TimerInfo;
-import org.meveo.model.shared.DateUtils;
-import org.meveo.service.billing.impl.RecurringChargeInstanceService;
-import org.meveo.service.billing.impl.WalletOperationService;
 import org.meveo.service.crm.impl.ProviderService;
 import org.meveo.services.job.Job;
 import org.meveo.services.job.JobExecutionService;
 import org.meveo.services.job.TimerEntityService;
-import org.meveo.util.MeveoJpaForJobs;
 import org.slf4j.Logger;
 
 @Startup
@@ -43,16 +33,16 @@ public class ReccuringRatingJob implements Job {
 
 	@Inject
 	private ProviderService providerService;
-	
+
 	@Inject
 	JobExecutionService jobExecutionService;
 
-	@Inject 
+	@Inject
 	RecurringRatingJobBean recurringRatingJobBean;
-	
-    @Inject
-    protected Logger log;
-    
+
+	@Inject
+	protected Logger log;
+
 	@PostConstruct
 	public void init() {
 		TimerEntityService.registerJob(this);
@@ -68,11 +58,13 @@ public class ReccuringRatingJob implements Job {
 	}
 
 	@Override
-	public TimerHandle createTimer(ScheduleExpression scheduleExpression, TimerInfo infos) {
+	public TimerHandle createTimer(ScheduleExpression scheduleExpression,
+			TimerInfo infos) {
 		TimerConfig timerConfig = new TimerConfig();
 		timerConfig.setInfo(infos);
-		//timerConfig.setPersistent(false);
-		Timer timer = timerService.createCalendarTimer(scheduleExpression, timerConfig);
+		// timerConfig.setPersistent(false);
+		Timer timer = timerService.createCalendarTimer(scheduleExpression,
+				timerConfig);
 		return timer.getHandle();
 	}
 
@@ -84,9 +76,11 @@ public class ReccuringRatingJob implements Job {
 		if (!running && info.isActive()) {
 			try {
 				running = true;
-                Provider provider=providerService.findById(info.getProviderId());
-                JobExecutionResult result=execute(info.getParametres(),provider);
-                jobExecutionService.persistResult(this, result,info,provider);
+				Provider provider = providerService.findById(info
+						.getProviderId());
+				JobExecutionResult result = execute(info.getParametres(),
+						provider);
+				jobExecutionService.persistResult(this, result, info, provider);
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
