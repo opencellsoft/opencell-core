6d82a5edb3959f5b8da4d0d57f25051d385dee97
smichea <sebastien.michea@manaty.net>
2014-11-07 12:28:24 +0100

    hourly calendar model

--- meveo-model/src/main/java/org/meveo/model/catalog/Calendar.java
+++ meveo-model/src/main/java/org/meveo/model/catalog/Calendar.java
@@ -17,29 +17,28 @@
 package org.meveo.model.catalog;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Size;
 
 import org.meveo.model.AuditableEntity;
-import org.meveo.model.shared.DateUtils;
 
 @Entity
 @Table(name = "CAT_CALENDAR", uniqueConstraints = @UniqueConstraint(columnNames = { "NAME",
 		"PROVIDER_ID" }))
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="CAL_TYPE")
 @SequenceGenerator(name = "ID_GENERATOR", sequenceName = "CAT_CALENDAR_SEQ")
-public class Calendar extends AuditableEntity {
+public abstract class Calendar extends AuditableEntity {
 
 	private static final long serialVersionUID = 1L;
 
@@ -49,11 +48,6 @@ public class Calendar extends AuditableEntity {
 
 	@Column(name = "DESCRIPTION")
 	private String description;
-
-	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "CAT_CALENDAR_DAYS", joinColumns = @JoinColumn(name = "CALENDAR_ID"), inverseJoinColumns = @JoinColumn(name = "DAY_ID"))
-	// @BatchSize(size = 365)
-	private List<DayInYear> days;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "CALENDAR_TYPE", length = 20)
@@ -67,14 +61,6 @@ public class Calendar extends AuditableEntity {
 		this.name = name;
 	}
 
-	public List<DayInYear> getDays() {
-		return days;
-	}
-
-	public void setDays(List<DayInYear> days) {
-		this.days = days;
-	}
-
 	public CalendarTypeEnum getType() {
 		return type;
 	}
@@ -84,80 +70,18 @@ public class Calendar extends AuditableEntity {
 	}
 
 	/**
-	 * Checks for next calendar date. If not found in this year checks next
-	 * years dates. Calendar has list of days (month/day), so if calendar has at
-	 * least one date it will be found in this or next year. For example today
-	 * is 2010.12.06. Calendar has only one day - 12.05. So nextCalendarDate
-	 * will be found for 2011.12.05.
-	 * 
 	 * @param date
 	 *            Current date.
 	 * @return Next calendar date.
 	 */
-	public Date nextCalendarDate(Date date) {
-		Date future = DateUtils.newDate(3000, 0, 1, 0, 0, 0);
-		Date result = future;
-		int currentYear = DateUtils.getYearFromDate(date);
-		for (DayInYear dayInYear : days) {
-			Date d = DateUtils.newDate(currentYear, dayInYear.getMonth().getId() - 1,
-					dayInYear.getDay(), 0, 0, 0);
-			if (d.after(date) && d.before(result)) {
-				result = d;
-			}
-		}
-		if (result == future) { // if result did not change
-			currentYear++; // check for date in next year
-			for (DayInYear dayInYear : days) {
-				Date d = DateUtils.newDate(currentYear, dayInYear.getMonth().getId() - 1,
-						dayInYear.getDay(), 0, 0, 0);
-				if (d.after(date) && d.before(result)) {
-					result = d;
-				}
-			}
-		}
-		if (result == future) {
-			throw new IllegalStateException("Next calendar date could not be found!");
-		}
-		return result;
-	}
-
+	public abstract Date nextCalendarDate(Date date);
+	
 	/**
-	 * Checks for previous calendar date. If not found in this year checks
-	 * previous years dates. Calendar has list of days (month/day), so if
-	 * calendar has at least one date it will be found in this or next year. For
-	 * example today is 2010.12.06. Calendar has only one day - 12.07. So
-	 * previousCalendarDate will be found for 2009.12.07.
-	 * 
 	 * @param date
 	 *            Current date.
 	 * @return Next calendar date.
 	 */
-	public Date previousCalendarDate(Date date) {
-		Date past = DateUtils.newDate(1970, 0, 1, 0, 0, 0);
-		Date result = past;
-		int currentYear = DateUtils.getYearFromDate(date);
-		for (DayInYear dayInYear : days) {
-			Date d = DateUtils.newDate(currentYear, dayInYear.getMonth().getId() - 1,
-					dayInYear.getDay(), 0, 0, 0);
-			if (d.before(date) && d.after(result)) {
-				result = d;
-			}
-		}
-		if (result == past) { // if result did not change
-			currentYear--; // check for date in previous year
-			for (DayInYear dayInYear : days) {
-				Date d = DateUtils.newDate(currentYear, dayInYear.getMonth().getId() - 1,
-						dayInYear.getDay(), 0, 0, 0);
-				if (d.before(date) && d.after(result)) {
-					result = d;
-				}
-			}
-		}
-		if (result == past) {
-			throw new IllegalStateException("Previous calendar date could not be found!");
-		}
-		return result;
-	}
+	public abstract Date previousCalendarDate(Date date);
 
 	@Override
 	public int hashCode() {
