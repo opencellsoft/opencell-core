596ba22fba7ef226ae6de3e306d8f2dcde907d3c
smichea <sebastien.michea@manaty.net>
2015-01-01 08:09:59 +0100

    missing model

---
+++ meveo-model/src/main/java/org/meveo/model/jaxb/customer/CustomField.java
@@ -0,0 +1,80 @@
+package org.meveo.model.jaxb.customer;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "customField")
+public class CustomField {
+    
+	@XmlAttribute(required=true)
+    protected String code;
+    
+    @XmlAttribute
+    protected String description;
+
+    @XmlAttribute
+    protected String stringValue;
+
+    @XmlAttribute
+    protected Date dateValue;
+
+    @XmlAttribute
+    protected Long longValue;
+
+    @XmlAttribute
+    protected Double doubleValue;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStringValue() {
+		return stringValue;
+	}
+
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+
+	public Date getDateValue() {
+		return dateValue;
+	}
+
+	public void setDateValue(Date dateValue) {
+		this.dateValue = dateValue;
+	}
+
+	public Long getLongValue() {
+		return longValue;
+	}
+
+	public void setLongValue(Long longValue) {
+		this.longValue = longValue;
+	}
+
+	public Double getDoubleValue() {
+		return doubleValue;
+	}
+
+	public void setDoubleValue(Double doubleValue) {
+		this.doubleValue = doubleValue;
+	}
+
+}
---
+++ meveo-model/src/main/java/org/meveo/model/jaxb/customer/CustomFields.java
@@ -0,0 +1,46 @@
+package org.meveo.model.jaxb.customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{}customerField" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "customField"
+})
+@XmlRootElement(name = "customFields")
+public class CustomFields {
+	protected List<CustomField> customField;
+
+	public List<CustomField> getCustomField() {
+		if(customField==null){
+			customField=new ArrayList<CustomField>();
+		}
+		return customField;
+	}
+	
+	
+}
