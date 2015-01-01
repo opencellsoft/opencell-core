f5764f91036cfc5ebe35d9b90140d098b55a3b35
smichea <sebastien.michea@manaty.net>
2015-01-01 09:39:37 +0100

    add missing model

---
+++ meveo-api/src/main/java/org/meveo/api/dto/CustomFieldDto.java
@@ -0,0 +1,79 @@
+package org.meveo.api.dto;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "CustomField")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CustomFieldDto {
+	@XmlAttribute(required=true)
+    protected String code;
+    
+    @XmlAttribute
+    protected String description;
+
+    @XmlAttribute
+    protected String stringValue;
+
+    @XmlAttribute
+    protected Date dateValue;
+
+    @XmlAttribute
+    protected Long longValue;
+
+    @XmlAttribute
+    protected Double doubleValue;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStringValue() {
+		return stringValue;
+	}
+
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+
+	public Date getDateValue() {
+		return dateValue;
+	}
+
+	public void setDateValue(Date dateValue) {
+		this.dateValue = dateValue;
+	}
+
+	public Long getLongValue() {
+		return longValue;
+	}
+
+	public void setLongValue(Long longValue) {
+		this.longValue = longValue;
+	}
+
+	public Double getDoubleValue() {
+		return doubleValue;
+	}
+
+	public void setDoubleValue(Double doubleValue) {
+		this.doubleValue = doubleValue;
+	}
+
+}
