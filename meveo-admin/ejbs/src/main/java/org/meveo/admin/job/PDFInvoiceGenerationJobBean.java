375aaafe3653986b315fc7bddf81f9d0cffa110f
rachid.aityaazza <rachid.aityaazza@manaty.net>
2015-01-05 17:34:28 +0100

    #495

---
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/PDFInvoiceGenerationJobBean.java
@@ -0,0 +1,77 @@
+package org.meveo.admin.job;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.interceptor.Interceptors;
+
+import org.meveo.admin.job.logging.JobLoggingInterceptor;
+import org.meveo.model.admin.User;
+import org.meveo.model.billing.Invoice;
+import org.meveo.model.jobs.JobExecutionResultImpl;
+import org.meveo.service.billing.impl.BillingRunService;
+import org.meveo.service.billing.impl.InvoiceService;
+import org.slf4j.Logger;
+
+@Stateless
+public class PDFInvoiceGenerationJobBean {
+
+	@Inject
+	private Logger log;
+
+	@Inject
+	private InvoiceService invoiceService;
+
+	@Inject
+	private PDFParametersConstruction pDFParametersConstruction;
+
+	@Inject
+	private PDFFilesOutputProducer pDFFilesOutputProducer;
+
+	@Inject
+	private BillingRunService billingRunService;
+
+	@Interceptors({ JobLoggingInterceptor.class })
+	public void execute(JobExecutionResultImpl result, String parameter,
+			User currentUser) {
+		List<Invoice> invoices = new ArrayList<Invoice>();
+
+		if (parameter != null && parameter.trim().length() > 0) {
+			try {
+				invoices = invoiceService.getInvoices(billingRunService
+						.getBillingRunById(Long.parseLong(parameter),
+								currentUser.getProvider()));
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				result.registerError(e.getMessage());
+			}
+		} else {
+			invoices = invoiceService.getValidatedInvoicesWithNoPdf(null,
+					currentUser.getProvider());
+		}
+
+		log.info("PDFInvoiceGenerationJob number of invoices to process="
+				+ invoices.size());
+
+		for (Invoice invoice : invoices) {
+			try {
+				Map<String, Object> parameters = pDFParametersConstruction
+						.constructParameters(invoice);
+
+				log.info("PDFInvoiceGenerationJob parameters=" + parameters);
+
+				Future<Boolean> isPdfgenerated = pDFFilesOutputProducer
+						.producePdf(parameters, result, currentUser);
+				isPdfgenerated.get();
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				result.registerError(e.getMessage());
+			}
+		}
+	}
+
+}
