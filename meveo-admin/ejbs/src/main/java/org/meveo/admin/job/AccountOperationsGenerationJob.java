8512e588d4bc5bb29234e3110d52b9121885b8a3
Edward P. Legaspi <czetsuya@gmail.com>
2015-01-14 14:36:09 +0800

    Test #497.

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJob.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJob.java
@@ -1,8 +1,6 @@
 package org.meveo.admin.job;
 
-import java.math.BigDecimal;
 import java.util.Collection;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -15,25 +13,11 @@ import javax.ejb.TimerConfig;
 import javax.ejb.TimerService;
 import javax.inject.Inject;
 
-import org.meveo.admin.exception.ImportInvoiceException;
-import org.meveo.admin.exception.InvoiceExistException;
-import org.meveo.commons.utils.ParamBean;
 import org.meveo.model.admin.User;
-import org.meveo.model.billing.BillingAccount;
-import org.meveo.model.billing.Invoice;
-import org.meveo.model.crm.Provider;
 import org.meveo.model.jobs.JobExecutionResult;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.jobs.TimerInfo;
-import org.meveo.model.payments.CustomerAccount;
-import org.meveo.model.payments.MatchingStatusEnum;
-import org.meveo.model.payments.OCCTemplate;
-import org.meveo.model.payments.RecordedInvoice;
-import org.meveo.model.shared.DateUtils;
 import org.meveo.service.admin.impl.UserService;
-import org.meveo.service.billing.impl.InvoiceService;
-import org.meveo.service.payments.impl.OCCTemplateService;
-import org.meveo.service.payments.impl.RecordedInvoiceService;
 import org.meveo.services.job.Job;
 import org.meveo.services.job.JobExecutionService;
 import org.meveo.services.job.TimerEntityService;
@@ -53,16 +37,10 @@ public class AccountOperationsGenerationJob implements Job {
 	private JobExecutionService jobExecutionService;
 
 	@Inject
-	private OCCTemplateService oCCTemplateService;
-
-	@Inject
-	private InvoiceService invoiceService;
-
-	@Inject
-	private RecordedInvoiceService recordedInvoiceService;
-
-	@Inject
 	private Logger log;
+
+	@Inject
+	private AccountOperationsGenerationJobBean accountOperationsGenerationJobBean;
 
 	@PostConstruct
 	public void init() {
@@ -71,158 +49,20 @@ public class AccountOperationsGenerationJob implements Job {
 
 	@Override
 	public JobExecutionResult execute(String parameter, User currentUser) {
-		log.info("execute XMLInvoiceGenerationJob.");
-
-		ParamBean paramBan = ParamBean.getInstance();
 		JobExecutionResultImpl result = new JobExecutionResultImpl();
-		List<Invoice> invoices = invoiceService
-				.getInvoicesWithNoAccountOperation(null);
-		Provider providerForHistory = null;
-		for (Invoice invoice : invoices) {
-			try {
 
-				CustomerAccount customerAccount = null;
-				OCCTemplate invoiceTemplate = null;
-				RecordedInvoice recordedInvoice = new RecordedInvoice();
-				BillingAccount billingAccount = invoice.getBillingAccount();
-				if (recordedInvoiceService.isRecordedInvoiceExist(
-						invoice.getInvoiceNumber(), invoice.getProvider())) {
-					throw new InvoiceExistException("Invoice id"
-							+ invoice.getId() + " already exist");
-				}
-				try {
-					customerAccount = invoice.getBillingAccount()
-							.getCustomerAccount();
-					recordedInvoice.setCustomerAccount(customerAccount);
-					recordedInvoice.setProvider(customerAccount.getProvider());
-					// set first provider from first customer account
-					if (providerForHistory != null) {
-						providerForHistory = customerAccount.getProvider();
-					}
-				} catch (Exception e) {
-					throw new ImportInvoiceException(
-							"Cannot found customerAccount");
-				}
+		accountOperationsGenerationJobBean.execute(result, parameter, currentUser);
 
-				try {
-					invoiceTemplate = oCCTemplateService.findByCode(paramBan
-							.getProperty(
-									"accountOperationsGenerationJob.occCode",
-									"FA_FACT"), customerAccount.getProvider()
-							.getCode());
-				} catch (Exception e) {
-					// TODO message fr|en
-					throw new ImportInvoiceException(
-							"Cannot found OCC Template for invoice");
-				}
-				recordedInvoice.setReference(invoice.getInvoiceNumber());
-				recordedInvoice
-						.setAccountCode(invoiceTemplate.getAccountCode());
-				recordedInvoice.setOccCode(invoiceTemplate.getCode());
-				recordedInvoice.setOccDescription(invoiceTemplate
-						.getDescription());
-				recordedInvoice.setTransactionCategory(invoiceTemplate
-						.getOccCategory());
-				recordedInvoice.setAccountCodeClientSide(invoiceTemplate
-						.getAccountCodeClientSide());
-
-				try {
-					recordedInvoice.setAmount(invoice.getAmountWithTax());
-					recordedInvoice.setUnMatchingAmount(invoice
-							.getAmountWithTax());
-					recordedInvoice.setMatchingAmount(BigDecimal.ZERO);
-				} catch (Exception e) {
-					throw new ImportInvoiceException("Error on amountWithTax");
-				}
-				try {
-					recordedInvoice.setAmountWithoutTax(invoice
-							.getAmountWithoutTax());
-				} catch (Exception e) {
-					throw new ImportInvoiceException(
-							"Error on amountWithoutTax");
-				}
-				try {
-					recordedInvoice.setNetToPay(invoice.getNetToPay());
-				} catch (Exception e) {
-					throw new ImportInvoiceException("Error on netToPay");
-				}
-
-				try {
-					recordedInvoice
-							.setDueDate(DateUtils.parseDateWithPattern(
-									invoice.getDueDate(),
-									paramBan.getProperty(
-											"accountOperationsGenerationJob.dateFormat",
-											"dd/MM/yyyy")));
-				} catch (Exception e) {
-					throw new ImportInvoiceException("Error on DueDate");
-				}
-				try {
-					recordedInvoice
-							.setInvoiceDate(DateUtils.parseDateWithPattern(
-									invoice.getInvoiceDate(),
-									paramBan.getProperty(
-											"accountOperationsGenerationJob.dateFormat",
-											"dd/MM/yyyy")));
-					recordedInvoice
-							.setTransactionDate(DateUtils.parseDateWithPattern(
-									invoice.getInvoiceDate(),
-									paramBan.getProperty(
-											"accountOperationsGenerationJob.dateFormat",
-											"dd/MM/yyyy")));
-
-				} catch (Exception e) {
-					throw new ImportInvoiceException("Error on invoiceDate");
-				}
-				try {
-					recordedInvoice.setPaymentMethod(billingAccount
-							.getPaymentMethod());
-				} catch (Exception e) {
-					throw new ImportInvoiceException("Error on paymentMethod");
-				}
-				try {
-					recordedInvoice.setTaxAmount(invoice.getAmountTax());
-				} catch (Exception e) {
-					throw new ImportInvoiceException("Error on total tax");
-				}
-				recordedInvoice.setPaymentInfo(billingAccount
-						.getBankCoordinates().getIban());
-				recordedInvoice.setPaymentInfo1(billingAccount
-						.getBankCoordinates().getBankCode());
-				recordedInvoice.setPaymentInfo2(billingAccount
-						.getBankCoordinates().getBranchCode());
-				recordedInvoice.setPaymentInfo3(billingAccount
-						.getBankCoordinates().getAccountNumber());
-				recordedInvoice.setPaymentInfo4(billingAccount
-						.getBankCoordinates().getKey());
-				recordedInvoice.setPaymentInfo5(billingAccount
-						.getBankCoordinates().getBankName());
-				recordedInvoice.setPaymentInfo6(billingAccount
-						.getBankCoordinates().getBic());
-				recordedInvoice.setBillingAccountName(billingAccount
-						.getBankCoordinates().getAccountOwner());
-				recordedInvoice.setMatchingStatus(MatchingStatusEnum.O);
-				recordedInvoiceService.create(recordedInvoice);
-				invoice.setRecordedInvoice(recordedInvoice);
-				invoiceService.update(invoice);
-
-			} catch (Exception e) {
-				log.error(e.getMessage());
-				result.registerError(e.getMessage());
-			}
-		}
 		result.close("");
 		return result;
 	}
 
 	@Override
-	public Timer createTimer(ScheduleExpression scheduleExpression,
-			TimerInfo infos) {
+	public Timer createTimer(ScheduleExpression scheduleExpression, TimerInfo infos) {
 		TimerConfig timerConfig = new TimerConfig();
 		timerConfig.setInfo(infos);
 		timerConfig.setPersistent(false);
-		return timerService
-				.createCalendarTimer(scheduleExpression, timerConfig);
+		return timerService.createCalendarTimer(scheduleExpression, timerConfig);
 	}
 
 	boolean running = false;
@@ -230,14 +70,13 @@ public class AccountOperationsGenerationJob implements Job {
 	@Timeout
 	public void trigger(Timer timer) {
 		TimerInfo info = (TimerInfo) timer.getInfo();
+
 		if (!running && info.isActive()) {
 			try {
 				running = true;
 				User currentUser = userService.findById(info.getUserId());
-				JobExecutionResult result = execute(info.getParametres(),
-						currentUser);
-				jobExecutionService.persistResult(this, result, info,
-						currentUser);
+				JobExecutionResult result = execute(info.getParametres(), currentUser);
+				jobExecutionService.persistResult(this, result, info, currentUser);
 			} catch (Exception e) {
 				log.error(e.getMessage());
 			} finally {
@@ -254,8 +93,7 @@ public class AccountOperationsGenerationJob implements Job {
 	@Override
 	public void cleanAllTimers() {
 		Collection<Timer> alltimers = timerService.getTimers();
-		log.info("Cancel " + alltimers.size() + " timers for"
-				+ this.getClass().getSimpleName());
+		log.info("Cancel " + alltimers.size() + " timers for" + this.getClass().getSimpleName());
 
 		for (Timer timer : alltimers) {
 			try {
---
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJobBean.java
@@ -0,0 +1,177 @@
+package org.meveo.admin.job;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+import javax.interceptor.Interceptors;
+
+import org.meveo.admin.exception.ImportInvoiceException;
+import org.meveo.admin.exception.InvoiceExistException;
+import org.meveo.admin.job.logging.JobLoggingInterceptor;
+import org.meveo.commons.utils.ParamBean;
+import org.meveo.model.admin.User;
+import org.meveo.model.billing.BillingAccount;
+import org.meveo.model.billing.Invoice;
+import org.meveo.model.crm.Provider;
+import org.meveo.model.jobs.JobExecutionResultImpl;
+import org.meveo.model.payments.CustomerAccount;
+import org.meveo.model.payments.MatchingStatusEnum;
+import org.meveo.model.payments.OCCTemplate;
+import org.meveo.model.payments.RecordedInvoice;
+import org.meveo.model.shared.DateUtils;
+import org.meveo.service.billing.impl.InvoiceService;
+import org.meveo.service.payments.impl.OCCTemplateService;
+import org.meveo.service.payments.impl.RecordedInvoiceService;
+import org.slf4j.Logger;
+
+/**
+ * @author Edward P. Legaspi
+ **/
+@Stateless
+public class AccountOperationsGenerationJobBean {
+
+	@Inject
+	private Logger log;
+
+	@Inject
+	private OCCTemplateService oCCTemplateService;
+
+	@Inject
+	private InvoiceService invoiceService;
+
+	@Inject
+	private RecordedInvoiceService recordedInvoiceService;
+
+	@Interceptors({ JobLoggingInterceptor.class })
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	public void execute(JobExecutionResultImpl result, String parameter, User currentUser) {
+		ParamBean paramBean = ParamBean.getInstance();
+		List<Invoice> invoices = invoiceService.getInvoicesWithNoAccountOperation(null);
+		Provider providerForHistory = null;
+
+		if (invoices != null) {
+			log.debug("processing {} invoice", invoices.size());
+		}
+
+		for (Invoice invoice : invoices) {
+			try {
+				CustomerAccount customerAccount = null;
+				OCCTemplate invoiceTemplate = null;
+				RecordedInvoice recordedInvoice = new RecordedInvoice();
+				BillingAccount billingAccount = invoice.getBillingAccount();
+
+				if (recordedInvoiceService.isRecordedInvoiceExist(invoice.getInvoiceNumber(), invoice.getProvider())) {
+					throw new InvoiceExistException("Invoice id" + invoice.getId() + " already exist");
+				}
+
+				try {
+					customerAccount = invoice.getBillingAccount().getCustomerAccount();
+					recordedInvoice.setCustomerAccount(customerAccount);
+					recordedInvoice.setProvider(customerAccount.getProvider());
+
+					// set first provider from first customer account
+					if (providerForHistory != null) {
+						providerForHistory = customerAccount.getProvider();
+					}
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Cannot found customerAccount");
+				}
+
+				try {
+					invoiceTemplate = oCCTemplateService.findByCode(paramBean.getProperty(
+							"accountOperationsGenerationJob.occCode", "FA_FACT"), customerAccount.getProvider()
+							.getCode());
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Cannot found OCC Template for invoice");
+				}
+
+				recordedInvoice.setReference(invoice.getInvoiceNumber());
+				recordedInvoice.setAccountCode(invoiceTemplate.getAccountCode());
+				recordedInvoice.setOccCode(invoiceTemplate.getCode());
+				recordedInvoice.setOccDescription(invoiceTemplate.getDescription());
+				recordedInvoice.setTransactionCategory(invoiceTemplate.getOccCategory());
+				recordedInvoice.setAccountCodeClientSide(invoiceTemplate.getAccountCodeClientSide());
+
+				try {
+					recordedInvoice.setAmount(invoice.getAmountWithTax());
+					recordedInvoice.setUnMatchingAmount(invoice.getAmountWithTax());
+					recordedInvoice.setMatchingAmount(BigDecimal.ZERO);
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Error on amountWithTax");
+				}
+
+				try {
+					recordedInvoice.setAmountWithoutTax(invoice.getAmountWithoutTax());
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Error on amountWithoutTax");
+				}
+
+				try {
+					recordedInvoice.setNetToPay(invoice.getNetToPay());
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Error on netToPay");
+				}
+
+				try {
+					recordedInvoice.setDueDate(DateUtils.parseDateWithPattern(invoice.getDueDate(),
+							paramBean.getProperty("accountOperationsGenerationJob.dateFormat", "dd/MM/yyyy")));
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Error on DueDate");
+				}
+
+				try {
+					recordedInvoice.setInvoiceDate(DateUtils.parseDateWithPattern(invoice.getInvoiceDate(),
+							paramBean.getProperty("accountOperationsGenerationJob.dateFormat", "dd/MM/yyyy")));
+					recordedInvoice.setTransactionDate(DateUtils.parseDateWithPattern(invoice.getInvoiceDate(),
+							paramBean.getProperty("accountOperationsGenerationJob.dateFormat", "dd/MM/yyyy")));
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Error on invoiceDate");
+				}
+
+				try {
+					recordedInvoice.setPaymentMethod(billingAccount.getPaymentMethod());
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Error on paymentMethod");
+				}
+
+				try {
+					recordedInvoice.setTaxAmount(invoice.getAmountTax());
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					throw new ImportInvoiceException("Error on total tax");
+				}
+
+				recordedInvoice.setPaymentInfo(billingAccount.getBankCoordinates().getIban());
+				recordedInvoice.setPaymentInfo1(billingAccount.getBankCoordinates().getBankCode());
+				recordedInvoice.setPaymentInfo2(billingAccount.getBankCoordinates().getBranchCode());
+				recordedInvoice.setPaymentInfo3(billingAccount.getBankCoordinates().getAccountNumber());
+				recordedInvoice.setPaymentInfo4(billingAccount.getBankCoordinates().getKey());
+				recordedInvoice.setPaymentInfo5(billingAccount.getBankCoordinates().getBankName());
+				recordedInvoice.setPaymentInfo6(billingAccount.getBankCoordinates().getBic());
+				recordedInvoice.setBillingAccountName(billingAccount.getBankCoordinates().getAccountOwner());
+				recordedInvoice.setMatchingStatus(MatchingStatusEnum.O);
+				recordedInvoiceService.create(recordedInvoice);
+
+				invoice.setRecordedInvoice(recordedInvoice);
+				invoiceService.update(invoice);
+
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				result.registerError(e.getMessage());
+			}
+		}
+	}
+
+}
