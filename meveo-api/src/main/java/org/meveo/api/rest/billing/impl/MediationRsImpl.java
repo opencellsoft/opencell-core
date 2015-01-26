29060d8e7865985a2da480f0bf52b7fd47a74bc7
Edward P. Legaspi <czetsuya@gmail.com>
2015-01-26 10:35:17 +0800

    Test #534.

---
+++ meveo-api/src/main/java/org/meveo/api/rest/billing/impl/MediationRsImpl.java
@@ -0,0 +1,52 @@
+package org.meveo.api.rest.billing.impl;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.interceptor.Interceptors;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
+
+import org.meveo.api.MeveoApiErrorCode;
+import org.meveo.api.billing.MediationApi;
+import org.meveo.api.dto.ActionStatus;
+import org.meveo.api.dto.ActionStatusEnum;
+import org.meveo.api.dto.rating.EdrDto;
+import org.meveo.api.exception.MeveoApiException;
+import org.meveo.api.logging.LoggingInterceptor;
+import org.meveo.api.rest.billing.MediationRs;
+import org.meveo.api.rest.impl.BaseRs;
+
+/**
+ * @author Edward P. Legaspi
+ **/
+@RequestScoped
+@Interceptors({ LoggingInterceptor.class })
+public class MediationRsImpl extends BaseRs implements MediationRs {
+
+	@Inject
+	private MediationApi edrApi;
+
+	@Context
+	private HttpServletRequest httpServletRequest;
+
+	@Override
+	public ActionStatus create(EdrDto postData) {
+		ActionStatus result = new ActionStatus(ActionStatusEnum.SUCCESS, "");
+
+		try {
+			postData.setIpAddress(httpServletRequest.getRemoteAddr());
+			edrApi.create(postData, getCurrentUser());
+		} catch (MeveoApiException e) {
+			result.setErrorCode(e.getErrorCode());
+			result.setStatus(ActionStatusEnum.FAIL);
+			result.setMessage(e.getMessage());
+		} catch (Exception e) {
+			result.setErrorCode(MeveoApiErrorCode.GENERIC_API_EXCEPTION);
+			result.setStatus(ActionStatusEnum.FAIL);
+			result.setMessage(e.getMessage());
+		}
+
+		return result;
+	}
+
+}
