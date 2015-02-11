63f43f47962443898f58254e5db2d66cff1a8b0f
Edward P. Legaspi <czetsuya@gmail.com>
2015-02-11 16:07:57 +0800

    Added discountPlan field in billingAccount.

--- meveo-model/src/main/java/org/meveo/model/catalog/DiscountPlan.java
+++ meveo-model/src/main/java/org/meveo/model/catalog/DiscountPlan.java
@@ -100,4 +100,29 @@ public class DiscountPlan extends AuditableEntity {
 				+ ", discountPlanItems=" + discountPlanItems + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DiscountPlan other = (DiscountPlan) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
+	}
+
 }
--- meveo-model/src/main/java/org/meveo/model/catalog/DiscountPlanItem.java
+++ meveo-model/src/main/java/org/meveo/model/catalog/DiscountPlanItem.java
@@ -126,7 +126,7 @@ public class DiscountPlanItem extends AuditableEntity {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!super.equals(obj))
+		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
