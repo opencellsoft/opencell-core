9e58baad7e9ab994201a66bf9e0f57ea2da159f3
mbarek <mbarek.omoh@manaty.net>
2015-02-13 03:30:19 +0100

    #532 : add strategies to csv import

--- meveo-admin/web/src/main/java/org/meveo/admin/action/notification/InboundRequestBean.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/notification/InboundRequestBean.java
@@ -17,11 +17,15 @@ import javax.inject.Named;
 import org.jboss.seam.international.status.builder.BundleKey;
 import org.meveo.admin.action.BaseBean;
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.admin.exception.RejectedImportException;
 import org.meveo.commons.utils.CsvBuilder;
 import org.meveo.commons.utils.CsvReader;
 import org.meveo.commons.utils.StringUtils;
 import org.meveo.model.notification.InboundRequest;
+import org.meveo.model.notification.Notification;
+import org.meveo.model.notification.NotificationEventTypeEnum;
 import org.meveo.model.notification.NotificationHistory;
+import org.meveo.model.notification.StrategyImportTypeEnum;
 import org.meveo.model.notification.WebHook;
 import org.meveo.model.shared.DateUtils;
 import org.meveo.service.base.PersistenceService;
@@ -46,6 +50,8 @@ public class InboundRequestBean extends BaseBean<InboundRequest> {
     
     CsvReader csvReader = null;
     private UploadedFile file; 
+    
+    private StrategyImportTypeEnum strategyImportType;
     
     private static final int FROM_IP= 0;
     private static final int PORT= 1;
@@ -169,41 +175,86 @@ public class InboundRequestBean extends BaseBean<InboundRequest> {
         
     }
 
-	private void upload() throws IOException, BusinessException {
+	public void upload() throws IOException, BusinessException {
 		if (file != null) {
-
 			csvReader = new CsvReader(file.getInputstream(), ';',
 					Charset.forName("ISO-8859-1"));
 			csvReader.readHeaders();
-
-			InboundRequest inboundRequest = null;
-			while (csvReader.readRecord()) {
-				String[] values = csvReader.getValues();
-				inboundRequest = new InboundRequest();
-				inboundRequest.setRemoteAddr(values[FROM_IP]);
-				inboundRequest
-						.setRemotePort(!StringUtils.isBlank(values[PORT]) ? Integer
+			try {
+				while (csvReader.readRecord()) {
+					String[] values = csvReader.getValues();
+					InboundRequest existingEntity = inboundRequestService
+							.findByCode(values[CODE], getCurrentProvider());
+					if (existingEntity != null) {
+						checkSelectedStrategy(values, existingEntity);
+					} else {
+						InboundRequest inboundRequest = new InboundRequest();
+						inboundRequest.setRemoteAddr(values[FROM_IP]);
+						inboundRequest.setRemotePort(!StringUtils
+								.isBlank(values[PORT]) ? Integer
 								.parseInt(values[PORT]) : null);
-				inboundRequest.setProtocol(values[PORTOCOL]);
-				inboundRequest.setPathInfo(values[PATH_INFO]);
-				inboundRequest.setCode(values[CODE]);
-				inboundRequest
-						.setDisabled(Boolean.parseBoolean(values[ACTIVE]));
-				inboundRequest.setScheme(values[SCHEME]);
-				inboundRequest.setContentType(values[CONTENT_TYPE]);
-				inboundRequest.setContentLength(Integer
-						.parseInt(values[CONTENT_LENGHT]));
-				inboundRequest.setMethod(values[METHOD]);
-				inboundRequest.setAuthType(values[AUTHENTIFICATION_TYPE]);
-				inboundRequest.setRequestURI(values[REQUEST_URI]);
-				inboundRequest
-						.setResponseContentType(values[RESPONSE_CONTENT_TYPE]);
-				inboundRequest.setResponseEncoding(values[ENCODING]);
-				inboundRequestService.create(inboundRequest);
+						inboundRequest.setProtocol(values[PORTOCOL]);
+						inboundRequest.setPathInfo(values[PATH_INFO]);
+						inboundRequest.setCode(values[CODE]);
+						inboundRequest.setDisabled(Boolean
+								.parseBoolean(values[ACTIVE]));
+						inboundRequest.setScheme(values[SCHEME]);
+						inboundRequest.setContentType(values[CONTENT_TYPE]);
+						inboundRequest.setContentLength(Integer
+								.parseInt(values[CONTENT_LENGHT]));
+						inboundRequest.setMethod(values[METHOD]);
+						inboundRequest
+								.setAuthType(values[AUTHENTIFICATION_TYPE]);
+						inboundRequest.setRequestURI(values[REQUEST_URI]);
+						inboundRequest
+								.setResponseContentType(values[RESPONSE_CONTENT_TYPE]);
+						inboundRequest.setResponseEncoding(values[ENCODING]);
+						inboundRequestService.create(inboundRequest);
+					}
+				}
 				messages.info(new BundleKey("messages", "commons.csv"));
+			} catch (RejectedImportException e) {
+				messages.error(new BundleKey("messages", e.getMessage()));
 			}
-
 		}
+	}
+
+	public void checkSelectedStrategy(String[] values,
+			InboundRequest existingEntity) throws RejectedImportException {
+		if (strategyImportType.equals(StrategyImportTypeEnum.UPDATED)) {
+			existingEntity.setRemoteAddr(values[FROM_IP]);
+			existingEntity
+					.setRemotePort(!StringUtils.isBlank(values[PORT]) ? Integer
+							.parseInt(values[PORT]) : null);
+			existingEntity.setProtocol(values[PORTOCOL]);
+			existingEntity.setPathInfo(values[PATH_INFO]);
+			existingEntity.setDisabled(Boolean.parseBoolean(values[ACTIVE]));
+			existingEntity.setScheme(values[SCHEME]);
+			existingEntity.setContentType(values[CONTENT_TYPE]);
+			existingEntity.setContentLength(Integer
+					.parseInt(values[CONTENT_LENGHT]));
+			existingEntity.setMethod(values[METHOD]);
+			existingEntity.setAuthType(values[AUTHENTIFICATION_TYPE]);
+			existingEntity.setRequestURI(values[REQUEST_URI]);
+			existingEntity
+					.setResponseContentType(values[RESPONSE_CONTENT_TYPE]);
+			existingEntity.setResponseEncoding(values[ENCODING]);
+			inboundRequestService.update(existingEntity);
+		} else if (strategyImportType
+				.equals(StrategyImportTypeEnum.REJECT_EXISTING_RECORDS)) {
+			// add to a new csv
+		} else if (strategyImportType
+				.equals(StrategyImportTypeEnum.REJECTE_IMPORT)) {
+			throw new RejectedImportException("notification.rejectImport");
+		}
+	}
+
+	public StrategyImportTypeEnum getStrategyImportType() {
+		return strategyImportType;
+	}
+
+	public void setStrategyImportType(StrategyImportTypeEnum strategyImportType) {
+		this.strategyImportType = strategyImportType;
 	}
     
     
