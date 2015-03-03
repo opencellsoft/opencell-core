6fc044fbbea1ffd694207e1ad2e2e8a6384877d2
mbarek <mbarek.omoh@manaty.net>
2015-03-03 12:15:19 +0100

    Review #578

--- meveo-model/src/main/java/org/meveo/model/billing/Subscription.java
+++ meveo-model/src/main/java/org/meveo/model/billing/Subscription.java
@@ -250,19 +250,19 @@ public class Subscription extends BusinessEntity implements ICustomFieldEntity{
 		return result;
 		}
 	
-	public String getICsvStringValue(String code){
+	public String getICsv(String code){
 		return getInheritedCustomStringValue(code);
 	}
 	
-	public Long getICsvLongValue(String code){
+	public Long getIClv(String code){
 		return getInheritedCustomLongValue(code);
 	}
 	
-	public Date getICsvDateValue(String code){
-		return getInheritedCustomDateValue (code);
+	public Date getICdav(String code){
+		return getInheritedCustomDateValue(code);
 	}
 	
-	public Double getICsvDoubleValue(String code){
+	public Double getICdov(String code){
 		return getInheritedCustomDoubleValue(code);
 	}
 
