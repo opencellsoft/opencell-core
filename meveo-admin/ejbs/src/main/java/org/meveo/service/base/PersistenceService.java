e6953c1f9434a2bd563adaab9a7174caa1b0ffff
Tyshan Shi <tyshanchn@hotmail.com>
2015-04-24 19:36:24 +0800

    #716 update

--- meveo-admin/ejbs/src/main/java/org/meveo/service/base/PersistenceService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/base/PersistenceService.java
@@ -19,6 +19,7 @@ package org.meveo.service.base;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.TypeVariable;
 import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -528,7 +529,49 @@ public abstract class PersistenceService<E extends IEntity> extends BaseService 
                             // if searching elements from list
                             String parsedKey = key.substring(7);
                             queryBuilder.addSql("a." + parsedKey + " in (" + filter + ")");
+                        } else if (key.contains("minmaxRange-")) {
+                            // if searching elements from list
+                            String parsedKey = key.substring(12);
+                            String[] kss=parsedKey.split("-");
+                            System.out.println("#####minmaxRange ####"+kss.length);
+                            if(kss.length==2){
+                            	System.out.println("#####minmaxRange ####"+kss[0]);
+                            	System.out.println("#####minmaxRange ####"+kss[1]);
+                            	if (filter instanceof Double) {
+                                    BigDecimal rationalNumber = new BigDecimal((Double) filter);
+                                    queryBuilder.addCriterion("a." + kss[0], " <= ", rationalNumber, false);
+                                    queryBuilder.addCriterion("a." + kss[1], " >= ", rationalNumber, false);
+                                }else if (filter instanceof Number) {
+                                    queryBuilder.addCriterion("a." + kss[0], " <= ", filter, false);
+                                    queryBuilder.addCriterion("a." + kss[1], " >= ", filter, false);
+                                }if(filter instanceof Date){
+                                	Date value=(Date)filter;
+                                	Calendar c = Calendar.getInstance();
+                            		c.setTime(value);
+                            		int year = c.get(Calendar.YEAR);
+                            		int month = c.get(Calendar.MONTH);
+                            		int date = c.get(Calendar.DATE);
+                            		c.set(year, month, date, 0, 0, 0);
+                            		value=c.getTime();
+                            		queryBuilder.addCriterion("a." + kss[0],"<=", value,false);
+                                    queryBuilder.addCriterion("a." + kss[1],">=", value,false);
+                            	} 
+                            }
+                        }else if (key.contains("likeCriterias-")) {
+                            // if searching elements from list
+                            String parsedKey = key.substring(14);
+                            String[] fields=parsedKey.split("-");
+                            System.out.println("#####likeCriterias ####"+fields.length);
+                            queryBuilder.startOrClause();
+                            for(String field:fields){
+                            	if(filter instanceof String){
+                            		String filterString = (String) filter;
+                                    queryBuilder.addCriterionWildcard("a." + field, filterString, true);
+                            	}
+                            }
+                            queryBuilder.endOrClause();
                         }
+                        
                         // if not ranged search
                         else {
                             if (filter instanceof String) {
