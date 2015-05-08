889782459fcdcc792e9f96436eb6e89b04972ccd
Tyshan Shi <tyshanchn@hotmail.com>
2015-05-09 00:16:32 +0800

    #738, stay on tab when click edit for customer,customeraccount,useraccount,billingaccount

--- meveo-admin/web/src/main/java/org/meveo/admin/action/AccountBean.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/AccountBean.java
@@ -1,12 +1,15 @@
 package org.meveo.admin.action;
 
 import org.meveo.model.AccountEntity;
+import org.primefaces.component.tabview.Tab;
+import org.primefaces.event.TabChangeEvent;
 
 public abstract class AccountBean<T extends AccountEntity> extends
 		BaseBean<T> {
 
 	private static final long serialVersionUID = 3407699633028715707L;
-
+	
+	private int activeTab=0;
 
 	public AccountBean() {
 
@@ -14,6 +17,29 @@ public abstract class AccountBean<T extends AccountEntity> extends
 
 	public AccountBean(Class<T> clazz) {
 		super(clazz);
+	}
+	
+	protected void onTabChange(TabChangeEvent event){
+		Tab currentTab=event.getTab();
+		if(currentTab!=null){
+			if("tab0".equals(currentTab.getId())){
+				this.activeTab=0;
+			}else if("tab1".equals(currentTab.getId())){
+				this.activeTab=1;
+			}else if("tab1".equals(currentTab.getId())){
+				this.activeTab=2;
+			}else if("tab2".equals(currentTab.getId())){
+				this.activeTab=3;
+			}else if("tab3".equals(currentTab.getId())){
+				this.activeTab=4;
+			}
+		}
+	}
+	protected int getActiveTab(){
+		return this.activeTab;
+	}
+	protected void setActiveTab(int activeTab){
+		//
 	}
 
 
