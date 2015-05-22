4404ce67430d120a07f581cc7909ad5825f4d33b
mbarek <mbarek.omoh@manaty.net>
2015-05-23 01:30:43 +0200

    #787 - [API] Have a webservice to create a job

--- meveo-api/src/main/java/org/meveo/api/job/TimerEntityApi.java
+++ meveo-api/src/main/java/org/meveo/api/job/TimerEntityApi.java
@@ -1,5 +1,6 @@
 package org.meveo.api.job;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -7,20 +8,20 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import org.apache.commons.lang.StringUtils;
+import org.meveo.admin.exception.BusinessException;
 import org.meveo.api.BaseApi;
 import org.meveo.api.MeveoApiErrorCode;
-import org.meveo.api.dto.CustomFieldDto;
 import org.meveo.api.dto.job.TimerEntityDto;
+import org.meveo.api.exception.EntityAlreadyExistsException;
 import org.meveo.api.exception.MeveoApiException;
 import org.meveo.api.exception.MissingParameterException;
 import org.meveo.model.admin.User;
-import org.meveo.model.crm.AccountLevelEnum;
-import org.meveo.model.crm.CustomFieldInstance;
 import org.meveo.model.crm.CustomFieldTemplate;
 import org.meveo.model.crm.Provider;
 import org.meveo.model.jobs.JobCategoryEnum;
 import org.meveo.model.jobs.TimerEntity;
 import org.meveo.service.crm.impl.CustomFieldTemplateService;
+import org.meveo.service.job.Job;
 import org.meveo.service.job.TimerEntityService;
 import org.slf4j.Logger;
 
@@ -36,6 +37,8 @@ public class TimerEntityApi extends BaseApi {
 	@Inject
 	private CustomFieldTemplateService customFieldTemplateService;
 
+	private List<CustomFieldTemplate> customFieldTemplates = new ArrayList<CustomFieldTemplate>();
+
 	public void create(TimerEntityDto postData, User currentUser) throws MeveoApiException {
 		if (!StringUtils.isBlank(postData.getJobCategory()) && !StringUtils.isBlank(postData.getJobName())
 				&& !StringUtils.isBlank(postData.getName())
@@ -44,7 +47,10 @@ public class TimerEntityApi extends BaseApi {
 				&& !StringUtils.isBlank(postData.getMonth()) && !StringUtils.isBlank(postData.getDayOfMonth())
 				&& !StringUtils.isBlank(postData.getDayOfWeek())) {
 			Provider provider = currentUser.getProvider();
- 
+
+			if (timerEntityService.findByName(postData.getName(), provider) != null) {
+				throw new EntityAlreadyExistsException(TimerEntity.class, postData.getName());
+			}
 			JobCategoryEnum jobCategory = null;
 			try {
 				jobCategory = JobCategoryEnum.valueOf(postData.getJobCategory().toUpperCase());
@@ -55,14 +61,33 @@ public class TimerEntityApi extends BaseApi {
 			   HashMap<String, String> jobs = new HashMap<String, String>();
 		        jobs = timerEntityService.jobEntries.get(jobCategory);
 			
-			if(!jobs.containsKey(postData.getJobName())){
-				throw new MeveoApiException(MeveoApiErrorCode.BUSINESS_API_EXCEPTION, "Invalid job name=" + postData.getJobName());
-				}}
-		   
+			if(jobs.containsKey(postData.getJobName())){
+			   customFieldTemplates.clear();
+	            Job job = timerEntityService.getJobByName(postData.getJobName());
+	            if (job.getCustomFields(currentUser) != null) {
+	              customFieldTemplates = customFieldTemplateService.findByJobName(postData.getJobName());
+	              if (customFieldTemplates != null && customFieldTemplates.size() != job.getCustomFields(currentUser).size()) {
+	                    for (CustomFieldTemplate cf : job.getCustomFields(currentUser)) {
+	                        if (!customFieldTemplates.contains(cf)) {
+	                            try {
+	                                customFieldTemplateService.create(cf);
+	                                customFieldTemplates.add(cf);
+	                            } catch (BusinessException e) {
+	                                log.error("Failed  to init custom fields",e);
+	                            }
+	                        }
+		                    }
+		                }
+	            			}
+						   }
+		   				}else{   
+						 throw new MeveoApiException(MeveoApiErrorCode.BUSINESS_API_EXCEPTION, "Invalid job name=" + postData.getJobName());
+						  }
+			
 			TimerEntity timerEntity = new TimerEntity(); 
 			timerEntity.getTimerInfo().setUserId(currentUser.getId());
 			timerEntity.getTimerInfo().setActive(postData.isActive());
-			timerEntity.getTimerInfo().setParametres(postData.getParametres());
+			timerEntity.getTimerInfo().setParametres(postData.getParameter());
 			 if (!StringUtils.isBlank(postData.getFollowingTimer())) {
 			 TimerEntity nextJob = timerEntityService.findByName(postData.getFollowingTimer(),provider);
 			 timerEntity.setFollowingTimer(timerEntityService.findByName(postData.getFollowingTimer(),provider));
@@ -80,32 +105,8 @@ public class TimerEntityApi extends BaseApi {
 			timerEntity.setMonth(postData.getMonth());
 			timerEntity.setDayOfMonth(postData.getDayOfMonth());
 			timerEntity.setDayOfWeek(postData.getDayOfWeek());
-			 
-			if (postData.getCustomFields() != null) {
-				for (CustomFieldDto cf : postData.getCustomFields().getCustomField()) { 
-					List<CustomFieldTemplate> customFieldTemplates = customFieldTemplateService.findByAccountLevel(AccountLevelEnum.TIMER, provider);
-			if (customFieldTemplates != null && customFieldTemplates.size() > 0) {
-				for (CustomFieldTemplate cft : customFieldTemplates) {
-				if (cf.getCode().equals(cft.getCode())) { 
-					CustomFieldInstance cfiNew = new CustomFieldInstance();
-					cfiNew.setTimerEntity(timerEntity);
-					cfiNew.setActive(true);
-					cfiNew.setCode(cf.getCode());
-					cfiNew.setDateValue(cf.getDateValue());
-					cfiNew.setDescription(cf.getDescription());
-					cfiNew.setDoubleValue(cf.getDoubleValue());
-					cfiNew.setLongValue(cf.getLongValue());
-					cfiNew.setProvider(currentUser.getProvider());
-					cfiNew.setStringValue(cf.getStringValue());
-					cfiNew.updateAudit(currentUser);
-					timerEntity.getCustomFields().put(cfiNew.getCode(), cfiNew);
-				}
-				}
-			} else {
-				log.warn("No custom field template defined.");
-			}
-				}
-			}
+				
+	
 			timerEntityService.create(timerEntity, currentUser, provider);
 		} else {
 				
@@ -141,4 +142,16 @@ public class TimerEntityApi extends BaseApi {
 		}
 	}
 
+	public List<CustomFieldTemplate> getCustomFieldTemplates() {
+		return customFieldTemplates;
+	}
+
+	public void setCustomFieldTemplates(
+			List<CustomFieldTemplate> customFieldTemplates) {
+		this.customFieldTemplates = customFieldTemplates;
+	}
+	
+	
 }
+
+		        
