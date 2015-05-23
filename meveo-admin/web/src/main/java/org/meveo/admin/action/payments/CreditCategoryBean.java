19d98785082946fa3634b4a444104eb5f417b887
Edward P. Legaspi <czetsuya@gmail.com>
2015-05-23 10:12:17 +0800

    Review #655.

---
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/payments/CreditCategoryBean.java
@@ -0,0 +1,33 @@
+package org.meveo.admin.action.payments;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.meveo.admin.action.BaseBean;
+import org.meveo.model.payments.CreditCategory;
+import org.meveo.service.base.local.IPersistenceService;
+import org.meveo.service.payments.impl.CreditCategoryService;
+import org.omnifaces.cdi.ViewScoped;
+
+/**
+ * @author Edward P. Legaspi
+ **/
+@Named
+@ViewScoped
+public class CreditCategoryBean extends BaseBean<CreditCategory> {
+
+	private static final long serialVersionUID = 2958011009867008164L;
+
+	@Inject
+	private CreditCategoryService creditCategoryService;
+
+	public CreditCategoryBean() {
+		super(CreditCategory.class);
+	}
+
+	@Override
+	protected IPersistenceService<CreditCategory> getPersistenceService() {
+		return creditCategoryService;
+	}
+
+}
---
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/payments/CreditCategoryListBean.java
@@ -0,0 +1,15 @@
+package org.meveo.admin.action.payments;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Named;
+
+/**
+ * @author Edward P. Legaspi
+ **/
+@Named
+@ConversationScoped
+public class CreditCategoryListBean extends CreditCategoryBean {
+
+	private static final long serialVersionUID = 8109076594426859889L;
+
+}
