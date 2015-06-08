583c2235f1f6f6298c1945208b399d10fe85be75
anasseh <anasseh@yahoo.fr>
2015-06-08 10:30:53 +0100

    re #855

--- meveo-model/src/main/db_resources/changelog/current/migrationJobs_afterLiquibase.sql
+++ meveo-model/src/main/db_resources/changelog/current/migrationJobs_afterLiquibase.sql
@@ -7,7 +7,7 @@ SELECT id,version,sc_d_o_month ,sc_d_o_week ,sc_end ,sc_hour ,sc_min ,sc_month ,
   sc_sec ,sc_start ,sc_year ,provider_id, 'code_'||id,current_timestamp,1,'',FALSE,null,null
 FROM temp_meveo_timer;
 
-ALTER SEQUENCE meveo_timer_seq RESTART WITH select max(id)+1 from meveo_timer ;
+
 
 INSERT INTO meveo_job_instance
 (id ,version ,disabled ,created ,updated ,code ,description ,
@@ -18,7 +18,7 @@ SELECT id,version,FALSE ,current_timestamp ,null ,name ,'' ,
 active , job_category ,job_name ,parametres ,userid, provider_id,1,null,id
 FROM temp_meveo_timer;
 
-ALTER SEQUENCE meveo_instance_job_seq RESTART WITH (select max(id)+1 from meveo_instance_job );
+
 
 INSERT INTO  crm_custom_field_inst (
    id, version ,disabled ,created ,updated ,code ,description ,
@@ -33,7 +33,10 @@ SELECT   id, version ,disabled ,created ,updated ,code ,description ,
   FALSE ,meveo_timer_id
   FROM temp_crm_custom_field_inst;
   
-  ALTER SEQUENCE CRM_CUSTOM_FIELD_INST_SEQ RESTART WITH select max(id)+1 from crm_custom_field_inst ;
+  drop table temp_meveo_timer;
+  drop table temp_crm_custom_field_inst;
   
-  
+  --ALTER SEQUENCE meveo_timer_seq RESTART WITH select max(id)+1 from meveo_timer ;
+  --ALTER SEQUENCE meveo_instance_job_seq RESTART WITH (select max(id)+1 from meveo_instance_job );
+ -- ALTER SEQUENCE CRM_CUSTOM_FIELD_INST_SEQ RESTART WITH select max(id)+1 from crm_custom_field_inst ;
 
