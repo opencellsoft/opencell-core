e780965ea76b90a1c9c488fdc857a28f9363b684
Edward P. Legaspi <czetsuya@gmail.com>
2015-06-08 21:00:49 +0800

    RE #861.

--- meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunService.java
@@ -575,7 +575,7 @@ public class BillingRunService extends PersistenceService<BillingRun> {
 		for (Invoice invoice : billingRun.getInvoices()) {
 			invoiceService.setInvoiceNumber(invoice, user);
 			BillingAccount billingAccount = invoice.getBillingAccount();
-			Date nextCalendarDate = billingAccount.getBillingCycle().getNextCalendarDate();
+			Date nextCalendarDate = billingAccount.getBillingCycle().getNextCalendarDate(billingAccount.getAuditable().getCreated());
 			billingAccount.setNextInvoiceDate(nextCalendarDate);
 			billingAccount.updateAudit(user);
 		}
