59b0469d4b5f56212ceadc989553309b7d0f0b81
mbarek <mbarek.omoh@manaty.net>
2015-06-08 20:11:59 +0200

    fix pb mandatory paymentTerm

--- meveo-api/src/main/java/org/meveo/api/account/BillingAccountApi.java
+++ meveo-api/src/main/java/org/meveo/api/account/BillingAccountApi.java
@@ -104,12 +104,13 @@ public class BillingAccountApi extends AccountApi {
 			billingAccount.setTradingCountry(tradingCountry);
 			billingAccount.setTradingLanguage(tradingLanguage);
 			billingAccount.setPaymentMethod(paymentMethod);
-			try {
+			if(!StringUtils.isBlank(postData.getPaymentTerms())){	
+			 try {
 				billingAccount.setPaymentTerm(PaymentTermEnum.valueOf(postData.getPaymentTerms()));
-			} catch (IllegalArgumentException e) {
+			} catch (IllegalArgumentException e) {				
 				log.error("InvalidEnum for paymentTerm with name={}", postData.getPaymentTerms());
 				throw new MeveoApiException(MeveoApiErrorCode.INVALID_ENUM_VALUE, "Enum for PaymentTerm with name=" + postData.getPaymentTerms() + " does not exists.");
-			}
+		    }}
 			billingAccount.setNextInvoiceDate(postData.getNextInvoiceDate());
 			billingAccount.setSubscriptionDate(postData.getSubscriptionDate());
 			billingAccount.setTerminationDate(postData.getTerminationDate());
