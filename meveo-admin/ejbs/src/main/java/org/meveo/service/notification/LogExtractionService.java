e542f3b940e0967fd2e60fa29ae729818f10d779
anasseh <anasseh@yahoo.fr>
2015-06-17 15:33:38 +0000

    review #883

--- meveo-admin/ejbs/src/main/java/org/meveo/service/notification/LogExtractionService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/notification/LogExtractionService.java
@@ -17,26 +17,26 @@ public class LogExtractionService {
 	
 	public static String  getLogs(Date fromDate,Date toDate){
 		String result="";
-		try {
-			Properties props = new Properties();
-			props.load(new FileInputStream(System.getProperty("logging.configuration").substring(5)));
-			String logFile = props.getProperty("handler.FILE.fileName");
-			String dateFormat = props.getProperty("formatter.FILE.pattern").substring(props.getProperty("formatter.FILE.pattern").indexOf("{")+1, props.getProperty("formatter.FILE.pattern").indexOf("}"));
-			List<String>  allLines = Files.readAllLines(Paths.get(logFile), StandardCharsets.UTF_8);
-			List<String>  foundedLines = new ArrayList<String>();
-			int length=0;
-			for(String line : allLines){
-				if(DateUtils.isDateTimeWithinPeriod(DateUtils.parseDateWithPattern(line.substring(0, dateFormat.length()), dateFormat), fromDate, toDate)){
-					foundedLines.add(line);
-					length += line.length();
-				}
-				if(length > Integer.parseInt(ParamBean.getInstance().getProperty("meveo.notifier.log.lenght", "100000"))){
-					break;
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Properties props = new Properties();
+//			props.load(new FileInputStream(System.getProperty("logging.configuration").substring(5)));
+//			String logFile = props.getProperty("handler.FILE.fileName");
+//			String dateFormat = props.getProperty("formatter.FILE.pattern").substring(props.getProperty("formatter.FILE.pattern").indexOf("{")+1, props.getProperty("formatter.FILE.pattern").indexOf("}"));
+//			List<String>  allLines = Files.readAllLines(Paths.get(logFile), StandardCharsets.UTF_8);
+//			List<String>  foundedLines = new ArrayList<String>();
+//			int length=0;
+//			for(String line : allLines){
+//				if(DateUtils.isDateTimeWithinPeriod(DateUtils.parseDateWithPattern(line.substring(0, dateFormat.length()), dateFormat), fromDate, toDate)){
+//					foundedLines.add(line);
+//					length += line.length();
+//				}
+//				if(length > Integer.parseInt(ParamBean.getInstance().getProperty("meveo.notifier.log.lenght", "100000"))){
+//					break;
+//				}
+//			}
+//		} catch (IOException e) {
+	//		e.printStackTrace();
+		//}
 		return result;
 	}
 	
