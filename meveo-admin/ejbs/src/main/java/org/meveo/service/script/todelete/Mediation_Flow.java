7a3ec416be37d64e3f037d8a2a7efdf8e9648e7a
anasseh <abdelhadi.nasseh@manaty.net>
2015-08-27 15:14:48 +0100

    #907

--- meveo-admin/ejbs/src/main/java/org/meveo/service/script/todelete/Mediation_Flow.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/script/todelete/Mediation_Flow.java
@@ -13,13 +13,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Mediation_Flow extends org.meveo.service.script.Script {
-
-	public Mediation_Flow() {
-	}
-
+	
 	private static final Logger log = LoggerFactory.getLogger(Mediation_Flow.class);
-
-	public void execute(Map<String, Object> initContext,Provider provider)throws BusinessException {
+	
+	public void execute(Map<String, Object> initContext, Provider provider) throws BusinessException {
 		log.info("Execute...");
 		JavaCompilerManager javaCompilerManager = (JavaCompilerManager) getServiceInterface("JavaCompilerManager");
 
@@ -27,27 +24,35 @@ public class Mediation_Flow extends org.meveo.service.script.Script {
 		Class<ScriptInterface> mediation_RoutingSub = javaCompilerManager.getScriptInterface(provider, "Mediation_RoutingSub");
 		Class<ScriptInterface> mediation_CreateEdr = javaCompilerManager.getScriptInterface(provider, "Mediation_CreateEdr");
 		CDR cdr = (CDR) initContext.get("record");
+
 		try {
 			mediation_DuplicatedCheck.newInstance().execute(initContext, provider);
-			Boolean duplicateFound = (Boolean)initContext.get("duplicateFound");
-			log.info("duplicateFound:"+duplicateFound);
-			
-			if(duplicateFound != null && duplicateFound.booleanValue()){
-				throw new BusinessException("DuplicateFound");
-			}
-			initContext.put("accessUserId",cdr.getAccess_id());
-			mediation_RoutingSub.newInstance().execute(initContext, provider);
-			
-			List<Access> accesses = (List<Access>) initContext.get("accesses");
-			if(accesses == null || accesses.isEmpty()){
-				throw new BusinessException("InvalidAccess");
-			}
-
-			mediation_CreateEdr.newInstance().execute(initContext, provider);
-			
-		} catch (Exception e) {
+		} catch (InstantiationException | IllegalAccessException e) {
 			throw new BusinessException("ErrTech");
 		}
+		Boolean duplicateFound = (Boolean) initContext.get("duplicateFound");
+		log.info("duplicateFound:" + duplicateFound);
+
+		if (duplicateFound != null && duplicateFound.booleanValue()) {
+			throw new BusinessException("DuplicateFound");
+		}
+		initContext.put("accessUserId", cdr.getAccess_id());
+		try {
+			mediation_RoutingSub.newInstance().execute(initContext, provider);
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new BusinessException("ErrTech");
+		}
+
+		List<Access> accesses = (List<Access>) initContext.get("accesses");
+		if (accesses == null || accesses.isEmpty()) {
+			throw new BusinessException("InvalidAccess");
+		}
+		try {
+			mediation_CreateEdr.newInstance().execute(initContext, provider);
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new BusinessException("ErrTech");
+		}
+
 		log.info("Execute update entity OK");
 	}
 
