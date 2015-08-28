51357c66ac2070671d54ff6592a98988b4625c45
Edward P. Legaspi <czetsuya@gmail.com>
2015-08-28 20:30:41 +0800

    RE #1031. Implement createOrUpdate api for country.

--- meveo-api/src/main/java/org/meveo/api/ws/impl/SettingsWsImpl.java
+++ meveo-api/src/main/java/org/meveo/api/ws/impl/SettingsWsImpl.java
@@ -1417,4 +1417,24 @@ public class SettingsWsImpl extends BaseWs implements SettingsWs {
 		return result;
 	}
 
+	@Override
+	public ActionStatus createOrUpdateCountry(CountryDto postData) {
+		ActionStatus result = new ActionStatus(ActionStatusEnum.SUCCESS, "");
+
+		try {
+			countryApi.createOrUpdate(postData, getCurrentUser());
+		} catch (MeveoApiException e) {
+			result.setErrorCode(e.getErrorCode());
+			result.setStatus(ActionStatusEnum.FAIL);
+			result.setMessage(e.getMessage());
+		} catch (Exception e) {
+			result.setErrorCode(MeveoApiErrorCode.GENERIC_API_EXCEPTION);
+			result.setStatus(ActionStatusEnum.FAIL);
+			result.setMessage(e.getMessage());
+		}
+
+		log.debug("RESPONSE={}", result);
+		return result;
+	}
+
 }
