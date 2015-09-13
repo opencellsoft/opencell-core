40097e9a8b44057d9a53aa181a735b1f4d5dcae6
carlo liwanag <ecaliwanag@gmail.com>
2015-09-14 00:50:00 +0800

    Re #1061 Updated query param in method find and remove for jobInstance.

--- meveo-api/src/main/java/org/meveo/api/rest/job/JobInstanceRs.java
+++ meveo-api/src/main/java/org/meveo/api/rest/job/JobInstanceRs.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.meveo.api.dto.ActionStatus;
@@ -39,9 +40,9 @@ public interface JobInstanceRs extends IBaseRs{
 	
 	@Path("/")
 	@GET
-	JobInstanceResponseDto find(String jobInstanceCode);
+	JobInstanceResponseDto find(@QueryParam("jobInstanceCode") String jobInstanceCode);
 	
 	@Path("/{jobInstanceCode}")
 	@DELETE
-	ActionStatus remove(String jobInstanceCode);
+	ActionStatus remove(@QueryParam("jobInstanceCode") String jobInstanceCode);
 }
