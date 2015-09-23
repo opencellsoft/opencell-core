4bfaf460a3d9702bbd989e7914807f1c9f536d5d
smichea <sebastien.michea@manaty.net>
2015-09-23 15:15:57 +0200

    review #1090

--- meveo-admin/ejbs/src/main/java/org/meveo/service/script/Script.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/script/Script.java
@@ -3,15 +3,26 @@ package org.meveo.service.script;
 import java.util.Map;
 
 import org.meveo.commons.utils.EjbUtils;
+import org.meveo.model.admin.User;
 import org.meveo.model.crm.Provider;
 
 
 
 public abstract class Script  implements ScriptInterface{
 
+    public void init(Map<String,Object> methodContext,Provider provider,User user){
+    }
+    
     public void init(Map<String,Object> methodContext,Provider provider){
     }
     
+    public void execute(Map<String,Object> methodContext,Provider provider,User user){
+    		execute(methodContext,provider,user);
+    }
+    
+    public void finalize(Map<String,Object> methodContext,Provider provider,User user){
+    }
+
     public void finalize(Map<String,Object> methodContext,Provider provider){
     }
 
