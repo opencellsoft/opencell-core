d48b8d3a94f8c9f918d456c5ff2c598efd06f61c
andriusk <exploresouthamerica@gmail.com>
2015-09-24 11:44:21 -0300

    Script calls itself

--- meveo-admin/ejbs/src/main/java/org/meveo/service/script/Script.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/script/Script.java
@@ -2,6 +2,7 @@ package org.meveo.service.script;
 
 import java.util.Map;
 
+import org.meveo.admin.exception.BusinessException;
 import org.meveo.commons.utils.EjbUtils;
 import org.meveo.model.admin.User;
 import org.meveo.model.crm.Provider;
@@ -10,19 +11,25 @@ import org.meveo.model.crm.Provider;
 
 public abstract class Script  implements ScriptInterface{
 
+	@Override
     public void init(Map<String,Object> methodContext,Provider provider,User user){
     }
     
+	@Override
     public void init(Map<String,Object> methodContext,Provider provider){
     }
     
-    public void execute(Map<String,Object> methodContext,Provider provider,User user){
-    		execute(methodContext,provider,user);
+    @SuppressWarnings("deprecation")
+	@Override
+    public void execute(Map<String,Object> methodContext,Provider provider,User user) throws BusinessException{
+    		execute(methodContext,provider);
     }
     
+    @Override
     public void finalize(Map<String,Object> methodContext,Provider provider,User user){
     }
 
+    @Override
     public void finalize(Map<String,Object> methodContext,Provider provider){
     }
 
