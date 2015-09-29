0a5cf95d095542ef9bd07ec17b0e8394c073cdfe
andriusk <exploresouthamerica@gmail.com>
2015-09-29 00:29:56 -0300

    Re #1099. Merge Liquibase scripts for 4.2 version

--- meveo-model/src/main/db_resources/changelog/current/migrationJobs_beforeLiquibase.sql
+++
@@ -1,4 +0,0 @@
-create table temp_meveo_timer as select *  from  meveo_timer;
-create table temp_CRM_CUSTOM_FIELD_INST as select *  from  CRM_CUSTOM_FIELD_INST;
-delete from CRM_CUSTOM_FIELD_INST;
-delete  from meveo_timer;
