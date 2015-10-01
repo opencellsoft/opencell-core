c35346217d9fed325024e1af58a37d7c6edd6f2b
Edward P. Legaspi <czetsuya@gmail.com>
2015-10-01 16:29:37 +0800

    RE #1107. Add credit note beans and pages.

---
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/CreditNoteService.java
@@ -0,0 +1,14 @@
+package org.meveo.service.billing.impl;
+
+import javax.ejb.Stateless;
+
+import org.meveo.model.billing.CreditNote;
+import org.meveo.service.base.PersistenceService;
+
+/**
+ * @author Edward P. Legaspi
+ **/
+@Stateless
+public class CreditNoteService extends PersistenceService<CreditNote> {
+
+}
