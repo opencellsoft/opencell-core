47b487599370142fc4424aa2f48f37485d2bdcc9
Edward P. Legaspi <czetsuya@gmail.com>
2015-10-20 11:55:32 +0800

    RE #1166. Invoice adjustment crud pages.
    Conflicts:
    	meveo-admin/web/src/main/webapp/pages/billing/invoices/invoiceDetail.xhtml

--- meveo-admin/web/src/main/java/org/meveo/admin/action/billing/InvoiceBean.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/billing/InvoiceBean.java
@@ -35,6 +35,7 @@ import java.util.Set;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.DiscriminatorValue;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jboss.seam.international.status.builder.BundleKey;
@@ -121,6 +122,7 @@ public class InvoiceBean extends BaseBean<Invoice> {
 		super(Invoice.class);
 	}
 
+	@SuppressWarnings({ "unchecked" })
 	@Override
 	public Invoice initEntity() {
 		Invoice invoice = super.initEntity();
@@ -177,6 +179,11 @@ public class InvoiceBean extends BaseBean<Invoice> {
 				invoice.setInvoiceAgregates(taxAmountsAggregate);
 				invoice.getInvoiceAgregates().addAll(subCategoryInvoiceAgregates);
 			}
+		} else if (adjustedInvoiceIdParam != null && adjustedInvoiceIdParam.get() != null) {
+			// load subCategoryInvoiceAggregates
+			subCategoryInvoiceAgregates = (List<SubCategoryInvoiceAgregate>) invoiceAgregateService
+					.listByInvoiceAndType(invoice,
+							SubCategoryInvoiceAgregate.class.getAnnotation(DiscriminatorValue.class).value());
 		} else {
 			getPersistenceService().refresh(invoice);
 		}
