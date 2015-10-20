41df995b61cc301f2f99f6c393e604a1281b1202
mbarek <mbarek.omoh@manaty.net>
2015-10-20 20:33:19 +0200

    Review #1178

--- meveo-api/src/main/java/org/meveo/api/payment/AccountOperationApi.java
+++ meveo-api/src/main/java/org/meveo/api/payment/AccountOperationApi.java
@@ -52,152 +52,159 @@ public class AccountOperationApi extends BaseApi {
 	private MatchingAmountService matchingAmountService;
 
 	public void create(AccountOperationDto postData, User currentUser) throws MeveoApiException {
-		AccountOperation accountOperation = null;
+		if(!StringUtils.isBlank(postData.getType())){
+			AccountOperation accountOperation = null;
 
-		CustomerAccount customerAccount = customerAccountService.findByCode(postData.getCustomerAccount(), currentUser.getProvider());
-		if (customerAccount == null) {
-			throw new EntityDoesNotExistsException(CustomerAccount.class, postData.getCustomerAccount());
-		}
-
-		if (postData.getType().equals("OCC") && postData.getOtherCreditAndCharge() != null) {
-			// otherCreditAndCharge
-			OtherCreditAndCharge otherCreditAndCharge = new OtherCreditAndCharge();
-			otherCreditAndCharge.setOperationDate(postData.getOtherCreditAndCharge().getOperationDate());
-			accountOperation = (AccountOperation) otherCreditAndCharge;
-		} else if (postData.getType().equals("I") && postData.getRecordedInvoice() != null) {
-			// recordedInvoice
-			RecordedInvoice recordedInvoice = new RecordedInvoice();
-			recordedInvoice.setProductionDate(postData.getRecordedInvoice().getProductionDate());
-			recordedInvoice.setInvoiceDate(postData.getRecordedInvoice().getInvoiceDate());
-			recordedInvoice.setAmountWithoutTax(postData.getRecordedInvoice().getAmountWithoutTax());
-			recordedInvoice.setTaxAmount(postData.getRecordedInvoice().getTaxAmount());
-			recordedInvoice.setNetToPay(postData.getRecordedInvoice().getNetToPay());
-
-			try {
-				recordedInvoice.setPaymentMethod(PaymentMethodEnum.valueOf(postData.getRecordedInvoice().getPaymentMethod()));
-			} catch (IllegalStateException e) {
-				log.warn("error occurred while setting payment methode",e);
-			} catch (NullPointerException e) {
-				log.warn("error generated while setting payment methode",e);
-			}
-            
-			recordedInvoice.setPaymentInfo(postData.getRecordedInvoice().getPaymentInfo());
-			recordedInvoice.setPaymentInfo1(postData.getRecordedInvoice().getPaymentInfo1());
-			recordedInvoice.setPaymentInfo2(postData.getRecordedInvoice().getPaymentInfo2());
-			recordedInvoice.setPaymentInfo3(postData.getRecordedInvoice().getPaymentInfo3());
-			recordedInvoice.setPaymentInfo4(postData.getRecordedInvoice().getPaymentInfo4()); 
-			recordedInvoice.setPaymentInfo5(postData.getRecordedInvoice().getPaymentInfo5());
-			recordedInvoice.setPaymentInfo6(postData.getRecordedInvoice().getPaymentInfo6());
-
-			// recordedInvoice.setDdRequestItem(postData.getRecordedInvoice().getDdRequestItem());
-			// recordedInvoice.setDdRequestLOT(postData.getRecordedInvoice().getDdRequestItem());
-			recordedInvoice.setBillingAccountName(postData.getRecordedInvoice().getBillingAccountName());
-
-			accountOperation = (AccountOperation) recordedInvoice;
-		} else if (postData.getType().equals("R") && postData.getRejectedPayment() != null) {
-			// rejectedPayment
-			RejectedPayment rejectedPayment = new RejectedPayment();
-
-			try {
-				rejectedPayment.setRejectedType(RejectedType.valueOf(postData.getRejectedPayment().getRejectedType()));
-			} catch (IllegalStateException e) {
-				log.warn("error occurred while setting rejected type", e);
-			} catch (NullPointerException e) {
-				log.warn("error generated while setting rejected type", e);
+			CustomerAccount customerAccount = customerAccountService.findByCode(postData.getCustomerAccount(), currentUser.getProvider());
+			if (customerAccount == null) {
+				throw new EntityDoesNotExistsException(CustomerAccount.class, postData.getCustomerAccount());
 			}
 
-			rejectedPayment.setBankLot(postData.getRejectedPayment().getBankLot());
-			rejectedPayment.setBankReference(postData.getRejectedPayment().getBankReference());
-			rejectedPayment.setRejectedDate(postData.getRejectedPayment().getRejectedDate());
-			rejectedPayment.setRejectedDescription(postData.getRejectedPayment().getRejectedDescription());
-			rejectedPayment.setRejectedCode(postData.getRejectedPayment().getRejectedCode());
+			if (postData.getType().equals("OCC") && postData.getOtherCreditAndCharge() != null) {
+				// otherCreditAndCharge
+				OtherCreditAndCharge otherCreditAndCharge = new OtherCreditAndCharge();
+				otherCreditAndCharge.setOperationDate(postData.getOtherCreditAndCharge().getOperationDate());
+				accountOperation = (AccountOperation) otherCreditAndCharge;
+			} else if (postData.getType().equals("I") && postData.getRecordedInvoice() != null) {
+				// recordedInvoice
+				RecordedInvoice recordedInvoice = new RecordedInvoice();
+				recordedInvoice.setProductionDate(postData.getRecordedInvoice().getProductionDate());
+				recordedInvoice.setInvoiceDate(postData.getRecordedInvoice().getInvoiceDate());
+				recordedInvoice.setAmountWithoutTax(postData.getRecordedInvoice().getAmountWithoutTax());
+				recordedInvoice.setTaxAmount(postData.getRecordedInvoice().getTaxAmount());
+				recordedInvoice.setNetToPay(postData.getRecordedInvoice().getNetToPay());
 
-			accountOperation = (AccountOperation) rejectedPayment;
-		}
+				try {
+					recordedInvoice.setPaymentMethod(PaymentMethodEnum.valueOf(postData.getRecordedInvoice().getPaymentMethod()));
+				} catch (IllegalStateException e) {
+					log.warn("error occurred while setting payment methode",e);
+				} catch (NullPointerException e) {
+					log.warn("error generated while setting payment methode",e);
+				}
 
-		if (accountOperation == null) {
-			throw new MeveoApiException("Type and data mismatch OCC=otherCreditAndCharge, I=recordedInvoice, R=rejectedPayment.");
-		}
+				recordedInvoice.setPaymentInfo(postData.getRecordedInvoice().getPaymentInfo());
+				recordedInvoice.setPaymentInfo1(postData.getRecordedInvoice().getPaymentInfo1());
+				recordedInvoice.setPaymentInfo2(postData.getRecordedInvoice().getPaymentInfo2());
+				recordedInvoice.setPaymentInfo3(postData.getRecordedInvoice().getPaymentInfo3());
+				recordedInvoice.setPaymentInfo4(postData.getRecordedInvoice().getPaymentInfo4()); 
+				recordedInvoice.setPaymentInfo5(postData.getRecordedInvoice().getPaymentInfo5());
+				recordedInvoice.setPaymentInfo6(postData.getRecordedInvoice().getPaymentInfo6());
 
-		accountOperation.setDueDate(postData.getDueDate());
-		accountOperation.setType(postData.getType());
-		accountOperation.setTransactionDate(postData.getTransactionDate());
-		try {
-			accountOperation.setTransactionCategory(OperationCategoryEnum.valueOf(postData.getTransactionCategory()));
-		} catch (IllegalStateException e) {
-			log.warn("error occurred while setting transaction category", e);
-		} catch (NullPointerException e) {
-			log.warn("error generated while setting transaction category", e);
-		}
-		accountOperation.setReference(postData.getReference());
-		accountOperation.setAccountCode(postData.getAccountCode());
-		accountOperation.setAccountCodeClientSide(postData.getAccountCodeClientSide());
-		accountOperation.setAmount(postData.getAmount());
-		accountOperation.setMatchingAmount(postData.getMatchingAmount());
-		accountOperation.setUnMatchingAmount(postData.getUnMatchingAmount());
-		accountOperation.setCustomerAccount(customerAccount);
+				// recordedInvoice.setDdRequestItem(postData.getRecordedInvoice().getDdRequestItem());
+				// recordedInvoice.setDdRequestLOT(postData.getRecordedInvoice().getDdRequestItem());
+				recordedInvoice.setBillingAccountName(postData.getRecordedInvoice().getBillingAccountName());
 
-		try {
-			accountOperation.setMatchingStatus(MatchingStatusEnum.valueOf(postData.getMatchingStatus()));
-		} catch (IllegalStateException e) {
-			log.warn("error occurred while setting matching status", e);
-		} catch (NullPointerException e) {
-			log.warn("error generated while setting matching status", e);
-		}
+				accountOperation = (AccountOperation) recordedInvoice;
+			} else if (postData.getType().equals("R") && postData.getRejectedPayment() != null) {
+				// rejectedPayment
+				RejectedPayment rejectedPayment = new RejectedPayment();
 
-		accountOperation.setOccCode(postData.getOccCode());
-		accountOperation.setOccDescription(postData.getOccDescription());
-		if(!StringUtils.isBlank(postData.getExcludedFromDunning())){
-			accountOperation.setExcludedFromDunning(postData.getExcludedFromDunning());	
-		}else{
-			accountOperation.setExcludedFromDunning(false);	
-		}
-		
-		accountOperationService.create(accountOperation, currentUser, currentUser.getProvider());
+				try {
+					rejectedPayment.setRejectedType(RejectedType.valueOf(postData.getRejectedPayment().getRejectedType()));
+				} catch (IllegalStateException e) {
+					log.warn("error occurred while setting rejected type", e);
+				} catch (NullPointerException e) {
+					log.warn("error generated while setting rejected type", e);
+				}
 
-		if (postData.getMatchingAmounts() != null) {
-			for (MatchingAmountDto matchingAmountDto : postData.getMatchingAmounts().getMatchingAmount()) {
-				MatchingAmount matchingAmount = new MatchingAmount();
-				matchingAmount.setMatchingAmount(matchingAmountDto.getMatchingAmount());
-				matchingAmount.setAccountOperation(accountOperation);
-				if (matchingAmountDto.getMatchingCodes() != null) {
-					for (MatchingCodeDto matchingCodeDto : matchingAmountDto.getMatchingCodes().getMatchingCode()) {
-						MatchingCode matchingCode = matchingCodeService.findByCode(matchingCodeDto.getCode(), currentUser.getProvider());
-						if (matchingCode == null) {
-							matchingCode = new MatchingCode();
-							matchingCode.setCode(matchingCodeDto.getCode());
+				rejectedPayment.setBankLot(postData.getRejectedPayment().getBankLot());
+				rejectedPayment.setBankReference(postData.getRejectedPayment().getBankReference());
+				rejectedPayment.setRejectedDate(postData.getRejectedPayment().getRejectedDate());
+				rejectedPayment.setRejectedDescription(postData.getRejectedPayment().getRejectedDescription());
+				rejectedPayment.setRejectedCode(postData.getRejectedPayment().getRejectedCode());
+
+				accountOperation = (AccountOperation) rejectedPayment;
+			}
+
+			if (accountOperation == null) {
+				throw new MeveoApiException("Type and data mismatch OCC=otherCreditAndCharge, I=recordedInvoice, R=rejectedPayment.");
+			}
+
+			accountOperation.setDueDate(postData.getDueDate());
+			accountOperation.setType(postData.getType());
+			accountOperation.setTransactionDate(postData.getTransactionDate());
+			try {
+				accountOperation.setTransactionCategory(OperationCategoryEnum.valueOf(postData.getTransactionCategory()));
+			} catch (IllegalStateException e) {
+				log.warn("error occurred while setting transaction category", e);
+			} catch (NullPointerException e) {
+				log.warn("error generated while setting transaction category", e);
+			}
+			accountOperation.setReference(postData.getReference());
+			accountOperation.setAccountCode(postData.getAccountCode());
+			accountOperation.setAccountCodeClientSide(postData.getAccountCodeClientSide());
+			accountOperation.setAmount(postData.getAmount());
+			accountOperation.setMatchingAmount(postData.getMatchingAmount());
+			accountOperation.setUnMatchingAmount(postData.getUnMatchingAmount());
+			accountOperation.setCustomerAccount(customerAccount);
+
+			try {
+				accountOperation.setMatchingStatus(MatchingStatusEnum.valueOf(postData.getMatchingStatus()));
+			} catch (IllegalStateException e) {
+				log.warn("error occurred while setting matching status", e);
+			} catch (NullPointerException e) {
+				log.warn("error generated while setting matching status", e);
+			}
+
+			accountOperation.setOccCode(postData.getOccCode());
+			accountOperation.setOccDescription(postData.getOccDescription());
+			if(!StringUtils.isBlank(postData.getExcludedFromDunning())){
+				accountOperation.setExcludedFromDunning(postData.getExcludedFromDunning());	
+			}else{
+				accountOperation.setExcludedFromDunning(false);	
+			}
+
+			accountOperationService.create(accountOperation, currentUser, currentUser.getProvider());
+
+			if (postData.getMatchingAmounts() != null) {
+				for (MatchingAmountDto matchingAmountDto : postData.getMatchingAmounts().getMatchingAmount()) {
+					MatchingAmount matchingAmount = new MatchingAmount();
+					matchingAmount.setMatchingAmount(matchingAmountDto.getMatchingAmount());
+					matchingAmount.setAccountOperation(accountOperation);
+					if (matchingAmountDto.getMatchingCodes() != null) {
+						for (MatchingCodeDto matchingCodeDto : matchingAmountDto.getMatchingCodes().getMatchingCode()) {
+							MatchingCode matchingCode = matchingCodeService.findByCode(matchingCodeDto.getCode(), currentUser.getProvider());
+							if (matchingCode == null) {
+								matchingCode = new MatchingCode();
+								matchingCode.setCode(matchingCodeDto.getCode());
+							}
+
+							try {
+								matchingCode.setMatchingType(MatchingTypeEnum.valueOf(matchingCodeDto.getMatchingType()));
+							} catch (IllegalStateException e) {
+								log.warn("error occurred while setting matching type",e);
+							} catch (NullPointerException e) {
+								log.warn("error generated while setting matching type",e);
+							}
+
+							matchingCode.setMatchingDate(matchingCodeDto.getMatchingDate());
+							matchingCode.setMatchingAmountCredit(matchingCodeDto.getMatchingAmountCredit());
+							matchingCode.setMatchingAmountDebit(matchingCodeDto.getMatchingAmountDebit());
+
+							if (matchingCode.isTransient()) {
+								matchingCodeService.create(matchingCode, currentUser, currentUser.getProvider());
+							} else {
+								matchingCodeService.update(matchingCode, currentUser);
+							}
+
+							matchingAmount.setMatchingCode(matchingCode);
 						}
-
-						try {
-							matchingCode.setMatchingType(MatchingTypeEnum.valueOf(matchingCodeDto.getMatchingType()));
-						} catch (IllegalStateException e) {
-							log.warn("error occurred while setting matching type",e);
-						} catch (NullPointerException e) {
-							log.warn("error generated while setting matching type",e);
-						}
-
-						matchingCode.setMatchingDate(matchingCodeDto.getMatchingDate());
-						matchingCode.setMatchingAmountCredit(matchingCodeDto.getMatchingAmountCredit());
-						matchingCode.setMatchingAmountDebit(matchingCodeDto.getMatchingAmountDebit());
-
-						if (matchingCode.isTransient()) {
-							matchingCodeService.create(matchingCode, currentUser, currentUser.getProvider());
-						} else {
-							matchingCodeService.update(matchingCode, currentUser);
-						}
-
-						matchingAmount.setMatchingCode(matchingCode);
 					}
-				}
 
-				if (matchingAmount.isTransient()) {
-					matchingAmountService.create(matchingAmount, currentUser, currentUser.getProvider());
-				} else {
-					matchingAmountService.update(matchingAmount, currentUser);
-				}
+					if (matchingAmount.isTransient()) {
+						matchingAmountService.create(matchingAmount, currentUser, currentUser.getProvider());
+					} else {
+						matchingAmountService.update(matchingAmount, currentUser);
+					}
 
-				accountOperation.getMatchingAmounts().add(matchingAmount);
+					accountOperation.getMatchingAmounts().add(matchingAmount);
+				}
 			}
+		}else {
+			if (StringUtils.isBlank(postData.getType())) {
+				missingParameters.add("Type");
+			}
+			throw new MissingParameterException(getMissingParametersExceptionMessage());
 		}
 	}
 
