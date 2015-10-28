0eaebfeee04fd87059971bb2f19e0377492b23fa
anasseh <abdelhadi.nasseh@manaty.net>
2015-10-28 22:48:15 +0000

    re #1206 : OutOff Memory Job AccountOperationsGenerationJob

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJob.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJob.java
@@ -1,11 +1,21 @@
 package org.meveo.admin.job;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.Asynchronous;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.admin.util.ResourceBundle;
 import org.meveo.model.admin.User;
+import org.meveo.model.crm.AccountLevelEnum;
+import org.meveo.model.crm.CustomFieldTemplate;
+import org.meveo.model.crm.CustomFieldTypeEnum;
 import org.meveo.model.jobs.JobCategoryEnum;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.jobs.JobInstance;
@@ -17,14 +27,52 @@ public class AccountOperationsGenerationJob extends Job {
 
     @Inject
     private AccountOperationsGenerationJobBean accountOperationsGenerationJobBean;
-
+    
+	 @Inject
+	 private ResourceBundle resourceMessages;
+    
     @Override
+    @Asynchronous
+    @TransactionAttribute(TransactionAttributeType.NEVER)
+    public void execute(JobInstance jobInstance, User currentUser) {
+        super.execute(jobInstance, currentUser);
+    }
+    
+    @Override
+    @TransactionAttribute(TransactionAttributeType.NEVER)
     protected void execute(JobExecutionResultImpl result, JobInstance jobInstance, User currentUser) throws BusinessException {
-        accountOperationsGenerationJobBean.execute(result, jobInstance.getParametres(), currentUser);
+        accountOperationsGenerationJobBean.execute(result,currentUser, jobInstance );
     }
 
     @Override
     public JobCategoryEnum getJobCategory() {
         return JobCategoryEnum.ACCOUNT_RECEIVABLES;
     }
+    
+    @Override
+   	public Map<String, CustomFieldTemplate> getCustomFields() {
+           Map<String, CustomFieldTemplate> result = new HashMap<String, CustomFieldTemplate>();
+
+   		CustomFieldTemplate nbRuns = new CustomFieldTemplate();
+   		nbRuns.setCode("AccOpGeneratioJobb_nbRuns");
+   		nbRuns.setAccountLevel(AccountLevelEnum.TIMER);
+   		nbRuns.setActive(true);
+   		nbRuns.setDescription(resourceMessages.getString("jobExecution.nbRuns"));
+   		nbRuns.setFieldType(CustomFieldTypeEnum.LONG);
+   		nbRuns.setValueRequired(false);
+   		nbRuns.setDefaultValue("1");
+   		result.put("AccOpGeneratioJobb_nbRuns", nbRuns);
+
+   		CustomFieldTemplate waitingMillis = new CustomFieldTemplate();
+   		waitingMillis.setCode("AccOpGeneratioJobb_waitingMillis");
+   		waitingMillis.setAccountLevel(AccountLevelEnum.TIMER);
+   		waitingMillis.setActive(true);
+   		waitingMillis.setDescription(resourceMessages.getString("jobExecution.waitingMillis"));
+   		waitingMillis.setFieldType(CustomFieldTypeEnum.LONG);
+   		waitingMillis.setValueRequired(false);
+   		waitingMillis.setDefaultValue("0");
+   		result.put("AccOpGeneratioJobb_waitingMillis", waitingMillis);
+
+   		return result;
+   	}
 }
--- meveo-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJobBean.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJobBean.java
@@ -1,7 +1,9 @@
 package org.meveo.admin.job;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -9,26 +11,15 @@ import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 
-import org.meveo.admin.exception.ImportInvoiceException;
-import org.meveo.admin.exception.InvoiceExistException;
+import org.meveo.admin.async.AccOpGenerationAsync;
+import org.meveo.admin.async.SubListCreator;
 import org.meveo.admin.job.logging.JobLoggingInterceptor;
 import org.meveo.interceptor.PerformanceInterceptor;
 import org.meveo.model.admin.User;
-import org.meveo.model.billing.BillingAccount;
-import org.meveo.model.billing.Invoice;
-import org.meveo.model.crm.AccountLevelEnum;
-import org.meveo.model.crm.CustomFieldInstance;
 import org.meveo.model.crm.Provider;
 import org.meveo.model.jobs.JobExecutionResultImpl;
-import org.meveo.model.payments.CustomerAccount;
-import org.meveo.model.payments.MatchingStatusEnum;
-import org.meveo.model.payments.OCCTemplate;
-import org.meveo.model.payments.RecordedInvoice;
-import org.meveo.model.shared.DateUtils;
+import org.meveo.model.jobs.JobInstance;
 import org.meveo.service.billing.impl.InvoiceService;
-import org.meveo.service.crm.impl.ProviderService;
-import org.meveo.service.payments.impl.OCCTemplateService;
-import org.meveo.service.payments.impl.RecordedInvoiceService;
 import org.slf4j.Logger;
 
 /**
@@ -41,173 +32,65 @@ public class AccountOperationsGenerationJobBean {
 	private Logger log;
 
 	@Inject
-	private OCCTemplateService oCCTemplateService;
-
-	@Inject
-	private ProviderService providerService;
-
-	@Inject
 	private InvoiceService invoiceService;
 
 	@Inject
-	private RecordedInvoiceService recordedInvoiceService;
+	private AccOpGenerationAsync accOpGenerationAsync;
 
 	@Interceptors({ JobLoggingInterceptor.class, PerformanceInterceptor.class })
-	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-	public void execute(JobExecutionResultImpl result, String parameter,
-			User currentUser) {
+	@TransactionAttribute(TransactionAttributeType.NEVER)
+	public void execute(JobExecutionResultImpl result, User currentUser, JobInstance jobInstance) {
 		Provider currentProvider = currentUser.getProvider();
-		log.info("Running for user={}, parameter={}, provider={}", currentUser,
-				parameter, currentProvider.getCode());
-
-		// ParamBean paramBean = ParamBean.getInstance();
-		List<Invoice> invoices = invoiceService
-				.getInvoicesWithNoAccountOperation(null, currentProvider);
-		// Provider providerForHistory = null;
-
-		log.info("processing {} invoice", invoices.size());
-
-		for (Invoice invoice : invoices) {
-			try {
-				CustomerAccount customerAccount = null;
-				OCCTemplate invoiceTemplate = null;
-				RecordedInvoice recordedInvoice = new RecordedInvoice();
-				BillingAccount billingAccount = invoice.getBillingAccount();
-
-				if (recordedInvoiceService.isRecordedInvoiceExist(
-						invoice.getInvoiceNumber(), invoice.getProvider())) {
-					throw new InvoiceExistException("Invoice id"
-							+ invoice.getId() + " already exist");
+		log.info("Running for user={}, provider={}", currentUser,currentProvider.getCode());
+		try {
+			
+			List<Long> ids = invoiceService.getInvoiceIdsWithNoAccountOperation(null, currentProvider);
+			log.debug("invoices to traite:" +( ids == null ? null:ids.size()));
+			
+			Long nbRuns = new Long(1);		
+			Long waitingMillis = new Long(0);
+			try{
+				nbRuns = (Long) jobInstance.getCFValue("AccOpGeneratioJobb_nbRuns");  			
+				waitingMillis = (Long) jobInstance.getCFValue("AccOpGeneratioJobb_waitingMillis");
+				if(nbRuns == -1){
+					nbRuns  = (long) Runtime.getRuntime().availableProcessors();
 				}
-
-				try {
-					customerAccount = invoice.getBillingAccount()
-							.getCustomerAccount();
-					recordedInvoice.setCustomerAccount(customerAccount);
-					recordedInvoice.setProvider(customerAccount.getProvider());
-				} catch (Exception e) {
-					log.error("error while getting customer account ", e);
-					throw new ImportInvoiceException(
-							"Cannot found customerAccount");
-				}
-
-				try {
-
-					CustomFieldInstance cfInstance = (CustomFieldInstance) providerService
-							.getCustomFieldOrProperty(
-									"accountOperationsGenerationJob.occCode",
-									"FA_FACT", customerAccount.getProvider(),
-									true, AccountLevelEnum.PROVIDER,
-									currentUser);
-					invoiceTemplate = oCCTemplateService.findByCode(cfInstance
-							.getValueAsString(), customerAccount.getProvider()
-							.getCode());
-				} catch (Exception e) {
-					log.error("error while getting occ template ", e);
-					throw new ImportInvoiceException(
-							"Cannot found OCC Template for invoice");
-				}
-
-				recordedInvoice.setReference(invoice.getInvoiceNumber());
-				recordedInvoice
-						.setAccountCode(invoiceTemplate.getAccountCode());
-				recordedInvoice.setOccCode(invoiceTemplate.getCode());
-				recordedInvoice.setOccDescription(invoiceTemplate
-						.getDescription());
-				recordedInvoice.setTransactionCategory(invoiceTemplate
-						.getOccCategory());
-				recordedInvoice.setAccountCodeClientSide(invoiceTemplate
-						.getAccountCodeClientSide());
-
-				try {
-					recordedInvoice.setAmount(invoice.getAmountWithTax());
-					recordedInvoice.setUnMatchingAmount(invoice
-							.getAmountWithTax());
-					recordedInvoice.setMatchingAmount(BigDecimal.ZERO);
-				} catch (Exception e) {
-					log.error("error with amount with tax", e);
-					throw new ImportInvoiceException("Error on amountWithTax");
-				}
-
-				try {
-					recordedInvoice.setAmountWithoutTax(invoice
-							.getAmountWithoutTax());
-				} catch (Exception e) {
-					log.error("error with amount without tax", e);
-					throw new ImportInvoiceException(
-							"Error on amountWithoutTax");
-				}
-
-				try {
-					recordedInvoice.setNetToPay(invoice.getNetToPay());
-				} catch (Exception e) {
-					log.error("error with netToPay", e);
-					throw new ImportInvoiceException("Error on netToPay");
-				}
-
-				try {
-					recordedInvoice.setDueDate(DateUtils.setTimeToZero(invoice
-							.getDueDate()));
-				} catch (Exception e) {
-					log.error("error with due date ", e);
-					throw new ImportInvoiceException("Error on DueDate");
-				}
-
-				try {
-					recordedInvoice.setInvoiceDate(DateUtils
-							.setTimeToZero(invoice.getInvoiceDate()));
-					recordedInvoice.setTransactionDate(DateUtils
-							.setTimeToZero(invoice.getInvoiceDate()));
-				} catch (Exception e) {
-					log.error("error with invoice date", e);
-					throw new ImportInvoiceException("Error on invoiceDate");
-				}
-
-				try {
-					recordedInvoice.setPaymentMethod(billingAccount
-							.getPaymentMethod());
-				} catch (Exception e) {
-					log.error("erro with payment method", e);
-					throw new ImportInvoiceException("Error on paymentMethod");
-				}
-
-				try {
-					recordedInvoice.setTaxAmount(invoice.getAmountTax());
-				} catch (Exception e) {
-					log.error("error with total tax", e);
-					throw new ImportInvoiceException("Error on total tax");
-				}
-
-				if (billingAccount.getBankCoordinates() != null) {
-					recordedInvoice.setPaymentInfo(billingAccount
-							.getBankCoordinates().getIban());
-					recordedInvoice.setPaymentInfo1(billingAccount
-							.getBankCoordinates().getBankCode());
-					recordedInvoice.setPaymentInfo2(billingAccount
-							.getBankCoordinates().getBranchCode());
-					recordedInvoice.setPaymentInfo3(billingAccount
-							.getBankCoordinates().getAccountNumber());
-					recordedInvoice.setPaymentInfo4(billingAccount
-							.getBankCoordinates().getKey());
-					recordedInvoice.setPaymentInfo5(billingAccount
-							.getBankCoordinates().getBankName());
-					recordedInvoice.setPaymentInfo6(billingAccount
-							.getBankCoordinates().getBic());
-					recordedInvoice.setBillingAccountName(billingAccount
-							.getBankCoordinates().getAccountOwner());
-				}
-
-				recordedInvoice.setMatchingStatus(MatchingStatusEnum.O);
-				recordedInvoiceService.create(recordedInvoice, currentUser,
-						currentProvider);
-				invoice.setRecordedInvoice(recordedInvoice);
-				invoice.updateAudit(currentUser);
-				invoiceService.updateNoCheck(invoice);
-				result.registerSucces();
-			} catch (Exception e) {
-				log.error("Failed to generate acount operations", e);
-				result.registerError(e.getMessage());
+			}catch(Exception e){
+				nbRuns = new Long(1);
+				waitingMillis = new Long(0);
+				log.warn("Cant get customFields for "+jobInstance.getJobTemplate());
 			}
-		}
+			List<Future<String>> futures = new ArrayList<Future<String>>();
+	    	SubListCreator subListCreator = new SubListCreator(ids,nbRuns.intValue());
+	    	log.debug("block to run:" + subListCreator.getBlocToRun());
+	    	log.debug("nbThreads:" + nbRuns);
+			while (subListCreator.isHasNext()) {	
+				futures.add(accOpGenerationAsync.launchAndForget((List<Long>) subListCreator.getNextWorkSet(),result, currentUser));
+                if (subListCreator.isHasNext()) {
+                    try {
+                        Thread.sleep(waitingMillis.longValue());
+                    } catch (InterruptedException e) {
+                        log.error("", e);
+                    }
+                }
+            }
+            // Wait for all async methods to finish
+            for (Future<String> future : futures) {
+                try {
+                    future.get();
+                } catch (InterruptedException e) {
+                    // It was cancelled from outside - no interest
+                    
+                } catch (ExecutionException e) {
+                    Throwable cause = e.getCause();
+                    result.registerError(cause.getMessage());
+                    log.error("Failed to execute async method", cause);
+                }
+            }
+		} catch (Exception e) {
+            log.error("Failed to run accountOperation generation  job",e);
+            result.registerError(e.getMessage());
+        }
 	}
+
 }
