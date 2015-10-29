2164b847dc8b531172a27baaac2bdd11aa829ad9
Tyshan Shi <tyshanchn@hotmail.com>
2015-10-29 20:19:07 +0800

    Re #1189 flat file import from sftp,ftp

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/job/FlatFileProcessingJob.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/job/FlatFileProcessingJob.java
@@ -1,8 +1,11 @@
 package org.meveo.admin.job;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ejb.Asynchronous;
@@ -12,8 +15,22 @@ import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.vfs.FileContent;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSelectInfo;
+import org.apache.commons.vfs.FileSelector;
+import org.apache.commons.vfs.FileSystemException;
+import org.apache.commons.vfs.FileSystemManager;
+import org.apache.commons.vfs.FileSystemOptions;
+import org.apache.commons.vfs.FileType;
+import org.apache.commons.vfs.VFS;
+import org.apache.commons.vfs.impl.DefaultFileSystemManager;
+import org.apache.commons.vfs.impl.StandardFileSystemManager;
+import org.apache.commons.vfs.provider.sftp.SftpFileSystemConfigBuilder;
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.admin.util.ResourceBundle;
+import org.meveo.admin.util.security.Sha1Encrypt;
 import org.meveo.commons.utils.FileUtils;
 import org.meveo.commons.utils.ParamBean;
 import org.meveo.model.admin.User;
@@ -25,7 +42,9 @@ import org.meveo.model.crm.CustomFieldTypeEnum;
 import org.meveo.model.jobs.JobCategoryEnum;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.jobs.JobInstance;
+import org.meveo.model.mediation.ImportedFile;
 import org.meveo.service.job.Job;
+import org.meveo.service.medina.impl.ImportedFileService;
 
 @Startup
 @Singleton
@@ -36,6 +55,8 @@ public class FlatFileProcessingJob extends Job {
 
 	@Inject
 	private ResourceBundle resourceMessages;
+	@Inject
+	private ImportedFileService importedFileService;
 	
 	@Override
 	@Asynchronous
@@ -56,6 +77,15 @@ public class FlatFileProcessingJob extends Job {
 			String originFilename = null;
 			String formatTransfo= null;
 			Map<String, Object> initContext = new HashMap<String, Object>();
+			
+			
+			String fileAccess=null;
+			String distantServer=null;
+			long distantPort=0L;
+			String removeDistantFile=null;
+			String ftpInputDirectory=null;
+			String ftpUsername=null;
+			String ftpPassword=null;
 			try {
 				recordVariableName = (String) jobInstance.getCFValue("FlatFileProcessingJob_recordVariableName");
 				originFilename = (String) jobInstance.getCFValue("FlatFileProcessingJob_originFilename");			
@@ -68,6 +98,14 @@ public class FlatFileProcessingJob extends Job {
 				fileNameExtension = (String) jobInstance.getCFValue("FlatFileProcessingJob_fileNameExtension");
 				scriptInstanceFlowCode = (String) jobInstance.getCFValue("FlatFileProcessingJob_scriptsFlow");
 				formatTransfo = (String) jobInstance.getCFValue("FlatFileProcessingJob_formatTransfo");
+				
+				fileAccess=(String)jobInstance.getCFValue("FlatFileProcessingJob_fileAccess");
+				distantServer=(String)jobInstance.getCFValue("FlatFileProcessingJob_distantServer");
+				distantPort=(Long)jobInstance.getCFValue("FlatFileProcessingJob_distantPort");
+				removeDistantFile=(String)jobInstance.getCFValue("FlatFileProcessingJob_removeDistantFile");
+				ftpInputDirectory=(String)jobInstance.getCFValue("FlatFileProcessingJob_ftpInputDirectory");
+				ftpUsername=(String)jobInstance.getCFValue("FlatFileProcessingJob_ftpUsername");
+				ftpPassword=(String)jobInstance.getCFValue("FlatFileProcessingJob_ftpPassword");
 
 			} catch (Exception e) {
 				log.warn("Cant get customFields for " + jobInstance.getJobTemplate(),e);
@@ -82,13 +120,50 @@ public class FlatFileProcessingJob extends Job {
 				f.mkdirs();
 				log.debug("inputDir {} creation ok",inputDir);
 			}
-			File[] files = FileUtils.getFilesForParsing(inputDir, fileExtensions);
+			
+			//ftp & sftp
+			Map<String,ImportedFile> importedFiles=null;
+			File[] files=null;
+			if(!"local".equalsIgnoreCase(fileAccess)){
+				log.debug("connect to {}",fileAccess);
+				importedFiles=connect2FTP(fileAccess,inputDir,ftpInputDirectory,distantServer,ftpUsername,ftpPassword,distantPort,fileNameExtension,currentUser);
+			}
+			files = FileUtils.getFilesForParsing(inputDir, fileExtensions);
 			if (files == null || files.length == 0) {
 				log.debug("there no file in {} with extension {}",inputDir,fileExtensions);
 				return;
 			}
+			log.debug("found files {}",files.length);
 	        for (File file : files) {
+	        	
+	        	//local file verified by hash
+	        	ImportedFile localImportedFile=null;
+	        	if("local".equalsIgnoreCase(fileAccess)){
+	        		localImportedFile=new ImportedFile(file.getAbsolutePath(),file.length(),file.lastModified());
+	        		String code=Sha1Encrypt.encodePassword(localImportedFile.getOriginHash(),Sha1Encrypt.SHA224);
+	        		localImportedFile.setCode(code);
+	        		localImportedFile.setDescription(file.getName());
+	        		List<ImportedFile> localExisteds=importedFileService.findByCodeLike(code, currentUser.getProvider());
+	        		if(localExisteds!=null&&localExisteds.size()>0){
+	        			log.debug("local file {} has imported",file.getAbsolutePath());
+	        			continue;
+	        		}
+	        		importedFileService.create(localImportedFile, currentUser,currentUser.getProvider());
+	        	}
 	        	flatFileProcessingJobBean.execute(result, inputDir, currentUser, file, mappingConf,scriptInstanceFlowCode,recordVariableName,initContext,originFilename,formatTransfo);
+	        	//ftp or sftp file, parse, create 
+	        	if(!"local".equalsIgnoreCase(fileAccess)){
+	        		ImportedFile importedFile=importedFiles.get(file.getName());
+	        		log.debug("try to create importedFile {}",importedFile);
+	        		log.debug("delete distant file {}",removeDistantFile);
+	        		if(importedFile!=null){
+	        			importedFileService.create(importedFile, currentUser, currentUser.getProvider());
+		        		if("true".equalsIgnoreCase(removeDistantFile)){
+		        			log.debug("start to remove file {}",removeDistantFile);
+		        			removeFtpfile(importedFile,fileAccess,distantServer,ftpUsername,ftpPassword,distantPort,ftpInputDirectory,fileNameExtension);
+		        		}
+	        		}
+	        	}
 	        }
 
 		} catch (Exception e) {
@@ -189,7 +264,222 @@ public class FlatFileProcessingJob extends Job {
 		listValues.put("Xlsx_to_Csv","Excel cvs");
 		formatTransfo.setListValues(listValues);
 		result.put("FlatFileProcessingJob_formatTransfo", formatTransfo);
+//		
+		CustomFieldTemplate fileAccess = new CustomFieldTemplate();
+		fileAccess.setCode("FlatFileProcessingJob_fileAccess");
+		fileAccess.setAccountLevel(AccountLevelEnum.TIMER);
+		fileAccess.setActive(true);
+		fileAccess.setDefaultValue("LOCAL");
+		fileAccess.setDescription(resourceMessages.getString("flatFile.fileAccess"));
+		fileAccess.setFieldType(CustomFieldTypeEnum.LIST);
+		fileAccess.setValueRequired(true);
+		Map<String,String> fileAccessListValues = new HashMap<String,String>();
+		fileAccessListValues.put("LOCAL","Local");
+		fileAccessListValues.put("FTP","FTP");
+		fileAccessListValues.put("SFTP","SFTP");
+		fileAccess.setListValues(fileAccessListValues);
+		result.put("FlatFileProcessingJob_fileAccess", fileAccess);
+		
+		CustomFieldTemplate distantServer = new CustomFieldTemplate();
+		distantServer.setCode("FlatFileProcessingJob_distantServer");
+		distantServer.setAccountLevel(AccountLevelEnum.TIMER);
+		distantServer.setActive(true);
+		distantServer.setDescription(resourceMessages.getString("flatFile.distantServer"));
+		distantServer.setFieldType(CustomFieldTypeEnum.STRING);
+		distantServer.setValueRequired(false);
+		result.put("FlatFileProcessingJob_distantServer", distantServer);
+		
+		CustomFieldTemplate distantPort = new CustomFieldTemplate();
+		distantPort.setCode("FlatFileProcessingJob_distantPort");
+		distantPort.setAccountLevel(AccountLevelEnum.TIMER);
+		distantPort.setActive(true);
+		distantPort.setDescription(resourceMessages.getString("flatFile.distantPort"));
+		distantPort.setFieldType(CustomFieldTypeEnum.LONG);
+		distantPort.setValueRequired(false);
+		result.put("FlatFileProcessingJob_distantPort", distantPort);
+		
+		CustomFieldTemplate removeDistantFile = new CustomFieldTemplate();
+		removeDistantFile.setCode("FlatFileProcessingJob_removeDistantFile");
+		removeDistantFile.setAccountLevel(AccountLevelEnum.TIMER);
+		removeDistantFile.setActive(true);
+		removeDistantFile.setDescription(resourceMessages.getString("flatFile.removeDistantFile"));
+		removeDistantFile.setFieldType(CustomFieldTypeEnum.LIST);
+		Map<String,String> removeDistantFileListValues = new HashMap<String,String>();
+		removeDistantFileListValues.put("TRUE","True");
+		removeDistantFileListValues.put("FALSE","False");
+		removeDistantFile.setListValues(removeDistantFileListValues);
+		removeDistantFile.setValueRequired(false);
+		result.put("FlatFileProcessingJob_removeDistantFile", removeDistantFile);
+		
+		CustomFieldTemplate ftpInputDirectory = new CustomFieldTemplate();
+		ftpInputDirectory.setCode("FlatFileProcessingJob_ftpInputDirectory");
+		ftpInputDirectory.setAccountLevel(AccountLevelEnum.TIMER);
+		ftpInputDirectory.setActive(true);
+		ftpInputDirectory.setDescription(resourceMessages.getString("flatFile.ftpInputDirectory"));
+		ftpInputDirectory.setFieldType(CustomFieldTypeEnum.STRING);
+		ftpInputDirectory.setValueRequired(false);
+		result.put("FlatFileProcessingJob_ftpInputDirectory", ftpInputDirectory);
+		
+		CustomFieldTemplate ftpUsername = new CustomFieldTemplate();
+		ftpUsername.setCode("FlatFileProcessingJob_ftpUsername");
+		ftpUsername.setAccountLevel(AccountLevelEnum.TIMER);
+		ftpUsername.setActive(true);
+		ftpUsername.setDescription(resourceMessages.getString("flatFile.ftpUsername"));
+		ftpUsername.setFieldType(CustomFieldTypeEnum.STRING);
+		ftpUsername.setValueRequired(false);
+		result.put("FlatFileProcessingJob_ftpUsername", ftpUsername);
+		
+		CustomFieldTemplate ftpPassword = new CustomFieldTemplate();
+		ftpPassword.setCode("FlatFileProcessingJob_ftpPassword");
+		ftpPassword.setAccountLevel(AccountLevelEnum.TIMER);
+		ftpPassword.setActive(true);
+		ftpPassword.setDescription(resourceMessages.getString("flatFile.ftpPassword"));
+		ftpPassword.setFieldType(CustomFieldTypeEnum.STRING);
+		ftpPassword.setValueRequired(false);
+		result.put("FlatFileProcessingJob_ftpPassword", ftpPassword);
 		
 		return result;
+	}
+	private static FileSystemOptions getSftpOptions(boolean isSftp) throws FileSystemException{
+		FileSystemOptions opts = new FileSystemOptions();
+		if(isSftp){
+			SftpFileSystemConfigBuilder.getInstance().setStrictHostKeyChecking(opts, "no");
+			SftpFileSystemConfigBuilder.getInstance().setUserDirIsRoot(opts, true);
+			SftpFileSystemConfigBuilder.getInstance().setTimeout(opts, 10000);
+		}
+		return opts;
+	}
+
+	private Map<String,ImportedFile> connect2FTP(String fileAccess,String inputDir,String ftpInputDirectory,String distantServer,String ftpUsername,String ftpPassword,long distantPort,final String ftpFileExtension,User currentUser) throws IOException {
+		
+		Map<String,ImportedFile> result=new HashMap<String,ImportedFile>();
+		if(ftpInputDirectory==null){
+			ftpInputDirectory="/";
+		}else{
+			ftpInputDirectory=ftpInputDirectory.startsWith("/")?ftpInputDirectory:"/"+ftpInputDirectory;
+		}
+
+		final String ftpAddress=String.format("%s://%s:%s@%s:%s%s", fileAccess.toLowerCase(),ftpUsername,ftpPassword,distantServer,distantPort,ftpInputDirectory);
+		String ftpUri=String.format("%s://%s:%s%s", fileAccess.toLowerCase(),distantServer,distantPort,ftpInputDirectory);
+		log.debug("ftp uri {}",ftpUri);
+		StandardFileSystemManager manager = null;
+		try{
+			manager = new StandardFileSystemManager();
+			manager.init();
+			FileObject fileObject=manager.resolveFile(ftpAddress,getSftpOptions("sftp".equalsIgnoreCase(fileAccess)));
+			FileObject[] fileObjects = null;
+			if(ftpFileExtension!=null){
+				fileObjects=fileObject.findFiles(new FileSelector() {
+					@Override
+					public boolean traverseDescendents(FileSelectInfo info) throws Exception {
+						return true;
+					}
+					@Override
+					public boolean includeFile(FileSelectInfo info) throws Exception {
+						return info.getDepth()==1&&ftpFileExtension.equalsIgnoreCase(info.getFile().getName().getExtension());
+					}
+				});
+			}else{
+				fileObjects=fileObject.getChildren();
+			}
+
+			ImportedFile importedFile=null;
+			if(fileObjects!=null){
+				for (FileObject o : fileObjects) {
+					if (o.getType() == FileType.FILE) {
+						String fileName=o.getName().getBaseName();
+						FileContent c = o.getContent();
+						long lastModified = c.getLastModifiedTime();
+						long size = c.getSize();
+						importedFile=new ImportedFile(((ftpUri.endsWith("/")?ftpUri:ftpUri+"/")+fileName),size,lastModified);
+						String code=Sha1Encrypt.encodePassword(importedFile.getOriginHash(),Sha1Encrypt.SHA224);
+						importedFile.setCode(code);
+						importedFile.setDescription(o.getName().getBaseName());
+
+						List<ImportedFile> files=importedFileService.findByCodeLike(code, currentUser.getProvider());
+						if(files!=null&&files.size()>0){
+							log.debug("has imported file {}",importedFile);
+							continue;
+						}
+						log.debug("add importedFile {}",importedFile);
+						
+						File localFile = new File(fileName);
+						if(inputDir==null){
+							inputDir="/";
+						}else{
+							inputDir=inputDir.startsWith("/")?inputDir:"/"+inputDir;
+							inputDir=inputDir.endsWith("/")?inputDir:inputDir+"/";
+						}
+						String outputFileName=String.format("%s%s", inputDir,localFile);
+						log.debug("copy file to {}",outputFileName);
+						FileOutputStream out = new FileOutputStream(outputFileName);
+						IOUtils.copy(c.getInputStream(), out);
+						result.put(fileName, importedFile);
+					}
+				}
+			}
+		}catch(Exception e){
+			log.error("Error when read ftp file from {} ",ftpInputDirectory,e);
+		}finally {
+			if(manager!=null){
+				try{
+					manager.close();
+				}catch(Exception e){}
+			}
+		}
+		return result;
+	}
+	private void removeFtpfile(ImportedFile importedFile, String fileAccess, String distantServer, String ftpUsername,
+			String ftpPassword, long distantPort,String ftpInputDirectory,final String ftpFileExtension) {
+		final String ftpAddress=String.format("%s://%s:%s@%s:%s/%s", fileAccess.toLowerCase(),ftpUsername,ftpPassword,distantServer,distantPort,ftpInputDirectory);
+		String ftpUri=String.format("%s://%s:%s%s", fileAccess.toLowerCase(),distantServer,distantPort,ftpInputDirectory);
+		StandardFileSystemManager manager = null;
+		try{
+			manager = new StandardFileSystemManager();
+			manager.init();
+			FileObject fileObject=manager.resolveFile(ftpAddress,getSftpOptions("sftp".equalsIgnoreCase(fileAccess)));
+			FileObject[] fileObjects = null;
+			if(ftpFileExtension!=null){
+				fileObjects=fileObject.findFiles(new FileSelector() {
+					@Override
+					public boolean traverseDescendents(FileSelectInfo info) throws Exception {
+						return true;
+					}
+					@Override
+					public boolean includeFile(FileSelectInfo info) throws Exception {
+						return info.getDepth()==1&&ftpFileExtension.equalsIgnoreCase(info.getFile().getName().getExtension());
+					}
+				});
+			}else{
+				fileObjects=fileObject.getChildren();
+			}
+			if(fileObjects!=null){
+				for (FileObject o : fileObjects) {
+					if (o.getType() == FileType.FILE) {
+						String fileName=o.getName().getBaseName();
+						log.debug("found ftp file {}",fileName);
+						FileContent c = o.getContent();
+						long lastModified = c.getLastModifiedTime();
+						long size = c.getSize();
+						importedFile=new ImportedFile((ftpUri.endsWith("/")?ftpUri:ftpUri+"/"+fileName),size,lastModified);
+						String code=Sha1Encrypt.encodePassword(importedFile.getOriginHash(), Sha1Encrypt.SHA224);
+						if(code.equals(importedFile.getCode())){
+							log.debug("found equal code");
+							boolean result=o.delete();
+							log.debug("deleted ftp file {}",result);
+							break;
+						}
+					}
+				}
+			}
+		}catch(Exception e){
+			log.error("Error when delete a file {} from server ",importedFile.getUri(),e);
+		}finally{
+			if(manager!=null){
+				try{
+					((DefaultFileSystemManager) manager).close();
+				}catch(Exception e){}
+			}
+		}
 	}
 }
