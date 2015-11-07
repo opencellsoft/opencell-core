31b8191e3c93f0208894767f6b6ce715aa59f153
mbarek <mbarek.omoh@manaty.net>
2015-11-07 13:44:03 +0100

    add ScriptNotification

---
+++ meveo-model/src/main/java/org/meveo/model/notification/ScriptNotification.java
@@ -0,0 +1,16 @@
+package org.meveo.model.notification;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ADM_SCRIPT_NOTIFICATION")
+public class ScriptNotification extends Notification {
+
+    private static final long serialVersionUID = -2527123286118840886L;
+
+  
+    
+    
+    
+}
