204a82e22aa6d51a0f39c52e1ed12a94f2e1a2ad
mbarek <mbarek.omoh@manaty.net>
2015-12-10 13:52:40 +0000

    re #1320

--- meveo-model/src/main/java/org/meveo/model/billing/WalletOperation.java
+++ meveo-model/src/main/java/org/meveo/model/billing/WalletOperation.java
@@ -165,13 +165,13 @@ public class WalletOperation extends BusinessEntity {
 	// @JoinColumn(name = "EDR_ID")
 	// private EDR usageEdr;
 
-	@Column(name = "PARAMETER_1", length = 50)
+	@Column(name = "PARAMETER_1")
 	private String parameter1;
 
-	@Column(name = "PARAMETER_2", length = 50)
+	@Column(name = "PARAMETER_2")
 	private String parameter2;
 
-	@Column(name = "PARAMETER_3", length = 50)
+	@Column(name = "PARAMETER_3")
 	private String parameter3;
 
 	@Temporal(TemporalType.TIMESTAMP)
