4c13276200514d7e8d86dbd62e4950a6a8f3ee8d
Edward P. Legaspi <czetsuya@gmail.com>
2016-01-25 19:05:29 +0800

    Return offer price in findProductOffering.

--- meveo-api/src/main/java/org/meveo/api/catalog/CatalogApi.java
+++ meveo-api/src/main/java/org/meveo/api/catalog/CatalogApi.java
@@ -33,8 +33,9 @@ public class CatalogApi extends BaseApi {
 
 	public ProductOffering findProductOffering(String code, User currentUser, UriInfo uriInfo, Category category) {
 		OfferTemplate offerTemplate = offerTemplateService.findByCode(code, currentUser.getProvider());
+		Map<String, BigDecimal> servicePrices = getOfferPrices(offerTemplate);
 		return offerTemplate == null ? null : ProductOffering.parseFromOfferTemplate(offerTemplate, uriInfo, category,
-				null);
+				servicePrices);
 	}
 
 	public List<ProductOffering> findProductOfferings(UriInfo uriInfo, Category category) {
@@ -42,64 +43,68 @@ public class CatalogApi extends BaseApi {
 		Map<String, Map<String, BigDecimal>> offerPrices = new HashMap<>();
 
 		for (OfferTemplate offerTemplate : offerTemplates) {
-			Map<String, BigDecimal> servicePrices = new HashMap<>();
-
-			for (ServiceTemplate st : offerTemplate.getServiceTemplates()) {
-				if (st.getServiceSubscriptionCharges() != null) {
-					BigDecimal totalPriceOneShotCharges = new BigDecimal(0);
-					for (ServiceChargeTemplateSubscription serviceChargeTemplateSubscription : st
-							.getServiceSubscriptionCharges()) {
-						BigDecimal chargePricePlans = new BigDecimal(0);
-
-						List<PricePlanMatrix> offerPricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(
-								offerTemplate, serviceChargeTemplateSubscription.getChargeTemplate().getCode());
-						if (offerPricePlans != null && offerPricePlans.size() > 0) {
-							chargePricePlans = offerPricePlans.get(0).getAmountWithTax();
-						} else {
-							List<PricePlanMatrix> pricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(
-									null, serviceChargeTemplateSubscription.getChargeTemplate().getCode());
-							if (pricePlans != null && pricePlans.size() > 0) {
-								chargePricePlans = pricePlans.get(0).getAmountWithTax();
-							}
-						}
-
-						totalPriceOneShotCharges = totalPriceOneShotCharges.add(chargePricePlans);
-					}
-
-					servicePrices.put(st.getCode() + "_SUB", totalPriceOneShotCharges);
-				}
-
-				if (st.getServiceRecurringCharges() != null) {
-					BigDecimal totalPriceRecurringCharges = new BigDecimal(0);
-					for (ServiceChargeTemplateRecurring serviceChargeTemplateRecurring : st
-							.getServiceRecurringCharges()) {
-						BigDecimal chargePricePlans = new BigDecimal(0);
-
-						List<PricePlanMatrix> offerPricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(
-								offerTemplate, serviceChargeTemplateRecurring.getChargeTemplate().getCode());
-						if (offerPricePlans != null && offerPricePlans.size() > 0) {
-							chargePricePlans = offerPricePlans.get(0).getAmountWithTax();
-						} else {
-							List<PricePlanMatrix> pricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(
-									null, serviceChargeTemplateRecurring.getChargeTemplate().getCode());
-							if (pricePlans != null && pricePlans.size() > 0) {
-								chargePricePlans = pricePlans.get(0).getAmountWithTax();
-							}
-						}
-
-						totalPriceRecurringCharges = totalPriceRecurringCharges.add(chargePricePlans);
-					}
-
-					servicePrices.put(st.getCode() + "_REC"
-							+ (st.getInvoicingCalendar() == null ? "" : "_" + st.getInvoicingCalendar().getCode()),
-							totalPriceRecurringCharges);
-				}
-			}
-
+			Map<String, BigDecimal> servicePrices = getOfferPrices(offerTemplate);
 			offerPrices.put(offerTemplate.getCode(), servicePrices);
 		}
 
 		return ProductOffering.parseFromOfferTemplates(offerTemplates, uriInfo, category, offerPrices);
+	}
+
+	private Map<String, BigDecimal> getOfferPrices(OfferTemplate offerTemplate) {
+		Map<String, BigDecimal> servicePrices = new HashMap<>();
+
+		for (ServiceTemplate st : offerTemplate.getServiceTemplates()) {
+			if (st.getServiceSubscriptionCharges() != null) {
+				BigDecimal totalPriceOneShotCharges = new BigDecimal(0);
+				for (ServiceChargeTemplateSubscription serviceChargeTemplateSubscription : st
+						.getServiceSubscriptionCharges()) {
+					BigDecimal chargePricePlans = new BigDecimal(0);
+
+					List<PricePlanMatrix> offerPricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(
+							offerTemplate, serviceChargeTemplateSubscription.getChargeTemplate().getCode());
+					if (offerPricePlans != null && offerPricePlans.size() > 0) {
+						chargePricePlans = offerPricePlans.get(0).getAmountWithTax();
+					} else {
+						List<PricePlanMatrix> pricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(null,
+								serviceChargeTemplateSubscription.getChargeTemplate().getCode());
+						if (pricePlans != null && pricePlans.size() > 0) {
+							chargePricePlans = pricePlans.get(0).getAmountWithTax();
+						}
+					}
+
+					totalPriceOneShotCharges = totalPriceOneShotCharges.add(chargePricePlans);
+				}
+
+				servicePrices.put(st.getCode() + "_SUB", totalPriceOneShotCharges);
+			}
+
+			if (st.getServiceRecurringCharges() != null) {
+				BigDecimal totalPriceRecurringCharges = new BigDecimal(0);
+				for (ServiceChargeTemplateRecurring serviceChargeTemplateRecurring : st.getServiceRecurringCharges()) {
+					BigDecimal chargePricePlans = new BigDecimal(0);
+
+					List<PricePlanMatrix> offerPricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(
+							offerTemplate, serviceChargeTemplateRecurring.getChargeTemplate().getCode());
+					if (offerPricePlans != null && offerPricePlans.size() > 0) {
+						chargePricePlans = offerPricePlans.get(0).getAmountWithTax();
+					} else {
+						List<PricePlanMatrix> pricePlans = pricePlanMatrixService.findByOfferTemplateAndEventCode(null,
+								serviceChargeTemplateRecurring.getChargeTemplate().getCode());
+						if (pricePlans != null && pricePlans.size() > 0) {
+							chargePricePlans = pricePlans.get(0).getAmountWithTax();
+						}
+					}
+
+					totalPriceRecurringCharges = totalPriceRecurringCharges.add(chargePricePlans);
+				}
+
+				servicePrices.put(st.getCode() + "_REC"
+						+ (st.getInvoicingCalendar() == null ? "" : "_" + st.getInvoicingCalendar().getCode()),
+						totalPriceRecurringCharges);
+			}
+		}
+
+		return servicePrices;
 	}
 
 	public ProductSpecification findProductSpecification(String code, User currentUser, UriInfo uriInfo) {
