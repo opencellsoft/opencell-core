58dbab66f14fde4dde3c2ea53c65f0fb02e1d88a
carlo liwanag <ecaliwanag@gmail.com>
2016-02-06 23:52:43 +0800

    Re #1461
    - Added new classes for OfferTemplateCategory-> Dto, Response, Api, Rs
    Interface and implementation
    - Added create and find test in Jax-Rs_4.2.xml

---
+++ meveo-api-dto/src/main/java/org/meveo/api/dto/response/catalog/GetOfferTemplateCategoryResponseDto.java
@@ -0,0 +1,33 @@
+package org.meveo.api.dto.response.catalog;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.meveo.api.dto.catalog.OfferTemplateCategoryDto;
+import org.meveo.api.dto.response.BaseResponse;
+
+@XmlRootElement(name = "GetOfferTemplateResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class GetOfferTemplateCategoryResponseDto extends BaseResponse {
+
+	private static final long serialVersionUID = 1L;
+	
+	private OfferTemplateCategoryDto offerTemplateCategory;
+
+	public OfferTemplateCategoryDto getOfferTemplateCategory() {
+		return offerTemplateCategory;
+	}
+
+	public void setOfferTemplateCategory(
+			OfferTemplateCategoryDto offerTemplateCategory) {
+		this.offerTemplateCategory = offerTemplateCategory;
+	}
+
+	@Override
+	public String toString() {
+		return "GetOfferTemplateCategoryResponseDto [offerTemplateCategory="
+				+ offerTemplateCategory + "]";
+	}
+
+}
