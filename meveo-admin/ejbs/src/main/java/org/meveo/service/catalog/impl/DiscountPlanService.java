77e63073ddbd9706ff0ba35b5841afd29c65763a
andriusk <exploresouthamerica@gmail.com>
2016-02-15 17:28:42 -0300

    Re #1427. Changed DiscountPlanService.java to BusinessService to fix compilation issue

--- meveo-admin/ejbs/src/main/java/org/meveo/service/catalog/impl/DiscountPlanService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/catalog/impl/DiscountPlanService.java
@@ -3,12 +3,12 @@ package org.meveo.service.catalog.impl;
 import javax.ejb.Stateless;
 
 import org.meveo.model.catalog.DiscountPlan;
-import org.meveo.service.base.PersistenceService;
+import org.meveo.service.base.BusinessService;
 
 /**
  * @author Edward P. Legaspi
  **/
 @Stateless
-public class DiscountPlanService extends PersistenceService<DiscountPlan> {
+public class DiscountPlanService extends BusinessService<DiscountPlan> {
 
 }
