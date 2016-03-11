b7db249037eaa92ee8c4c7bc2063c76f3bc907bb
mbarek <mbarek.omoh@manaty.net>
2016-03-11 21:44:00 +0000

    re #1512

--- meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -276,7 +276,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
 				int endIndex = prefix.indexOf("%", startIndex);
 				if (endIndex > 0) {
 					String datePattern = prefix.substring(startIndex, endIndex);
-					String invioceDate = DateUtils.formatDateWithPattern(new Date(), datePattern);
+					String invioceDate = DateUtils.formatDateWithPattern(invoice.getInvoiceDate(), datePattern);
 					prefix = prefix.replace("%" + datePattern + "%", invioceDate);
 				}
 			}
@@ -765,7 +765,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
 				int endIndex = prefix.indexOf("%", startIndex);
 				if (endIndex > 0) {
 					String datePattern = prefix.substring(startIndex, endIndex);
-					String invoiceAdjustmentDate = DateUtils.formatDateWithPattern(new Date(), datePattern);
+					String invoiceAdjustmentDate = DateUtils.formatDateWithPattern(invoiceAdjustment.getInvoiceDate(), datePattern);
 					prefix = prefix.replace("%" + datePattern + "%", invoiceAdjustmentDate);
 				}
 			}
