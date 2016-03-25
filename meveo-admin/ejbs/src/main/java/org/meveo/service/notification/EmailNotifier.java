f6b0b2fd00864d5beb393d67d47d14be2570a8b0
andriusk <exploresouthamerica@gmail.com>
2016-03-25 02:55:13 -0300

    Re #1585 GUI tweekings for documentation

--- meveo-admin/ejbs/src/main/java/org/meveo/service/notification/EmailNotifier.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/notification/EmailNotifier.java
@@ -30,78 +30,70 @@ import org.slf4j.Logger;
 @Stateless
 public class EmailNotifier {
 
-	@Resource(lookup = "java:/MeveoMail")
-	private Session mailSession;
+    @Resource(lookup = "java:/MeveoMail")
+    private Session mailSession;
 
-	@Inject 
-	NotificationHistoryService notificationHistoryService;
-	
-	@Inject
+    @Inject
+    NotificationHistoryService notificationHistoryService;
+
+    @Inject
     private Logger log;
-	
-	@Asynchronous
-	public void sendEmail(EmailNotification notification, IEntity e,Map<String,Object> context){
-		MimeMessage msg = new MimeMessage(mailSession);
-		try {
-			msg.setFrom(new InternetAddress(notification.getEmailFrom()));
-			msg.setSentDate(new Date());
-			HashMap<Object,Object> userMap = new HashMap<Object, Object>();
-			userMap.put("event", e);
-			userMap.put("context", context);
-			log.debug("event[{}], context[{}]",e,context);			
-			msg.setSubject((String)ValueExpressionWrapper.evaluateExpression(notification.getSubject(), userMap, String.class));
-			if(!StringUtils.isBlank(notification.getHtmlBody())){
-				String htmlBody=(String)ValueExpressionWrapper.evaluateExpression(notification.getHtmlBody(), userMap, String.class);
-				msg.setContent(htmlBody, "text/html");
-			} else {
-				String body=(String)ValueExpressionWrapper.evaluateExpression(notification.getBody(), userMap, String.class);
-				msg.setContent(body, "text/plain");
-			}
-			List<InternetAddress> addressTo = new ArrayList<InternetAddress>();
-			
-			if(!StringUtils.isBlank(notification.getEmailToEl())){
-				addressTo.add(new InternetAddress((String)ValueExpressionWrapper.evaluateExpression(notification.getEmailToEl(), userMap, String.class)));
-			}
-			if(notification.getEmails()!=null){
-				for (String address:notification.getEmails()) {
-					addressTo.add(new InternetAddress(address));
-				}
-			}
-			msg.setRecipients(RecipientType.TO,addressTo.toArray(new InternetAddress[addressTo.size()]));
 
-			InternetAddress[] replytoAddress = { new InternetAddress(
-					notification.getEmailFrom()) };
-			msg.setReplyTo(replytoAddress);
-			if(!StringUtils.isBlank(notification.getBody())){
-				String htmlBody=(String)ValueExpressionWrapper.evaluateExpression(notification.getBody(), userMap, String.class);
-				msg.setContent(htmlBody, "text/plain");
-			}
-			if(!StringUtils.isBlank(notification.getHtmlBody())){
-				String htmlBody=(String)ValueExpressionWrapper.evaluateExpression(notification.getHtmlBody(), userMap, String.class);
-				msg.setContent(htmlBody, "text/html");
-			}
-			Transport.send(msg);
-			notificationHistoryService.create(notification, e, "", NotificationHistoryStatusEnum.SENT);
+    @Asynchronous
+    public void sendEmail(EmailNotification notification, IEntity e, Map<String, Object> context) {
+        MimeMessage msg = new MimeMessage(mailSession);
+        try {
+            msg.setFrom(new InternetAddress(notification.getEmailFrom()));
+            msg.setSentDate(new Date());
+            HashMap<Object, Object> userMap = new HashMap<Object, Object>();
+            userMap.put("event", e);
+            userMap.put("context", context);
+            log.debug("event[{}], context[{}]", e, context);
+            msg.setSubject((String) ValueExpressionWrapper.evaluateExpression(notification.getSubject(), userMap, String.class));
+            if (!StringUtils.isBlank(notification.getHtmlBody())) {
+                String htmlBody = (String) ValueExpressionWrapper.evaluateExpression(notification.getHtmlBody(), userMap, String.class);
+                msg.setContent(htmlBody, "text/html");
+            } else {
+                String body = (String) ValueExpressionWrapper.evaluateExpression(notification.getBody(), userMap, String.class);
+                msg.setContent(body, "text/plain");
+            }
+            List<InternetAddress> addressTo = new ArrayList<InternetAddress>();
 
-		} catch (BusinessException e1) {
-			try {
-				notificationHistoryService.create(notification, e, e1.getMessage(), NotificationHistoryStatusEnum.FAILED);
-			} catch (BusinessException e2) {
-				log.error("Failed to create notification history business",e);
-			}
-			
-		} catch (AddressException e1) {
-			try {
-				notificationHistoryService.create(notification, e, e1.getMessage(), NotificationHistoryStatusEnum.FAILED);
-			} catch (BusinessException e2) {
-				log.error("Failed to create notification history address",e);
-			}
-		} catch (MessagingException e1) {
-			try {
-				notificationHistoryService.create(notification, e, e1.getMessage(), NotificationHistoryStatusEnum.TO_RETRY);
-			} catch (BusinessException e2) {
-				log.error("Failed to create notification history messaging ",e);
-			}
-		}
-	}
+            if (!StringUtils.isBlank(notification.getEmailToEl())) {
+                addressTo.add(new InternetAddress((String) ValueExpressionWrapper.evaluateExpression(notification.getEmailToEl(), userMap, String.class)));
+            }
+            if (notification.getEmails() != null) {
+                for (String address : notification.getEmails()) {
+                    addressTo.add(new InternetAddress(address));
+                }
+            }
+            msg.setRecipients(RecipientType.TO, addressTo.toArray(new InternetAddress[addressTo.size()]));
+
+            InternetAddress[] replytoAddress = { new InternetAddress(notification.getEmailFrom()) };
+            msg.setReplyTo(replytoAddress);
+
+            Transport.send(msg);
+            notificationHistoryService.create(notification, e, "", NotificationHistoryStatusEnum.SENT);
+
+        } catch (BusinessException e1) {
+            try {
+                notificationHistoryService.create(notification, e, e1.getMessage(), NotificationHistoryStatusEnum.FAILED);
+            } catch (BusinessException e2) {
+                log.error("Failed to create notification history business", e);
+            }
+
+        } catch (AddressException e1) {
+            try {
+                notificationHistoryService.create(notification, e, e1.getMessage(), NotificationHistoryStatusEnum.FAILED);
+            } catch (BusinessException e2) {
+                log.error("Failed to create notification history address", e);
+            }
+        } catch (MessagingException e1) {
+            try {
+                notificationHistoryService.create(notification, e, e1.getMessage(), NotificationHistoryStatusEnum.TO_RETRY);
+            } catch (BusinessException e2) {
+                log.error("Failed to create notification history messaging ", e);
+            }
+        }
+    }
 }
--- meveo-admin/ejbs/src/main/java/org/meveo/service/notification/WebHookNotifier.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/notification/WebHookNotifier.java
@@ -34,128 +34,124 @@ import org.slf4j.Logger;
 @Stateless
 public class WebHookNotifier {
 
-	@Inject
-	@MeveoJpaForJobs
-	private EntityManager em;
+    @Inject
+    @MeveoJpaForJobs
+    private EntityManager em;
 
-	@Inject
-	Logger log;
+    @Inject
+    Logger log;
 
-	@Inject
-	NotificationHistoryService notificationHistoryService;
-	
-	@Inject
-	ScriptInstanceService scriptInstanceService;
+    @Inject
+    NotificationHistoryService notificationHistoryService;
 
-	private String evaluate(String expression, IEntity e,Map<String, Object> context) throws BusinessException {
-		HashMap<Object, Object> userMap = new HashMap<Object, Object>();
-		userMap.put("event", e);
-		userMap.put("context", context);
-		return (String) ValueExpressionWrapper.evaluateExpression(expression, userMap, String.class);
-	}
+    @Inject
+    ScriptInstanceService scriptInstanceService;
 
-	private Map<String, String> evaluateMap(Map<String, String> map, IEntity e,Map<String, Object> context) throws BusinessException {
-		Map<String, String> result = new HashMap<String, String>();
-		HashMap<Object, Object> userMap = new HashMap<Object, Object>();
-		userMap.put("event", e);
-		userMap.put("context", context);
+    private String evaluate(String expression, IEntity e, Map<String, Object> context) throws BusinessException {
+        HashMap<Object, Object> userMap = new HashMap<Object, Object>();
+        userMap.put("event", e);
+        userMap.put("context", context);
+        return (String) ValueExpressionWrapper.evaluateExpression(expression, userMap, String.class);
+    }
 
-		for (String key : map.keySet()) {
-			result.put(key, (String) ValueExpressionWrapper.evaluateExpression(map.get(key), userMap, String.class));
-		}
+    private Map<String, String> evaluateMap(Map<String, String> map, IEntity e, Map<String, Object> context) throws BusinessException {
+        Map<String, String> result = new HashMap<String, String>();
+        HashMap<Object, Object> userMap = new HashMap<Object, Object>();
+        userMap.put("event", e);
+        userMap.put("context", context);
 
-		return result;
-	}
+        for (String key : map.keySet()) {
+            result.put(key, (String) ValueExpressionWrapper.evaluateExpression(map.get(key), userMap, String.class));
+        }
 
-	@Asynchronous
-	public void sendRequest(WebHook webHook, IEntity e,Map<String, Object> context) {
-		log.debug("webhook sendRequest");
-		String result = "";
+        return result;
+    }
 
-		try {
-			String url = webHook.getHost().startsWith("http") ? webHook.getHost() : "http://" + webHook.getHost();
-			if (webHook.getPort() > 0) {
-				url += ":" + webHook.getPort();
-			}
+    @Asynchronous
+    public void sendRequest(WebHook webHook, IEntity e, Map<String, Object> context) {
+        log.debug("webhook sendRequest");
+        String result = "";
 
-			if (!StringUtils.isBlank(webHook.getPage())) {
-				url += "/" + evaluate(webHook.getPage(), e,context);
-			}
-			Map<String,String> params = evaluateMap(webHook.getWebhookParams(), e,context);
-			
-            String paramQuery="";
-            String sep="";
-            for(String paramKey:params.keySet()){
-            	paramQuery+=sep+URLEncoder.encode(paramKey, "UTF-8")+"="+URLEncoder.encode(params.get(paramKey), "UTF-8");
-            	sep="&";
+        try {
+            String url = webHook.getHost().startsWith("http") ? webHook.getHost() : "http://" + webHook.getHost();
+            if (webHook.getPort() > 0) {
+                url += ":" + webHook.getPort();
+            }
+
+            if (!StringUtils.isBlank(webHook.getPage())) {
+                url += "/" + evaluate(webHook.getPage(), e, context);
+            }
+            Map<String, String> params = evaluateMap(webHook.getWebhookParams(), e, context);
+
+            String paramQuery = "";
+            String sep = "";
+            for (String paramKey : params.keySet()) {
+                paramQuery += sep + URLEncoder.encode(paramKey, "UTF-8") + "=" + URLEncoder.encode(params.get(paramKey), "UTF-8");
+                sep = "&";
             }
             String bodyEL_evaluated = null;
-            log.debug("paramQuery={}",paramQuery);
-            if(WebHookMethodEnum.HTTP_GET == webHook.getHttpMethod()){
-            	url+="?"+paramQuery;
-            } else if (WebHookMethodEnum.HTTP_POST == webHook.getHttpMethod()) {            	
-            	bodyEL_evaluated = evaluate(webHook.getBodyEL(), e, context);
-				log.debug("Evaluated BodyEL={}",bodyEL_evaluated);
-				if(StringUtils.isBlank(bodyEL_evaluated)){
-					paramQuery += "&"+ bodyEL_evaluated;
-				}				
-            } 
-			log.debug("webhook url: {}", url);
-			URL obj = new URL(url);
+            log.debug("paramQuery={}", paramQuery);
+            if (WebHookMethodEnum.HTTP_GET == webHook.getHttpMethod()) {
+                url += "?" + paramQuery;
+            } else if (WebHookMethodEnum.HTTP_POST == webHook.getHttpMethod()) {
+                bodyEL_evaluated = evaluate(webHook.getBodyEL(), e, context);
+                log.debug("Evaluated BodyEL={}", bodyEL_evaluated);
+                if (StringUtils.isBlank(bodyEL_evaluated)) {
+                    paramQuery += "&" + bodyEL_evaluated;
+                }
+            }
+            log.debug("webhook url: {}", url);
+            URL obj = new URL(url);
 
             HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 
-            Map<String, String> headers = evaluateMap(webHook.getHeaders(), e,context);
-            if(!StringUtils.isBlank(webHook.getUsername()) && !headers.containsKey("Authorization")){
-     			byte[] bytes = Base64.encodeBase64((webHook.getUsername() + ":" + webHook.getPassword()).getBytes());
-     			headers.put("Authorization", "Basic "+new String(bytes));
-			}
-           
-			for (String key : headers.keySet()) {
-		        conn.setRequestProperty(key, headers.get(key));
-			}
-						
-			
-			if (WebHookMethodEnum.HTTP_GET == webHook.getHttpMethod()) {
-				conn.setRequestMethod("GET");
-			} else if (WebHookMethodEnum.HTTP_POST == webHook.getHttpMethod()) {
-				conn.setRequestMethod("POST");
-			} else if (WebHookMethodEnum.HTTP_PUT == webHook.getHttpMethod()) {
-				conn.setRequestMethod("PUT");
-			} else if (WebHookMethodEnum.HTTP_DELETE == webHook.getHttpMethod()) {
-				conn.setRequestMethod("DELETE");
-			}
-			conn.setUseCaches(false);
-			
-            if(WebHookMethodEnum.HTTP_GET != webHook.getHttpMethod()){
-            	conn.setDoOutput(true);
-	            OutputStream os = conn.getOutputStream();
-	            BufferedWriter writer = new BufferedWriter(
-	                    new OutputStreamWriter(os, "UTF-8"));
-	            writer.write(paramQuery);
-	            writer.flush();
-	            writer.close();
-	            os.close();
+            Map<String, String> headers = evaluateMap(webHook.getHeaders(), e, context);
+            if (!StringUtils.isBlank(webHook.getUsername()) && !headers.containsKey("Authorization")) {
+                byte[] bytes = Base64.encodeBase64((webHook.getUsername() + ":" + webHook.getPassword()).getBytes());
+                headers.put("Authorization", "Basic " + new String(bytes));
             }
-			int responseCode = conn.getResponseCode();
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(conn.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-	 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			result = response.toString();
-			if(responseCode!=200){
-				try {
-					log.debug("webhook httpStatus error : " + responseCode + " response="+result);
-					notificationHistoryService.create(webHook, e,"http error status="+responseCode +" response="+result,
-							NotificationHistoryStatusEnum.FAILED);
-				} catch (BusinessException e2) {
-					log.error("Failed to create webhook ",e);
-				}
+
+            for (String key : headers.keySet()) {
+                conn.setRequestProperty(key, headers.get(key));
+            }
+
+            if (WebHookMethodEnum.HTTP_GET == webHook.getHttpMethod()) {
+                conn.setRequestMethod("GET");
+            } else if (WebHookMethodEnum.HTTP_POST == webHook.getHttpMethod()) {
+                conn.setRequestMethod("POST");
+            } else if (WebHookMethodEnum.HTTP_PUT == webHook.getHttpMethod()) {
+                conn.setRequestMethod("PUT");
+            } else if (WebHookMethodEnum.HTTP_DELETE == webHook.getHttpMethod()) {
+                conn.setRequestMethod("DELETE");
+            }
+            conn.setUseCaches(false);
+
+            if (WebHookMethodEnum.HTTP_GET != webHook.getHttpMethod()) {
+                conn.setDoOutput(true);
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(paramQuery);
+                writer.flush();
+                writer.close();
+                os.close();
+            }
+            int responseCode = conn.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            result = response.toString();
+            if (responseCode != 200) {
+                try {
+                    log.debug("webhook httpStatus error : " + responseCode + " response=" + result);
+                    notificationHistoryService.create(webHook, e, "http error status=" + responseCode + " response=" + result, NotificationHistoryStatusEnum.FAILED);
+                } catch (BusinessException e2) {
+                    log.error("Failed to create webhook ", e);
+                }
             } else {
                 if (webHook.getScriptInstance() != null) {
                     HashMap<Object, Object> userMap = new HashMap<Object, Object>();
@@ -166,50 +162,49 @@ public class WebHookNotifier {
                         Map<String, Object> paramsEvaluated = new HashMap<String, Object>();
 
                         for (@SuppressWarnings("rawtypes")
-                        Map.Entry entry : paramsEvaluated.entrySet()) {
+                        Map.Entry entry : webHook.getParams().entrySet()) {
                             paramsEvaluated.put((String) entry.getKey(), ValueExpressionWrapper.evaluateExpression((String) entry.getValue(), userMap, String.class));
                         }
                         paramsEvaluated.put("response", result);
-                        scriptInstanceService.execute(webHook.getScriptInstance().getCode(), paramsEvaluated, webHook
-                            .getScriptInstance().getAuditable().getCreator());
+                        scriptInstanceService.execute(webHook.getScriptInstance().getCode(), paramsEvaluated, webHook.getScriptInstance().getAuditable().getCreator());
 
                     } catch (Exception ee) {
                         log.error("Failed to execute a script {}", webHook.getScriptInstance().getCode(), ee);
                     }
                 }
-				notificationHistoryService.create(webHook, e, result, NotificationHistoryStatusEnum.SENT);
-				log.debug("webhook answer : " + result);
-			}
-		} catch (BusinessException e1) {
-			try {
-				log.debug("webhook business error : ",e1);
-				notificationHistoryService.create(webHook, e, e1.getMessage(), NotificationHistoryStatusEnum.FAILED);
-			} catch (BusinessException e2) {
-				log.error("Failed to create webhook business ",e2);
-				 
-			}
-		} catch (IOException e1) {
-			try { 
-				log.debug("webhook io error : ",e1);
-				notificationHistoryService.create(webHook, e, e1.getMessage(), NotificationHistoryStatusEnum.TO_RETRY);
-			} catch (BusinessException e2) {
-				log.error("Failed to create webhook io ",e2);
-			}
-		}
-	}
-	
-	public static void main(String[] args){
-		String test="{  \"sid\": \"CLb2f57233976448368708c754b3c1efb7\",  \"date_created\": \"Sat, 21 Feb 2015 18:37:49 +0000\","
-				+ "  \"date_updated\": \"Sat, 21 Feb 2015 18:37:49 +0000\",  \"account_sid\": \"ACae6e420f425248d6a26948c17a9e2acf\","
-				+ "  \"api_version\": \"2012-04-24\",  \"friendly_name\": \"RC_A1\",  \"login\": \"RC_A1\","
-				+ "  \"password\": \"toto\",  \"status\": \"1\",  \"voice_method\": \"POST\",  \"voice_fallback_method\": \"POST\","
-				+ "  \"uri\": \"/restcomm/2012-04-24/Accounts/ACae6e420f425248d6a26948c17a9e2acf/Clients/CLb2f57233976448368708c754b3c1efb7.json\"}";
-		try {
-			JSONObject json = new JSONObject(test);
-			System.out.println(json.getString("sid"));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace(); 
-		}
-	}
+                notificationHistoryService.create(webHook, e, result, NotificationHistoryStatusEnum.SENT);
+                log.debug("webhook answer : " + result);
+            }
+        } catch (BusinessException e1) {
+            try {
+                log.debug("webhook business error : ", e1);
+                notificationHistoryService.create(webHook, e, e1.getMessage(), NotificationHistoryStatusEnum.FAILED);
+            } catch (BusinessException e2) {
+                log.error("Failed to create webhook business ", e2);
+
+            }
+        } catch (IOException e1) {
+            try {
+                log.debug("webhook io error : ", e1);
+                notificationHistoryService.create(webHook, e, e1.getMessage(), NotificationHistoryStatusEnum.TO_RETRY);
+            } catch (BusinessException e2) {
+                log.error("Failed to create webhook io ", e2);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String test = "{  \"sid\": \"CLb2f57233976448368708c754b3c1efb7\",  \"date_created\": \"Sat, 21 Feb 2015 18:37:49 +0000\","
+                + "  \"date_updated\": \"Sat, 21 Feb 2015 18:37:49 +0000\",  \"account_sid\": \"ACae6e420f425248d6a26948c17a9e2acf\","
+                + "  \"api_version\": \"2012-04-24\",  \"friendly_name\": \"RC_A1\",  \"login\": \"RC_A1\","
+                + "  \"password\": \"toto\",  \"status\": \"1\",  \"voice_method\": \"POST\",  \"voice_fallback_method\": \"POST\","
+                + "  \"uri\": \"/restcomm/2012-04-24/Accounts/ACae6e420f425248d6a26948c17a9e2acf/Clients/CLb2f57233976448368708c754b3c1efb7.json\"}";
+        try {
+            JSONObject json = new JSONObject(test);
+            System.out.println(json.getString("sid"));
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
