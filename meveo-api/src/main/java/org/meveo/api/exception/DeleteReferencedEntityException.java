dfdc54edd0d571da8a8f76e0e56f459d971beb19
anasseh <abdelhadi.nasseh@manaty.net>
2016-04-29 12:24:26 +0100

    fix #1675

--- meveo-api/src/main/java/org/meveo/api/exception/DeleteReferencedEntityException.java
+++ meveo-api/src/main/java/org/meveo/api/exception/DeleteReferencedEntityException.java
@@ -10,7 +10,7 @@ public class DeleteReferencedEntityException extends MeveoApiException {
 	private static final long serialVersionUID = -979336515558551662L;
 
 	public DeleteReferencedEntityException(String entityName, String code) {
-		super(entityName + " with code=" + code + " is refenced");
+		super(entityName + " with code=" + code + " is referenced");
 		setErrorCode(MeveoApiErrorCodeEnum.DELETE_REFERENCED_ENTITY_EXCEPTION);
 	}
 
@@ -20,7 +20,7 @@ public class DeleteReferencedEntityException extends MeveoApiException {
 	}
 
 	public DeleteReferencedEntityException(Class<?> clazz, String code) {
-		super(clazz.getSimpleName() + " with code=" + code + " is refenced.");
+		super(clazz.getSimpleName() + " with code=" + code + " is referenced.");
 		setErrorCode(MeveoApiErrorCodeEnum.DELETE_REFERENCED_ENTITY_EXCEPTION);
 	}
 
