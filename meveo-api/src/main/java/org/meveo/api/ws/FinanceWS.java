5479f4968d30653e3a59e8e95fbbec40c1160868
smichea <sebastien.michea@manaty.net>
2016-05-07 18:37:30 +0200

    added financeWs

---
+++ meveo-api/src/main/java/org/meveo/api/ws/FinanceWS.java
@@ -0,0 +1,32 @@
+package org.meveo.api.ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import org.meveo.api.dto.ActionStatus;
+import org.meveo.api.dto.payment.RevenueRecognitionRuleDto;
+import org.meveo.api.dto.response.payment.RevenueRecognitionRuleDtoResponse;
+import org.meveo.api.dto.response.payment.RevenueRecognitionRuleDtosResponse;
+
+@WebService
+public interface FinanceWS extends IBaseWs {
+
+	@WebMethod
+	ActionStatus createRevenueRecognitionRule(@WebParam(name = "revenueRecognitionRule") RevenueRecognitionRuleDto moduleDto);
+
+	@WebMethod
+	ActionStatus updateRevenueRecognitionRule(@WebParam(name = "revenueRecognitionRule") RevenueRecognitionRuleDto moduleDto);
+
+	@WebMethod
+	ActionStatus deleteRevenueRecognitionRule(@WebParam(name = "code") String code);
+
+	@WebMethod
+	RevenueRecognitionRuleDtosResponse listRevenueRecognitionRules();
+
+	@WebMethod
+	RevenueRecognitionRuleDtoResponse getRevenueRecognitionRule(@WebParam(name = "code") String code);
+
+	@WebMethod
+	ActionStatus createOrUpdateRevenueRecognitionRule(@WebParam(name = "module") RevenueRecognitionRuleDto moduleDto);
+}
