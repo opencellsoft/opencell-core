b14bdcebe74327b00bebb8b435ca736dd2c64983
Tony Alejandro <tonysviews@gmail.com>
2016-05-14 00:25:58 +0800

    RE #1314 - Changed page-access.xml implementation - allow separate constraints for read and write access for pages - allow usage of regex expressions to match pages - modified "rendered" attribute of page actions so that they do not appear for "read only" users. - Wiki added on page-access.xml file configuration: https://www.assembla.com/spaces/meveo/wiki/Page_Access_XML_Configuration_Guide

--- meveo-admin/web/src/main/java/org/meveo/admin/web/listener/InitializationListener.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/web/listener/InitializationListener.java
@@ -1,9 +1,6 @@
 package org.meveo.admin.web.listener;
 
 import java.io.File;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -12,7 +9,6 @@ import javax.servlet.annotation.WebListener;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
-import org.meveo.admin.web.filter.config.Page;
 import org.meveo.admin.web.filter.config.PageAccess;
 import org.meveo.util.view.PagePermission;
 import org.slf4j.Logger;
@@ -22,7 +18,6 @@ import org.slf4j.LoggerFactory;
 public class InitializationListener implements ServletContextListener {
 
     private static final String PAGE_ACCESS_FILE = "pageAccessFile";
-    private static final String PAGES_DIRECTORY = "pagesDirectory";
 
     private static final Logger logger = LoggerFactory.getLogger(InitializationListener.class);
 
@@ -42,15 +37,9 @@ public class InitializationListener implements ServletContextListener {
         try {
             JAXBContext context = JAXBContext.newInstance(PageAccess.class);
             PageAccess pageAccess = (PageAccess) context.createUnmarshaller().unmarshal(pageAccessFile);
-            Map<String, Page> pages = Collections.synchronizedMap(new HashMap<String, Page>());
             if (pageAccess != null) {
-                String pagesDirectory = servletContext.getContextPath() + servletContext.getInitParameter(PAGES_DIRECTORY);
-
-                for (Page page : pageAccess.getPages()) {
-                    // key is a concatenation of the pageDirectory, and the view-id
-                    pages.put(pagesDirectory + page.getViewId(), page);
-                }
-                PagePermission.getInstance().init(pagesDirectory, pages);
+                String pagesDirectory = servletContext.getContextPath() + pageAccess.getPath();
+                PagePermission.getInstance().init(pagesDirectory, pageAccess.getPages());
             }
         } catch (JAXBException e) {
             logger.error("Unable to unmarshall page-access rules.", e);
