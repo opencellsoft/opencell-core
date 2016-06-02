4b5b49314f5978e6ab77566e04b53db074070ab0
mbarek <mbarek.omoh@manaty.net>
2016-06-02 14:35:37 +0000

    updated messages

--- meveo-api/src/main/java/org/meveo/api/invoice/Invoice4_2Api.java
+++ meveo-api/src/main/java/org/meveo/api/invoice/Invoice4_2Api.java
@@ -467,7 +467,7 @@ public class Invoice4_2Api extends BaseApi {
         List<Invoice> invoices = invoiceService.getInvoices(billingRun);
         log.info((invoices == null) ? "getInvoice is null" : "size=" + invoices.size());
         if (invoices == null || invoices.isEmpty()) {
-            throw new BusinessApiException("Cant found invoice");
+            throw new BusinessApiException("Can't find invoice");
         }
 
         GenerateInvoiceResultDto generateInvoiceResultDto = new GenerateInvoiceResultDto();
