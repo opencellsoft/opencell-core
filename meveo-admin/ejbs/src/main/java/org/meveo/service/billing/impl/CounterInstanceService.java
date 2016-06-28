12db88e0baf9dcd47a232109c92abf4ec8dc0a49
mbarek <mbarek.omoh@manaty.net>
2016-06-28 14:53:22 +0000

    re #1814

--- meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/CounterInstanceService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/billing/impl/CounterInstanceService.java
@@ -19,6 +19,7 @@
 package org.meveo.service.billing.impl;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -284,6 +285,7 @@ public class CounterInstanceService extends PersistenceService<CounterInstance> 
 	
     public BigDecimal evaluateCeilingElExpression(String expression, ChargeInstance charge, ServiceInstance serviceInstance,Subscription subscription)
 			throws BusinessException {
+    	int rounding = subscription.getProvider().getRounding() == null ? 3 : subscription.getProvider().getRounding();
     	BigDecimal result = null;
 		if (StringUtils.isBlank(expression)) {
 			return result;
@@ -305,7 +307,7 @@ public class CounterInstanceService extends PersistenceService<CounterInstance> 
 		} catch (Exception e) {
 			throw new BusinessException("Expression " + expression + " do not evaluate to BigDecimal but " + res);
 		}
-		return result;
+		return result.setScale(rounding,RoundingMode.HALF_UP);
 	}
 
     /**
