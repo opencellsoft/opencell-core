832a9a4f396d49b0919b26ce1b99e8f37e93122b
anasseh <abdelhadi.nasseh@manaty.net>
2016-07-13 17:13:08 +0100

    re #1841 - WF engine

---
+++ meveo-api/src/main/java/org/meveo/api/rest/wf/WorkflowRs.java
@@ -0,0 +1,51 @@
+package org.meveo.api.rest.wf;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.meveo.api.dto.ActionStatus;
+import org.meveo.api.dto.payment.WorkflowDto;
+import org.meveo.api.dto.wf.WorkflowResponseDto;
+import org.meveo.api.dto.wf.WorkflowsResponseDto;
+import org.meveo.api.rest.IBaseRs;
+import org.meveo.api.rest.security.RSSecured;
+
+@Path("/admin/workflow")
+@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@RSSecured
+public interface WorkflowRs extends IBaseRs {
+
+    @POST
+    @Path("/")
+    ActionStatus create(WorkflowDto workflowDto);
+    
+    @PUT
+    @Path("/")
+    ActionStatus update(WorkflowDto workflowDto);
+
+    @GET
+    @Path("/")
+    WorkflowResponseDto find(@QueryParam("code") String code);
+
+    @DELETE
+    @Path("/{code}")
+    ActionStatus remove(@PathParam("code") String code);
+
+    @GET
+    @Path("/list")
+    WorkflowsResponseDto list();
+
+    @POST
+    @Path("/createOrUpdate")
+    ActionStatus createOrUpdate(WorkflowDto workflowDto);
+}
+
