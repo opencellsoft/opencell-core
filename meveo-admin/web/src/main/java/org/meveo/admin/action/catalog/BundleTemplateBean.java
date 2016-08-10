f7b66dff844c264177c6a456c31a5a990e2d2a5b
Luis Mance <luis.mance@manaty.net>
2016-08-10 12:56:28 +0800

    RE #1884 Updated list for selecting products to add to bundle

--- meveo-admin/web/src/main/java/org/meveo/admin/action/catalog/BundleTemplateBean.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/catalog/BundleTemplateBean.java
@@ -1,13 +1,19 @@
 package org.meveo.admin.action.catalog;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.meveo.admin.action.CustomFieldBean;
+import org.meveo.admin.exception.BusinessException;
+import org.meveo.admin.web.interceptor.ActionMethod;
+import org.meveo.model.catalog.BundleProductTemplate;
 import org.meveo.model.catalog.BundleTemplate;
 import org.meveo.model.catalog.PricePlanMatrix;
+import org.meveo.model.catalog.ProductTemplate;
 import org.meveo.model.crm.CustomFieldInstance;
 import org.meveo.service.base.local.IPersistenceService;
 import org.meveo.service.catalog.impl.BundleTemplateService;
@@ -30,6 +36,10 @@ public class BundleTemplateBean extends CustomFieldBean<BundleTemplate> {
 	private BigDecimal discountedAmount;
 	private CustomFieldInstance catalogPriceCF;
 
+	private String editMode;
+
+	private List<ProductTemplate> productTemplatesToAdd;
+
 	public BundleTemplateBean() {
 		super(BundleTemplate.class);
 	}
@@ -37,6 +47,32 @@ public class BundleTemplateBean extends CustomFieldBean<BundleTemplate> {
 	@Override
 	protected IPersistenceService<BundleTemplate> getPersistenceService() {
 		return bundleTemplateService;
+	}
+
+	@Override
+	@ActionMethod
+	public String saveOrUpdate(boolean killConversation) throws BusinessException {
+
+		String outcome = super.saveOrUpdate(killConversation);
+
+		if (editMode != null && editMode.length() > 0) {
+			outcome = "mmProductTemplates";
+		}
+
+		return outcome;
+	}
+
+	public String addProductTemplateToBundle(ProductTemplate prod) {
+		if (entity != null) {
+			BundleProductTemplate bpt = new BundleProductTemplate();
+			bpt.setProductTemplate(prod);
+			bpt.setQuantity(1);
+			if (entity.getBundleProducts() == null) {
+				entity.setBundleProducts(new ArrayList<BundleProductTemplate>());
+			}
+			entity.getBundleProducts().add(bpt);
+		}
+		return "mmBundleTemplateDetail";
 	}
 
 	public PricePlanMatrix getEntityPricePlan() {
@@ -69,6 +105,22 @@ public class BundleTemplateBean extends CustomFieldBean<BundleTemplate> {
 
 	public void setCatalogPriceCF(CustomFieldInstance catalogPriceCF) {
 		this.catalogPriceCF = catalogPriceCF;
+	}
+
+	public String getEditMode() {
+		return editMode;
+	}
+
+	public void setEditMode(String editMode) {
+		this.editMode = editMode;
+	}
+
+	public List<ProductTemplate> getProductTemplatesToAdd() {
+		return productTemplatesToAdd;
+	}
+
+	public void setProductTemplatesToAdd(List<ProductTemplate> productTemplatesToAdd) {
+		this.productTemplatesToAdd = productTemplatesToAdd;
 	}
 
 }
