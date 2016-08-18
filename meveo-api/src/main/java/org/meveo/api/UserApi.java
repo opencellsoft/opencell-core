7e5bb68e4aa8e5d919f20519a104a0ee71ef45fb
anasseh <anasseh@maciforce.local.home>
2016-08-18 13:58:58 +0100

    fix #1915 - [API]user update faild

--- meveo-api/src/main/java/org/meveo/api/UserApi.java
+++ meveo-api/src/main/java/org/meveo/api/UserApi.java
@@ -30,184 +30,184 @@ import org.meveo.service.crm.impl.ProviderService;
 @Stateless
 public class UserApi extends BaseApi {
 
-    @Inject
-    private ProviderService providerService;
+	@Inject
+	private ProviderService providerService;
 
-    @Inject
-    private RoleService roleService;
+	@Inject
+	private RoleService roleService;
 
-    @Inject
-    private UserService userService;
+	@Inject
+	private UserService userService;
 
-      public void create(UserDto postData, User currentUser) throws MeveoApiException, BusinessException {
+	public void create(UserDto postData, User currentUser) throws MeveoApiException, BusinessException {
 
-        if (StringUtils.isBlank(postData.getUsername())) {
-            missingParameters.add("username");
-        }
-        if (StringUtils.isBlank(postData.getEmail())) {
-            missingParameters.add("email");
-        }
+		if (StringUtils.isBlank(postData.getUsername())) {
+			missingParameters.add("username");
+		}
+		if (StringUtils.isBlank(postData.getEmail())) {
+			missingParameters.add("email");
+		}
 
-        if ((postData.getRoles() == null || postData.getRoles().isEmpty()) && StringUtils.isBlank(postData.getRole())) {
-            missingParameters.add("roles");
-        }
+		if ((postData.getRoles() == null || postData.getRoles().isEmpty()) && StringUtils.isBlank(postData.getRole())) {
+			missingParameters.add("roles");
+		}
 
-        handleMissingParameters();
-        
-        if(!StringUtils.isBlank(postData.getRole())){
-        	if(postData.getRoles() == null){
-        		postData.setRoles(new ArrayList<String>());
-        	}
-        	postData.getRoles().add(postData.getRole());
-        }
+		handleMissingParameters();
 
-        // Find provider and check if user has access to manage that provider data
-        Provider provider = null;
-        if (!StringUtils.isBlank(postData.getProvider())) {
-            provider = providerService.findByCode(postData.getProvider());
-            if (provider == null) {
-                throw new EntityDoesNotExistsException(Provider.class, postData.getProvider());
-            }
-        } else {
-            provider = currentUser.getProvider();
-        }
+		if (!StringUtils.isBlank(postData.getRole())) {
+			if (postData.getRoles() == null) {
+				postData.setRoles(new ArrayList<String>());
+			}
+			postData.getRoles().add(postData.getRole());
+		}
 
-        if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationManagement") && provider
-            .equals(currentUser.getProvider())))) {
-            throw new LoginException("User has no permission to manage users for provider " + provider.getCode());
-        }
+		// Find provider and check if user has access to manage that provider
+		// data
+		Provider provider = null;
+		if (!StringUtils.isBlank(postData.getProvider())) {
+			provider = providerService.findByCode(postData.getProvider());
+			if (provider == null) {
+				throw new EntityDoesNotExistsException(Provider.class, postData.getProvider());
+			}
+		} else {
+			provider = currentUser.getProvider();
+		}
 
-        // check if the user already exists
-        if (userService.findByUsername(postData.getUsername()) != null) {
-            throw new EntityAlreadyExistsException(User.class, postData.getUsername(), "username");
-        }
+		if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationManagement") && provider.equals(currentUser.getProvider())))) {
+			throw new LoginException("User has no permission to manage users for provider " + provider.getCode());
+		}
 
-        // find role
-        Set<Role> roles = new HashSet<Role>();
-        for (String rl : postData.getRoles()) {
-            Role role = roleService.findByName(rl, provider);
-            if (role == null) {
-                throw new EntityDoesNotExistsException(Role.class, rl);
-            }
-            roles.add(role);
-        }
+		// check if the user already exists
+		if (userService.findByUsername(postData.getUsername()) != null) {
+			throw new EntityAlreadyExistsException(User.class, postData.getUsername(), "username");
+		}
 
-        User user = new User();
-        user.setUserName(postData.getUsername().toUpperCase());
-        user.setEmail((postData.getEmail()));
-        Name name = new Name();
-        name.setLastName(postData.getLastName());
-        name.setFirstName(postData.getFirstName());
-        user.setName(name);
-        user.setPassword(postData.getPassword());
-        user.setLastPasswordModification(new Date());
-        user.setProvider(provider);
-        user.setRoles(roles);
+		// find role
+		Set<Role> roles = new HashSet<Role>();
+		for (String rl : postData.getRoles()) {
+			Role role = roleService.findByName(rl, provider);
+			if (role == null) {
+				throw new EntityDoesNotExistsException(Role.class, rl);
+			}
+			roles.add(role);
+		}
 
-        userService.create(user, currentUser);
-    }
+		User user = new User();
+		user.setUserName(postData.getUsername().toUpperCase());
+		user.setEmail((postData.getEmail()));
+		Name name = new Name();
+		name.setLastName(postData.getLastName());
+		name.setFirstName(postData.getFirstName());
+		user.setName(name);
+		user.setPassword(postData.getPassword());
+		user.setLastPasswordModification(new Date());
+		user.setProvider(provider);
+		user.setRoles(roles);
 
-    public void update(UserDto postData, User currentUser) throws MeveoApiException, BusinessException {
+		userService.create(user, currentUser);
+	}
 
-        if (StringUtils.isBlank(postData.getUsername())) {
-            missingParameters.add("username");
-        }
-        if (StringUtils.isBlank(postData.getEmail())) {
-            missingParameters.add("email");
-        }
-        if ((postData.getRoles() == null || postData.getRoles().isEmpty()) && StringUtils.isBlank(postData.getRole())) {
-            missingParameters.add("roles");
-        }
+	public void update(UserDto postData, User currentUser) throws MeveoApiException, BusinessException {
+		if (StringUtils.isBlank(postData.getUsername())) {
+			missingParameters.add("username");
+		}
+		handleMissingParameters();
+		
+		//we support old dto that containt only one role
+		if (!StringUtils.isBlank(postData.getRole())) {
+			if (postData.getRoles() == null) {
+				postData.setRoles(new ArrayList<String>());
+			}
+			postData.getRoles().add(postData.getRole());
+		}
 
-        handleMissingParameters();
-        
-        if(!StringUtils.isBlank(postData.getRole())){
-        	if(postData.getRoles() == null){
-        		postData.setRoles(new ArrayList<String>());
-        	}
-        	postData.getRoles().add(postData.getRole());
-        }
+		// find user
+		User user = userService.findByUsername(postData.getUsername());
 
-        // find user
-        User user = userService.findByUsername(postData.getUsername());
+		if (user == null) {
+			throw new EntityDoesNotExistsException(User.class, postData.getUsername(), "username");
+		}
 
-        if (user == null) {
-            throw new EntityDoesNotExistsException(User.class, postData.getUsername(), "username");
-        }
+		if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationVisualization") && user.getProvider().equals(currentUser.getProvider())))) {
+			throw new LoginException("User has no permission to manage users for provider " + user.getProvider().getCode());
+		}
 
-        if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationVisualization") && user.getProvider()
-            .equals(currentUser.getProvider())))) {
-            throw new LoginException("User has no permission to manage users for provider " + user.getProvider().getCode());
-        }
+		// find roles
+		Set<Role> roles = new HashSet<Role>();
+		for (String rl : postData.getRoles()) {
+			Role role = roleService.findByName(rl, user.getProvider());
+			if (role == null) {
+				throw new EntityDoesNotExistsException(Role.class, rl);
+			}
+			roles.add(role);
+		}
 
-        // find roles
-        Set<Role> roles = new HashSet<Role>();
-        for (String rl : postData.getRoles()) {
-            Role role = roleService.findByName(rl, user.getProvider());
-            if (role == null) {
-                throw new EntityDoesNotExistsException(Role.class, rl);
-            }
-            roles.add(role);
-        }
+		user.setUserName(postData.getUsername());
+		if (!StringUtils.isBlank(postData.getEmail())) {
+			user.setEmail(postData.getEmail());
+		}
+		if (!StringUtils.isBlank(postData.getPassword())) {
+			user.setNewPassword(postData.getPassword());
+		}
+		Name name = new Name();
+		if (!StringUtils.isBlank(postData.getLastName())) {
+			name.setLastName(postData.getLastName());
+			user.setName(name);
+		}
+		if (!StringUtils.isBlank(postData.getFirstName())) {
+			name.setFirstName(postData.getFirstName());
+			user.setName(name);
+		}
+		if (!roles.isEmpty()) {
+			user.setRoles(roles);
+		}
 
-        user.setUserName(postData.getUsername());
-        user.setEmail((postData.getEmail()));
-        user.setNewPassword(postData.getPassword());
-        Name name = new Name();
-        name.setLastName(postData.getLastName());
-        name.setFirstName(postData.getFirstName());
-        user.setName(name);
-        user.setRoles(roles);
+		userService.update(user, currentUser);
+	}
 
-        userService.update(user, currentUser);
-    }
+	public void remove(String username, User currentUser) throws MeveoApiException {
+		User user = userService.findByUsername(username);
 
-    public void remove(String username, User currentUser) throws MeveoApiException {
-        User user = userService.findByUsername(username);
+		if (user == null) {
+			throw new EntityDoesNotExistsException(User.class, username, "username");
+		}
 
-        if (user == null) {
-            throw new EntityDoesNotExistsException(User.class, username, "username");
-        }
+		if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationVisualization") && user.getProvider().equals(currentUser.getProvider())))) {
+			throw new LoginException("User has no permission to manage users for provider " + user.getProvider().getCode());
+		}
 
-        if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationVisualization") && user.getProvider()
-            .equals(currentUser.getProvider())))) {
-            throw new LoginException("User has no permission to manage users for provider " + user.getProvider().getCode());
-        }
+		userService.remove(user);
+	}
 
-        userService.remove(user);
-    }
+	public UserDto find(String username, User currentUser) throws MeveoApiException {
 
+		if (StringUtils.isBlank(username)) {
+			missingParameters.add("username");
+		}
 
-    public UserDto find(String username, User currentUser) throws MeveoApiException {
+		handleMissingParameters();
 
-        if (StringUtils.isBlank(username)) {
-            missingParameters.add("username");
-        }
+		User user = userService.findByUsernameWithFetch(username, Arrays.asList("provider", "roles"));
 
-        handleMissingParameters();
+		if (user == null) {
+			throw new EntityDoesNotExistsException(User.class, username, "username");
+		}
 
-        User user = userService.findByUsernameWithFetch(username, Arrays.asList("provider", "roles"));
+		if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationVisualization") && user.getProvider().equals(currentUser.getProvider())))) {
+			throw new LoginException("User has no permission to access users for provider " + user.getProvider().getCode());
+		}
 
-        if (user == null) {
-            throw new EntityDoesNotExistsException(User.class, username, "username");
-        }
+		UserDto result = new UserDto(user);
 
-        if (!(currentUser.hasPermission("superAdmin", "superAdminManagement") || (currentUser.hasPermission("administration", "administrationVisualization") && user.getProvider()
-            .equals(currentUser.getProvider())))) {
-            throw new LoginException("User has no permission to access users for provider " + user.getProvider().getCode());
-        }
+		return result;
+	}
 
-        UserDto result = new UserDto(user);
-
-        return result;
-    }
-
-    public void createOrUpdate(UserDto postData, User currentUser) throws MeveoApiException, BusinessException {
-        User user = userService.findByUsername(postData.getUsername());
-        if (user == null) {
-            create(postData, currentUser);
-        } else {
-            update(postData, currentUser);
-        }
-    }
+	public void createOrUpdate(UserDto postData, User currentUser) throws MeveoApiException, BusinessException {
+		User user = userService.findByUsername(postData.getUsername());
+		if (user == null) {
+			create(postData, currentUser);
+		} else {
+			update(postData, currentUser);
+		}
+	}
 }
