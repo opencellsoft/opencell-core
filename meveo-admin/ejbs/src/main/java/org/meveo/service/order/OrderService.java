87ebc0e23925107749c1eccca3030b81cb1b2ad4
phubach <pbach1982@yahoo.com>
2016-09-07 09:58:00 -0700

    Merge remote from #1839
    # Conflicts:
    #	meveo-admin/ejbs/src/main/java/org/meveo/service/order/OrderService.java
    #	meveo-admin/web/src/main/resources/messages_en.properties
    #	meveo-admin/web/src/main/resources/messages_fr.properties
    #	meveo-model/src/main/db_resources/changelog/current/structure.xml

--- meveo-admin/ejbs/src/main/java/org/meveo/service/order/OrderService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/order/OrderService.java
@@ -3,14 +3,21 @@ package org.meveo.service.order;
 import java.util.Calendar;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.Query;
 
+import org.meveo.admin.exception.BusinessException;
+import org.meveo.model.hierarchy.UserHierarchyLevel;
 import org.meveo.model.order.Order;
 import org.meveo.model.order.OrderStatusEnum;
 import org.meveo.service.base.BusinessService;
+import org.meveo.service.hierarchy.impl.UserHierarchyLevelService;
 
 @Stateless
 public class OrderService extends BusinessService<Order> {
+
+    @Inject
+    private UserHierarchyLevelService userHierarchyLevelService;
 
 	public Long countNewOrders(Calendar endDate) {
 
@@ -38,4 +45,13 @@ public class OrderService extends BusinessService<Order> {
 
 		return count.longValue();
 	}
+
+    public Order routedToUserGroup(Order entity, String userGroupCode) throws BusinessException {
+        UserHierarchyLevel userHierarchyLevel = userHierarchyLevelService.findByCode(userGroupCode, getCurrentUser().getProvider());
+        if (userHierarchyLevel == null) {
+            log.trace("No UserHierarchyLevel found {}/{}", entity, userGroupCode);
+        }
+        entity.setRoutedToUserGroup(userHierarchyLevel);
+        return this.update(entity, getCurrentUser());
+    }
 }
