102e1f2e8eecaf8224d530c415d74144e08189b9
Tony Alejandro <tonysviews@gmail.com>
2017-01-27 15:20:21 +0800

    RE #2345 Catch persistence and constraint violation errors - change message to Entity already in use if there is a constraint violation

--- meveo-admin/web/src/main/java/org/meveo/admin/web/interceptor/BackingBeanActionMethodInterceptor.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/web/interceptor/BackingBeanActionMethodInterceptor.java
@@ -8,6 +8,7 @@ import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
 import javax.persistence.TransactionRequiredException;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -20,9 +21,8 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Commits transaction and handles exceptions of backing bean action methods
- * 
+ *
  * @author Andrius Karpavicius
- * 
  */
 @ActionMethod
 @Interceptor
@@ -48,25 +48,36 @@ public class BackingBeanActionMethodInterceptor implements Serializable {
             em.flush();
             return result;
 
-        }catch (TransactionRequiredException e){
+        } catch (TransactionRequiredException e) {
             log.error("Transaction must have been rollbacked already (probably by exception thown in service and caught in backing bean): {}", e.getMessage());
-            
+
         } catch (ConstraintViolationException e) {
             log.error("Failed to execute {}.{} method due to validation errors ", invocationContext.getMethod().getDeclaringClass().getName(), invocationContext.getMethod()
-                .getName(), e);
+                    .getName(), e);
 
             StringBuilder builder = new StringBuilder();
             builder.append("Invalid values passed: ");
             for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
                 builder.append(String.format("    %s.%s: value '%s' - %s;", violation.getRootBeanClass().getSimpleName(), violation.getPropertyPath().toString(),
-                    violation.getInvalidValue(), violation.getMessage()));
+                        violation.getInvalidValue(), violation.getMessage()));
             }
 
             messages.getAll();
             messages.clear();
             messages.error(new BundleKey("messages", "error.action.failed"), builder.toString());
             FacesContext.getCurrentInstance().validationFailed();
-            
+
+        } catch (PersistenceException e) {
+            messages.getAll();
+            messages.clear();
+            if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
+                log.error("Delete was unsuccessful because entity is already in use.", e.getCause());
+                messages.error(new BundleKey("messages", "error.delete.entityUsed"));
+            } else {
+                log.error("Failed to execute {}.{} method due to database errors. ", invocationContext.getMethod().getDeclaringClass().getName(), invocationContext.getMethod().getName(), e);
+                messages.error(new BundleKey("messages", "error.action.failed"), e.getMessage());
+            }
+            FacesContext.getCurrentInstance().validationFailed();
         } catch (Exception e) {
             log.error("Failed to execute {}.{} method due to errors ", invocationContext.getMethod().getDeclaringClass().getName(), invocationContext.getMethod().getName(), e);
 
