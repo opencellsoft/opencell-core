5efb74d1f5d541bbf0e793e0ff3958aadcfd7364
Andrius Karpavicius <exploresouthamerica@gmail.com>
2017-05-19 12:51:11 -0300

    Re #2589 Fixed link to BSM from BOM list. Added link to BPM from BOM list.

--- opencell-admin/web/src/main/java/org/meveo/admin/action/catalog/BusinessProductModelBean.java
+++ opencell-admin/web/src/main/java/org/meveo/admin/action/catalog/BusinessProductModelBean.java
@@ -1,11 +1,17 @@
 package org.meveo.admin.action.catalog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.meveo.admin.action.admin.module.GenericModuleBean;
+import org.meveo.model.catalog.BusinessOfferModel;
 import org.meveo.model.catalog.BusinessProductModel;
+import org.meveo.model.module.MeveoModule;
+import org.meveo.model.module.MeveoModuleItem;
 import org.meveo.service.base.local.IPersistenceService;
 import org.meveo.service.catalog.impl.BusinessProductModelService;
 
@@ -16,18 +22,35 @@ import org.meveo.service.catalog.impl.BusinessProductModelService;
 @ViewScoped
 public class BusinessProductModelBean extends GenericModuleBean<BusinessProductModel> {
 
-	private static final long serialVersionUID = -1731438369618574084L;
+    private static final long serialVersionUID = -1731438369618574084L;
 
-	@Inject
-	protected BusinessProductModelService businessProductModelService;
+    @Inject
+    protected BusinessProductModelService businessProductModelService;
 
-	public BusinessProductModelBean() {
-		super(BusinessProductModel.class);
-	}
+    public BusinessProductModelBean() {
+        super(BusinessProductModel.class);
+    }
 
-	@Override
-	protected IPersistenceService<BusinessProductModel> getPersistenceService() {
-		return businessProductModelService;
-	}
+    @Override
+    protected IPersistenceService<BusinessProductModel> getPersistenceService() {
+        return businessProductModelService;
+    }
 
+    public List<BusinessOfferModel> getBusinessOfferModels(BusinessProductModel bpmEntity) {
+        List<BusinessOfferModel> result = new ArrayList<>();
+
+        if (bpmEntity != null) {
+            List<MeveoModuleItem> meveoModuleItems = meveoModuleService.findByCodeAndItemType(bpmEntity.getCode(), BusinessProductModel.class.getName());
+            if (meveoModuleItems != null) {
+                for (MeveoModuleItem meveoModuleItem : meveoModuleItems) {
+                    MeveoModule meveoModule = meveoModuleItem.getMeveoModule();
+                    if (meveoModule instanceof BusinessOfferModel) {
+                        result.add((BusinessOfferModel) meveoModule);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 }
