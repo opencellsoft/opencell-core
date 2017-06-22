96aef0c407ff6d3f222028b31d0cfa6f981f7618
Edward P. Legaspi <czetsuya@gmail.com>
2017-06-22 17:17:05 +0800

    Test: #2708. Redirect user New button to keycloak.

--- opencell-security/src/main/java/org/meveo/security/keycloak/AuthenticationProvider.java
+++ opencell-security/src/main/java/org/meveo/security/keycloak/AuthenticationProvider.java
@@ -5,6 +5,8 @@ import javax.inject.Named;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.keycloak.common.util.KeycloakUriBuilder;
+import org.keycloak.constants.ServiceUrlConstants;
 import org.slf4j.Logger;
 
 @Named
@@ -24,4 +26,19 @@ public class AuthenticationProvider {
         }
         return "indexPage";
     }
+    
+	public String getAccountUrl() {
+		String kcUrl = System.getProperty("opencell.keycloak.url");
+		String kcRealm = System.getProperty("opencell.keycloak.realm");
+		String acctUri = KeycloakUriBuilder.fromUri(kcUrl).path(ServiceUrlConstants.ACCOUNT_SERVICE_PATH).build(kcRealm)
+				.toString();
+		return acctUri + "?faces-redirect=true";
+	}
+
+	public String getAuthPath() {
+		String kcUrl = System.getProperty("opencell.keycloak.url");
+		String kcRealm = System.getProperty("opencell.keycloak.realm");
+		String uri = KeycloakUriBuilder.fromUri(kcUrl).path(ServiceUrlConstants.AUTH_PATH).build(kcRealm).toString();
+		return uri + "?faces-redirect=true";
+	}
 }
