a816f0d8399c6e0c59f46c5d3486c12e240386f8
Edward P. Legaspi <czetsuya@gmail.com>
2017-07-18 17:46:17 +0800

    Test: #2654. Removed extra productInstances tag.

--- opencell-api-dto/src/main/java/org/meveo/api/dto/billing/ProductInstancesDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/billing/ProductInstancesDto.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 
 /**
  * @author Edward P. Legaspi
@@ -17,8 +15,6 @@ public class ProductInstancesDto implements Serializable {
 
 	private static final long serialVersionUID = -3365845607399516795L;
 
-	@XmlElementWrapper(name = "productInstances")
-	@XmlElement(name = "productInstance")
 	private List<ProductInstanceDto> productInstances = new ArrayList<ProductInstanceDto>();
 
 	public List<ProductInstanceDto> getProductInstances() {
