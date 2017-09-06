d9e49bb10cd74b792cc1a33859a574407bf9edfa
Edward P. Legaspi <czetsuya@gmail.com>
2017-09-06 15:20:17 +0800

    RE: #2586.

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/OneShotChargeInstanceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/OneShotChargeInstanceService.java
@@ -206,8 +206,8 @@ public class OneShotChargeInstanceService extends BusinessService<OneShotChargeI
 
 		create(oneShotChargeInstance);
 		
-		if (!walletOperationService.isChargeMatch(oneShotChargeInstance,
-				((OneShotChargeTemplate) oneShotChargeInstance.getChargeTemplate()).getFilterExpression())) {
+		OneShotChargeTemplate oneShotChargeTemplate = oneShotChargeTemplateService.findByCode(oneShotChargeInstance.getCode());
+		if (!walletOperationService.isChargeMatch(oneShotChargeInstance, oneShotChargeTemplate.getFilterExpression())) {
 			log.debug("not rating chargeInstance with code={}, filter expression not evaluated to true",
 					oneShotChargeInstance.getCode());
 			return oneShotChargeInstance;
@@ -222,8 +222,8 @@ public class OneShotChargeInstanceService extends BusinessService<OneShotChargeI
 	public void oneShotChargeApplication(Subscription subscription,
 			OneShotChargeInstance oneShotChargeInstance, Date effectiveDate, BigDecimal quantity, String orderNumberOverride)
 			throws BusinessException {
-		if (!walletOperationService.isChargeMatch(oneShotChargeInstance,
-				((OneShotChargeTemplate) oneShotChargeInstance.getChargeTemplate()).getFilterExpression())) {
+		OneShotChargeTemplate oneShotChargeTemplate = oneShotChargeTemplateService.findByCode(oneShotChargeInstance.getCode());
+		if (!walletOperationService.isChargeMatch(oneShotChargeInstance, oneShotChargeTemplate.getFilterExpression())) {
 			log.debug("not rating chargeInstance with code={}, filter expression not evaluated to true",
 					oneShotChargeInstance.getCode());
 			return;
@@ -251,8 +251,8 @@ public class OneShotChargeInstanceService extends BusinessService<OneShotChargeI
         log.debug("Apply one shot charge on Virtual operation. User account {}, offer {}, charge {}, quantity {}", oneShotChargeInstance.getUserAccount().getCode(), subscription
             .getOffer().getCode(), oneShotChargeInstance.getChargeTemplate().getCode(), quantity);
         
-        if (!walletOperationService.isChargeMatch(oneShotChargeInstance,
-				((OneShotChargeTemplate) oneShotChargeInstance.getChargeTemplate()).getFilterExpression())) {
+        OneShotChargeTemplate oneShotChargeTemplate = oneShotChargeTemplateService.findByCode(oneShotChargeInstance.getCode());
+		if (!walletOperationService.isChargeMatch(oneShotChargeInstance, oneShotChargeTemplate.getFilterExpression())) {
 			log.debug("not rating chargeInstance with code={}, filter expression not evaluated to true",
 					oneShotChargeInstance.getCode());
 			return null;
