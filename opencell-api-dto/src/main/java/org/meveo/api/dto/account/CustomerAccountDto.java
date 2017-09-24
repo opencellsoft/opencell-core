d2064893615005a13e4a6b465daf7fab89833708
anasseh <abdelhadi.nasseh@manaty.net>
2017-09-24 14:23:24 +0100

    javaDocs

--- opencell-api-dto/src/main/java/org/meveo/api/dto/account/CustomerAccountDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/account/CustomerAccountDto.java
@@ -15,7 +15,11 @@ import org.meveo.model.billing.BillingAccount;
 import org.meveo.model.payments.CustomerAccountStatusEnum;
 import org.meveo.model.payments.DunningLevelEnum;
 import org.meveo.model.payments.PaymentMethodEnum;
-
+/**
+ * Customer Account DTO
+ * @author anasseh
+ *
+ */
 @XmlRootElement()
 @XmlAccessorType(XmlAccessType.FIELD)
 @FilterResults(property = "billingAccounts.billingAccount", entityClass = BillingAccount.class)
@@ -50,7 +54,7 @@ public class CustomerAccountDto extends AccountDto {
      */
     @Deprecated
     private Date mandateDate;
-    
+
     private BigDecimal balance = BigDecimal.ZERO;
     private BigDecimal totalInvoiceBalance = BigDecimal.ZERO;
     // currently not use
@@ -60,11 +64,11 @@ public class CustomerAccountDto extends AccountDto {
     @XmlElementWrapper(name = "paymentMethods")  
     @XmlElement(name="methodOfPayment")
     private List<PaymentMethodDto> paymentMethods;
-    
+
     private boolean excludedFromPayment;
 
     /**
-     * Field was deprecated in 4.6 version. Use 'paymentMethods' field instead
+     * Field was deprecated in 4.6 version. Use 'paymentMethods' field instead.
      */
     @Deprecated
     private PaymentMethodEnum paymentMethod;
@@ -75,167 +79,280 @@ public class CustomerAccountDto extends AccountDto {
     private BillingAccountsDto billingAccounts;
 
     public CustomerAccountDto() {
-        super();
-    }
-
-    public CustomerAccountStatusEnum getStatus() {
-        return status;
-    }
-
-    public void setStatus(CustomerAccountStatusEnum status) {
-        this.status = status;
-    }
-
-    public String getCreditCategory() {
-        return creditCategory;
-    }
-
-    public void setCreditCategory(String creditCategory) {
-        this.creditCategory = creditCategory;
-    }
-
-    public Date getDateStatus() {
-        return dateStatus;
-    }
-
-    public void setDateStatus(Date dateStatus) {
-        this.dateStatus = dateStatus;
-    }
-
-    public Date getDateDunningLevel() {
-        return dateDunningLevel;
-    }
-
-    public void setDateDunningLevel(Date dateDunningLevel) {
-        this.dateDunningLevel = dateDunningLevel;
-    }
-
-    public String getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(String customer) {
-        this.customer = customer;
-    }
-
-    public DunningLevelEnum getDunningLevel() {
-        return dunningLevel;
-    }
-
-    public void setDunningLevel(DunningLevelEnum dunningLevel) {
-        this.dunningLevel = dunningLevel;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public BigDecimal getTotalInvoiceBalance() {
-        return totalInvoiceBalance;
-    }
-
-    public void setTotalInvoiceBalance(BigDecimal totalInvoiceBalance) {
-        this.totalInvoiceBalance = totalInvoiceBalance;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
+	super();
     }
 
     @Override
     public String toString() {
-        return "CustomerAccountDto [customer=" + customer + ", currency=" + currency + ", language=" + language + ", status=" + status + ", creditCategory=" + creditCategory
-                + ", dateStatus=" + dateStatus + ", dateDunningLevel=" + dateDunningLevel + ", contactInformation=" + contactInformation + ", dunningLevel=" + dunningLevel
-                + ",  balance=" + balance + ", terminationDate=" + terminationDate
-                + ", billingAccounts=" + billingAccounts + "]";
+	return "CustomerAccountDto [customer=" + customer + ", currency=" + currency + ", language=" + language + ", status=" + status + ", creditCategory=" + creditCategory
+		+ ", dateStatus=" + dateStatus + ", dateDunningLevel=" + dateDunningLevel + ", contactInformation=" + contactInformation + ", dunningLevel=" + dunningLevel
+		+ ",  balance=" + balance + ", terminationDate=" + terminationDate
+		+ ", billingAccounts=" + billingAccounts + "]";
     }
 
-    public Date getTerminationDate() {
-        return terminationDate;
+    /**
+     * @return the customer
+     */
+    public String getCustomer() {
+        return customer;
     }
 
-    public void setTerminationDate(Date terminationDate) {
-        this.terminationDate = terminationDate;
+    /**
+     * @param customer the customer to set
+     */
+    public void setCustomer(String customer) {
+        this.customer = customer;
     }
 
-    public BillingAccountsDto getBillingAccounts() {
-        return billingAccounts;
+    /**
+     * @return the currency
+     */
+    public String getCurrency() {
+        return currency;
     }
 
-    public void setBillingAccounts(BillingAccountsDto billingAccounts) {
-        this.billingAccounts = billingAccounts;
+    /**
+     * @param currency the currency to set
+     */
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 
+    /**
+     * @return the language
+     */
+    public String getLanguage() {
+        return language;
+    }
+
+    /**
+     * @param language the language to set
+     */
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    /**
+     * @return the status
+     */
+    public CustomerAccountStatusEnum getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(CustomerAccountStatusEnum status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the creditCategory
+     */
+    public String getCreditCategory() {
+        return creditCategory;
+    }
+
+    /**
+     * @param creditCategory the creditCategory to set
+     */
+    public void setCreditCategory(String creditCategory) {
+        this.creditCategory = creditCategory;
+    }
+
+    /**
+     * @return the dateStatus
+     */
+    public Date getDateStatus() {
+        return dateStatus;
+    }
+
+    /**
+     * @param dateStatus the dateStatus to set
+     */
+    public void setDateStatus(Date dateStatus) {
+        this.dateStatus = dateStatus;
+    }
+
+    /**
+     * @return the dateDunningLevel
+     */
+    public Date getDateDunningLevel() {
+        return dateDunningLevel;
+    }
+
+    /**
+     * @param dateDunningLevel the dateDunningLevel to set
+     */
+    public void setDateDunningLevel(Date dateDunningLevel) {
+        this.dateDunningLevel = dateDunningLevel;
+    }
+
+    /**
+     * @return the contactInformation
+     */
     public ContactInformationDto getContactInformation() {
         return contactInformation;
     }
 
+    /**
+     * @param contactInformation the contactInformation to set
+     */
     public void setContactInformation(ContactInformationDto contactInformation) {
         this.contactInformation = contactInformation;
     }
 
+    /**
+     * @return the dunningLevel
+     */
+    public DunningLevelEnum getDunningLevel() {
+        return dunningLevel;
+    }
+
+    /**
+     * @param dunningLevel the dunningLevel to set
+     */
+    public void setDunningLevel(DunningLevelEnum dunningLevel) {
+        this.dunningLevel = dunningLevel;
+    }
+
+    /**
+     * @return the mandateIdentification
+     */
+    public String getMandateIdentification() {
+        return mandateIdentification;
+    }
+
+    /**
+     * @param mandateIdentification the mandateIdentification to set
+     */
+    public void setMandateIdentification(String mandateIdentification) {
+        this.mandateIdentification = mandateIdentification;
+    }
+
+    /**
+     * @return the mandateDate
+     */
+    public Date getMandateDate() {
+        return mandateDate;
+    }
+
+    /**
+     * @param mandateDate the mandateDate to set
+     */
+    public void setMandateDate(Date mandateDate) {
+        this.mandateDate = mandateDate;
+    }
+
+    /**
+     * @return the balance
+     */
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param balance the balance to set
+     */
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    /**
+     * @return the totalInvoiceBalance
+     */
+    public BigDecimal getTotalInvoiceBalance() {
+        return totalInvoiceBalance;
+    }
+
+    /**
+     * @param totalInvoiceBalance the totalInvoiceBalance to set
+     */
+    public void setTotalInvoiceBalance(BigDecimal totalInvoiceBalance) {
+        this.totalInvoiceBalance = totalInvoiceBalance;
+    }
+
+    /**
+     * @return the terminationDate
+     */
+    public Date getTerminationDate() {
+        return terminationDate;
+    }
+
+    /**
+     * @param terminationDate the terminationDate to set
+     */
+    public void setTerminationDate(Date terminationDate) {
+        this.terminationDate = terminationDate;
+    }
+
+    /**
+     * @return the dueDateDelayEL
+     */
     public String getDueDateDelayEL() {
         return dueDateDelayEL;
     }
 
+    /**
+     * @param dueDateDelayEL the dueDateDelayEL to set
+     */
     public void setDueDateDelayEL(String dueDateDelayEL) {
         this.dueDateDelayEL = dueDateDelayEL;
     }
 
+    /**
+     * @return the paymentMethods
+     */
     public List<PaymentMethodDto> getPaymentMethods() {
         return paymentMethods;
     }
 
+    /**
+     * @param paymentMethods the paymentMethods to set
+     */
     public void setPaymentMethods(List<PaymentMethodDto> paymentMethods) {
         this.paymentMethods = paymentMethods;
     }
 
+    /**
+     * @return the excludedFromPayment
+     */
+    public boolean isExcludedFromPayment() {
+        return excludedFromPayment;
+    }
+
+    /**
+     * @param excludedFromPayment the excludedFromPayment to set
+     */
+    public void setExcludedFromPayment(boolean excludedFromPayment) {
+        this.excludedFromPayment = excludedFromPayment;
+    }
+
+    /**
+     * @return the paymentMethod
+     */
     public PaymentMethodEnum getPaymentMethod() {
         return paymentMethod;
     }
 
+    /**
+     * @param paymentMethod the paymentMethod to set
+     */
     public void setPaymentMethod(PaymentMethodEnum paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
 
-	public String getMandateIdentification() {
-		return mandateIdentification;
-	}
+    /**
+     * @return the billingAccounts
+     */
+    public BillingAccountsDto getBillingAccounts() {
+        return billingAccounts;
+    }
 
-	public void setMandateIdentification(String mandateIdentification) {
-		this.mandateIdentification = mandateIdentification;
-	}
-
-	public Date getMandateDate() {
-		return mandateDate;
-	}
-
-	public void setMandateDate(Date mandateDate) {
-		this.mandateDate = mandateDate;
-	}
-
-	public boolean isExcludedFromPayment() {
-		return excludedFromPayment;
-	}
-
-	public void setExcludedFromPayment(boolean excludedFromPayment) {
-		this.excludedFromPayment = excludedFromPayment;
-	}
-    
+    /**
+     * @param billingAccounts the billingAccounts to set
+     */
+    public void setBillingAccounts(BillingAccountsDto billingAccounts) {
+        this.billingAccounts = billingAccounts;
+    }
 }
