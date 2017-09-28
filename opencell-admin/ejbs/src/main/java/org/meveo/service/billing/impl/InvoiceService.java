c59a7545d8548163c7e58dfb801861e957b2dcd7
anasseh <anasseh@172.20.10.2>
2017-09-28 14:03:51 +0100

    re #2893 revert a wrong change

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -607,8 +607,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
      * @param invoice invoice to generate pdf
      * @throws BusinessException
      */
-    public void produceInvoicePdfNoUpdate(Invoice invoice) throws BusinessException {
-	if(true) return;
+    public void produceInvoicePdfNoUpdate(Invoice invoice) throws BusinessException {	
         long startDate = System.currentTimeMillis();
         String meveoDir = paramBean.getProperty("providers.rootDir", "./opencelldata/") + File.separator + appProvider.getCode() + File.separator;
         String invoiceXmlFileName = getFullXmlFilePath(invoice, false);
