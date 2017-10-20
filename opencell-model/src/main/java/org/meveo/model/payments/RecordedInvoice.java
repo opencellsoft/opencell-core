7f9c1c36bbbe8d7d0a5fb515ea85ee974b9859cc
anasseh <abdelhadi.nasseh@manaty.net>
2017-10-20 15:46:23 +0100

    re #3010 - [job] Payment job should not process AO with amount 0

--- opencell-model/src/main/java/org/meveo/model/payments/RecordedInvoice.java
+++ opencell-model/src/main/java/org/meveo/model/payments/RecordedInvoice.java
@@ -41,205 +41,205 @@ import javax.validation.constraints.Size;
 @Entity
 @DiscriminatorValue(value = "I")
 @NamedQueries({
-		@NamedQuery(name = "RecordedInvoice.listRecordedInvoiceIdsToPay", query = "Select ri.id from RecordedInvoice as ri,PaymentMethod as pm  where ri.matchingStatus ='O' and "
-		        + " ri.customerAccount.excludedFromPayment = false and ri.customerAccount.id = pm.customerAccount.id and pm.paymentType =:payMethod  and pm.preferred is true"),
-		@NamedQuery(name = "RecordedInvoice.listRecordedInvoiceToPayByDate", query = "Select ri.id from RecordedInvoice as ri,PaymentMethod as pm  where ri.matchingStatus ='O' "
-                + "and  ri.customerAccount.excludedFromPayment = false and ri.dueDate >=:fromDueDate and ri.dueDate<=:toDueDate and ri.customerAccount.id = pm.customerAccount.id and pm.paymentType =:payMethod  and pm.preferred is true")})
+	@NamedQuery(name = "RecordedInvoice.listRecordedInvoiceIdsToPay", query = "Select ri.id from RecordedInvoice as ri,PaymentMethod as pm  where ri.matchingStatus ='O' and "
+		+ " ri.customerAccount.excludedFromPayment = false and ri.customerAccount.id = pm.customerAccount.id and pm.paymentType =:payMethod  and pm.preferred is true and ri.unMatchingAmount <> 0"),
+	@NamedQuery(name = "RecordedInvoice.listRecordedInvoiceToPayByDate", query = "Select ri.id from RecordedInvoice as ri,PaymentMethod as pm  where ri.matchingStatus ='O' "
+		+ "and  ri.customerAccount.excludedFromPayment = false and ri.dueDate >=:fromDueDate and ri.dueDate<=:toDueDate and ri.customerAccount.id = pm.customerAccount.id and pm.paymentType =:payMethod  and pm.preferred is true and ri.unMatchingAmount <> 0") })
 public class RecordedInvoice extends AccountOperation {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Column(name = "production_date")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date productionDate;
+    @Column(name = "production_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date productionDate;
 
-	@Column(name = "invoice_date")
-	@Temporal(TemporalType.DATE)
-	private Date invoiceDate;
+    @Column(name = "invoice_date")
+    @Temporal(TemporalType.DATE)
+    private Date invoiceDate;
 
-	@Column(name = "amount_without_tax", precision = 23, scale = 12)
-	private BigDecimal amountWithoutTax;
+    @Column(name = "amount_without_tax", precision = 23, scale = 12)
+    private BigDecimal amountWithoutTax;
 
-	@Column(name = "tax_amount", precision = 23, scale = 12)
-	private BigDecimal taxAmount;
+    @Column(name = "tax_amount", precision = 23, scale = 12)
+    private BigDecimal taxAmount;
 
-	@Column(name = "net_to_pay", precision = 23, scale = 12)
-	private BigDecimal netToPay;
+    @Column(name = "net_to_pay", precision = 23, scale = 12)
+    private BigDecimal netToPay;
 
-	@Column(name = "payment_method")
-	@Enumerated(EnumType.STRING)
-	private PaymentMethodEnum paymentMethod;
+    @Column(name = "payment_method")
+    @Enumerated(EnumType.STRING)
+    private PaymentMethodEnum paymentMethod;
 
-	@Column(name = "payment_info", length = 255)
+    @Column(name = "payment_info", length = 255)
     @Size(max = 255)
-	private String paymentInfo;// IBAN for direct debit
+    private String paymentInfo;// IBAN for direct debit
 
-	@Column(name = "payment_info1", length = 255)
+    @Column(name = "payment_info1", length = 255)
     @Size(max = 255)
-	private String paymentInfo1;// bank code
+    private String paymentInfo1;// bank code
 
-	@Column(name = "payment_info2", length = 255)
+    @Column(name = "payment_info2", length = 255)
     @Size(max = 255)
-	private String paymentInfo2;// code guichet
+    private String paymentInfo2;// code guichet
 
-	@Column(name = "payment_info3", length = 255)
+    @Column(name = "payment_info3", length = 255)
     @Size(max = 255)
-	private String paymentInfo3;// Num compte
+    private String paymentInfo3;// Num compte
 
-	@Column(name = "payment_info4", length = 255)
+    @Column(name = "payment_info4", length = 255)
     @Size(max = 255)
-	private String paymentInfo4;// RIB
+    private String paymentInfo4;// RIB
 
-	@Column(name = "payment_info5", length = 255)
+    @Column(name = "payment_info5", length = 255)
     @Size(max = 255)
-	private String paymentInfo5;// bankName
-	
-	@Column(name = "payment_info6", length = 255)
+    private String paymentInfo5;// bankName
+
+    @Column(name = "payment_info6", length = 255)
     @Size(max = 255)
-	private String paymentInfo6;// bic
+    private String paymentInfo6;// bic
 
-	@Column(name = "billing_account_name", length = 255)
+    @Column(name = "billing_account_name", length = 255)
     @Size(max = 255)
-	private String billingAccountName;
-	
-	@OneToMany(mappedBy = "recordedInvoice", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private List<DDRequestItem> ddrequestItems = new ArrayList<DDRequestItem>();
-	
-	public Date getProductionDate() {
-		return productionDate;
-	}
+    private String billingAccountName;
 
-	public void setProductionDate(Date productionDate) {
-		this.productionDate = productionDate;
-	}
+    @OneToMany(mappedBy = "recordedInvoice", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<DDRequestItem> ddrequestItems = new ArrayList<DDRequestItem>();
 
-	public Date getInvoiceDate() {
-		return invoiceDate;
-	}
+    public Date getProductionDate() {
+	return productionDate;
+    }
 
-	public void setInvoiceDate(Date invoiceDate) {
-		this.invoiceDate = invoiceDate;
-	}
+    public void setProductionDate(Date productionDate) {
+	this.productionDate = productionDate;
+    }
 
-	public BigDecimal getAmountWithoutTax() {
-		return amountWithoutTax;
-	}
+    public Date getInvoiceDate() {
+	return invoiceDate;
+    }
 
-	public void setAmountWithoutTax(BigDecimal amountWithoutTax) {
-		this.amountWithoutTax = amountWithoutTax;
-	}
+    public void setInvoiceDate(Date invoiceDate) {
+	this.invoiceDate = invoiceDate;
+    }
 
-	public BigDecimal getTaxAmount() {
-		return taxAmount;
-	}
+    public BigDecimal getAmountWithoutTax() {
+	return amountWithoutTax;
+    }
 
-	public void setTaxAmount(BigDecimal taxAmount) {
-		this.taxAmount = taxAmount;
-	}
+    public void setAmountWithoutTax(BigDecimal amountWithoutTax) {
+	this.amountWithoutTax = amountWithoutTax;
+    }
 
-	public BigDecimal getNetToPay() {
-		return netToPay;
-	}
+    public BigDecimal getTaxAmount() {
+	return taxAmount;
+    }
 
-	public void setNetToPay(BigDecimal netToPay) {
-		this.netToPay = netToPay;
-	}
+    public void setTaxAmount(BigDecimal taxAmount) {
+	this.taxAmount = taxAmount;
+    }
 
-	public PaymentMethodEnum getPaymentMethod() {
-		return paymentMethod;
-	}
+    public BigDecimal getNetToPay() {
+	return netToPay;
+    }
 
-	public void setPaymentMethod(PaymentMethodEnum paymentMethod) {
-		this.paymentMethod = paymentMethod;
-	}
+    public void setNetToPay(BigDecimal netToPay) {
+	this.netToPay = netToPay;
+    }
 
-	public String getPaymentInfo() {
-		return paymentInfo;
-	}
+    public PaymentMethodEnum getPaymentMethod() {
+	return paymentMethod;
+    }
 
-	public void setPaymentInfo(String paymentInfo) {
-		this.paymentInfo = paymentInfo;
-	}
+    public void setPaymentMethod(PaymentMethodEnum paymentMethod) {
+	this.paymentMethod = paymentMethod;
+    }
 
-	public String getPaymentInfo1() {
-		return paymentInfo1;
-	}
+    public String getPaymentInfo() {
+	return paymentInfo;
+    }
 
-	public void setPaymentInfo1(String paymentInfo1) {
-		this.paymentInfo1 = paymentInfo1;
-	}
+    public void setPaymentInfo(String paymentInfo) {
+	this.paymentInfo = paymentInfo;
+    }
 
-	public String getPaymentInfo2() {
-		return paymentInfo2;
-	}
+    public String getPaymentInfo1() {
+	return paymentInfo1;
+    }
 
-	public void setPaymentInfo2(String paymentInfo2) {
-		this.paymentInfo2 = paymentInfo2;
-	}
+    public void setPaymentInfo1(String paymentInfo1) {
+	this.paymentInfo1 = paymentInfo1;
+    }
 
-	public String getPaymentInfo3() {
-		return paymentInfo3;
-	}
+    public String getPaymentInfo2() {
+	return paymentInfo2;
+    }
 
-	public void setPaymentInfo3(String paymentInfo3) {
-		this.paymentInfo3 = paymentInfo3;
-	}
+    public void setPaymentInfo2(String paymentInfo2) {
+	this.paymentInfo2 = paymentInfo2;
+    }
 
-	public String getPaymentInfo4() {
-		return paymentInfo4;
-	}
+    public String getPaymentInfo3() {
+	return paymentInfo3;
+    }
 
-	public void setPaymentInfo4(String paymentInfo4) {
-		this.paymentInfo4 = paymentInfo4;
-	}
+    public void setPaymentInfo3(String paymentInfo3) {
+	this.paymentInfo3 = paymentInfo3;
+    }
 
+    public String getPaymentInfo4() {
+	return paymentInfo4;
+    }
 
-	public void setBillingAccountName(String billingAccountName) {
-		this.billingAccountName = billingAccountName;
-	}
+    public void setPaymentInfo4(String paymentInfo4) {
+	this.paymentInfo4 = paymentInfo4;
+    }
 
-	public String getBillingAccountName() {
-		return billingAccountName;
-	}
+    public void setBillingAccountName(String billingAccountName) {
+	this.billingAccountName = billingAccountName;
+    }
 
-	public void setPaymentInfo5(String paymentInfo5) {
-		this.paymentInfo5 = paymentInfo5;
-	}
+    public String getBillingAccountName() {
+	return billingAccountName;
+    }
 
-	public String getPaymentInfo5() {
-		return paymentInfo5;
-	}
+    public void setPaymentInfo5(String paymentInfo5) {
+	this.paymentInfo5 = paymentInfo5;
+    }
 
-	public String getPaymentInfo6() {
-		return paymentInfo6;
-	}
+    public String getPaymentInfo5() {
+	return paymentInfo5;
+    }
 
-	public void setPaymentInfo6(String paymentInfo6) {
-		this.paymentInfo6 = paymentInfo6;
-	}
+    public String getPaymentInfo6() {
+	return paymentInfo6;
+    }
 
-	/**
-	 * @return the ddrequestItems
-	 */
-	public List<DDRequestItem> getDdrequestItems() {
-		return ddrequestItems;
-	}
+    public void setPaymentInfo6(String paymentInfo6) {
+	this.paymentInfo6 = paymentInfo6;
+    }
 
-	/**
-	 * @param ddrequestItems the ddrequestItems to set
-	 */
-	public void setDdrequestItems(List<DDRequestItem> ddrequestItems) {
-		this.ddrequestItems = ddrequestItems;
-	}
+    /**
+     * @return the ddrequestItems
+     */
+    public List<DDRequestItem> getDdrequestItems() {
+	return ddrequestItems;
+    }
 
-	@Transient
-	public DDRequestItem getPayedDDRequestItem(){
-		for(DDRequestItem ddRequestItem : ddrequestItems){
-			if(!ddRequestItem.hasError()){
-				return ddRequestItem;
-			}
-		}
-		return null;
+    /**
+     * @param ddrequestItems
+     *            the ddrequestItems to set
+     */
+    public void setDdrequestItems(List<DDRequestItem> ddrequestItems) {
+	this.ddrequestItems = ddrequestItems;
+    }
+
+    @Transient
+    public DDRequestItem getPayedDDRequestItem() {
+	for (DDRequestItem ddRequestItem : ddrequestItems) {
+	    if (!ddRequestItem.hasError()) {
+		return ddRequestItem;
+	    }
 	}
-	
+	return null;
+    }
+
 }
