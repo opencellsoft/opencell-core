7dac3c1baaab6cbeabea3a7e86ddcaa4201ac069
anasseh <abdelhadi.nasseh@manaty.net>
2018-05-03 09:17:24 +0100

    re #3420 - [subs] Termination fails with NPE

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/WalletOperationService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/WalletOperationService.java
@@ -1123,12 +1123,9 @@ public class WalletOperationService extends BusinessService<WalletOperation> {
         log.debug("Will apply recurring charge {} for supplement charge agreement for {} - {}", chargeInstance.getId(), applyChargeFromDate, endAgreementDate);
 
         Date applyChargeOnDate = applyChargeFromDate;
-
-        while (applyChargeOnDate.getTime() < endAgreementDate.getTime()) {
-            Date nextChargeDate = cal.nextCalendarDate(applyChargeOnDate);
-            if (nextChargeDate == null) {
-                throw new BusinessException("EndAgreementDate out off calendar");
-            }
+        
+        Date nextChargeDate = null;
+        while (applyChargeOnDate.getTime() < endAgreementDate.getTime() && (nextChargeDate = cal.nextCalendarDate(applyChargeOnDate)) != null ) {
             Double prorataRatio = null;
             ApplicationTypeEnum type = ApplicationTypeEnum.RECURRENT;
             BigDecimal inputQuantity = chargeInstance.getQuantity();
