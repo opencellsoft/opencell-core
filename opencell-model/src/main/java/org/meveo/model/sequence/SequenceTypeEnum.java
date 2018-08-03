6931e6a38c749f0bfacc2bf994d9600300662230
Edward P. Legaspi <czetsuya@gmail.com>
2018-08-03 18:09:55 +0800

    RE: #3575. Implement api that returns a unique sequence. Use for customer number.

---
+++ opencell-model/src/main/java/org/meveo/model/sequence/SequenceTypeEnum.java
@@ -0,0 +1,11 @@
+package org.meveo.model.sequence;
+
+/**
+ * Type of generic sequence.
+ * 
+ * @author Edward P. Legaspi
+ * @lastModifiedVersion 5.2
+ */
+public enum SequenceTypeEnum {
+	RUM, CUSTOMER_NO
+}
