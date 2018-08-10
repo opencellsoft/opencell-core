c1a2b461f58289d7bce92011d3f6b84ca2d1bc16
said ramli <said.ramli@opencellsoft.com>
2018-08-11 00:40:02 +0100

    Test #3540 : adding @RequestScoped + @Interceptors

--- opencell-api/src/main/java/org/meveo/api/rest/document/impl/PDFContractRsImpl.java
+++ opencell-api/src/main/java/org/meveo/api/rest/document/impl/PDFContractRsImpl.java
@@ -1,11 +1,14 @@
 package org.meveo.api.rest.document.impl;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.interceptor.Interceptors;
 
 import org.meveo.api.document.PDFContractApi;
 import org.meveo.api.dto.ActionStatusEnum;
 import org.meveo.api.dto.document.PDFContractRequestDto;
 import org.meveo.api.dto.response.document.PDFContractResponseDto;
+import org.meveo.api.logging.WsRestApiInterceptor;
 import org.meveo.api.rest.document.PDFContractRs;
 import org.meveo.api.rest.impl.BaseRs;
 
@@ -13,6 +16,8 @@ import org.meveo.api.rest.impl.BaseRs;
 /**
  * The Default implementation of PDFContractRs.
  */
+@RequestScoped
+@Interceptors({ WsRestApiInterceptor.class })
 public class PDFContractRsImpl extends BaseRs implements PDFContractRs {
 
     /** The pdf contract api. */
