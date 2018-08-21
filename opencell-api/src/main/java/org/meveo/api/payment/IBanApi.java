18413d5aca335e7bcd3c72e61c4543297e00e7e0
mounir.bahije <mounir.bahije@opencellsoft.com>
2018-08-21 09:54:32 +0100

    ticket-3565-API-service-to-validate-an-IBAN-BIC

--- opencell-api/src/main/java/org/meveo/api/payment/IBanApi.java
+++ opencell-api/src/main/java/org/meveo/api/payment/IBanApi.java
@@ -1,5 +1,6 @@
 package org.meveo.api.payment;
 
+import org.iban4j.BicUtil;
 import org.iban4j.IbanFormat;
 import org.iban4j.IbanUtil;
 import org.meveo.api.BaseApi;
@@ -15,24 +16,74 @@ import javax.ejb.Stateless;
 public class IBanApi extends BaseApi {
 
     /**
-     * Validate the IBAN string
-     * 
+     * Validate the IBAN string and the BIC string
      * @param iban
-     *
+     * @param bic
      */
-    public void validate(String iban) {
+    public boolean validate(String iban, String bic) {
 
-        try {
-            IbanUtil.validate(iban);
-        } catch (Exception e1) {
-           try {
-               IbanUtil.validate(iban, IbanFormat.Default);
-           } catch (Exception e2) {
-               log.error("the string does not have the appropriate format.", e2);
-               throw e2;
-           }
+        boolean result = false ;
+        if ((iban == null) && (bic == null)) {
+            result = false;
+
+        } else {
+            boolean resultIBAN = true;
+            boolean resultBIC = true;
+            if (iban != null) {
+                resultIBAN = validateIBAN(iban);
+            }
+            if (bic != null) {
+                resultBIC = validateBIC(bic);
+            }
+             result = resultBIC && resultIBAN;
+
         }
 
+        return result;
+
     }
+
+    /**
+     * validate IBAN
+     *
+     * @param iban
+     * @return
+     */
+    private boolean validateIBAN(String iban) {
+
+        boolean result = true;
+        try {
+            IbanUtil.validate(iban);
+        }
+        catch (Exception e1) {
+            try {
+                IbanUtil.validate(iban, IbanFormat.Default);
+            } catch (Exception e2) {
+                log.error("the iban string does not have the appropriate format.", e2);
+                result = false;
+            }
+        }
+        return result;
+
+    }
+
+    /**
+     * validate BIC
+     *
+     * @param bic
+     * @return
+     */
+    private boolean validateBIC(String bic) {
+        boolean result = true;
+        try {
+            BicUtil.validate(bic);
+        }
+        catch (Exception e1) {
+                log.error("the bic string does not have the appropriate format.", e1);
+                result = false;
+        }
+        return result;
+    }
+
 
 }
