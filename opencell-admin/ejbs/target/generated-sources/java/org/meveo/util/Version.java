d938f1ec50a74358715d27fb7eec3d84d879bfa4
Houssine ZNIBAR <elhoussine.znibar@opencellsoft.com>
2018-08-31 16:57:24 +0100

    Set the good BIC, IBAN and ICS

---
+++ opencell-admin/ejbs/target/generated-sources/java/org/meveo/util/Version.java
@@ -0,0 +1,13 @@
+package org.meveo.util;
+
+public final class Version {
+
+    public static final String build_time="2018-09-13T09:38:20Z";
+
+    public static final String appVersion="5.1.1";
+    
+    public static final String buildNumber="0c636f3b9bc7f6366c2e894a27216fc6716cde49";
+    
+    
+
+}
