d1180374a963f01eea8114c8cdb480fc56cec6a2
said ramli <said.ramli@opencellsoft.com>
2018-09-08 16:46:39 +0100

    Test #3600 : perf enhancement & fixing null issue for 'invoiceType.getPrefixEL'

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunService.java
@@ -897,26 +897,10 @@ public class BillingRunService extends PersistenceService<BillingRun> {
         if (BillingRunStatusEnum.POSTVALIDATED.equals(billingRun.getStatus())) {
             assignInvoiceNumberAndIncrementBAInvoiceDates(billingRun, nbRuns, waitingMillis, jobInstanceId);
             billingRunExtensionService.updateBillingRun(billingRun.getId(), null, null, BillingRunStatusEnum.VALIDATED, null);
-            this.nullifyInvoiceFileNames(billingRun); // #3600
+            invoiceService.nullifyInvoiceFileNames(billingRun); // #3600
         }
     }
     
-    /**
-     * Nullify invoice file names.
-     *
-     * @param billingRun the billing run
-     * @throws BusinessException 
-     */
-    private void nullifyInvoiceFileNames(BillingRun billingRun) throws BusinessException {
-        List<Invoice> brInvoices = this.invoiceService.getInvoices(billingRun);
-        if (CollectionUtils.isNotEmpty(brInvoices)) {
-            for (Invoice invoice : brInvoices) {
-                invoice.setPdfFilename(null);
-                invoice.setXmlFilename(null);
-            }
-        }
-    }
-
     /**
      * Force validate.
      *
