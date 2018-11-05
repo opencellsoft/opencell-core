e944fbf3ddf55061a516d58276b9f2aa687a941d
Hatim Oudad <hatim.oudad@opencellsoft.com>
2018-11-05 20:32:11 +0100

    Review #3743 : rename categories for jobs

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/AccountOperationsGenerationJob.java
@@ -37,7 +37,7 @@ public class AccountOperationsGenerationJob extends Job {
     
     @Override
     public JobCategoryEnum getJobCategory() {
-        return JobCategoryEnum.ACCOUNT_RECEIVABLES;
+        return JobCategoryEnum.PAYMENT;
     }
     
     
