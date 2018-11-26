e0711e3382ad52eff8ed27f9bf05e86dcd71cd81
Edward P. Legaspi <czetsuya@gmail.com>
2018-11-26 16:22:49 +0800

    RE: #3630. Implement paymentGateway RUM sequence in GUI.

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentGatewayRumSequenceService.java
@@ -0,0 +1,15 @@
+package org.meveo.service.payments.impl;
+
+import javax.ejb.Stateless;
+
+import org.meveo.model.payments.PaymentGatewayRumSequence;
+import org.meveo.service.base.BusinessService;
+
+/**
+ * @author Edward P. Legaspi
+ * @lastModifiedVersion 5.3
+ */
+@Stateless
+public class PaymentGatewayRumSequenceService extends BusinessService<PaymentGatewayRumSequence> {
+
+}
