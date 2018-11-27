a63bbd9b2b3d8e311b71f6f90e9b3e5d094a256c
Edward P. Legaspi <czetsuya@gmail.com>
2018-11-27 22:25:36 +0800

    RE: #3630. Implement paymentGateway RUM sequence API.

--- opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentGatewayRumSequenceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentGatewayRumSequenceService.java
@@ -1,9 +1,11 @@
 package org.meveo.service.payments.impl;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import org.meveo.model.payments.PaymentGatewayRumSequence;
 import org.meveo.service.base.BusinessService;
+import org.meveo.service.billing.impl.ServiceSingleton;
 
 /**
  * @author Edward P. Legaspi
@@ -11,5 +13,12 @@ import org.meveo.service.base.BusinessService;
  */
 @Stateless
 public class PaymentGatewayRumSequenceService extends BusinessService<PaymentGatewayRumSequence> {
+
+	@Inject
+	private ServiceSingleton serviceSingleton;
+	
+	public PaymentGatewayRumSequence getNextNumber(PaymentGatewayRumSequence rumSequence) {
+		return serviceSingleton.getPaymentGatewayRumSequenceNumber(rumSequence);		
+	}
 
 }
