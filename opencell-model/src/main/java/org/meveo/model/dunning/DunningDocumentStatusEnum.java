e584ecf87623184dc71f355fd365180a359e72c9
Mounir BOUKAYOUA <mounir.boukayoua@opencellsoft.com>
2019-01-07 11:34:07 +0100

    Dunning model and IHM

--- opencell-model/src/main/java/org/meveo/model/dunning/DunningDocumentStatusEnum.java
+++ opencell-model/src/main/java/org/meveo/model/dunning/DunningDocumentStatusEnum.java
@@ -19,4 +19,8 @@ public enum DunningDocumentStatusEnum {
     DunningDocumentStatusEnum(String description){
         this.description = description;
     }
+
+    public String getDescription() {
+        return description;
+    }
 }
