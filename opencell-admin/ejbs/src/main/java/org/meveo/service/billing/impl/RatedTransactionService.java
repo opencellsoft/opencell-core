87ab8388a596294e381e824fc949909b753364a5
Edward P. Legaspi <czetsuya@gmail.com>
2019-01-09 16:41:43 +0800

    RE: #3904. Add description to discount aggregate.

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatedTransactionService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatedTransactionService.java
@@ -1051,6 +1051,7 @@ public class RatedTransactionService extends PersistenceService<RatedTransaction
 
             BigDecimal discountAmountWithTax = discountAmount.add(discountAmountTax);
 
+            discountAggregate.setDescription(invoiceSubCat.getDescription());
             discountAggregate.updateAudit(currentUser);
             discountAggregate.setBillingRun(billingRun);
             discountAggregate.setInvoice(invoice);
