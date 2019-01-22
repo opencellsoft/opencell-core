6e1adf3855fa90dc104ef5e2786d50620c987265
Houssine ZNIBAR <elhoussine.znibar@opencellsoft.com>
2019-01-22 13:01:54 +0100

    ticket-3514-General_Ledger

--- opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentVentilationService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentVentilationService.java
@@ -6,8 +6,6 @@ import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import org.meveo.admin.exception.BusinessException;
