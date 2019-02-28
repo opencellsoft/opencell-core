afeaf22405e44ea12170022c2a65a294b301e320
mounir.bahije <mounir.bahije@opencellsoft.com>
2019-02-28 19:27:30 +0100

    Test #3975 : Charges with periodic calendar activated at end of month

--- opencell-model/src/main/java/org/meveo/model/catalog/CalendarPeriod.java
+++ opencell-model/src/main/java/org/meveo/model/catalog/CalendarPeriod.java
@@ -28,6 +28,13 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Transient;
 
+/**
+ * The CalendarPeriod class.
+ *
+ * @author Andrius Karpavicius
+ * @author Mounir Bahije
+ * @lastModifiedVersion 5.0
+ */
 @Entity
 @DiscriminatorValue("PERIOD")
 public class CalendarPeriod extends Calendar {
@@ -109,15 +116,18 @@ public class CalendarPeriod extends Calendar {
         // calendar.setTime(cleanDate);
 
         GregorianCalendar calendar = new GregorianCalendar();
+        GregorianCalendar calendarToReturn = new GregorianCalendar();
         calendar.setTime(getInitDate());
+        calendarToReturn.setTime(getInitDate());
 
         int i = 1;
         while (date.compareTo(calendar.getTime()) >= 0) {
             Date oldDate = calendar.getTime();
             calendar.add(periodUnit, periodLength);
             if (date.compareTo(oldDate) >= 0 && date.compareTo(calendar.getTime()) < 0) {
-                truncateDateTime(calendar);
-                return calendar.getTime();
+                calendarToReturn.add(periodUnit, i);
+                truncateDateTime(calendarToReturn);
+                return calendarToReturn.getTime();
             }
 
             i++;
