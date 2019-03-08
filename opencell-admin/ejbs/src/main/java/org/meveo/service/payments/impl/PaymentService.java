90e52764dc650321b7159d7a12d9e378764a68b2
Khalid HORRI <khalid.horri@opencellsoft.com>
2019-03-08 11:02:33 +0100

    Review code #4057 create and display payment history for the API payment/history/list

--- opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentService.java
@@ -22,6 +22,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -106,6 +108,25 @@ public class PaymentService extends PersistenceService<Payment> {
     @Override
     public void create(Payment entity) throws BusinessException {
         super.create(entity);
+        if (entity.getId() != null && entity.getPaymentMethod().isSimple()) {
+            PaymentMethod paymentMethod  = getPaymentMathod(entity);
+            paymentHistoryService.addHistory(entity.getCustomerAccount(), entity, null, entity.getAmount().multiply(new BigDecimal(100)).longValue(),PaymentStatusEnum.ACCEPTED,null,null,null,OperationCategoryEnum.CREDIT,null,paymentMethod);
+        }
+    }
+
+    /**
+     * Return the a Method payment corresponds to method payment type
+     * @param payment the payment
+     * @return A method payment.
+     */
+    private PaymentMethod getPaymentMathod(Payment payment) {
+        if (payment == null || payment.getCustomerAccount() == null || payment.getCustomerAccount().getPaymentMethods() == null ) {
+            return null;
+        }
+        List<PaymentMethod> paymentMethods = payment.getCustomerAccount().getPaymentMethods().stream().filter(paymentMethod -> {
+            return paymentMethod.getPaymentType().equals(payment.getPaymentMethod());
+        }).collect(Collectors.toList());
+        return (paymentMethods != null && !paymentMethods.isEmpty()) ? paymentMethods.get(0) : null;
     }
 
     /**
@@ -522,7 +543,7 @@ public class PaymentService extends PersistenceService<Payment> {
                 }
                 CustomerAccount ca = accountOperation.getCustomerAccount();
                 List<AccountOperation> listAoThatSupposedPaid = getAccountOperationThatWasPaid(accountOperation);
-                
+
                 Long aoPaymentIdWasRejected = accountOperation.getId();
 
                 matchingCodeService.unmatchingByAOid(aoPaymentIdWasRejected);
@@ -587,7 +608,7 @@ public class PaymentService extends PersistenceService<Payment> {
     public  List<AccountOperation> getAccountOperationThatWasPaid(AccountOperation paymentOrRefund) throws BusinessException {
         if (paymentOrRefund.getMatchingStatus() != MatchingStatusEnum.L) {
             return null;
-        }       
+        }
         List<AccountOperation> listAoThatSupposedPaid = new ArrayList<AccountOperation>();
         List<MatchingAmount> matchingAmounts = paymentOrRefund.getMatchingAmounts();
         log.trace("matchingAmounts:" + matchingAmounts);
