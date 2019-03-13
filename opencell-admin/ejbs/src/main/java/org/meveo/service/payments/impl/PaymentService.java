26b4672edf3168622bb28672b4616e34e6675bb8
Khalid HORRI <khalid.horri@opencellsoft.com>
2019-03-13 16:14:16 +0100

    Review #4057 correct CheckStyle

--- opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/PaymentService.java
@@ -113,8 +113,8 @@ public class PaymentService extends PersistenceService<Payment> {
     public void create(Payment entity) throws BusinessException {
         super.create(entity);
         if (entity.getId() != null && entity.getPaymentMethod().isSimple()) {
-            PaymentMethod paymentMethod  = getPaymentMathod(entity);
-            paymentHistoryService.addHistory(entity.getCustomerAccount(), entity, null, entity.getAmount().multiply(new BigDecimal(100)).longValue(),PaymentStatusEnum.ACCEPTED,null,null,null,OperationCategoryEnum.CREDIT,null,paymentMethod);
+            PaymentMethod paymentMethod  = getPaymentMethod(entity);
+            paymentHistoryService.addHistory(entity.getCustomerAccount(), entity, null, entity.getAmount().multiply(new BigDecimal(100)).longValue(), PaymentStatusEnum.ACCEPTED, null, null, null, OperationCategoryEnum.CREDIT, null, paymentMethod);
         }
     }
 
@@ -123,7 +123,7 @@ public class PaymentService extends PersistenceService<Payment> {
      * @param payment the payment
      * @return A method payment.
      */
-    private PaymentMethod getPaymentMathod(Payment payment) {
+    private PaymentMethod getPaymentMethod(Payment payment) {
         if (payment == null || payment.getCustomerAccount() == null || payment.getCustomerAccount().getPaymentMethods() == null ) {
             return null;
         }
