7c5de3c725ed131b987ff20cb6d959a2bc441ac2
Tien Lan PHUNG <tien_lan.phung@opencellsoft.com>
2019-03-21 17:41:44 +0100

    fix compilation

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/UsageRatingJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/UsageRatingJob.java
@@ -69,7 +69,7 @@ public class UsageRatingJob extends Job {
         rateUntilDate.setDescription(resourceMessages.getString("jobExecution.rateUntilDate"));
         rateUntilDate.setFieldType(CustomFieldTypeEnum.DATE);
         rateUntilDate.setValueRequired(false);
-        billingCycle.setMaxValue(50L);
+        rateUntilDate.setMaxValue(50L);
         result.put("rateUntilDate", rateUntilDate);
         
         CustomFieldTemplate ratingGroup = new CustomFieldTemplate();
