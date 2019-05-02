d64018bd07a9fc01eca025d67c7eb16f095485c5
said ramli <said.ramli@opencellsoft.com>
2019-05-02 09:17:45 +0100

    Test #4158 : don't override CFs with default values

--- opencell-api/src/main/java/org/meveo/api/job/JobInstanceApi.java
+++ opencell-api/src/main/java/org/meveo/api/job/JobInstanceApi.java
@@ -224,8 +224,6 @@ public class JobInstanceApi extends BaseCrudApi<JobInstance, JobInstanceDto> {
             throw new EntityDoesNotExistsException(JobInstance.class, code);
         }
         
-        customFieldInstanceService.instantiateCFWithDefaultValue(jobInstance);
-        
         JobInstanceDto jobInstanceDto = new JobInstanceDto(jobInstance, entityToDtoConverter.getCustomFieldsDTO(jobInstance, CustomFieldInheritanceEnum.INHERIT_NONE));
         return jobInstanceDto;
     }
