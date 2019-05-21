c203e1a1c07933458364966c91b806f93e24d2ad
Khalid HORRI <khalid.horri@opencellsoft.com>
2019-05-21 10:39:23 +0000

    Re # 4192 associate counter with subscription and service instance for 7.X

--- opencell-model/src/main/java/org/meveo/model/billing/Subscription.java
+++ opencell-model/src/main/java/org/meveo/model/billing/Subscription.java
@@ -58,6 +58,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.MapKey;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
@@ -74,6 +75,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Subscription
