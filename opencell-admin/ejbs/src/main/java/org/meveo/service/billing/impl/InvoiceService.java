bef266bca6807ae7897b649f25f05d4e96a772d2
Khalid HORRI <khalid.horri@opencellsoft.com>
2019-05-22 08:31:59 +0000

    Reveiw code #4279 exclude duplicates RT from Default XML invoice when discount is applied

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -2250,7 +2250,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
     public List<SubCategoryInvoiceAgregate> listByInvoice(Invoice invoice) {
         QueryBuilder qb = new QueryBuilder(SubCategoryInvoiceAgregate.class, "c");
         qb.addCriterionEntity("invoice", invoice);
-
+        qb.addBooleanCriterion("discountAggregate", false);
         try {
             List<SubCategoryInvoiceAgregate> resultList = (List<SubCategoryInvoiceAgregate>) qb.getQuery(getEntityManager()).getResultList();
             return resultList;
