d8f5f841ff4cd4ae5f764d69bd75e990cda9c44f
Andrius Karpavicius <exploresouthamerica@gmail.com>
2019-05-27 13:18:20 -0300

    Re #4199 Set nbRuns =-1 to missing job definitions.

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/FilteringJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/FilteringJob.java
@@ -54,7 +54,7 @@ public class FilteringJob extends Job {
         nbRuns.setDescription(resourceMessages.getString("jobExecution.nbRuns"));
         nbRuns.setFieldType(CustomFieldTypeEnum.LONG);
         nbRuns.setValueRequired(false);
-        nbRuns.setDefaultValue("1");
+        nbRuns.setDefaultValue("-1");
         result.put("nbRuns", nbRuns);
 
         CustomFieldTemplate waitingMillis = new CustomFieldTemplate();
