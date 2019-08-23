405c4fc796d3003e8962eab89b39323a9dfb6d3e
abdelkader.bouazza <abdelkader.bouazza@opencellsoft.com>
2019-08-23 16:04:59 +0100

    RE #4498 : improve error handling with more expressive message, prevent calculated fields from preventing update, handel enums in filters

--- opencell-api/src/main/java/org/meveo/apiv2/exception/UnhandledExceptionMapper.java
+++ opencell-api/src/main/java/org/meveo/apiv2/exception/UnhandledExceptionMapper.java
@@ -14,25 +14,13 @@ import java.util.stream.Collectors;
 
 public class UnhandledExceptionMapper implements ExceptionMapper<Exception> {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final ExceptionSerializer exceptionSerializer = new ExceptionSerializer("500");
 
     @Override
     public Response toResponse(Exception exception) {
         log.error("An unhandled exception occurred ", exception);
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(toApiError(exception))
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(exceptionSerializer.toApiError(exception))
                 .type(MediaType.APPLICATION_JSON).header(Validation.VALIDATION_HEADER, "true")
                 .build();
-    }
-
-    private ApiException toApiError(Exception exception) {
-        return ImmutableApiException.builder()
-                .code("500")
-                .details(exception.getMessage() != null ? exception.getMessage() : getStackTrace(exception.getStackTrace()))
-                .build();
-    }
-
-    private String getStackTrace(StackTraceElement[] stackTrace) {
-        return Arrays.stream(stackTrace)
-                .map(StackTraceElement::toString)
-                .collect(Collectors.joining("\n"));
     }
 }
