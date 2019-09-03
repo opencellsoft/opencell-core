ef9ef20d8d0fdd996c1223adaab535cf309d9991
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2019-09-03 11:51:17 +0100

    Test #4403 Fixing import

--- opencell-api-dto/src/main/java/org/meveo/api/dto/RatedTransactionDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/RatedTransactionDto.java
@@ -27,8 +27,8 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-import org.meveo.model.billing.BillingAccount;
 import org.meveo.model.billing.RatedTransaction;
+import org.meveo.model.billing.RatedTransactionStatusEnum;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
