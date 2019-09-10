6e589dfbf8f00aeb35ebf269d7c7d4aca3110b3c
Abdellatif BARI <abdellatif.bari@opencellsoft.com>
2019-09-10 13:09:53 +0100

    Reveiw #4539 : Business exception not displayed in InboundServlet response

--- opencell-admin/web/src/main/java/org/meveo/admin/action/notification/InboundServlet.java
+++ opencell-admin/web/src/main/java/org/meveo/admin/action/notification/InboundServlet.java
@@ -130,7 +130,7 @@ public class InboundServlet extends HttpServlet {
                 }
             } catch (BusinessException be) {
                 log.error("Failed when processing the notifications", be);
-                status = HttpURLConnection.HTTP_NOT_FOUND;
+                status = HttpURLConnection.HTTP_INTERNAL_ERROR;
             }
 
             log.debug("triggered {} notification, resp body= {}", inReq.getNotificationHistories().size(), inReq.getResponseBody());
