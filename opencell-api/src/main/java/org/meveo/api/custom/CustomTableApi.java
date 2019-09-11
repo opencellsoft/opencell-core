2f00c579ecd91ca778e613c1a1ea28fe832550d3
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2019-09-11 13:44:51 +0100

    RE #4516 Misspelled property overwrite in CustomTableDataDto and UnitaryCustomTableDataDto

--- opencell-api/src/main/java/org/meveo/api/custom/CustomTableApi.java
+++ opencell-api/src/main/java/org/meveo/api/custom/CustomTableApi.java
@@ -75,8 +75,8 @@ public class CustomTableApi extends BaseApi {
 
         handleMissingParameters();
 
-        if (dto.getOverrwrite() == null) {
-            dto.setOverrwrite(false);
+        if (dto.getOverwrite() == null) {
+            dto.setOverwrite(false);
         }
 
         CustomEntityTemplate cet = customEntityTemplateService.findByCode(dto.getCustomTableCode());
@@ -90,7 +90,7 @@ public class CustomTableApi extends BaseApi {
             values.add(record.getValues());
         }
 
-        customTableService.importData(cet, values, !dto.getOverrwrite());
+        customTableService.importData(cet, values, !dto.getOverwrite());
 
     }
 
