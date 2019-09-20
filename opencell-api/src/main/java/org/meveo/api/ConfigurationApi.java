7552d1fa6deb8b8998bf982689b042b403513870
Mohamed STITANE <mohamed.stitane@opencellsoft.com>
2019-09-20 16:49:17 +0100

    adding missing roles to allow the use of the method

--- opencell-api/src/main/java/org/meveo/api/ConfigurationApi.java
+++ opencell-api/src/main/java/org/meveo/api/ConfigurationApi.java
@@ -6,16 +6,14 @@ import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 
-import org.meveo.api.dto.response.GetConfigurationResponse;
-import org.meveo.commons.utils.ParamBean;
-
 import com.google.gson.Gson;
+
+import org.meveo.commons.utils.ParamBean;
 
 /**
  * @author Wassim Drira
  * @author Edward P. Legaspi
  * @lastModifiedVersion 5.2
- *
  */
 @Stateless
 @DeclareRoles(value = { "superAdministrateur" })
@@ -23,28 +21,29 @@ public class ConfigurationApi extends BaseApi {
 
     /**
      * Set configuration/settings property
-     * 
+     *
      * @param property Property key
-     * @param value Property value as string
+     * @param value    Property value as string
      */
+    @RolesAllowed(value = { "superAdministrateur" })
     public void setProperty(String property, String value) {
         ParamBean paramBean = paramBeanFactory.getInstance();
         paramBean.setProperty(property, value);
         paramBean.saveProperties();
     }
-    
-	@RolesAllowed(value = { "superAdministrateur" })
-	public String getPropertiesAsJsonString() {
-		ParamBean paramBean = paramBeanFactory.getInstance();
-		Properties props = paramBean.getProperties();
-		Gson gsonObj = new Gson();
-		return gsonObj.toJson(props);
-	}
 
-	@RolesAllowed(value = { "superAdministrateur" })
-	public Properties getProperties() {
-		ParamBean paramBean = paramBeanFactory.getInstance();
-		return paramBean.getProperties();
-	}
+    @RolesAllowed(value = { "superAdministrateur" })
+    public String getPropertiesAsJsonString() {
+        ParamBean paramBean = paramBeanFactory.getInstance();
+        Properties props = paramBean.getProperties();
+        Gson gsonObj = new Gson();
+        return gsonObj.toJson(props);
+    }
+
+    @RolesAllowed(value = { "superAdministrateur" })
+    public Properties getProperties() {
+        ParamBean paramBean = paramBeanFactory.getInstance();
+        return paramBean.getProperties();
+    }
 
 }
