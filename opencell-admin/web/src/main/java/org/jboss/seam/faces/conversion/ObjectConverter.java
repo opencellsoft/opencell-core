980276ee073f3b0de9df20eb81644a8275352127
Mohamed STITANE <mohamed.stitane@opencellsoft.com>
2019-09-25 21:47:45 +0100

    migrating from java 8 to java 11

--- opencell-admin/web/src/main/java/org/jboss/seam/faces/conversion/ObjectConverter.java
+++ opencell-admin/web/src/main/java/org/jboss/seam/faces/conversion/ObjectConverter.java
@@ -53,7 +53,7 @@ public class ObjectConverter implements javax.faces.convert.Converter, Serializa
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        if (this.conversation.isTransient()) {
+        if (this.conversation != null && this.conversation.isTransient()) {
             log.warn("Conversion attempted without a long running conversation");
         }
 
