ba0d696784fb38898ec9b66779f0fc477b03844e
Mohamed STITANE <mohamed.stitane@opencellsoft.com>
2019-11-18 20:25:46 +0100

    #4793 - Fix sonar blocker issues

--- opencell-api/src/main/java/org/meveo/api/MdSwaggerModification.java
+++ opencell-api/src/main/java/org/meveo/api/MdSwaggerModification.java
@@ -8,54 +8,44 @@ public class MdSwaggerModification {
     public static void main(String[] args) throws Exception {
         String parentpath = System.getProperty("user.dir");
         parentpath=parentpath+File.separator+"opencell-api";
-        FileReader fr = null, md = null;
-        BufferedReader lnr = null;
         String filePath = parentpath+File.separator+"target"+File.separator+"doc"+File.separator+"swagger"+File.separator+"swagger.yaml";
-        String fileTempPath=filePath.replace(".yaml","Md.yaml"), str = "", tmp = "";
-        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileTempPath)));
-        try {
-            fr = new FileReader(filePath);
-            lnr = new BufferedReader(fr);
-            while ((str = lnr.readLine()) != null) {
-                if (str.contains("@#")) {/*This is for the tag balise it have some a different type of identation compare to the introduction*/
-                    str = str.replaceAll("'", "");
-                    String[] tmp2 = str.split("(@#)");
-                    tmp = mdLineReturn(tmp2[1],parentpath);
-                    str=str.substring(0,str.indexOf(":"))+":"+ " |\n"+tmp+"\n  " +
-                            "x-logo:\n" +
-                            "    url: https://opencellsoft.com/img/logo-opencell-red.png\n" +
-                            "    href: https://opencellsoft.com/\n" +
-                            "    altText: \"Opencell Logo\"";
-                }/*This is for the introduction balise it have some a different type of identation compare to the other one*/
-                else if(str.contains("@%")){
-                    str = str.replaceAll("'", "");
-                    String[] tmp2 = str.split("(@%)");
-                    tmp = mdLineReturnTag(tmp2[1],parentpath);
-                    str=str.substring(0,str.indexOf(":"))+":"+ " |\n"+tmp+"";
+        String fileTempPath = filePath.replace(".yaml", "Md.yaml"), str = "", tmp = "";
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileTempPath)))) {
+            try {
+                try (BufferedReader lnr = new BufferedReader(new FileReader(filePath))) {
+                    while ((str = lnr.readLine()) != null) {
+                        if (str.contains("@#")) {/*This is for the tag balise it have some a different type of identation compare to the introduction*/
+                            str = str.replace("'", "");
+                            String[] tmp2 = str.split("(@#)");
+                            tmp = mdLineReturn(tmp2[1], parentpath);
+                            str = str.substring(0, str.indexOf(":")) + ":" + " |\n" + tmp + "\n  " + "x-logo:\n" + "    url: https://opencellsoft.com/img/logo-opencell-red.png\n"
+                                    + "    href: https://opencellsoft.com/\n" + "    altText: \"Opencell Logo\"";
+                        }/*This is for the introduction balise it have some a different type of identation compare to the other one*/ else if (str.contains("@%")) {
+                            str = str.replace("'", "");
+                            String[] tmp2 = str.split("(@%)");
+                            tmp = mdLineReturnTag(tmp2[1], parentpath);
+                            str = str.substring(0, str.indexOf(":")) + ":" + " |\n" + tmp + "";
 
+                        }
+                        writer.println(str);
+                    }
                 }
-                writer.println(str);
+            } catch (Exception e) {
+                System.out.println("File not found");
+                e.printStackTrace();
             }
-            writer.close();
-            lnr.close();
-        } catch (Exception e) {
-            System.out.println("File not found");
-            e.printStackTrace();
         }
     }
 
     public static String mdLineReturnTag(String nameFile,String parentpath) throws IOException {
-        FileReader md = null;
-        BufferedReader lnr = null;
         String filePath = parentpath+File.separator+"src"+File.separator+"test"+File.separator+"resources" +File.separator+"md" +File.separator+ nameFile + ".md";
         String returnline = "", str = "";
         try {
-            md = new FileReader(filePath);
-            lnr = new BufferedReader(md);
-            while ((str = lnr.readLine()) != null) {
-                    returnline = returnline + "      " + str + "  \n";
+            try (BufferedReader lnr = new BufferedReader(new FileReader(filePath))) {
+                while ((str = lnr.readLine()) != null) {
+                        returnline = returnline + "      " + str + "  \n";
+                }
             }
-            lnr.close();
 
         } catch (FileNotFoundException e1) {
             //e1.printStackTrace();
@@ -68,17 +58,14 @@ public class MdSwaggerModification {
         return returnline;
     }
     public static String mdLineReturn(String nameFile,String parentpath) throws IOException {
-        FileReader md = null;
-        BufferedReader lnr = null;
         String filePath = parentpath+File.separator+"src"+File.separator+"test"+File.separator+"resources" +File.separator+"md" +File.separator+ nameFile + ".md";
         String returnline = "", str = "";
         try {
-            md = new FileReader(filePath);
-            lnr = new BufferedReader(md);
-            while ((str = lnr.readLine()) != null) {
-                        returnline = returnline + "    " + str + "  \n";
+            try (BufferedReader lnr = new BufferedReader(new FileReader(filePath))) {
+                while ((str = lnr.readLine()) != null) {
+                    returnline = returnline + "    " + str + "  \n";
+                }
             }
-            lnr.close();
 
         } catch (FileNotFoundException e1) {
             //e1.printStackTrace();
