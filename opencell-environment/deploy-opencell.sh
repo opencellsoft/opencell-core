84e0c65a58c479d119d39401b758d4b3bc326262
Antoine Michéa <antoine.michea@opencellsoft.com>
2019-12-06 16:53:30 +0100

    RE #4753 update deploy script

--- opencell-environment/deploy-opencell.sh
+++ opencell-environment/deploy-opencell.sh
@@ -57,7 +57,7 @@ echo ">>> Waiting opencell is ready, don't matter about 404 errors"
 export CONNEXION_TIMEOUT=${CONNEXION_TIMEOUT:-120}
 sleep 5
 i=0;
-until [ "`docker inspect -f {{.State.Health.Status}} opencell-${TENANT:-demo}`"=="healthy" ]; do
+until [ "$(docker inspect --format '{{json .State.Health.Status }}' opencell-${TENANT:-demo})" == "\"healthy\"" ]; do
   echo "$i not yet up"
   sleep 1
   if [ "${i}" -gt "${CONNEXION_TIMEOUT}"  ]; then
