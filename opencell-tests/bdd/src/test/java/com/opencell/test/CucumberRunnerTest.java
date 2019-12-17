3c85d003d424bb5311492084ab9eb79145e853bd
franckvalot <franck.valot@opencellsoft.com>
2019-12-17 15:49:53 +0700

    Test urls

--- opencell-tests/bdd/src/test/java/com/opencell/test/CucumberRunnerTest.java
+++ opencell-tests/bdd/src/test/java/com/opencell/test/CucumberRunnerTest.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty","json:target/cucumber-reports/Cucumber.json",
-        "junit:target/cucumber-reports/Cucumber.xml"}, tags = {"@accounts"})
+        "junit:target/cucumber-reports/Cucumber.xml"}, tags = {"@url or @accounts"})
 public class CucumberRunnerTest {
 
 }
