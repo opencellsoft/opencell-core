2ad592fdad27ed77f9eebd4c20399e0f6a72dff0
Mohamed STITANE <mohamed.stitane@opencellsoft.com>
2020-01-10 10:17:56 +0100

    #4865 - fix NPE when old service has no CFs

--- opencell-api/src/main/java/org/meveo/api/catalog/BusinessOfferApi.java
+++ opencell-api/src/main/java/org/meveo/api/catalog/BusinessOfferApi.java
@@ -154,8 +154,10 @@ public class BusinessOfferApi extends BaseApi {
                     CustomFieldValues customFieldValues = oldService.getCfValuesNullSafe();
                     Map<String, List<CustomFieldValue>> cfValues = customFieldValues.getValuesByCode();
 
-                    CustomFieldsDto cfs = entityToDtoConverter.getCustomFieldsDTO(oldService, cfValues, CustomFieldInheritanceEnum.INHERIT_NONE);
-                    serviceConfigurationDto.setCustomFields(cfs.getCustomField());
+                    if (!cfValues.isEmpty()) {
+                        CustomFieldsDto cfs = entityToDtoConverter.getCustomFieldsDTO(oldService, cfValues, CustomFieldInheritanceEnum.INHERIT_NONE);
+                        serviceConfigurationDto.setCustomFields(cfs.getCustomField());
+                    }
                 }
 
                 if (serviceTemplateCode.equals(serviceTemplate.getCode()) && serviceConfigurationDto.getCustomFields() != null && !serviceConfigurationDto.isMatch()) {
