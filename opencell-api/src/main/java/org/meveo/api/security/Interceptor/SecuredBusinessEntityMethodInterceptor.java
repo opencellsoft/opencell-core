afdb3b35e278095edad0985a545aa650ea5c6c91
Mohamed STITANE <mohamed.stitane@opencellsoft.com>
2020-02-05 19:02:57 +0100

    RE #4812 fix the total count when used secured entities on 8.X

--- opencell-api/src/main/java/org/meveo/api/security/Interceptor/SecuredBusinessEntityMethodInterceptor.java
+++ opencell-api/src/main/java/org/meveo/api/security/Interceptor/SecuredBusinessEntityMethodInterceptor.java
@@ -1,18 +1,25 @@
 package org.meveo.api.security.Interceptor;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
+import org.meveo.api.dto.response.PagingAndFiltering;
 import org.meveo.api.exception.AccessDeniedException;
 import org.meveo.api.security.filter.SecureMethodResultFilter;
 import org.meveo.api.security.filter.SecureMethodResultFilterFactory;
 import org.meveo.api.security.parameter.SecureMethodParameter;
 import org.meveo.api.security.parameter.SecureMethodParameterHandler;
 import org.meveo.commons.utils.ParamBeanFactory;
+import org.meveo.commons.utils.StringUtils;
 import org.meveo.model.BusinessEntity;
+import org.meveo.model.admin.SecuredEntity;
 import org.meveo.model.admin.User;
 import org.meveo.security.CurrentUser;
 import org.meveo.security.MeveoUser;
@@ -22,13 +29,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 
  * This will handle the processing of {@link SecuredBusinessEntityMethod} annotated methods.
  * 
  * @author Tony Alejandro
  * @author Wassim Drira
+ * @author mohamed stitane
  * @lastModifiedVersion 5.0
- *
  */
 public class SecuredBusinessEntityMethodInterceptor implements Serializable {
 
@@ -52,7 +58,9 @@ public class SecuredBusinessEntityMethodInterceptor implements Serializable {
     @Inject
     private UserService userService;
 
-    /** paramBean Factory allows to get application scope paramBean or provider specific paramBean */
+    /**
+     * paramBean Factory allows to get application scope paramBean or provider specific paramBean
+     */
     @Inject
     private ParamBeanFactory paramBeanFactory;
 
@@ -100,6 +108,8 @@ public class SecuredBusinessEntityMethodInterceptor implements Serializable {
 
         Object[] values = context.getParameters();
 
+        addSecuredEntitiesToFilters(user.getSecuredEntities(), values);
+
         SecureMethodParameter[] parametersForValidation = annotation.validate();
         for (SecureMethodParameter parameter : parametersForValidation) {
             BusinessEntity entity = parameterHandler.getParameterValue(parameter, values, BusinessEntity.class);
@@ -122,5 +132,62 @@ public class SecuredBusinessEntityMethodInterceptor implements Serializable {
         result = filter.filterResult(context.getMethod(), result, currentUser, user);
         return result;
 
+    }
+
+    /**
+     * Adding a secured entities code to the filters for paging
+     *
+     * @param securedEntities all secured entities
+     * @param values          the context parameter
+     */
+    private void addSecuredEntitiesToFilters(List<SecuredEntity> securedEntities, Object[] values) {
+        log.debug("Adding a secured entities code to the filters for paging");
+        for (Object obj : values) {
+            if (obj instanceof PagingAndFiltering) {
+                PagingAndFiltering pagingAndFiltering = (PagingAndFiltering) obj;
+                updateFilters(securedEntities, pagingAndFiltering);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Adding a secured entities code to the filters
+     *
+     * @param securedEntities    a secured entities
+     * @param pagingAndFiltering a paging and filtering object
+     */
+    private void updateFilters(List<SecuredEntity> securedEntities, PagingAndFiltering pagingAndFiltering) {
+        if (isNotNull(pagingAndFiltering)) {
+            Map<String, Object> filters = Optional.ofNullable(pagingAndFiltering.getFilters()).orElse(new HashMap<>());
+            for (SecuredEntity securedEntity : securedEntities) {
+                final String entityClass = securedEntity.getEntityClass();
+                //extract the field name from entity class, I supposed that the field name is the same as the Class name.
+                final String fieldName = entityClass.substring(entityClass.lastIndexOf('.') + 1).toLowerCase();
+                log.debug("Code = {} for entity = {}", securedEntity.getCode(), fieldName);
+                final String keyInList = "inList " + fieldName + ".code";
+                if (filters.containsKey(fieldName)) {
+                    final Object initialValue = filters.get(fieldName);
+                    filters.put(keyInList, StringUtils.concat(initialValue, ",", securedEntity.getCode()));
+                    filters.remove(fieldName);
+                } else if (filters.containsKey(keyInList)) {
+                    final Object initialList = filters.get(keyInList);
+                    filters.replace(keyInList, StringUtils.concat(initialList, ",", securedEntity.getCode()));
+                } else {
+                    filters.put(fieldName, securedEntity.getCode());
+                }
+            }
+            pagingAndFiltering.setFilters(filters);
+        }
+    }
+
+    /**
+     * check if the object is null
+     *
+     * @param pagingAndFiltering a paging and filtering object
+     * @return true or false
+     */
+    private boolean isNotNull(PagingAndFiltering pagingAndFiltering) {
+        return pagingAndFiltering != null && pagingAndFiltering.getLimit() != null && pagingAndFiltering.getOffset() != null;
     }
 }
