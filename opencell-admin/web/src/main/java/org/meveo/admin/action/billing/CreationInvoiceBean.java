d9ea4d83c15abdd3a810866166637c0d9218f94c
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2020-02-28 09:20:29 +0100

    RE #5010 Impossible to update unitAmountWithoutTax on Detailled invoice create screen

--- opencell-admin/web/src/main/java/org/meveo/admin/action/billing/CreationInvoiceBean.java
+++ opencell-admin/web/src/main/java/org/meveo/admin/action/billing/CreationInvoiceBean.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
