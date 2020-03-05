1bb2650c3bebb1009962087a41c68eccd9742e27
Andrius Karpavicius <andrius.karpavicius@opencellsoft.com>
2020-03-05 10:44:33 -0300

    Re #4892 Added rating script to charge template GUI

--- opencell-admin/web/src/main/java/org/meveo/admin/action/catalog/UsageChargeTemplateBean.java
+++ opencell-admin/web/src/main/java/org/meveo/admin/action/catalog/UsageChargeTemplateBean.java
@@ -45,131 +45,124 @@ import org.primefaces.model.DualListModel;
 @Named
 @ViewScoped
 public class UsageChargeTemplateBean extends CustomFieldBean<UsageChargeTemplate> {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Inject
-	private UsageChargeTemplateService usageChargeTemplateService;
+    @Inject
+    private UsageChargeTemplateService usageChargeTemplateService;
 
-	@Inject
-	private RecurringChargeTemplateService recurringChargeTemplateService;
+    @Inject
+    private RecurringChargeTemplateService recurringChargeTemplateService;
 
-	@Inject
-	private OneShotChargeTemplateService oneShotChargeTemplateService;
+    @Inject
+    private OneShotChargeTemplateService oneShotChargeTemplateService;
 
-	@Inject
-	private TriggeredEDRTemplateService triggeredEDRTemplateService;
-	
-	@Inject
+    @Inject
+    private TriggeredEDRTemplateService triggeredEDRTemplateService;
+
+    @Inject
     protected CustomFieldInstanceService customFieldInstanceService;
-	
-	@Inject
+
+    @Inject
     protected ProductChargeTemplateService productChargeTemplateService;
 
-	private DualListModel<TriggeredEDRTemplate> edrTemplates;
-	
-	/**
-	 * Constructor. Invokes super constructor and provides class type of this
-	 * bean for {@link BaseBean}.
-	 */
-	public UsageChargeTemplateBean() {
-		super(UsageChargeTemplate.class);
-	}
+    private DualListModel<TriggeredEDRTemplate> edrTemplates;
 
-	@Override
-	protected List<String> getFormFieldsToFetch() {
-		return Arrays.asList("edrTemplates");
-	}
+    /**
+     * Constructor. Invokes super constructor and provides class type of this bean for {@link BaseBean}.
+     */
+    public UsageChargeTemplateBean() {
+        super(UsageChargeTemplate.class);
+    }
 
-	@Override
-	public void search() {
-		getFilters();
-		if (!filters.containsKey("disabled")) {
-			filters.put("disabled", false);
-		}
-		super.search();
-	}
+    @Override
+    protected List<String> getFormFieldsToFetch() {
+        return Arrays.asList("edrTemplates");
+    }
 
-	/**
-	 * Conversation is ended and user is redirected from edit to his previous
-	 * window.
-	 * 
-	 * @throws BusinessException General business exception
-	 * 
-	 * @see org.meveo.admin.action.BaseBean#saveOrUpdate(org.meveo.model.IEntity)
-	 */
-	@Override
+    @Override
+    public void search() {
+        getFilters();
+        if (!filters.containsKey("disabled")) {
+            filters.put("disabled", false);
+        }
+        super.search();
+    }
+
+    /**
+     * Conversation is ended and user is redirected from edit to his previous window.
+     * 
+     * @throws BusinessException General business exception
+     * 
+     * @see org.meveo.admin.action.BaseBean#saveOrUpdate(org.meveo.model.IEntity)
+     */
+    @Override
     @ActionMethod
-	public String saveOrUpdate(boolean killConversation) throws BusinessException {
-		// check for unicity
-		if (oneShotChargeTemplateService.findByCode(entity.getCode()) != null
-				|| recurringChargeTemplateService.findByCode(entity.getCode()) != null
-				|| productChargeTemplateService.findByCode(entity.getCode()) != null) {
-			messages.error(new BundleKey("messages", "chargeTemplate.uniqueField.code"));
-			return null;
-		}
+    public String saveOrUpdate(boolean killConversation) throws BusinessException {
+        // check for unicity
+        if (oneShotChargeTemplateService.findByCode(entity.getCode()) != null || recurringChargeTemplateService.findByCode(entity.getCode()) != null || productChargeTemplateService.findByCode(entity.getCode()) != null) {
+            messages.error(new BundleKey("messages", "chargeTemplate.uniqueField.code"));
+            return null;
+        }
 
-        getEntity().getEdrTemplates().clear();
-        getEntity().getEdrTemplates().addAll(edrTemplateService.refreshOrRetrieve(edrTemplates.getTarget()));
-        
+        getEntity().setEdrTemplates(edrTemplateService.refreshOrRetrieve(edrTemplates.getTarget()));
+
         String outcome = super.saveOrUpdate(killConversation);
 
         if (outcome != null) {
             return getEditViewName();
         }
         return null;
-	}
+    }
 
-	/**
-	 * @see org.meveo.admin.action.BaseBean#getPersistenceService()
-	 */
-	@Override
-	protected IPersistenceService<UsageChargeTemplate> getPersistenceService() {
-		return usageChargeTemplateService;
-	}
+    /**
+     * @see org.meveo.admin.action.BaseBean#getPersistenceService()
+     */
+    @Override
+    protected IPersistenceService<UsageChargeTemplate> getPersistenceService() {
+        return usageChargeTemplateService;
+    }
 
-	@Override
-	protected String getDefaultSort() {
-		return "code";
-	}
+    @Override
+    protected String getDefaultSort() {
+        return "code";
+    }
 
-	public DualListModel<TriggeredEDRTemplate> getEdrTemplatesModel() {
-		if (edrTemplates == null) {
-			List<TriggeredEDRTemplate> source = triggeredEDRTemplateService.list();
-			List<TriggeredEDRTemplate> target = new ArrayList<TriggeredEDRTemplate>();
-			if (getEntity().getEdrTemplates() != null) {
-				target.addAll(getEntity().getEdrTemplates());
-			}
-			source.removeAll(target);
-			edrTemplates = new DualListModel<TriggeredEDRTemplate>(source, target);
-		}
-		return edrTemplates;
-	}
+    public DualListModel<TriggeredEDRTemplate> getEdrTemplatesModel() {
+        if (edrTemplates == null) {
+            List<TriggeredEDRTemplate> source = triggeredEDRTemplateService.list();
+            List<TriggeredEDRTemplate> target = new ArrayList<TriggeredEDRTemplate>();
+            if (getEntity().getEdrTemplates() != null) {
+                target.addAll(getEntity().getEdrTemplates());
+            }
+            source.removeAll(target);
+            edrTemplates = new DualListModel<TriggeredEDRTemplate>(source, target);
+        }
+        return edrTemplates;
+    }
 
-	public void setEdrTemplatesModel(DualListModel<TriggeredEDRTemplate> edrTemplates) {
-		this.edrTemplates = edrTemplates;
-	}
-	
+    public void setEdrTemplatesModel(DualListModel<TriggeredEDRTemplate> edrTemplates) {
+        this.edrTemplates = edrTemplates;
+    }
 
-	@Inject
-	private TriggeredEDRTemplateService edrTemplateService;
-	
-	@ActionMethod
-	public void duplicate() {
-        
+    @Inject
+    private TriggeredEDRTemplateService edrTemplateService;
+
+    @ActionMethod
+    public void duplicate() {
+
         if (entity != null && entity.getId() != null) {
-        	try {
+            try {
                 usageChargeTemplateService.duplicate(entity);
                 messages.info(new BundleKey("messages", "duplicate.successfull"));
             } catch (BusinessException e) {
                 log.error("Error encountered duplicaing usage charge template entity: {}", entity.getCode(), e);
                 messages.error(new BundleKey("messages", "error.duplicate.unexpected"));
             }
-		}
-	}
-	
-	public boolean isUsedInSubscription() {
-		return (getEntity() != null && !getEntity().isTransient() && (usageChargeTemplateService.findByCode(getEntity()
-				.getCode()) != null)) ? true : false;
-	}
-	
+        }
+    }
+
+    public boolean isUsedInSubscription() {
+        return (getEntity() != null && !getEntity().isTransient() && (usageChargeTemplateService.findByCode(getEntity().getCode()) != null)) ? true : false;
+    }
+
 }
