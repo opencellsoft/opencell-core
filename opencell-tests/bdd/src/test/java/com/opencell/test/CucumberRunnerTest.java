95b54b4a3af8406ad371816c843b00ec6106cae2
franckvalot <franck.valot@opencellsoft.com>
2020-03-30 15:10:40 +0700

    Re #5101 - Run full crud

--- opencell-tests/bdd/src/test/java/com/opencell/test/CucumberRunnerTest.java
+++ opencell-tests/bdd/src/test/java/com/opencell/test/CucumberRunnerTest.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
-        "junit:target/cucumber-reports/Cucumber.xml" }, tags = { "not @full and not @ignore" })
+        "junit:target/cucumber-reports/Cucumber.xml" }, tags = { "@full and not @ignore" })
 public class CucumberRunnerTest {
 
 }
