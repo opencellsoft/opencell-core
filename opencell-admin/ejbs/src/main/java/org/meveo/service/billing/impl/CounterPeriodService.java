ca4b1ccea151c87f5b0fda49441a3b5a84b5f70b
Khalid HORRI <horrikhalid@gmail.com>
2020-04-17 18:08:25 +0100

    Review #5130 processing multi accumulator Counter

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/CounterPeriodService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/CounterPeriodService.java
@@ -19,6 +19,7 @@ package org.meveo.service.billing.impl;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.NoResultException;
@@ -33,6 +34,7 @@ import org.meveo.model.billing.CounterPeriod;
 import org.meveo.model.billing.ServiceInstance;
 import org.meveo.model.billing.Subscription;
 import org.meveo.model.billing.UserAccount;
+import org.meveo.model.catalog.AccumulatorCounterTypeEnum;
 import org.meveo.model.crm.Customer;
 import org.meveo.model.payments.CustomerAccount;
 import org.meveo.service.base.PersistenceService;
@@ -69,31 +71,41 @@ public class CounterPeriodService extends PersistenceService<CounterPeriod> {
     /**
      * Return the counter value of a ICounterEntity UserAccount, BillingAccount, Subscription or ServiceInstance
      *
-     * @param entity the ICounterEntity
+     * @param entity      the ICounterEntity
      * @param counterCode the counter code
      * @return the counter value.
      */
-    public BigDecimal getCounterValue(ICounterEntity entity, String counterCode) {
+    public Object getCounterValue(ICounterEntity entity, String counterCode) {
         return getSingleCounterValue(entity, counterCode, null);
     }
 
     /**
      * Return the counter value of a ICounterEntity UserAccount, BillingAccount, Subscription or ServiceInstance where the startDate<=date<endDate
      *
-     * @param entity the ICounterEntity
+     * @param entity      the ICounterEntity
      * @param counterCode the counter code
-     * @param date the date to be compared to start and end date of a CounterPeriod
+     * @param date        the date to be compared to start and end date of a CounterPeriod
      * @return the counter value.
      */
-    public BigDecimal getCounterValueByDate(ICounterEntity entity, String counterCode, Date date) {
+    public Object getCounterValueByDate(ICounterEntity entity, String counterCode, Date date) {
         return getSingleCounterValue(entity, counterCode, date);
     }
 
-    private BigDecimal getSingleCounterValue(ICounterEntity entity, String counterCode, Date date) {
-        Query query = getEntityManager().createNamedQuery("CounterPeriod.findByCounterEntity");
+    /**
+     * Gets the counter period value or a map of values if the counter is a multi values accumulator.
+     *
+     * @param entity      The account entity, can be a service instance, a subscription or an account level entity.
+     * @param counterCode the counter code
+     * @param date        a date that can be inculuded iin the counter period
+     * @return the counter period value or Map of values for multi values accumulator.
+     */
+    private Object getSingleCounterValue(ICounterEntity entity, String counterCode, Date date) {
+        Query query;
+        query = getEntityManager().createNamedQuery("CounterPeriod.findByCounterEntityAndPeriodDate");
         if (date != null) {
-            query = getEntityManager().createNamedQuery("CounterPeriod.findByCounterEntityAndPeriodDate");
             query.setParameter("date", date, TemporalType.TIMESTAMP);
+        } else {
+            query.setParameter("date", new Date(), TemporalType.TIMESTAMP);
         }
         query.setParameter("serviceInstance", null);
         query.setParameter("subscription", null);
@@ -123,6 +135,9 @@ public class CounterPeriodService extends PersistenceService<CounterPeriod> {
         }
         try {
             CounterPeriod cp = (CounterPeriod) query.getSingleResult();
+            if (AccumulatorCounterTypeEnum.MULTI_VALUE.equals(cp.getAccumulatorType()) && cp.getAccumulatedValues() != null && !cp.getAccumulatedValues().isEmpty()) {
+                return cp.getAccumulatedValues();
+            }
             return cp.getValue();
         } catch (NoResultException e) {
             return null;
