b75aa5cd4935d41825f6ad27dcf5f1425a20ed86
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2020-05-05 13:30:57 +0000

    RE #4004 - [cf] Add CustomField to OrderItem

--- opencell-api/src/main/java/org/meveo/apiv2/services/OrderItemApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/services/OrderItemApiService.java
@@ -277,6 +277,7 @@ public class OrderItemApiService implements ApiService<OrderItem> {
                 oldOrderItem.setUserAccount(orderItem.getUserAccount());
                 oldOrderItem.setProductInstances(orderItem.getProductInstances());
                 oldOrderItem.setSubscription(orderItem.getSubscription());
+                oldOrderItem.setCfValues(orderItem.getCfValues());
 
                 orderItemService.update(oldOrderItem);
                 refreshOrRetrieveOrderItemProductInstanceProduct(oldOrderItem);
@@ -302,6 +303,9 @@ public class OrderItemApiService implements ApiService<OrderItem> {
                 }
                 if(orderItem.getAction() != null){
                     oldOrderItem.setAction(orderItem.getAction());
+                }
+                if(orderItem.getCfValues() != null){
+                    oldOrderItem.setCfValues(orderItem.getCfValues());
                 }
                 populateOrderItemFields(orderItem);
                 if(orderItem.getUserAccount() != null){
