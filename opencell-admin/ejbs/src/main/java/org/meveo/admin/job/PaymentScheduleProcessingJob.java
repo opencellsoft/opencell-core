151a417cdf9ddfcc465ee45f06fb0ded0dab84cf
anasseh <abdelhadi.nasseh@opencellsoft.com>
2020-05-11 04:09:10 +0000

    re #5208 - [PAYMENT SCHEDULE] Not working

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/PaymentScheduleProcessingJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/PaymentScheduleProcessingJob.java
@@ -71,7 +71,7 @@ public class PaymentScheduleProcessingJob extends Job {
         nbRuns.setDescription(resourceMessages.getString("jobExecution.nbRuns"));
         nbRuns.setFieldType(CustomFieldTypeEnum.LONG);
         nbRuns.setValueRequired(false);
-        nbRuns.setDefaultValue("-1");
+        nbRuns.setDefaultValue("1");
         result.put("nbRuns", nbRuns);
 
         CustomFieldTemplate waitingMillis = new CustomFieldTemplate();
