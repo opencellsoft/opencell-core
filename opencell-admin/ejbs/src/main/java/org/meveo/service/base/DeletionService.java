1e980da41f80ee77fee263c7a1555f2bf06953ac
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2020-05-20 16:18:24 +0000

    RE #5235 - [cf] Reference check on custom fields can be too slow

--- opencell-admin/ejbs/src/main/java/org/meveo/service/base/DeletionService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/base/DeletionService.java
@@ -1,21 +1,3 @@
-/*
- * (C) Copyright 2015-2020 Opencell SAS (https://opencellsoft.com/) and contributors.
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
- * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
- * OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS
- * IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO
- * THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE,
- * YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
- *
- * For more information on the GNU Affero General Public License, please consult
- * <https://www.gnu.org/licenses/agpl-3.0.en.html>.
- */
-
 package org.meveo.service.base;
 
 import java.util.Map;
@@ -75,6 +57,7 @@ public class DeletionService {
 
     public void checkEntityIsNotreferenced(IEntity entity) {
         String entityClass = entity instanceof CustomEntityInstance ? String.format(CUSTOM_ENTITY_CLASS_PREFIX, ((CustomEntityInstance)entity).getCetCode()) : entity.getClass().getName();
+
         boolean isIncluded = isIncluded(entity, entityClass);
 
         if (isIncluded) {
@@ -97,7 +80,7 @@ public class DeletionService {
             		if(customField.getAppliesTo().startsWith(Job.CFT_PREFIX)) {
             			entityClass = JobInstance.class;
             		} else {
-            			entityClass = customEntityTemplateService.findByCode(EntityCustomizationUtils.getEntityCode(appliesTo));
+            			entityClass = entitiesByName.get(customEntityTemplateService.findByCode(EntityCustomizationUtils.getEntityCode(appliesTo)));
             		}
             		
                 }
