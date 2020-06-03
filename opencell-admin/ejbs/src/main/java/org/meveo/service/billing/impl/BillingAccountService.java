8baf2bf19819ccc8b9956ee3ff807123d2b7932d
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2020-06-03 12:16:24 +0100

    #5217 - [br] billingRun.endDate should be excluded

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingAccountService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingAccountService.java
@@ -280,7 +280,7 @@ public class BillingAccountService extends AccountService<BillingAccount> {
             }
             
             if (endDate != null) {
-            	if (Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("date.range.includeEndDate", "false"))) {
+            	if (Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("date.range.includeEndDate", "true"))) {
                 	endDate = DateUtils.setTimeToZero(endDate);
                 }
                 qb.addCriterionDateRangeToTruncatedToDay("nextInvoiceDate", endDate);
