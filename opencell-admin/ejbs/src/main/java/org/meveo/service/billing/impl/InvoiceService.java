1675de269cbee284cc9bb4a003775d66e2d07880
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2020-06-03 23:04:34 +0100

    Test #5217 billingRun.endDate should be excluded

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -713,7 +713,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
                 invoiceDate = billingRun.getInvoiceDate();
             }
 
-            if (Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("date.range.includeEndDate", "false"))) {
+            if (Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("date.range.excludeEndDate", "true"))) {
                 lastTransactionDate = DateUtils.setTimeToZero(lastTransactionDate);
             }
 
