287ccb4d0ce46f54105b268983fef8683c3633c3
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2020-06-04 18:58:09 +0100

    Test #5217 changing option name # Conflicts: #	opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingAccountService.java

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingAccountService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingAccountService.java
@@ -279,7 +279,7 @@ public class BillingAccountService extends AccountService<BillingAccount> {
             }
 
             if (endDate != null) {
-                if (Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("date.range.excludeEndDate", "true"))) {
+                if (!Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("invoicing.includeEndDate", "false"))) {
                     endDate = DateUtils.setTimeToZero(endDate);
                 }
                 qb.addCriterionDateRangeToTruncatedToDay("nextInvoiceDate", endDate);
