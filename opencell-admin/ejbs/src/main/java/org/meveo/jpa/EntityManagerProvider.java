956b244a3fb7065e04456bcd3a8dabcf7d66ef3e
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2020-06-18 11:16:28 +0100

    RE #5236 - [cf] Analyze and explain : need to manually update CF in scripts

--- opencell-admin/ejbs/src/main/java/org/meveo/jpa/EntityManagerProvider.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/jpa/EntityManagerProvider.java
@@ -21,7 +21,6 @@ import java.lang.reflect.Proxy;
 import java.util.Map;
 import java.util.TreeMap;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.enterprise.context.RequestScoped;
@@ -35,9 +34,6 @@ import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 
-import org.hibernate.event.service.spi.EventListenerRegistry;
-import org.hibernate.event.spi.EventType;
-import org.hibernate.internal.SessionFactoryImpl;
 import org.infinispan.Cache;
 import org.infinispan.context.Flag;
 import org.meveo.commons.utils.ParamBean;
@@ -68,7 +64,7 @@ public class EntityManagerProvider {
     private Cache<String, EntityManagerFactory> entityManagerFactories;
 
     private static boolean isMultiTenancyEnabled = ParamBean.isMultitenancyEnabled();
-    
+
     /**
      * Instantiates an Entity manager for use CDI injection. Will consider a tenant that currently connected user belongs to.
      * 
