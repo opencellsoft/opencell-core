f6e6e0d2668a1b186f9510d2294f90629999e840
Nabil OUACHI <nabil.ouachi@opencellsoft.com>
2020-09-28 18:36:33 +0100

    RE #5524 refactor PersistenceService

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/base/expressions/Expression.java
@@ -0,0 +1,43 @@
+package org.meveo.service.base.expressions;
+
+import java.util.Arrays;
+
+public class Expression {
+
+    private String condition;
+    private String fieldName;
+    private String fieldName2;
+    private String[] fieldInfo;
+
+    public Expression(String[] fieldInfo) {
+        this.fieldInfo = fieldInfo;
+        this.condition = "eq";
+        this.fieldName = fieldInfo[0];
+        this.fieldName2 = null;
+
+        if (fieldInfo.length == 2) {
+            condition = fieldInfo[0];
+            fieldName = fieldInfo[1];
+        } else if (fieldInfo.length > 2) {
+            condition = fieldInfo[0];
+            fieldName = fieldInfo[1];
+            this.fieldName2 = fieldInfo[2];
+        }
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getFieldName2() {
+        return fieldName2;
+    }
+
+    public String[] getAllFields() {
+        return Arrays.copyOfRange(fieldInfo, 1, fieldInfo.length);
+    }
+}
