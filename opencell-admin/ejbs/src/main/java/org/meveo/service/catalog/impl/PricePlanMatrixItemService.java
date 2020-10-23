3365916401885ef18fed6023fe5b8184fb2ea54b
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2020-10-23 14:17:08 +0100

    RE #5595 540-30 PricePlanMatrixItem + business services

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/catalog/impl/PricePlanMatrixItemService.java
@@ -0,0 +1,8 @@
+package org.meveo.service.catalog.impl;
+
+import org.meveo.model.catalog.PricePlanMatrixItem;
+import org.meveo.service.base.PersistenceService;
+
+public class PricePlanMatrixItemService extends PersistenceService<PricePlanMatrixItem> {
+
+}
