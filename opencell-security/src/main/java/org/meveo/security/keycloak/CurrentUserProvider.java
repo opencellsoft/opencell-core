db720839f1c725601ab7a770c28ec25a8964bff3
abdelkader.bouazza <abdelkader.bouazza@opencellsoft.com>
2020-11-11 16:53:06 +0100

    RE #5588 : make the user info updates in a separate transactions to prevent optimisticLockException from rollingback the entire transaction when there is a multiple requests

--- opencell-security/src/main/java/org/meveo/security/keycloak/CurrentUserProvider.java
+++ opencell-security/src/main/java/org/meveo/security/keycloak/CurrentUserProvider.java
@@ -18,30 +18,13 @@
 
 package org.meveo.security.keycloak;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import javax.annotation.Resource;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
-import javax.enterprise.context.ContextNotActiveException;
-import javax.enterprise.event.Event;
-import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-
 import org.keycloak.KeycloakPrincipal;
-import org.meveo.model.admin.User;
-import org.meveo.model.security.Permission;
-import org.meveo.model.security.Role;
-import org.meveo.model.shared.Name;
 import org.meveo.security.MeveoUser;
-import org.meveo.security.UserAuthTimeProducer;
 import org.slf4j.Logger;
 import org.slf4j.MDC;
 
@@ -53,19 +36,10 @@ import org.slf4j.MDC;
 @Stateless
 public class CurrentUserProvider {
 
-    /**
-     * Map<providerCode, Map<roleName, rolePermissions>>
-     */
-    private static Map<String, Map<String, Set<String>>> roleToPermissionMapping;
-
+    @Inject
+    private UserInfoManagement userInfoManagement;
     @Resource
     private SessionContext ctx;
-
-    @Inject
-    private Instance<UserAuthTimeProducer> userAuthTimeProducer;
-    
-    @Inject
-    private Event<User> userEventProducer;
 
     @Inject
     private Logger log;
@@ -104,7 +78,7 @@ public class CurrentUserProvider {
             MDC.put("providerCode", providerCode);
         }
         log.debug("Force authentication to {}/{}", providerCode, userName);
-        setForcedUsername(userName);
+        forcedUserUsername.set(userName);
         setCurrentTenant(providerCode);
     }
 
@@ -125,7 +99,7 @@ public class CurrentUserProvider {
                 MDC.put("providerCode", lastCurrentUser.getProviderCode());
             }
 
-            setForcedUsername(lastCurrentUser.getUserName());
+            forcedUserUsername.set(lastCurrentUser.getUserName());
             setCurrentTenant(lastCurrentUser.getProviderCode());
             log.debug("Reestablished authentication to {}/{}", lastCurrentUser.getUserName(), lastCurrentUser.getProviderCode());
         }
@@ -181,20 +155,20 @@ public class CurrentUserProvider {
      */
     public MeveoUser getCurrentUser(String providerCode, EntityManager em) {
 
-        String username = MeveoUserKeyCloakImpl.extractUsername(ctx, getForcedUsername());
+        String username = MeveoUserKeyCloakImpl.extractUsername(ctx, forcedUserUsername.get());
 
         MeveoUser user = null;
 
         // User was forced authenticated, so need to lookup the rest of user information
-        if (!(ctx.getCallerPrincipal() instanceof KeycloakPrincipal) && getForcedUsername() != null) {
-            user = new MeveoUserKeyCloakImpl(ctx, getForcedUsername(), getCurrentTenant(), getAdditionalRoles(username, em), getRoleToPermissionMapping(providerCode, em));
+        if (!(ctx.getCallerPrincipal() instanceof KeycloakPrincipal) && forcedUserUsername.get() != null) {
+            user = new MeveoUserKeyCloakImpl(ctx, forcedUserUsername.get(), getCurrentTenant(), userInfoManagement.getAdditionalRoles(username, em), userInfoManagement.getRoleToPermissionMapping(providerCode, em));
 
         } else {
-            user = new MeveoUserKeyCloakImpl(ctx, null, null, getAdditionalRoles(username, em), getRoleToPermissionMapping(providerCode, em));
+            user = new MeveoUserKeyCloakImpl(ctx, null, null, userInfoManagement.getAdditionalRoles(username, em), userInfoManagement.getRoleToPermissionMapping(providerCode, em));
         }
         // log.trace("getCurrentUser username={}, providerCode={}, forcedAuthentication {}/{} ", username, user != null ? user.getProviderCode() : null, getForcedUsername(),
         // getCurrentTenant());
-        supplementOrCreateUserInApp(user, em);
+        userInfoManagement.supplementOrCreateUserInApp(user, em, forcedUserUsername.get());
 
         log.trace("Current user is {}", user.toStringLong());
         return user;
@@ -205,145 +179,20 @@ public class CurrentUserProvider {
      * 
      * @param currentUser Authenticated current user
      */
-    private void supplementOrCreateUserInApp(MeveoUser currentUser, EntityManager em) {
 
-        // Takes care of anonymous users
-        if (currentUser.getUserName() == null) {
-            return;
-        }
-
-        // Create or retrieve current user
-        try {
-            User user = null;
-            try {
-                user = em.createNamedQuery("User.getByUsername", User.class).setParameter("username", currentUser.getUserName().toLowerCase()).getSingleResult();
-                currentUser.setFullName(user.getNameOrUsername());
-
-                if (!userAuthTimeProducer.isUnsatisfied() && userAuthTimeProducer.get().getAuthTime() != currentUser.getAuthTime()) {
-                    userAuthTimeProducer.get().setAuthTime(currentUser.getAuthTime());
-                    user.setLastLoginDate(new Date());
-                    em.merge(user);
-                    em.flush();
-                }
-
-            } catch (NoResultException e) {
-
-                user = new User();
-                user.setUserName(currentUser.getUserName().toUpperCase());
-                if (currentUser.getFullName() != null) {
-                    if (user.getName() == null) {
-                        user.setName(new Name());
-                    }
-                    int spacePos = currentUser.getFullName().indexOf(' ');
-                    if (spacePos > 0) {
-                        user.getName().setFirstName(currentUser.getFullName().substring(0, spacePos));
-                        user.getName().setLastName(currentUser.getFullName().substring(spacePos + 1));
-                    } else {
-                        user.getName().setFirstName(currentUser.getFullName());
-                    }
-                }
-                user.setLastLoginDate(new Date());
-                user.setEmail(currentUser.getEmail());
-                user.updateAudit(currentUser);
-                em.persist(user);
-                em.flush();
-                log.info("A new application user was registered with username {} and name {}", user.getUserName(), user.getName() != null ? user.getName().getFullName() : "");
-                triggerNewUserNotification(user);
-            } catch (ContextNotActiveException e) {
-                // Commented out as no context is available for scheduled jobs to retrieve userAuthTimeProducer instance
-                // log.error("No session context={}", e.getMessage());
-            }
-            
-        } catch (Exception e) {
-            log.error("Failed to supplement current user information from db and/or create new user in db", e);
-        }
-    }
     
-    /**
-	 * @param user
-	 */
-	private void triggerNewUserNotification(User user) {
-		if(user!=null && user.getUserName()!=null && !user.getUserName().equalsIgnoreCase(getForcedUsername())) {
-			userEventProducer.fire(user);
-		}
-	}
 
 
-    /**
-     * Return and load if necessary a mapping between roles and permissions
-     * 
-     * @return A mapping between roles and permissions
-     */
-    private Map<String, Set<String>> getRoleToPermissionMapping(String providerCode, EntityManager em) {
 
-        synchronized (this) {
-            if (CurrentUserProvider.roleToPermissionMapping == null || roleToPermissionMapping.get(providerCode) == null) {
-                CurrentUserProvider.roleToPermissionMapping = new HashMap<>();
 
-                try {
-                    List<Role> userRoles = em.createNamedQuery("Role.getAllRoles", Role.class).getResultList();
-                    Map<String, Set<String>> roleToPermissionMappingForProvider = new HashMap<>();
-
-                    for (Role role : userRoles) {
-                        Set<String> rolePermissions = new HashSet<>();
-                        for (Permission permission : role.getAllPermissions()) {
-                            rolePermissions.add(permission.getPermission());
-                        }
-
-                        roleToPermissionMappingForProvider.put(role.getName(), rolePermissions);
-                    }
-                    CurrentUserProvider.roleToPermissionMapping.put(providerCode, roleToPermissionMappingForProvider);
-                } catch (Exception e) {
-                    log.error("Failed to construct role to permission mapping", e);
-                }
-            }
-
-            return CurrentUserProvider.roleToPermissionMapping.get(providerCode);
-        }
-    }
 
     /**
      * Invalidate cached role to permission mapping (usually after role save/update event)
      */
     public void invalidateRoleToPermissionMapping() {
-        CurrentUserProvider.roleToPermissionMapping = null;
+        userInfoManagement.roleToPermissionMapping = null;
     }
 
-    /**
-     * Get additional roles that user has assigned in application
-     * 
-     * @param username Username to check
-     * @return A set of role names that given username has in application
-     */
-    private Set<String> getAdditionalRoles(String username, EntityManager em) {
-
-        // Takes care of anonymous users
-        if (username == null) {
-            return null;
-        }
-
-        try {
-            User user = em.createNamedQuery("User.getByUsername", User.class).setParameter("username", username.toLowerCase()).getSingleResult();
-
-            Set<String> additionalRoles = new HashSet<>();
-
-            for (Role role : user.getRoles()) {
-                additionalRoles.add(role.getName());
-                for (Role subRole : role.getRoles()) {
-                    additionalRoles.add(subRole.getName());
-                }
-            }
-
-            return additionalRoles;
-
-        } catch (NoResultException e) {
-            return null;
-
-        } catch (Exception e) {
-            log.error("Failed to retrieve additional roles for a user {}", username, e);
-            return null;
-        }
-    }
 
     /**
      * Check if current tenant value is set (differs from the initial value)
@@ -372,23 +221,5 @@ public class CurrentUserProvider {
     private static void setCurrentTenant(final String tenantName) {
         currentTenant.remove();
         currentTenant.set(tenantName);
-    }
-
-    /**
-     * Get forced authentication username value
-     * 
-     * @return Forced authentication username
-     */
-    private static String getForcedUsername() {
-        return forcedUserUsername.get();
-    }
-
-    /**
-     * Set forced authentication username value
-     * 
-     * @param username Forced authentication username
-     */
-    private static void setForcedUsername(final String username) {
-        forcedUserUsername.set(username);
     }
 }
