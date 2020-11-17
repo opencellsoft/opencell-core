65805d00b4014df9e8224cef029f927737359c7f
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2020-11-17 20:48:31 +0000

    RE #5647 Fix secure cookie

--- opencell-admin/web/src/main/java/org/meveo/filters/SecurityFilter.java
+++ opencell-admin/web/src/main/java/org/meveo/filters/SecurityFilter.java
@@ -1,8 +1,6 @@
 package org.meveo.filters;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,13 +11,15 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.HttpHeaders;
 
 public class SecurityFilter implements Filter {
     
     private static final String POLICY = "frame-src 'self'";
     private static final String XFRAME_SAMEORIGIN = "SAMEORIGIN";
     private static final String XCONTENT_NOSNIFF = "nosniff";
+    private static final String SET_COOKIE = "Set-Cookie";
+    private static final String JSESSION_ID = "JSESSIONID=";
+    private static final String HTTP_ONLY = ";Secure;HttpOnly";
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -31,6 +31,17 @@ public class SecurityFilter implements Filter {
         res.setHeader("X-Frame-Options", SecurityFilter.XFRAME_SAMEORIGIN);
         res.setHeader("X-Content-Type-Options", SecurityFilter.XCONTENT_NOSNIFF);
         
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for(Cookie cookie :cookies){
+                if("JSESSIONID".equals(cookie.getName())) {
+                    cookie.setValue(req.getSession().getId() + HTTP_ONLY);
+                    cookie.setSecure(true);
+                }
+            }
+        }
+        
+        res.setHeader(SET_COOKIE, JSESSION_ID + req.getSession().getId() + HTTP_ONLY);
         chain.doFilter(request, res);
     }
 
