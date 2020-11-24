726c88d976c125208bd04bd411bade5d5cc8b8b6
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2020-11-24 22:25:41 +0000

    RE #5659 #5665 add pagination to new Rs endpoints

--- opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/AccountOperationService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/AccountOperationService.java
@@ -119,11 +119,21 @@ public class AccountOperationService extends PersistenceService<AccountOperation
         return accountOperations.size() > 0 ? accountOperations.get(0) : null;
     }
     
-    
     @SuppressWarnings("unchecked")
-    public List<AccountOperation> listByCustomerAccount(CustomerAccount customerAccount) {
+    public List<AccountOperation> listByCustomerAccount(CustomerAccount customerAccount, Integer firstRow, Integer numberOfRows) {
         try {
-            return getEntityManager().createNamedQuery("AccountOperation.listByCustomerAccount").setParameter("customerAccount", customerAccount).getResultList();
+            
+            Query query = getEntityManager().createNamedQuery("AccountOperation.listByCustomerAccount");
+            query.setParameter("customerAccount", customerAccount);
+            
+            if (firstRow != null) {
+                query.setFirstResult(firstRow);
+            }
+            if (numberOfRows != null) {
+                query.setMaxResults(numberOfRows);
+            }
+            
+            return query.getResultList();
         } catch (NoResultException e) {
             log.warn("error while getting list AccountOperation by customerAccount", e);
             return null;
