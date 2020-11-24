f4c5b12b6d7ed5acc427b15f5ba3af1653b333b6
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2020-11-24 08:27:14 +0000

    RE #5642 Fix Backport Invoice Number Assigned Event

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceSingleton.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceSingleton.java
@@ -25,10 +25,12 @@ import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.commons.utils.StringUtils;
+import org.meveo.event.qualifier.InvoiceNumberAssigned;
 import org.meveo.jpa.JpaAmpNewTx;
 import org.meveo.model.admin.CustomGenericEntityCode;
 import org.meveo.model.admin.Seller;
@@ -92,6 +94,10 @@ public class ServiceSingleton {
     
     @Inject
     private ProviderService providerService;
+
+    @Inject
+    @InvoiceNumberAssigned
+    private Event<Invoice> invoiceNumberAssignedEventProducer;
 
     @Inject
     private Logger log;
