0fb589e1d26616eb849e61c80492d6c4a065394e
anasseh <abdelhadi.nasseh@opencellsoft.com>
2020-11-28 13:23:27 +0100

    re #5386 - SEPA : job performance

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/async/PaymentAsync.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/async/PaymentAsync.java
@@ -1,33 +1,13 @@
-/*
- * (C) Copyright 2015-2020 Opencell SAS (https://opencellsoft.com/) and contributors.
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
- * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
- * OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS
- * IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO
- * THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE,
- * YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
- *
- * For more information on the GNU Affero General Public License, please consult
- * <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+/**
+ * 
  */
-
 package org.meveo.admin.async;
-
-import static org.meveo.service.script.payment.AccountOperationFilterScript.LIST_AO_TO_PAY;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
@@ -36,18 +16,15 @@ import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.meveo.admin.job.UnitPaymentJobBean;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.payments.AccountOperation;
-import org.meveo.model.payments.MatchingStatusEnum;
 import org.meveo.model.payments.OperationCategoryEnum;
 import org.meveo.model.payments.PaymentGateway;
 import org.meveo.model.payments.PaymentMethodEnum;
 import org.meveo.security.MeveoUser;
 import org.meveo.security.keycloak.CurrentUserProvider;
 import org.meveo.service.job.JobExecutionService;
-import org.meveo.service.payments.impl.AccountOperationService;
 import org.meveo.service.script.payment.AccountOperationFilterScript;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,7 +34,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author anasseh
  * @author Said Ramli
- * @lastModifiedVersion 9.3.1
+ * @lastModifiedVersion 5.2
  */
 
 @Stateless
@@ -75,17 +52,13 @@ public class PaymentAsync {
 	@Inject
 	private CurrentUserProvider currentUserProvider;
 
-	/** The account operation service. */
-	@Inject
-	private AccountOperationService accountOperationService;
-
 	protected Logger log = LoggerFactory.getLogger(this.getClass());
 
 	/**
 	 * Process card payments for a list of given account operation ids. One account
 	 * operation at a time in a separate transaction.
 	 *
-	 * @param caIds             List of customerAccount ids
+	 * @param aos               List of AO to pay/refund
 	 * @param result            Job execution result
 	 * @param createAO          True/ false to create account operation
 	 * @param matchingAO        Matching account operation
@@ -106,70 +79,25 @@ public class PaymentAsync {
 	 */
 	@Asynchronous
 	@TransactionAttribute(TransactionAttributeType.NEVER)
-	public Future<String> launchAndForget(List<Long> caIds, JobExecutionResultImpl result, boolean createAO, boolean matchingAO, PaymentGateway paymentGateway,
-			OperationCategoryEnum operationCategory, PaymentMethodEnum paymentMethodType, MeveoUser lastCurrentUser, String paymentPerAOorCA, Date fromDueDate, Date toDueDate,
-			AccountOperationFilterScript aoFilterScript) { // TODO : nbr of method arguments is disturbing , refactor it by using a
-															// dedicated bean/dto
+	public Future<String> launchAndForget(List<AccountOperation> aos, JobExecutionResultImpl result, boolean createAO, boolean matchingAO, PaymentGateway paymentGateway,
+			OperationCategoryEnum operationCategory, PaymentMethodEnum paymentMethodType, MeveoUser lastCurrentUser, Date fromDueDate, Date toDueDate,
+			AccountOperationFilterScript aoFilterScript) {
 
 		currentUserProvider.reestablishAuthentication(lastCurrentUser);
 		BigDecimal oneHundred = new BigDecimal("100");
 		int i = 0;
-		for (Long caID : caIds) {
+		for (AccountOperation ao : aos) {
 			i++;
 			if (i % JobExecutionService.CHECK_IS_JOB_RUNNING_EVERY_NR == 0 && !jobExecutionService.isJobRunningOnThis(result.getJobInstance().getId())) {
 				break;
 			}
+			List<Long> aoIds = new ArrayList<Long>();
+			aoIds.add(ao.getId());
+			unitPaymentJobBean.execute(result, ao.getCustomerAccount().getId(), aoIds, ao.getUnMatchingAmount().multiply(oneHundred).longValue(), createAO, matchingAO,
+					operationCategory, paymentGateway, paymentMethodType, aoFilterScript);
 
-			List<AccountOperation> listAoToPayOrRefund = null;
-			if (operationCategory == OperationCategoryEnum.CREDIT) {
-				List<AccountOperation> listAoToPay = accountOperationService.getAOsToPayOrRefund(paymentMethodType, fromDueDate, toDueDate, OperationCategoryEnum.DEBIT, caID);
-				log.info("listAoToPay size before filter :" + (listAoToPay == null ? "null" : listAoToPay.size()));
-				listAoToPayOrRefund = this.filterAoToPayOrRefund(aoFilterScript, listAoToPay, paymentMethodType, OperationCategoryEnum.DEBIT);
-				log.info("listAoToPay size after filter :" + (listAoToPayOrRefund == null ? "null" : listAoToPayOrRefund.size()));
-			} else {
-				List<AccountOperation> listAoToRefund = accountOperationService.getAOsToPayOrRefund(paymentMethodType, fromDueDate, toDueDate, OperationCategoryEnum.CREDIT, caID);
-				log.info("listAoToRefund size before filter :" + (listAoToRefund == null ? "null" : listAoToRefund.size()));
-				listAoToPayOrRefund = this.filterAoToPayOrRefund(aoFilterScript, listAoToRefund, paymentMethodType, OperationCategoryEnum.CREDIT);
-				log.info("listAoToRefund size after filter :" + (listAoToPayOrRefund == null ? "null" : listAoToPayOrRefund.size()));
-			}
-			if ("CA".equals(paymentPerAOorCA)) {
-				List<Long> aoIds = new ArrayList<Long>();
-				BigDecimal amountToPay = BigDecimal.ZERO;
-				for (AccountOperation ao : listAoToPayOrRefund) {
-					aoIds.add(ao.getId());
-					amountToPay = amountToPay.add(ao.getUnMatchingAmount());
-				}
-				if (amountToPay.compareTo(BigDecimal.ZERO) != 0) {
-					unitPaymentJobBean.execute(result, caID, aoIds, amountToPay.multiply(oneHundred).longValue(), createAO, matchingAO, operationCategory, paymentGateway,
-							paymentMethodType, aoFilterScript);
-				}
-			} else {
-				for (AccountOperation ao : listAoToPayOrRefund) {
-					if (ao.getUnMatchingAmount().compareTo(BigDecimal.ZERO) != 0) {
-						List<Long> aoIds = new ArrayList<Long>();
-						aoIds.add(ao.getId());
-						unitPaymentJobBean.execute(result, caID, aoIds, ao.getUnMatchingAmount().multiply(oneHundred).longValue(), createAO, matchingAO, operationCategory,
-								paymentGateway, paymentMethodType, aoFilterScript);
-					}
-				}
-			}
 		}
 		return new AsyncResult<String>("OK");
 	}
 
-	private List<AccountOperation> filterAoToPayOrRefund(AccountOperationFilterScript aoFilterScript, List<AccountOperation> listAoToPay, PaymentMethodEnum paymentMethodType,
-			OperationCategoryEnum aoCategory) {
-		if (aoFilterScript != null) {
-			Map<String, Object> methodContext = new HashMap<>();
-			methodContext.put(LIST_AO_TO_PAY, listAoToPay);
-			List<AccountOperation> filteredAOs = aoFilterScript.filterAoToPay(methodContext);
-			if (CollectionUtils.isNotEmpty(filteredAOs)) {
-				return filteredAOs.stream().filter((ao) -> (ao.getPaymentMethod() == paymentMethodType && ao.getTransactionCategory() == aoCategory
-						&& (ao.getMatchingStatus() == MatchingStatusEnum.O || ao.getMatchingStatus() == MatchingStatusEnum.P))).collect(Collectors.toList());
-			} else {
-				return new ArrayList<AccountOperation>();
-			}
-		}
-		return listAoToPay;
-	}
 }
