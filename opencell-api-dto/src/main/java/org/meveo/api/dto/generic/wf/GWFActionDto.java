peeter-piegaze-sonarsource/core

RE #5214: workflow transition actions

opencell-api-dto/src/main/java/org/meveo/api/dto/generic/wf/GWFActionDto.java
@@ -1,0 +1,173 @@
+package org.meveo.api.dto.generic.wf;
+
+import org.meveo.api.dto.BaseEntityDto;
+import org.meveo.model.generic.wf.Action;
+import org.meveo.model.notification.Notification;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static java.util.Optional.ofNullable;
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+@XmlRootElement
+@XmlAccessorType(FIELD)
+public class GWFActionDto extends BaseEntityDto {
+
+    private String description;
+
+    private String uuid;
+
+    private int priority;
+
+    private String type;
+
+    private String conditionEl;
+
+    @XmlElement(name = "isAsynchronous")
+    private boolean asynchronous;
+
+    private String actionScriptCode;
+
+    private String notificationCode;
+
+    private String valueEl;
+
+    private LogLevel logLevel;
+
+    private String field;
+
+    private Map<String, String> parameters;
+
+    public GWFActionDto() {
+    }
+
+    public GWFActionDto(Action action) {
+        this.setUuid(action.getUuid());
+        this.setDescription(action.getDescription());
+        this.setConditionEl(action.getConditionEl());
+        this.setPriority(action.getPriority());
+        this.setAsynchronous(action.isAsynchronous());
+        ofNullable(action.getActionScript()).ifPresent(script -> this.setActionScriptCode(script.getCode()));
+        if(action.getNotification() != null) {
+            Notification notification = action.getNotification();
+            this.setNotificationCode(notification.getCode());
+            this.setParameters(notification.getParams());
+        }
+        this.setType(action.getType());
+        this.setValueEl(action.getValueEL());
+        this.setField(action.getFieldToUpdate());
+        ofNullable(action.getLogLevel()).ifPresent(level -> this.setLogLevel(LogLevel.fromValue(level)));
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getConditionEl() {
+        return conditionEl;
+    }
+
+    public void setConditionEl(String conditionEl) {
+        this.conditionEl = conditionEl;
+    }
+
+    public boolean isAsynchronous() {
+        return this.asynchronous;
+    }
+
+    public void setAsynchronous(boolean asynchronous) {
+        this.asynchronous = asynchronous;
+    }
+
+    public String getActionScriptCode() {
+        return actionScriptCode;
+    }
+
+    public void setActionScriptCode(String actionScriptCode) {
+        this.actionScriptCode = actionScriptCode;
+    }
+
+    public String getNotificationCode() {
+        return notificationCode;
+    }
+
+    public void setNotificationCode(String notificationCode) {
+        this.notificationCode = notificationCode;
+    }
+
+    public String getValueEl() {
+        return valueEl;
+    }
+
+    public void setValueEl(String valueEl) {
+        this.valueEl = valueEl;
+    }
+
+    public LogLevel getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(LogLevel logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public enum LogLevel {
+        INFO, DEBUG, TRACE;
+
+        public static LogLevel fromValue(String name) {
+            return Arrays.stream(values())
+                    .filter(log -> log.name().equalsIgnoreCase(name))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
+    }
+}

opencell-model/src/main/java/org/meveo/model/generic/wf/Action.java
@@ -1,0 +1,158 @@
+package org.meveo.model.generic.wf;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.meveo.model.BaseEntity;
+import org.meveo.model.notification.Notification;
+import org.meveo.model.scripts.ScriptInstance;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "wf_generic_action", uniqueConstraints = @UniqueConstraint(columnNames = { "uuid" }))
+@GenericGenerator(name = "ID_GENERATOR", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+        @org.hibernate.annotations.Parameter(name = "sequence_name", value = "wf_generic_action_seq") })
+public class Action extends BaseEntity {
+
+    @Column(name = "uuid", nullable = false, updatable = false, length = 60)
+    @Size(max = 60)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "priority")
+    private int priority;
+
+    @Column(name = "condition_el", length = 2000)
+    @Size(max = 2000)
+    private String conditionEl;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "log_level")
+    private String logLevel;
+
+    @Column(name = "field_to_update")
+    private String fieldToUpdate;
+
+    @Column(name = "is_asynchronous")
+    @Type(type = "numeric_boolean")
+    private boolean isAsynchronous;
+
+    @Column(name = "value_el", length = 2000)
+    @Size(max = 2000)
+    private String valueEL;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "action_script_id")
+    private ScriptInstance actionScript;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "notification_id")
+    private Notification notification;
+
+    @ManyToOne
+    @JoinColumn(name = "transition_id")
+    private GWFTransition transition;
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getConditionEl() {
+        return conditionEl;
+    }
+
+    public void setConditionEl(String conditionEl) {
+        this.conditionEl = conditionEl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isAsynchronous() {
+        return isAsynchronous;
+    }
+
+    public void setAsynchronous(boolean asynchronous) {
+        isAsynchronous = asynchronous;
+    }
+
+    public ScriptInstance getActionScript() {
+        return actionScript;
+    }
+
+    public void setActionScript(ScriptInstance actionScript) {
+        this.actionScript = actionScript;
+    }
+
+    public Notification getNotification() {
+        return notification;
+    }
+
+    public void setNotification(Notification notification) {
+        this.notification = notification;
+    }
+
+    public GWFTransition getTransition() {
+        return transition;
+    }
+
+    public void setTransition(GWFTransition transition) {
+        this.transition = transition;
+    }
+
+    public String getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(String logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getFieldToUpdate() {
+        return fieldToUpdate;
+    }
+
+    public void setFieldToUpdate(String fieldToUpdate) {
+        this.fieldToUpdate = fieldToUpdate;
+    }
+
+    public String getValueEL() {
+        return valueEL;
+    }
+
+    public void setValueEL(String valueEL) {
+        this.valueEL = valueEL;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}

opencellsoft/opencell-core

opencell-api-dto/src/main/java/org/meveo/api/dto/generic/wf/GWFActionDto.java
@@ -1,0 +1,173 @@
+package org.meveo.api.dto.generic.wf;
+
+import static java.util.Optional.ofNullable;
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import org.meveo.api.dto.BaseEntityDto;
+import org.meveo.model.generic.wf.Action;
+import org.meveo.model.notification.Notification;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@XmlRootElement
+@XmlAccessorType(FIELD)
+public class GWFActionDto extends BaseEntityDto {
+
+    private String description;
+
+    private String uuid;
+
+    private int priority;
+
+    private ActionTypesEnum type;
+
+    private String conditionEl;
+
+    @XmlElement(name = "isAsynchronous")
+    private boolean asynchronous;
+
+    private String actionScriptCode;
+
+    private String notificationCode;
+
+    private String valueEl;
+
+    private LogLevel logLevel;
+
+    private String field;
+
+    private Map<String, String> parameters;
+
+    public GWFActionDto() {
+    }
+
+    public GWFActionDto(Action action) {
+        this.setUuid(action.getUuid());
+        this.setDescription(action.getDescription());
+        this.setConditionEl(action.getConditionEl());
+        this.setPriority(action.getPriority());
+        this.setAsynchronous(action.isAsynchronous());
+        ofNullable(action.getActionScript()).ifPresent(script -> this.setActionScriptCode(script.getCode()));
+        if(action.getNotification() != null) {
+            Notification notification = action.getNotification();
+            this.setNotificationCode(notification.getCode());
+            this.setParameters(notification.getParams());
+        }
+        this.setType(ActionTypesEnum.valueOf(action.getType()));
+        this.setValueEl(action.getValueEL());
+        this.setField(action.getFieldToUpdate());
+        ofNullable(action.getLogLevel()).ifPresent(level -> this.setLogLevel(LogLevel.fromValue(level)));
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getConditionEl() {
+        return conditionEl;
+    }
+
+    public void setConditionEl(String conditionEl) {
+        this.conditionEl = conditionEl;
+    }
+
+    public boolean isAsynchronous() {
+        return this.asynchronous;
+    }
+
+    public void setAsynchronous(boolean asynchronous) {
+        this.asynchronous = asynchronous;
+    }
+
+    public String getActionScriptCode() {
+        return actionScriptCode;
+    }
+
+    public void setActionScriptCode(String actionScriptCode) {
+        this.actionScriptCode = actionScriptCode;
+    }
+
+    public String getNotificationCode() {
+        return notificationCode;
+    }
+
+    public void setNotificationCode(String notificationCode) {
+        this.notificationCode = notificationCode;
+    }
+
+    public String getValueEl() {
+        return valueEl;
+    }
+
+    public void setValueEl(String valueEl) {
+        this.valueEl = valueEl;
+    }
+
+    public LogLevel getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(LogLevel logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public ActionTypesEnum getType() {
+        return type;
+    }
+
+    public void setType(ActionTypesEnum type) {
+        this.type = type;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public enum LogLevel {
+        INFO, DEBUG, TRACE;
+
+        public static LogLevel fromValue(String name) {
+            return Arrays.stream(values())
+                    .filter(log -> log.name().equalsIgnoreCase(name))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
+    }
+}

opencell-model/src/main/java/org/meveo/model/generic/wf/Action.java
@@ -1,0 +1,165 @@
+package org.meveo.model.generic.wf;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.meveo.model.BaseEntity;
+import org.meveo.model.notification.Notification;
+import org.meveo.model.scripts.ScriptInstance;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "wf_generic_action", uniqueConstraints = @UniqueConstraint(columnNames = { "uuid" }))
+@GenericGenerator(name = "ID_GENERATOR", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+        @org.hibernate.annotations.Parameter(name = "sequence_name", value = "wf_generic_action_seq") })
+
+public class Action extends BaseEntity implements Comparable<Action> {
+
+    @Column(name = "uuid", nullable = false, updatable = false, length = 60)
+    @Size(max = 60)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "priority")
+    private int priority;
+
+    @Column(name = "condition_el", length = 2000)
+    @Size(max = 2000)
+    private String conditionEl;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "log_level")
+    private String logLevel;
+
+    @Column(name = "field_to_update")
+    private String fieldToUpdate;
+
+    @Column(name = "is_asynchronous")
+    @Type(type = "numeric_boolean")
+
+    private boolean asynchronous;
+
+    @Column(name = "value_el", length = 2000)
+    @Size(max = 2000)
+    private String valueEL;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "action_script_id")
+    private ScriptInstance actionScript;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "notification_id")
+    private Notification notification;
+
+    @ManyToOne
+    @JoinColumn(name = "transition_id")
+    private GWFTransition transition;
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getConditionEl() {
+        return conditionEl;
+    }
+
+    public void setConditionEl(String conditionEl) {
+        this.conditionEl = conditionEl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isAsynchronous() {
+        return asynchronous;
+    }
+
+    public void setAsynchronous(boolean asynchronous) {
+        this.asynchronous = asynchronous;
+    }
+
+    public ScriptInstance getActionScript() {
+        return actionScript;
+    }
+
+    public void setActionScript(ScriptInstance actionScript) {
+        this.actionScript = actionScript;
+    }
+
+    public Notification getNotification() {
+        return notification;
+    }
+
+    public void setNotification(Notification notification) {
+        this.notification = notification;
+    }
+
+    public GWFTransition getTransition() {
+        return transition;
+    }
+
+    public void setTransition(GWFTransition transition) {
+        this.transition = transition;
+    }
+
+    public String getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(String logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getFieldToUpdate() {
+        return fieldToUpdate;
+    }
+
+    public void setFieldToUpdate(String fieldToUpdate) {
+        this.fieldToUpdate = fieldToUpdate;
+    }
+
+    public String getValueEL() {
+        return valueEL;
+    }
+
+    public void setValueEL(String valueEL) {
+        this.valueEL = valueEL;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public int compareTo(Action action) {
+        return this.priority - action.priority;
+    }
+}