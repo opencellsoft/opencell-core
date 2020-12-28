66da1e5232051c2e909595eb6c8962c70943972f
rachid.aityaazza <rachid.aityaazza@manaty.net>
2020-12-28 11:43:32 +0100

    re #5866 Commercial rules : update dataModel + APIs design

---
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/cpq/CommercialRuleItemDTO.java
@@ -0,0 +1,68 @@
+package org.meveo.api.dto.cpq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.meveo.api.dto.BaseEntityDto;
+import org.meveo.model.cpq.enums.OperatorEnum;
+
+@XmlRootElement(name = "CommercialRuleItemDTO")
+@XmlType(name = "CommercialRuleItemDTO")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CommercialRuleItemDTO extends BaseEntityDto{
+
+    
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2921006853398452396L;
+	
+	private OperatorEnum operator = OperatorEnum.ET;
+	
+	private List<CommercialRuleLineDTO> commercialRuleLines=new ArrayList<CommercialRuleLineDTO>();
+	
+	
+
+	public CommercialRuleItemDTO() {
+		super();
+	}
+
+	/**
+	 * @return the operator
+	 */
+	public OperatorEnum getOperator() {
+		return operator;
+	}
+
+	/**
+	 * @param operator the operator to set
+	 */
+	public void setOperator(OperatorEnum operator) {
+		this.operator = operator;
+	}
+
+	/**
+	 * @return the commercialRuleLines
+	 */
+	public List<CommercialRuleLineDTO> getCommercialRuleLines() {
+		return commercialRuleLines;
+	}
+
+	/**
+	 * @param commercialRuleLines the commercialRuleLines to set
+	 */
+	public void setCommercialRuleLines(List<CommercialRuleLineDTO> commercialRuleLines) {
+		this.commercialRuleLines = commercialRuleLines;
+	}
+	
+
+	
+
+	
+    
+}
