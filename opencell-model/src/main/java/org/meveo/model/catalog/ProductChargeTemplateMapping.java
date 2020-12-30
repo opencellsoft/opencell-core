4d799186db9a7f29ce360e4dcd7d6b87e22eb7e8
rachid.aityaazza <rachid.aityaazza@manaty.net>
2020-12-30 16:00:28 +0100

    re #5870 : add ProductChargeTemplateMapping

---
+++ opencell-model/src/main/java/org/meveo/model/catalog/ProductChargeTemplateMapping.java
@@ -0,0 +1,117 @@
+/*
+ * (C) Copyright 2015-2020 Opencell SAS (https://opencellsoft.com/) and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+ * OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS
+ * IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO
+ * THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE,
+ * YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ * For more information on the GNU Affero General Public License, please consult
+ * <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+ */
+
+package org.meveo.model.catalog;
+
+import java.util.List;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OrderColumn;
+import javax.persistence.Transient;
+
+import org.meveo.model.BaseEntity;
+import org.meveo.model.cpq.Product;
+
+/**
+ * product to charge template mapping
+ * 
+ * @author Rachid Aity-yaazza
+ * @lastModifiedVersion 11.0
+ * @param <T> Charge template type
+ */
+
+
+public class ProductChargeTemplateMapping<T extends ChargeTemplate> extends BaseEntity {
+
+    private static final long serialVersionUID = -1872859127097329926L;
+
+    /**
+     * Service template
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    protected Product product;
+
+    /**
+     * Charge template
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "charge_template_id")
+    protected T chargeTemplate;
+
+    /**
+     * Counter associated to a charge template.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "counter_template_id")
+    private CounterTemplate counterTemplate;
+
+    /**
+     * Prepaid wallet templates to charge on.
+     */
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "cat_product_wallet_template", joinColumns = @JoinColumn(name = "product_templt_id"), inverseJoinColumns = @JoinColumn(name = "wallet_template_id"))
+    @OrderColumn(name = "INDX")
+    private List<WalletTemplate> walletTemplates;
+
+    /**
+     * Counters associated to a charge template.
+     */
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "cat_product_counter_template", joinColumns = @JoinColumn(name = "product_templt_id"), inverseJoinColumns = @JoinColumn(name = "counter_template_id"))
+    @OrderColumn(name = "INDX")
+    private List<CounterTemplate> accumulatorCounterTemplates;
+
+  
+
+    public T getChargeTemplate() {
+        return chargeTemplate;
+    }
+
+    public void setChargeTemplate(T chargeTemplate) {
+        this.chargeTemplate = chargeTemplate;
+    }
+
+    public CounterTemplate getCounterTemplate() {
+        return counterTemplate;
+    }
+
+    public void setCounterTemplate(CounterTemplate counterTemplate) {
+        this.counterTemplate = counterTemplate;
+    }
+
+
+    /**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
