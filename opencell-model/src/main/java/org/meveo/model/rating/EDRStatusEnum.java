bb2b51892f98253d5166c95be420e655c81de6e5
BEN AICHA AMINE <amine.benaicha@opencellsoft.com>
2021-01-04 20:47:46 +0100

    RE #5872 Add Duplicate Status for EDRs

--- opencell-model/src/main/java/org/meveo/model/rating/EDRStatusEnum.java
+++ opencell-model/src/main/java/org/meveo/model/rating/EDRStatusEnum.java
@@ -2,7 +2,8 @@ package org.meveo.model.rating;
 
 public enum EDRStatusEnum {
 
-    OPEN(1, "edrStatus.open"), RATED(2, "edrStatus.rated"), REJECTED(3, "edrStatus.rejected"), MEDIATING(4, "edrStatus.mediating"), AGGREGATED(5, "edrStatus.aggregated");
+    OPEN(1, "edrStatus.open"), RATED(2, "edrStatus.rated"), REJECTED(3, "edrStatus.rejected"), MEDIATING(4, "edrStatus.mediating"), AGGREGATED(5,
+            "edrStatus.aggregated"), DUPLICATED(5, "edrStatus.duplicated");
 
     private Integer id;
     private String label;
