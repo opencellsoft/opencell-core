5ba64a694408daece1911c824f6421d4f930e4d8
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2021-01-09 00:34:35 +0100

    RE 540-116 [AccountingArticleResource] add CRUD API

--- opencell-api/src/main/java/org/meveo/apiv2/article/impl/AccountingArticleResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/article/impl/AccountingArticleResourceImpl.java
@@ -1,14 +1,27 @@
 package org.meveo.apiv2.article.impl;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+import org.meveo.apiv2.article.AccountingArticles;
 import org.meveo.apiv2.article.ImmutableAccountingArticle;
+import org.meveo.apiv2.article.ImmutableAccountingArticles;
 import org.meveo.apiv2.article.resource.AccountingArticleResource;
 import org.meveo.apiv2.article.service.AccountingArticleApiService;
 import org.meveo.apiv2.ordering.common.LinkGenerator;
+//import org.meveo.apiv2.ordering.resource.order.ImmutableOrder;
 import org.meveo.model.article.AccountingArticle;
-
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.ws.rs.core.Response;
 
 public class AccountingArticleResourceImpl implements AccountingArticleResource {
 
@@ -29,6 +42,16 @@ public class AccountingArticleResourceImpl implements AccountingArticleResource 
                 .build();
     }
 
+	@Override
+	public Response updateAccountingArticle(Long id, org.meveo.apiv2.article.AccountingArticle accountingArticle) {
+        AccountingArticle accountingArticleEntity = mapper.toEntity(accountingArticle);
+        Optional<AccountingArticle> accoutningUpdated = accountingArticleApiService.update(id, accountingArticleEntity);
+        return Response
+                .created(LinkGenerator.getUriBuilderFromResource(AccountingArticleResource.class, accountingArticleEntity.getId()).build())
+                .entity(toResourceOrderWithLink(mapper.toResource(accoutningUpdated.get())))
+                .build();
+	}
+
     private org.meveo.apiv2.article.AccountingArticle toResourceOrderWithLink(org.meveo.apiv2.article.AccountingArticle accountingResource) {
         return ImmutableAccountingArticle.copyOf(accountingResource)
                 .withLinks(
@@ -38,5 +61,50 @@ public class AccountingArticleResourceImpl implements AccountingArticleResource 
                                 .build()
                 );
     }
+
+    @Override
+	public Response find(String accountingArticleCode, Request request) {
+		return accountingArticleApiService.findByCode(accountingArticleCode)
+					.map(accounting -> {
+						EntityTag etag = new EntityTag(Integer.toString(accounting.hashCode()));
+	                    CacheControl cc = new CacheControl();
+	                    cc.setMaxAge(1000);
+	                    Response.ResponseBuilder builder = request.evaluatePreconditions(etag);
+	                    if (builder != null) {
+	                        builder.cacheControl(cc);
+	                        return builder.build();
+	                    }
+	                    return Response.ok().cacheControl(cc).tag(etag)
+	                            .entity(toResourceOrderWithLink(mapper.toResource(accounting))).build();
+					}).orElseThrow(NotFoundException::new);
+	}
+
+    @Override
+	public Response delete(String accountingArticleCode, Request request) {
+		return accountingArticleApiService.delete(accountingArticleCode)
+											.map(accountingArticle -> Response.ok().entity(toResourceOrderWithLink(mapper.toResource(accountingArticle))).build())
+												.orElseThrow(NotFoundException::new);
+	}
+
+	public Response list(Long offset, Long limit, String sort, String orderBy, Map<String, Object> filter, Request request) {
+        List<AccountingArticle> accoutnigArticleEntities = accountingArticleApiService.list(offset, limit, sort, orderBy, filter);
+        EntityTag etag = new EntityTag(Integer.toString(accoutnigArticleEntities.hashCode()));
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(1000);
+        Response.ResponseBuilder builder = request.evaluatePreconditions(etag);
+        if (builder != null) {
+            builder.cacheControl(cc);
+            return builder.build();
+        }
+        ImmutableAccountingArticle[] accountingList = accoutnigArticleEntities
+                .stream()
+                .map(accoutingArticle -> toResourceOrderWithLink(mapper.toResource(accoutingArticle)))
+                .toArray(ImmutableAccountingArticle[]::new);
+        Long orderCount = accountingArticleApiService.getCount(filter);
+        AccountingArticles articles = ImmutableAccountingArticles.builder().addData(accountingList).offset(offset).limit(limit).total(orderCount)
+                .build().withLinks(new LinkGenerator.PaginationLinkGenerator(AccountingArticleResource.class)
+                        .offset(offset).limit(limit).total(orderCount).build());
+        return Response.ok().cacheControl(cc).tag(etag).entity(articles).build();
+	}
 
 }
