94c5348dcb38c58b1e9447c078b8f6d9df44f29e
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2021-01-14 08:28:27 +0000

    RE #5527 fix eventDate checks

--- opencell-admin/ejbs/src/main/java/org/meveo/service/medina/impl/CDRParsingService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/medina/impl/CDRParsingService.java
@@ -266,9 +266,9 @@ public class CDRParsingService extends PersistenceService<EDR> {
 						&& (accessPoint.getEndDate() == null || accessPoint.getEndDate().getTime() > cdr.getEventDate().getTime())) {
 					foundMatchingAccess = true;
 					EDR edr = cdrToEdr(cdr, accessPoint, null);
-					if(cdr.getTimestamp().getTime() > edr.getSubscription().getTerminationDate().getTime()) {
+					if(cdr.getEventDate().getTime() > edr.getSubscription().getTerminationDate().getTime()) {
 	                    throw new InvalidAccessException(cdr, CDRRejectionCauseEnum.SUBSCRIPTION_TERMINATED);
-	                } else if(cdr.getTimestamp().getTime() < edr.getSubscription().getSubscriptionDate().getTime()) {
+	                } else if(cdr.getEventDate().getTime() < edr.getSubscription().getSubscriptionDate().getTime()) {
 	                    throw new InvalidAccessException(cdr, CDRRejectionCauseEnum.SUBSCRIPTION_NOT_ACTIVATED);
 	                }
 					edrs.add(edr);
