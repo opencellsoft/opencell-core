peeter-piegaze-sonarsource/core

RE #5921 : new billing process V2

opencell-admin/ejbs/src/main/java/org/meveo/admin/job/InvoiceLinesFactory.java
@@ -1,5 +1,6 @@
 package org.meveo.admin.job;
 
+import static java.math.BigDecimal.ZERO;
 import static java.util.Optional.ofNullable;
 import static java.util.UUID.randomUUID;
 import static org.meveo.admin.job.AggregationConfiguration.AggregationOption.NO_AGGREGATION;
@@ -12,7 +13,6 @@ import java.util.Map;
 
 import org.meveo.model.DatePeriod;
 import org.meveo.model.billing.Subscription;
-import org.meveo.model.cpq.ProductVersion;
 import org.meveo.model.cpq.commercial.InvoiceLine;
 import org.meveo.service.billing.impl.BillingAccountService;
 import org.meveo.service.billing.impl.BillingRunService;
@@ -20,7 +20,6 @@ import org.meveo.service.billing.impl.ServiceInstanceService;
 import org.meveo.service.billing.impl.SubscriptionService;
 import org.meveo.service.billing.impl.article.AccountingArticleService;
 import org.meveo.service.catalog.impl.OfferTemplateService;
-import org.meveo.service.cpq.ProductService;
 import org.meveo.service.cpq.ProductVersionService;
 import org.meveo.service.cpq.order.CommercialOrderService;
 import org.meveo.service.cpq.order.OrderLotService;
@@ -80,13 +79,15 @@ public class InvoiceLinesFactory {
         invoiceLine.setValueDate((Date) record.get("valueDate"));
         invoiceLine.setOrderNumber((String) record.get("order_number"));
         invoiceLine.setQuantity((BigDecimal) record.get("quantity"));
-        invoiceLine.setDiscountAmount(BigDecimal.ZERO);
-        invoiceLine.setDiscountRate(BigDecimal.ZERO);
+        invoiceLine.setDiscountAmount(ZERO);
+        invoiceLine.setDiscountRate(ZERO);
         BigDecimal taxPercent = (BigDecimal) record.get("tax_percent");
         invoiceLine.setTaxRate(taxPercent);
-        BigDecimal amountWithTax = (BigDecimal) record.get("amount_with_tax");
+        BigDecimal amountWithTax = ofNullable((BigDecimal) record.get("amount_with_tax"))
+                .orElse(ZERO);
         invoiceLine.setAmountWithTax(amountWithTax);
-        invoiceLine.setAmountWithoutTax((BigDecimal) record.get("amount_without_tax"));
+        invoiceLine.setAmountWithoutTax(ofNullable((BigDecimal) record.get("amount_without_tax"))
+                .orElse(ZERO));
         invoiceLine.setAmountTax(taxPercent.divide(new BigDecimal(100)).multiply(amountWithTax));
         return invoiceLine;
     }

opencell-admin/ejbs/src/main/java/org/meveo/admin/job/InvoiceLinesJobBean.java
@@ -1,5 +1,12 @@
 package org.meveo.admin.job;
 
+import static java.lang.Long.valueOf;
+import static java.lang.String.format;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toList;
+import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
+import static org.meveo.model.billing.BillingRunStatusEnum.NEW;
+
 import org.apache.commons.collections.map.HashedMap;
 import org.meveo.admin.job.AggregationConfiguration.AggregationOption;
 import org.meveo.admin.job.logging.JobLoggingInterceptor;
@@ -24,13 +31,6 @@ import javax.ejb.TransactionAttribute;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 import java.util.*;
-
-import static java.lang.Long.valueOf;
-import static java.lang.String.format;
-import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.toList;
-import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
-import static org.meveo.model.billing.BillingRunStatusEnum.NEW;
 
 @Stateless
 public class InvoiceLinesJobBean extends BaseJobBean {
@@ -113,7 +113,7 @@ public class InvoiceLinesJobBean extends BaseJobBean {
         String query = "SELECT rt.billing_account__id, \n" +
                 "                 rt.article_id, rt.description as label, SUM(rt.quantity) AS quantity, \n" +
                 "                 rt.unit_amount_without_tax, rt.unit_amount_with_tax,\n" +
-                "                 SUM(rt.amount_without_tax), SUM(rt.amount_with_tax), \n" +
+                "                 SUM(rt.amount_without_tax) as sumWithoutTax, SUM(rt.amount_with_tax) as sumWithTax, \n" +
                 "                 rt.offer_id, rt.service_instance_id,\n" +
                 "                 rt.usage_date, rt.start_date, rt.end_date,\n" +
                 "                 rt.order_number, rt.subscription_id, rt.tax_percent, " + 
@@ -152,7 +152,7 @@ public class InvoiceLinesJobBean extends BaseJobBean {
                 InvoiceLine invoiceLine = linesFactory.create(record, aggregationConfiguration);
                 invoiceLinesService.create(invoiceLine);
             } catch (Exception exception) {
-                log.info(exception.getMessage());
+                log.error(exception.getMessage());
             }
         }
     }

opencellsoft/opencell-core

opencell-admin/ejbs/src/main/java/org/meveo/admin/job/InvoiceLinesFactory.java
@@ -1,5 +1,6 @@
 package org.meveo.admin.job;
 
+import static java.math.BigDecimal.ZERO;
 import static java.util.Optional.ofNullable;
 import static java.util.UUID.randomUUID;
 import static org.meveo.admin.job.AggregationConfiguration.AggregationOption.NO_AGGREGATION;
@@ -12,7 +13,6 @@ import java.util.Map;
 
 import org.meveo.model.DatePeriod;
 import org.meveo.model.billing.Subscription;
-import org.meveo.model.cpq.ProductVersion;
 import org.meveo.model.cpq.commercial.InvoiceLine;
 import org.meveo.service.billing.impl.BillingAccountService;
 import org.meveo.service.billing.impl.BillingRunService;
@@ -20,7 +20,6 @@ import org.meveo.service.billing.impl.ServiceInstanceService;
 import org.meveo.service.billing.impl.SubscriptionService;
 import org.meveo.service.billing.impl.article.AccountingArticleService;
 import org.meveo.service.catalog.impl.OfferTemplateService;
-import org.meveo.service.cpq.ProductService;
 import org.meveo.service.cpq.ProductVersionService;
 import org.meveo.service.cpq.order.CommercialOrderService;
 import org.meveo.service.cpq.order.OrderLotService;
@@ -80,13 +79,15 @@ public class InvoiceLinesFactory {
         invoiceLine.setValueDate((Date) record.get("valueDate"));
         invoiceLine.setOrderNumber((String) record.get("order_number"));
         invoiceLine.setQuantity((BigDecimal) record.get("quantity"));
-        invoiceLine.setDiscountAmount(BigDecimal.ZERO);
-        invoiceLine.setDiscountRate(BigDecimal.ZERO);
+        invoiceLine.setDiscountAmount(ZERO);
+        invoiceLine.setDiscountRate(ZERO);
         BigDecimal taxPercent = (BigDecimal) record.get("tax_percent");
         invoiceLine.setTaxRate(taxPercent);
-        BigDecimal amountWithTax = (BigDecimal) record.get("amount_with_tax");
+        BigDecimal amountWithTax = ofNullable((BigDecimal) record.get("sum_with_tax"))
+                .orElse(ZERO);
         invoiceLine.setAmountWithTax(amountWithTax);
-        invoiceLine.setAmountWithoutTax((BigDecimal) record.get("amount_without_tax"));
+        invoiceLine.setAmountWithoutTax(ofNullable((BigDecimal) record.get("sum_without_Tax"))
+                .orElse(ZERO));
         invoiceLine.setAmountTax(taxPercent.divide(new BigDecimal(100)).multiply(amountWithTax));
         return invoiceLine;
     }

opencell-admin/ejbs/src/main/java/org/meveo/admin/job/InvoiceLinesJobBean.java
@@ -1,5 +1,12 @@
 package org.meveo.admin.job;
 
+import static java.lang.Long.valueOf;
+import static java.lang.String.format;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toList;
+import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
+import static org.meveo.model.billing.BillingRunStatusEnum.NEW;
+
 import org.apache.commons.collections.map.HashedMap;
 import org.meveo.admin.job.AggregationConfiguration.AggregationOption;
 import org.meveo.admin.job.logging.JobLoggingInterceptor;
@@ -24,13 +31,6 @@ import javax.ejb.TransactionAttribute;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 import java.util.*;
-
-import static java.lang.Long.valueOf;
-import static java.lang.String.format;
-import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.toList;
-import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
-import static org.meveo.model.billing.BillingRunStatusEnum.NEW;
 
 @Stateless
 public class InvoiceLinesJobBean extends BaseJobBean {
@@ -113,7 +113,7 @@ public class InvoiceLinesJobBean extends BaseJobBean {
         String query = "SELECT rt.billing_account__id, \n" +
                 "                 rt.article_id, rt.description as label, SUM(rt.quantity) AS quantity, \n" +
                 "                 rt.unit_amount_without_tax, rt.unit_amount_with_tax,\n" +
-                "                 SUM(rt.amount_without_tax), SUM(rt.amount_with_tax), \n" +
+                "                 SUM(rt.amount_without_tax) as sum_without_Tax, SUM(rt.amount_with_tax) as sum_with_tax, \n" +
                 "                 rt.offer_id, rt.service_instance_id,\n" +
                 "                 rt.usage_date, rt.start_date, rt.end_date,\n" +
                 "                 rt.order_number, rt.subscription_id, rt.tax_percent, " + 
@@ -152,7 +152,7 @@ public class InvoiceLinesJobBean extends BaseJobBean {
                 InvoiceLine invoiceLine = linesFactory.create(record, aggregationConfiguration);
                 invoiceLinesService.create(invoiceLine);
             } catch (Exception exception) {
-                log.info(exception.getMessage());
+                log.error(exception.getMessage());
             }
         }
     }