0c947f24f58e0cbd383655eb13f2e929cd16a060
rachid.aityaazza <rachid.aityaazza@manaty.net>
2021-02-19 12:38:48 +0100

    #5869 update validateOrder

--- opencell-api/src/main/java/org/meveo/api/cpq/CommercialOrderApi.java
+++ opencell-api/src/main/java/org/meveo/api/cpq/CommercialOrderApi.java
@@ -438,7 +438,7 @@ public class CommercialOrderApi extends BaseApi {
 	public CommercialOrderDto validateOrder(CommercialOrder order, boolean orderCompleted) {
 		ParamBean paramBean = ParamBean.getInstance();
 		String sellerCode = order.getBillingAccount().getCustomerAccount().getCustomer().getSeller().getCode();
-		String quoteScriptCode = paramBean.getProperty("seller." + sellerCode + ".orderValidationScript", "org.meveo.service.script.OrderValidationScript");
+		String quoteScriptCode = paramBean.getProperty("seller." + sellerCode + ".orderValidationScript", "");
 		if (!StringUtils.isBlank(quoteScriptCode)) {
 			ScriptInstance scriptInstance = scriptInstanceService.findByCode(quoteScriptCode);
 			if (scriptInstance != null) {
