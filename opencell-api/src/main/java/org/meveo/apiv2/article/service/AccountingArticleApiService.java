eb6e35377caddb50f645a666a4f6fd92abe8f1b8
Mbarek-Ay <mbarek.aityaazza@opencellsoft.com>
2021-03-01 19:40:53 +0100

    re #5623 540-302 - update Acounting article: descriptionI18n is not accepted as a parameter

--- opencell-api/src/main/java/org/meveo/apiv2/article/service/AccountingArticleApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/article/service/AccountingArticleApiService.java
@@ -133,6 +133,9 @@ public class AccountingArticleApiService implements AccountingArticleServiceBase
         if(!Strings.isEmpty(baseEntity.getDescription())) {
         	accountingArticle.setDescription(baseEntity.getDescription());
         }
+        if(baseEntity.getDescriptionI18n()!=null && !baseEntity.getDescriptionI18n().isEmpty() ) {
+        	accountingArticle.getDescriptionI18n().putAll((baseEntity.getDescriptionI18n()));
+        }
         accountingArticleService.update(accountingArticle);
         
         return accountingArticleOtional;
