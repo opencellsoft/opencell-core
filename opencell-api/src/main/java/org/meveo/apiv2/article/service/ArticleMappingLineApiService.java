c8ee27afe2dd93115e1cb002e012b638263bacb7
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2021-03-03 10:42:43 +0100

    RE #5937--cpq-v11--bug-fixes 540-305 [Article Mapping Line] - CRUD article lines (add update api)

--- opencell-api/src/main/java/org/meveo/apiv2/article/service/ArticleMappingLineApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/article/service/ArticleMappingLineApiService.java
@@ -13,6 +13,7 @@ import org.meveo.model.cpq.Product;
 import org.meveo.service.billing.impl.article.AccountingArticleService;
 import org.meveo.service.billing.impl.article.ArticleMappingLineService;
 import org.meveo.service.billing.impl.article.ArticleMappingService;
+import org.meveo.service.billing.impl.article.AttributeMappingService;
 import org.meveo.service.catalog.impl.ChargeTemplateService;
 import org.meveo.service.catalog.impl.OfferTemplateService;
 import org.meveo.service.catalog.impl.ProductTemplateService;
@@ -21,6 +22,9 @@ import org.meveo.service.cpq.ProductService;
 
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -41,6 +45,8 @@ public class ArticleMappingLineApiService implements ApiService<ArticleMappingLi
     private AttributeService attributeService;
     @Inject
     private ProductService productService;
+    @Inject
+    private AttributeMappingService AttributeMappingService;
 
     @Override
     public List<ArticleMappingLine> list(Long offset, Long limit, String sort, String orderBy, String filter) {
@@ -65,38 +71,22 @@ public class ArticleMappingLineApiService implements ApiService<ArticleMappingLi
         ArticleMapping articleMapping = articleMappingApiService.findById(articleMappingLine.getArticleMapping().getId());
         if(articleMapping == null)
             throw new BadRequestException("No article mapping found with id: " + articleMappingLine.getArticleMapping().getId());
-        if(articleMappingLine.getOfferTemplate() != null){
-            OfferTemplate offerTemplate = offerTemplateService.findById(articleMappingLine.getOfferTemplate().getId());
-            if(offerTemplate == null)
-                throw new BadRequestException("No offer template found with id: " + articleMappingLine.getOfferTemplate().getId());
-            articleMappingLine.setOfferTemplate(offerTemplate);
-        }
-        if(articleMappingLine.getProduct() != null){
-            Product product = productService.findById(articleMappingLine.getProduct().getId());
-            if(product == null)
-                throw new BadRequestException("No product template found with id: " + articleMappingLine.getProduct().getId());
-            articleMappingLine.setProduct(product);
-        }
-        if(articleMappingLine.getChargeTemplate() != null){
-            ChargeTemplate chargeTemplate = chargeTemplateService.findById(articleMappingLine.getChargeTemplate().getId());
-            if(chargeTemplate == null)
-                throw new BadRequestException("No charge template found with id: " + articleMappingLine.getChargeTemplate().getId());
-            articleMappingLine.setChargeTemplate(chargeTemplate);
-        }
+
         if(articleMappingLine.getAttributesMapping() != null && !articleMappingLine.getAttributesMapping().isEmpty()){
             List<AttributeMapping> attributesMapping = articleMappingLine.getAttributesMapping()
                     .stream()
                     .map(am -> {
                         Attribute attribute = attributeService.findById(am.getAttribute().getId());
                         if (attribute == null)
-                            throw new BadRequestException("No attribute found with Id: " + attribute.getId());
+                            throw new BadRequestException("No attribute found with Id: " + am.getAttribute().getId());
                         AttributeMapping attributeMapping = new AttributeMapping(attribute, am.getAttributeValue());
                         attributeMapping.setArticleMappingLine(articleMappingLine);
                         return attributeMapping;
                     })
                     .collect(Collectors.toList());
-            articleMappingLine.setAttributesMapping(attributesMapping);
+            articleMappingLine.getAttributesMapping().addAll(attributesMapping);
         }
+        populateArtcileMappingLine(articleMappingLine);
         articleMappingLine.setAccountingArticle(accountingArticle);
         articleMappingLine.setArticleMapping(articleMapping);
         articleMappingLineService.create(articleMappingLine);
@@ -105,9 +95,65 @@ public class ArticleMappingLineApiService implements ApiService<ArticleMappingLi
 
     @Override
     public Optional<ArticleMappingLine> update(Long id, ArticleMappingLine articleMappingLine) {
-        ArticleMapping articleMapping = Optional.ofNullable(articleMappingApiService.findById(id)).get();
-        return Optional.empty();
+//    	List<String> fields = Arrays.asList("accountingArticle", "articleMapping", "offerTemplate", "product", "chargeTemplate");
+        ArticleMappingLine articleMappingLineUpdated = articleMappingLineService.findById(id, true);
+        if(articleMappingLineUpdated == null) return Optional.empty();
+        if(articleMappingLine.getAccountingArticle().getId() != null) {
+	        AccountingArticle accountingArticle = accountingArticleApiService.findById(articleMappingLine.getAccountingArticle().getId());
+	        if(accountingArticle == null)
+	            throw new BadRequestException("No accounting article found with id: " + articleMappingLine.getAccountingArticle().getId());
+	        articleMappingLineUpdated.setAccountingArticle(accountingArticle);
+        }
+        if(articleMappingLine.getArticleMapping().getId() != null) {
+	        ArticleMapping articleMapping = articleMappingApiService.findById(articleMappingLine.getArticleMapping().getId());
+	        if(articleMapping == null)
+	            throw new BadRequestException("No article mapping found with id: " + articleMappingLine.getArticleMapping().getId());
+	        articleMappingLineUpdated.setArticleMapping(articleMapping);
+        }
+        populateArtcileMappingLine(articleMappingLineUpdated);
+//        articleMappingLineUpdated.getAttributesMapping().forEach(am -> AttributeMappingService.remove(am));
+        articleMappingLineUpdated.getAttributesMapping().clear();
+        if(articleMappingLine.getAttributesMapping() != null && !articleMappingLine.getAttributesMapping().isEmpty()){
+            List<AttributeMapping> attributesMapping = articleMappingLine.getAttributesMapping()
+                    .stream()
+                    .map(am -> {
+                        Attribute attribute = attributeService.findById(am.getAttribute().getId());
+                        if (attribute == null)
+                            throw new BadRequestException("No attribute found with Id: " + am.getAttribute().getId());
+                        AttributeMapping attributeMapping = new AttributeMapping(attribute, am.getAttributeValue());
+                        attributeMapping.setArticleMappingLine(articleMappingLineUpdated);
+                        return attributeMapping;
+                    })
+                    .collect(Collectors.toList());
+            articleMappingLineUpdated.getAttributesMapping().addAll(attributesMapping);
+        }
+        articleMappingLineService.update(articleMappingLineUpdated);
+        return Optional.of(articleMappingLineUpdated);
     }
+    
+    private void populateArtcileMappingLine(ArticleMappingLine articleMappingLine) {
+    	 if(articleMappingLine.getOfferTemplate() != null){
+             OfferTemplate offerTemplate = offerTemplateService.findById(articleMappingLine.getOfferTemplate().getId());
+             if(offerTemplate == null)
+                 throw new BadRequestException("No offer template found with id: " + articleMappingLine.getOfferTemplate().getId());
+             articleMappingLine.setOfferTemplate(offerTemplate);
+         }
+         if(articleMappingLine.getProduct() != null){
+             Product product = productService.findById(articleMappingLine.getProduct().getId());
+             if(product == null)
+                 throw new BadRequestException("No product template found with id: " + articleMappingLine.getProduct().getId());
+             articleMappingLine.setProduct(product);
+         }
+         if(articleMappingLine.getChargeTemplate() != null){
+             ChargeTemplate chargeTemplate = chargeTemplateService.findById(articleMappingLine.getChargeTemplate().getId());
+             if(chargeTemplate == null)
+                 throw new BadRequestException("No charge template found with id: " + articleMappingLine.getChargeTemplate().getId());
+             articleMappingLine.setChargeTemplate(chargeTemplate);
+         } 
+    }
+    
+   
+    
 
     @Override
     public Optional<ArticleMappingLine> patch(Long id, ArticleMappingLine baseEntity) {
