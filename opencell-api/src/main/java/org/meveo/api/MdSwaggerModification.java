5fb89545407b6b9cfd74422190ab7a015e1e5cfe
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2021-03-17 16:25:37 +0100

    re #5962 use logger

--- opencell-api/src/main/java/org/meveo/api/MdSwaggerModification.java
+++ opencell-api/src/main/java/org/meveo/api/MdSwaggerModification.java
@@ -59,7 +59,7 @@ public class MdSwaggerModification {
                 writer.println(str);
             }
         } catch (Exception e) {
-            System.out.println("File not found");
+            log.error("File not found");
             log.error("error = {}", e);
         }
     }
@@ -73,7 +73,7 @@ public class MdSwaggerModification {
             }
         } catch (FileNotFoundException e1) {
             //e1.printStackTrace();
-            System.out.println("File Not Found for " + nameFile);
+            log.error("File Not Found for " + nameFile);
             returnline = "    @%" + nameFile;
 
         } catch (IOException e1) {
@@ -90,8 +90,8 @@ public class MdSwaggerModification {
                 returnline = returnline + "    " + str + "  \n";
             }
         } catch (FileNotFoundException e1) {
-            //e1.printStackTrace();
-            System.out.println("File not Found for " + nameFile);
+            
+            log.error("File not Found for " + nameFile);
             returnline = "    @%" + nameFile;
 
         } catch (IOException e1) {
