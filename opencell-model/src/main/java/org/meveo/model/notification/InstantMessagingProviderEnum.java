f67fd46c19d692ce213c4700406275564970173f
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2021-03-30 16:24:02 +0100

    re #6005 remove vulnerable Skype lib

--- opencell-model/src/main/java/org/meveo/model/notification/InstantMessagingProviderEnum.java
+++ opencell-model/src/main/java/org/meveo/model/notification/InstantMessagingProviderEnum.java
@@ -19,7 +19,7 @@
 package org.meveo.model.notification;
 
 public enum InstantMessagingProviderEnum {
-    SKYPE(1, "enum.InstantMessagingProviderEnum.SKYPE"), FACEBOOK(2, "enum.InstantMessagingProviderEnum.FACEBOOK"), TWITTER(3, "enum.InstantMessagingProviderEnum.TWITTER"), GTALK(
+    FACEBOOK(2, "enum.InstantMessagingProviderEnum.FACEBOOK"), TWITTER(3, "enum.InstantMessagingProviderEnum.TWITTER"), GTALK(
             4, "enum.InstantMessagingProviderEnum.GTALK"), YAHOO_MESSENGER(5, "enum.InstantMessagingProviderEnum.YAHOO_MESSENGER");
 
     private Integer id;
