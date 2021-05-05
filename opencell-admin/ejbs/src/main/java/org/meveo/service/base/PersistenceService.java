f8fb1e6633dca1f477f1bcf9c34f9b0acf8b6041
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2021-05-05 15:04:50 +0000

    RE #6092 - [Generic API] - Sort by properties is not working on custom fields

--- opencell-admin/ejbs/src/main/java/org/meveo/service/base/PersistenceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/base/PersistenceService.java
@@ -1163,6 +1163,9 @@ public abstract class PersistenceService<E extends IEntity> extends BaseService 
     }
 
 	private void adaptOrdering(PaginationConfiguration config, Map<String, Object> filters) {
+		if(config==null) {
+			return;
+		}
 		List<String> orderings = new ArrayList<String>();
 		for (Object x : config.getOrderings()) {
 			String orderElement = x.toString();
