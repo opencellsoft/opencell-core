b0462a1bdbd943a13f88c924a73ce01dcab316ae
Andrius Karpavicius <andrius.karpavicius@opencellsoft.com>
2021-05-11 11:03:54 -0300

    Re #6117 - Invoicing job performance improvement

--- opencell-model/src/main/java/org/meveo/model/billing/ChargeInstance.java
+++ opencell-model/src/main/java/org/meveo/model/billing/ChargeInstance.java
@@ -126,8 +126,7 @@ public abstract class ChargeInstance extends BusinessCFEntity {
     protected ChargeTemplate chargeTemplate;
 
     /**
-     * Calendar to use when creating Wallet operations. Service subscription start date is taken as calendar's initiation date. Invoicing calendar to calculate if operation should
-     * be invoiced on an future date.
+     * Invoicing calendar to calculate if operation should be invoiced on an future date.
      */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "invoicing_calendar_id")
