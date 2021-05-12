58aeb5303e5959837d66807921e0992a6bb53ff5
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2021-05-12 12:59:32 +0000

    re #6119 Fix Sonar Vulnerabilities and Bugs

--- opencell-admin/web/src/main/java/org/meveo/admin/web/servlet/PictureServlet.java
+++ opencell-admin/web/src/main/java/org/meveo/admin/web/servlet/PictureServlet.java
@@ -21,7 +21,6 @@ package org.meveo.admin.web.servlet;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
