74f4c247b89c5bd96c641e00ce015adac70100ac
Nabil OUACHI <nabil.ouachi@opencellsoft.com>
2021-06-01 17:13:37 +0100

    RE rollback new transaction creation

--- opencell-model/src/main/java/org/meveo/model/cpq/commercial/CommercialOrder.java
+++ opencell-model/src/main/java/org/meveo/model/cpq/commercial/CommercialOrder.java
@@ -364,7 +364,7 @@ public class CommercialOrder extends AuditableCFEntity implements IBillableEntit
 	/**
 	 * @param orderProgress the orderProgress to set
 	 */
-	public void OsetOrderProgress(Integer orderProgress) {
+	public void setOrderProgress(Integer orderProgress) {
 		this.orderProgress = orderProgress;
 	}
 
