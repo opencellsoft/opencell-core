ede930baa67c2a06365cdacea77db0968b57b757
anasseh <abdelhadi.nasseh@opencellsoft.com>
2021-06-04 10:44:21 +0100

    re #5416 - Add list payment history by invoice (part 2)

--- opencell-api-dto/src/main/java/org/meveo/api/dto/payment/AccountOperationDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/payment/AccountOperationDto.java
@@ -25,6 +25,8 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.meveo.api.dto.AuditableEntityDto;
