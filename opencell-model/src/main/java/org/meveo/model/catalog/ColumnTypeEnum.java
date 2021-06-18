507088970a648c0798725296fff5e61d7408ba86
rachid.aityaazza <rachid.aityaazza@manaty.net>
2021-06-18 12:39:59 +0100

    #5937 : Range PPM : fix NP when doubleValue is null

--- opencell-model/src/main/java/org/meveo/model/catalog/ColumnTypeEnum.java
+++ opencell-model/src/main/java/org/meveo/model/catalog/ColumnTypeEnum.java
@@ -133,16 +133,17 @@ public enum ColumnTypeEnum {
             if (attributeValue.getDoubleValue() == null && (pricePlanMatrixValue.getFromDoubleValue() == null && pricePlanMatrixValue.getToDoubleValue() == null)) {
                 return true;
             }
-            if (pricePlanMatrixValue.getFromDoubleValue() != null && pricePlanMatrixValue.getToDoubleValue() == null &&  attributeValue.getDoubleValue() > pricePlanMatrixValue.getFromDoubleValue()) {
+            if (attributeValue.getDoubleValue() != null && pricePlanMatrixValue.getFromDoubleValue() != null && pricePlanMatrixValue.getToDoubleValue() == null &&  attributeValue.getDoubleValue() > pricePlanMatrixValue.getFromDoubleValue()) {
                 return true;
             }
-            if (pricePlanMatrixValue.getFromDoubleValue() == null && pricePlanMatrixValue.getToDoubleValue() != null &&  attributeValue.getDoubleValue() <= pricePlanMatrixValue.getToDoubleValue()) {
+            if (attributeValue.getDoubleValue() != null && pricePlanMatrixValue.getFromDoubleValue() == null && pricePlanMatrixValue.getToDoubleValue() != null &&  attributeValue.getDoubleValue() <= pricePlanMatrixValue.getToDoubleValue()) {
                 return true;
             }
-            else {
-                return attributeValue.getDoubleValue() > pricePlanMatrixValue.getFromDoubleValue()
-                        && attributeValue.getDoubleValue() <= pricePlanMatrixValue.getToDoubleValue();
+            if(attributeValue.getDoubleValue() != null && attributeValue.getDoubleValue() > pricePlanMatrixValue.getFromDoubleValue()
+                    && attributeValue.getDoubleValue() <= pricePlanMatrixValue.getToDoubleValue()){
+                return true;
             }
+           return false;
 
         }
 
