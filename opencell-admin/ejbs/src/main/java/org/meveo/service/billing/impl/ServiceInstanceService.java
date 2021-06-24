2a67d9ec9f5f3e35d20bb24b8bc92b5f8f869965
mboukayoua <mounir.boukayoua@opencellsoft.com>
2021-06-24 16:14:18 +0200

    INTRD-273 Terminate a suspended service is not possible (after review)

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceInstanceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceInstanceService.java
@@ -888,13 +888,13 @@ public class ServiceInstanceService extends BusinessService<ServiceInstance> {
     public List<Long> getSubscriptionsToRenewOrNotify(Date untillDate) {
         List<Long> ids = getEntityManager().createNamedQuery("ServiceInstance.getExpired", Long.class) //
             .setParameter("date", untillDate) //
-            .setParameter("subscriptionStatuses", Arrays.asList(SubscriptionStatusEnum.ACTIVE, SubscriptionStatusEnum.CREATED, SubscriptionStatusEnum.SUSPENDED)) //
+            .setParameter("subscriptionStatuses", Arrays.asList(SubscriptionStatusEnum.ACTIVE, SubscriptionStatusEnum.SUSPENDED)) //
             .setParameter("statuses", Arrays.asList(InstanceStatusEnum.ACTIVE, InstanceStatusEnum.SUSPENDED)) //
             .getResultList();
 
         ids.addAll(getEntityManager().createNamedQuery("ServiceInstance.getToNotifyExpiration", Long.class) //
             .setParameter("date", untillDate) //
-            .setParameter("subscriptionStatuses", Arrays.asList(SubscriptionStatusEnum.ACTIVE, SubscriptionStatusEnum.CREATED, SubscriptionStatusEnum.SUSPENDED)) //
+            .setParameter("subscriptionStatuses", Arrays.asList(SubscriptionStatusEnum.ACTIVE, SubscriptionStatusEnum.SUSPENDED)) //
             .setParameter("statuses", Arrays.asList(InstanceStatusEnum.ACTIVE, InstanceStatusEnum.SUSPENDED)) //
             .getResultList());
 
