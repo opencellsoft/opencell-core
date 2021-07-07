038c7b0887e94abd4dec31bab8b5b9031b542e1a
Mbarek-Ay <mbarek.aityaazza@opencellsoft.com>
2021-07-07 19:50:53 +0100

    re #INTRD-680 - Create Global setting JPa services

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/InvoiceDunningStatusesService.java
@@ -0,0 +1,18 @@
+package org.meveo.service.payments.impl;
+import javax.ejb.Stateless;
+
+import org.meveo.model.dunning.InvoiceDunningStatuses;
+import org.meveo.service.base.PersistenceService;
+
+/**
+ * Service implementation to manage InvoiceDunningStatuses entity.
+ * It extends {@link PersistenceService} class
+ * 
+ * @author Mbarek-Ay
+ * @version 11.0
+ *
+ */
+@Stateless
+public class InvoiceDunningStatusesService extends PersistenceService<InvoiceDunningStatuses> {
+
+}
