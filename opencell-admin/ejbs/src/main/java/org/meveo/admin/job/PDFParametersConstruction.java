465a1a944f5084079662efb7a2c4a8f48e01aaf7
Amine TAZIn,n,n,** <amine.tazi@opencellsoft.com>
2021-07-16 10:52:51 +0100

    EIROPENE2E-510

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/PDFParametersConstruction.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/PDFParametersConstruction.java
@@ -86,7 +86,7 @@ public class PDFParametersConstruction {
                 ClassLoader cl = new URLClassLoader(new URL[] { resource });
                 parameters.put(JRParameter.REPORT_CLASS_LOADER, cl);
             } else {
-                log.error("the reports/fonts.jar not found");
+                log.warn("the reports/fonts.jar not found");
             }
             currencyFormat.setMinimumFractionDigits(2);
 
