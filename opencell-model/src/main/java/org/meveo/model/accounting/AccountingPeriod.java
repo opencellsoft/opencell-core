d6209e9bdb4a3345cb897c3b6fe827e441a89ad8
BEN AICHA AMINE <amine.benaicha@opencellsoft.com>
2021-08-11 18:12:52 +0100

    merge 11.X

--- opencell-model/src/main/java/org/meveo/model/accounting/AccountingPeriod.java
+++ opencell-model/src/main/java/org/meveo/model/accounting/AccountingPeriod.java
@@ -8,80 +8,124 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
-import org.meveo.model.AuditableEntity;
 import org.meveo.model.BaseEntity;
 
 @Entity
 @Table(name = "accounting_period")
 @GenericGenerator(name = "ID_GENERATOR", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
         @Parameter(name = "sequence_name", value = "accounting_period_seq"), })
-public class AccountingPeriod extends BaseEntity{
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5424629380105543225L;
+public class AccountingPeriod extends BaseEntity {
 
-	@Column(name = "accounting_period_year")
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5424629380105543225L;
+
+    @Column(name = "accounting_period_year")
     private Integer accountingPeriodYear;
-	
-	@Enumerated(value = EnumType.STRING)
+
+    @Enumerated(value = EnumType.STRING)
     @Column(name = "status")
     private AccountingPeriodStatusEnum accountingPeriodStatus;
-	
-	@Enumerated(value = EnumType.STRING)
+
+    @Enumerated(value = EnumType.STRING)
     @Column(name = "sub_accounting_period_type")
     private SubAccountingPeriodTypeEnum subAccountingPeriodType;
-	
-	@Column(name = "sub_accounting_period_progress")
+
+    @Column(name = "sub_accounting_period_progress")
     private String subAccountingPeriodProgress;
-	
-	@Column(name = "ongoing_sub_accounting_periods")
+
+    @Column(name = "ongoing_sub_accounting_periods")
     private String ongoingSubAccountingPeriods;
-	
 
-	public AccountingPeriod() {
-		super();
-	}
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "accounting_operation_action")
+    private AccountingOperationAction accountingOperationAction;
 
-	public Integer getAccountingPeriodYear() {
-		return accountingPeriodYear;
-	}
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "regular_user_lock_option")
+    private RegularUserLockOption regularUserLockOption;
 
-	public void setAccountingPeriodYear(Integer accountingPeriodYear) {
-		this.accountingPeriodYear = accountingPeriodYear;
-	}
+    @Column(name = "custom_lock_number_days ")
+    private Integer customLockNumberDays;
 
-	public AccountingPeriodStatusEnum getAccountingPeriodStatus() {
-		return accountingPeriodStatus;
-	}
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "custom_lock_option ")
+    private CustomLockOption customLockOption;
 
-	public void setAccountingPeriodStatus(AccountingPeriodStatusEnum accountingPeriodStatus) {
-		this.accountingPeriodStatus = accountingPeriodStatus;
-	}
+    public AccountingPeriod() {
+        super();
+    }
 
-	public SubAccountingPeriodTypeEnum getSubAccountingPeriodType() {
-		return subAccountingPeriodType;
-	}
+    public Integer getAccountingPeriodYear() {
+        return accountingPeriodYear;
+    }
 
-	public void setSubAccountingPeriodType(SubAccountingPeriodTypeEnum subAccountingPeriodType) {
-		this.subAccountingPeriodType = subAccountingPeriodType;
-	}
+    public void setAccountingPeriodYear(Integer accountingPeriodYear) {
+        this.accountingPeriodYear = accountingPeriodYear;
+    }
 
-	public String getSubAccountingPeriodProgress() {
-		return subAccountingPeriodProgress;
-	}
+    public AccountingPeriodStatusEnum getAccountingPeriodStatus() {
+        return accountingPeriodStatus;
+    }
 
-	public void setSubAccountingPeriodProgress(String subAccountingPeriodProgress) {
-		this.subAccountingPeriodProgress = subAccountingPeriodProgress;
-	}
+    public void setAccountingPeriodStatus(AccountingPeriodStatusEnum accountingPeriodStatus) {
+        this.accountingPeriodStatus = accountingPeriodStatus;
+    }
 
-	public String getOngoingSubAccountingPeriods() {
-		return ongoingSubAccountingPeriods;
-	}
+    public SubAccountingPeriodTypeEnum getSubAccountingPeriodType() {
+        return subAccountingPeriodType;
+    }
 
-	public void setOngoingSubAccountingPeriods(String ongoingSubAccountingPeriods) {
-		this.ongoingSubAccountingPeriods = ongoingSubAccountingPeriods;
-	}
+    public void setSubAccountingPeriodType(SubAccountingPeriodTypeEnum subAccountingPeriodType) {
+        this.subAccountingPeriodType = subAccountingPeriodType;
+    }
 
+    public String getSubAccountingPeriodProgress() {
+        return subAccountingPeriodProgress;
+    }
+
+    public void setSubAccountingPeriodProgress(String subAccountingPeriodProgress) {
+        this.subAccountingPeriodProgress = subAccountingPeriodProgress;
+    }
+
+    public String getOngoingSubAccountingPeriods() {
+        return ongoingSubAccountingPeriods;
+    }
+
+    public void setOngoingSubAccountingPeriods(String ongoingSubAccountingPeriods) {
+        this.ongoingSubAccountingPeriods = ongoingSubAccountingPeriods;
+    }
+
+    public AccountingOperationAction getAccountingOperationAction() {
+        return accountingOperationAction;
+    }
+
+    public void setAccountingOperationAction(AccountingOperationAction accountingOperationAction) {
+        this.accountingOperationAction = accountingOperationAction;
+    }
+
+    public RegularUserLockOption getRegularUserLockOption() {
+        return regularUserLockOption;
+    }
+
+    public void setRegularUserLockOption(RegularUserLockOption regularUserLockOption) {
+        this.regularUserLockOption = regularUserLockOption;
+    }
+
+    public Integer getCustomLockNumberDays() {
+        return customLockNumberDays;
+    }
+
+    public void setCustomLockNumberDays(Integer customLockNumberDays) {
+        this.customLockNumberDays = customLockNumberDays;
+    }
+
+    public CustomLockOption getCustomLockOption() {
+        return customLockOption;
+    }
+
+    public void setCustomLockOption(CustomLockOption customLockOption) {
+        this.customLockOption = customLockOption;
+    }
 }
