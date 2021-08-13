ac183c779578fbc8829c64f2710f7cf9a4578594
BEN AICHA AMINE <amine.benaicha@opencellsoft.com>
2021-08-13 17:13:59 +0100

    Merge on 10.X

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/accounting/impl/SubAccountingPeriodService.java
@@ -0,0 +1,27 @@
+package org.meveo.service.accounting.impl;
+
+import java.util.Date;
+
+import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import org.meveo.model.accounting.AccountingPeriod;
+import org.meveo.model.accounting.SubAccountingPeriod;
+import org.meveo.service.base.PersistenceService;
+
+@Stateless
+public class SubAccountingPeriodService extends PersistenceService<SubAccountingPeriod> {
+
+    public SubAccountingPeriod findByAccountingPeriod(AccountingPeriod accountingPeriod, Date accountingDate) {
+        TypedQuery<SubAccountingPeriod> query = getEntityManager()
+            .createQuery("select s from " + entityClass.getSimpleName() + " s where accountingPeriod=:accountingPeriod and (:accountingDate >= startDate and :accountingDate <= endDate)",
+                entityClass)
+            .setParameter("accountingPeriod", accountingPeriod).setParameter("accountingDate", accountingDate);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+}
