82e88fd846780a8962c5ed069851b2bf58e4ef4e
mboukayoua <mounir.boukayoua@opencellsoft.com>
2021-09-15 16:36:11 +0200

    INTRD-278 Impossible de trigger de la notification sur la facture type "updateStatus" par le job UpdateUnpaidInvoiceStatusJob

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -3000,8 +3000,10 @@ public class InvoiceService extends PersistenceService<Invoice> {
     /**
      * Update unpaid invoices status
      */
-    public void updateUnpaidInvoicesStatus() {
-        getEntityManager().createNamedQuery("Invoice.updateUnpaidInvoicesStatus").executeUpdate();
+    public List<Long> listUnpaidInvoicesIds() {
+        return getEntityManager()
+                .createNamedQuery("Invoice.listUnpaidInvoicesIds", Long.class)
+                .getResultList();
     }
 
     /**
