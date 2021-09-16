4b684435a311f9c555d023876c83f41fdedaabec
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2021-09-16 13:14:15 +0100

    Adding nbRun & waitingMills custom in entity inherent job

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/dwh/DWHQueryJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/dwh/DWHQueryJob.java
@@ -18,10 +18,16 @@
 
 package org.meveo.admin.job.dwh;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.admin.job.cdr.CDRBackoutJob;
+import org.meveo.model.crm.CustomFieldTemplate;
+import org.meveo.model.crm.custom.CustomFieldTypeEnum;
 import org.meveo.model.jobs.JobCategoryEnum;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.jobs.JobInstance;
@@ -48,5 +54,36 @@ public class DWHQueryJob extends Job {
     @Override
     public JobCategoryEnum getJobCategory() {
         return MeveoJobCategoryEnum.DWH;
+    }
+
+    @Override
+    public Map<String, CustomFieldTemplate> getCustomFields() {
+        Map<String, CustomFieldTemplate> result = new HashMap<>();
+
+        CustomFieldTemplate nbRuns = new CustomFieldTemplate();
+        nbRuns.setCode(CF_NB_RUNS);
+        nbRuns.setAppliesTo("JobInstance_" + this.getClass().getName());
+        nbRuns.setActive(true);
+        nbRuns.setDescription(resourceMessages.getString("jobExecution.nbRuns"));
+        nbRuns.setFieldType(CustomFieldTypeEnum.LONG);
+        nbRuns.setValueRequired(false);
+        nbRuns.setDefaultValue("-1");
+        nbRuns.setGuiPosition("tab:Configuration:0;field:0");
+        result.put(CF_NB_RUNS, nbRuns);
+
+        CustomFieldTemplate waitingMillis = new CustomFieldTemplate();
+        waitingMillis.setCode(Job.CF_WAITING_MILLIS);
+        waitingMillis.setAppliesTo("JobInstance_" + this.getClass().getName());
+        waitingMillis.setActive(true);
+        waitingMillis.setDescription(resourceMessages.getString("jobExecution.waitingMillis"));
+        waitingMillis.setFieldType(CustomFieldTypeEnum.LONG);
+        waitingMillis.setValueRequired(false);
+        waitingMillis.setDefaultValue("0");
+
+        waitingMillis.setGuiPosition("tab:Configuration:0;field:1");
+        result.put(Job.CF_WAITING_MILLIS, waitingMillis);
+
+        result.put("waitingMillis", waitingMillis);
+        return result;
     }
 }
