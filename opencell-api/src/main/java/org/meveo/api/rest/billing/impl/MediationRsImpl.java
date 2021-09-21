df71ee19fd0a959664f0783299928859080b70ae
nabilouachi <nabil.ouachi@opencellsoft.com>
2021-09-21 07:02:56 +0100

    RE #INTRD-1565 Cdr process policy

--- opencell-api/src/main/java/org/meveo/api/rest/billing/impl/MediationRsImpl.java
+++ opencell-api/src/main/java/org/meveo/api/rest/billing/impl/MediationRsImpl.java
@@ -25,6 +25,8 @@ import javax.interceptor.Interceptors;
 import org.meveo.api.billing.MediationApi;
 import org.meveo.api.dto.ActionStatus;
 import org.meveo.api.dto.ActionStatusEnum;
+import org.meveo.api.dto.billing.CdrErrorDto;
+import org.meveo.api.dto.billing.CdrErrorListDto;
 import org.meveo.api.dto.billing.CdrListDto;
 import org.meveo.api.dto.billing.ChargeCDRDto;
 import org.meveo.api.dto.billing.ChargeCDRResponseDto;
@@ -34,6 +36,8 @@ import org.meveo.api.logging.WsRestApiInterceptor;
 import org.meveo.api.rest.billing.MediationRs;
 import org.meveo.api.rest.impl.BaseRs;
 import org.meveo.commons.utils.StringUtils;
+
+import java.util.List;
 
 /**
  * Mediation related API REST implementation
@@ -57,7 +61,10 @@ public class MediationRsImpl extends BaseRs implements MediationRs {
         try {
             String ip = StringUtils.isBlank(httpServletRequest.getHeader("x-forwarded-for")) ? httpServletRequest.getRemoteAddr() : httpServletRequest.getHeader("x-forwarded-for");
             postData.setIpAddress(ip);
-            mediationApi.registerCdrList(postData);
+            List<CdrErrorDto> cdrErrorDtos = mediationApi.registerCdrList(postData);
+            if(!cdrErrorDtos.isEmpty())
+                return new CdrErrorListDto(ActionStatusEnum.FAIL, "error while creating CDRs", cdrErrorDtos);
+
         } catch (Exception e) {
             processException(e, result);
         }
