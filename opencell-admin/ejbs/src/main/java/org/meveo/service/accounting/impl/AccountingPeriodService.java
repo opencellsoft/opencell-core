25220f3ffc99a75b60c87274b8ecad57d8b82f27
mstitane <mohamed.stitane@opencellsoft.com>
2021-09-30 18:26:06 +0100

    INTRD-2131 add control on end date

--- opencell-admin/ejbs/src/main/java/org/meveo/service/accounting/impl/AccountingPeriodService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/accounting/impl/AccountingPeriodService.java
@@ -1,7 +1,9 @@
 package org.meveo.service.accounting.impl;
 
 import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
@@ -76,6 +78,11 @@ public class AccountingPeriodService extends PersistenceService<AccountingPeriod
 		}
 		if (endDate.before(new Date())) {
 			throw new ValidationException("the given endDate " + DateUtils.formatAsDate(endDate) + " is incorrect , the endDate must be greater than today");
+		}
+
+		List<String> allowedDates = Arrays.asList("03-31", "06-30", "09-30", "12-31");
+		if (!allowedDates.contains(DateUtils.formatDateWithPattern(endDate, "MM-dd"))) {
+			throw new ValidationException("End date will only be March 31st, June 30th, September 30th, December 31st.");
 		}
 	}
     
