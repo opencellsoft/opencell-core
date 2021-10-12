f5dd6f176ed9fe28aec8c528ba7dd9a29d8b5386
nabilouachi <nabil.ouachi@opencellsoft.com>
2021-10-12 11:37:38 +0100

    RE #MED540-75 fix pre requisite commercial rule

--- opencell-api/src/main/java/org/meveo/api/cpq/ProductApi.java
+++ opencell-api/src/main/java/org/meveo/api/cpq/ProductApi.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
@@ -943,7 +944,7 @@ public class ProductApi extends BaseApi {
                         commercialRuleDtoList.add(commercialRuleDto);
                     }
                     offerProduct.setCommercialRules(commercialRuleDtoList);
-                    boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, commercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes());
+                    boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, commercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes(), (rule) -> rule.getTargetAttribute() == null);
                     //processReplacementRules(commercialRules, offerContextDTO.getSelectedProducts(), null);
                     offerProduct.setSelectable(isSelectable);
                 }
@@ -963,7 +964,7 @@ public class ProductApi extends BaseApi {
                             commercialRuleCodes.add(rule.getCode());
                         }
                         attributeDto.setCommercialRuleCodes(commercialRuleCodes);
-                        boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, attributeCommercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes());
+                        boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, attributeCommercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes(), (rule) -> true);
                         //processReplacementRules(attributeCommercialRules, offerContextDTO.getSelectedProducts(), attributeDto);
                         attributeDto.setSelectable(isSelectable);
                     }
@@ -982,7 +983,7 @@ public class ProductApi extends BaseApi {
                             commercialRuleCodes.add(rule.getCode());
                         }
                         groupedAttributeDTO.setCommercialRuleCodes(commercialRuleCodes);
-                        boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, groupedAttributeCommercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes());
+                        boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, groupedAttributeCommercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes(), (rule) -> true);
                         //processReplacementRules(groupedAttributeCommercialRules, offerContextDTO.getSelectedProducts(), null);
                         groupedAttributeDTO.setSelectable(isSelectable);
                     }
@@ -1003,7 +1004,7 @@ public class ProductApi extends BaseApi {
                 }
                 attributeDto.setCommercialRuleCodes(commercialRuleCodes);
                 attributeDto.setRuled(true);
-                boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, commercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes());
+                boolean isSelectable = commercialRuleHeaderService.isElementSelectable(offerCode, commercialRules, offerContextDTO.getSelectedProducts(),offerContextDTO.getSelectedOfferAttributes(), rule -> true);
                 //processReplacementRules(commercialRules, offerContextDTO.getSelectedProducts(), attributeDto);
                 attributeDto.setSelectable(isSelectable);
             }
