11364decdbb3779c1aa0401ab457c67f8c08f178
Mbarek-Ay <mbarek.aityaazza@opencellsoft.com>
2021-10-15 23:49:59 +0100

    MED540-63 - API + XML Quotation : régression sur le total par offre dans le devis

--- opencell-api-dto/src/main/java/org/meveo/common/UtilsDto.java
+++ opencell-api-dto/src/main/java/org/meveo/common/UtilsDto.java
@@ -25,8 +25,8 @@ public class UtilsDto {
             quotePrice.setTaxAmount(a.getTaxAmount().add(b.getTaxAmount()));
             quotePrice.setAmountWithTax(a.getAmountWithTax().add(b.getAmountWithTax()));
             quotePrice.setAmountWithoutTax(a.getAmountWithoutTax().add(b.getAmountWithoutTax()));
-            quotePrice.setUnitPriceWithoutTax(a.getUnitPriceWithoutTax().add(b.getUnitPriceWithoutTax()));
-            quotePrice.setTaxRate(a.getTaxRate().add(b.getTaxRate()));
+            quotePrice.setUnitPriceWithoutTax(a.getUnitPriceWithoutTax());
+            quotePrice.setTaxRate(a.getTaxRate());
             if(a.getRecurrenceDuration()!=null) {
             	quotePrice.setRecurrenceDuration(a.getRecurrenceDuration());
             }
