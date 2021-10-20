bbe99aef38bf9432abcf244c3dca14bfc2b4e9ee
youssef izem <youssef.izem@opencellsoft.com>
2021-10-20 22:41:36 +0100

    Dunning level creation API

--- opencell-api/src/main/java/org/meveo/apiv2/dunning/resource/DunningLevelResource.java
+++ opencell-api/src/main/java/org/meveo/apiv2/dunning/resource/DunningLevelResource.java
@@ -2,6 +2,7 @@ package org.meveo.apiv2.dunning.resource;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
@@ -22,4 +23,12 @@ public interface DunningLevelResource {
 			@ApiResponse(responseCode = "200", description = "dunning level successfully created"),
 			@ApiResponse(responseCode = "400", description = "dunning level creation is failed") })
 	Response create(@Parameter(required = true) DunningLevel dunningLevel);
+
+	@PUT
+	@Path("/{dunningId}")
+	@Operation(summary = "Update an existing Dunning Level", tags = { "Dunning" }, description = "Update a dunning level", responses = {
+			@ApiResponse(responseCode = "200", description = "dunning level successfully updated"),
+			@ApiResponse(responseCode = "404", description = "dunning level successfully updated"),
+			@ApiResponse(responseCode = "400", description = "dunning level with given code does not exist") })
+	Response update(@Parameter(required = true) DunningLevel dunningLevel);
 }
