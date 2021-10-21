e8071520b25ff04dbe0f567b7651f4efb3eb41c3
Mbarek-Ay <mbarek.aityaazza@opencellsoft.com>
2021-10-21 09:28:15 +0100

    INTRD-2424 - API + XML Quotation : régression sur le total par offre dans le devis

--- opencell-api/src/main/java/org/meveo/api/billing/CpqQuoteApi.java
+++ opencell-api/src/main/java/org/meveo/api/billing/CpqQuoteApi.java
@@ -1244,8 +1244,9 @@ public class CpqQuoteApi extends BaseApi {
             if(a.getRecurrencePeriodicity()!=null) {
             	quotePrice.setRecurrencePeriodicity(a.getRecurrencePeriodicity());
             }
-
+            if(!PriceLevelEnum.OFFER.equals(level)) {
             quotePriceService.create(quotePrice);
+            }
             log.debug("reducePrices2 quotePriceId={}, level={}",quotePrice.getId(),quotePrice.getPriceLevelEnum());
 
             return quotePrice;
@@ -1332,6 +1333,7 @@ public class CpqQuoteApi extends BaseApi {
         .filter(Optional::isPresent)
         .map(price -> {
             QuotePrice quotePrice = price.get();
+            quotePriceService.create(quotePrice);
             pricesDTO.add(new PriceDTO(quotePrice));
             return pricesDTO;
         }).collect(Collectors.toList());
