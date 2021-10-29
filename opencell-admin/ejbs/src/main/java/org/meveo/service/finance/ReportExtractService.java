ef184087989171a9c353ac8c078691d6ef60fdda
hichamhanine <hicham.hanine@opencellsoft.com>
2021-10-29 08:47:49 +0100

    bugfix/INTRD-2568-report-extract-maxim-line-11X

--- opencell-admin/ejbs/src/main/java/org/meveo/service/finance/ReportExtractService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/finance/ReportExtractService.java
@@ -182,7 +182,7 @@ public class ReportExtractService extends BusinessService<ReportExtract> {
             if (!StringUtils.isBlank(entity.getFilenameFormat())) {
                 context.put(ReportExtractScript.FILENAME, filename);
             }
-
+            context.put(ReportExtractScript.LINE_COUNT, entity.getMaximumLine().intValue());
             Map<String, Object> resultContext = scriptInstanceService.execute(entity.getScriptInstance().getCode(), context);
             List<Map<String, Object>> resultList = readGeneratedFile(resultContext.get("DIR") + "\\" + resultContext.get("FILENAME"), ofNullable(entity.getFileSeparator()).orElse(";"));
             splitFileCSVinMultiFileCSVByMaxLine(resultContext.get("DIR") +"\\", resultContext.get("FILENAME") + "", entity.getMaximumLine().intValue());
