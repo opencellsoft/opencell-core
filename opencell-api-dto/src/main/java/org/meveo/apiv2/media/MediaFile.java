df03c1936f44d26b8f51da95d660674349c6bf18
rachid.aityaazza <rachid.aityaazza@manaty.net>
2021-11-01 11:09:42 +0100

    add media upload APi to Swagger file

--- opencell-api-dto/src/main/java/org/meveo/apiv2/media/MediaFile.java
+++ opencell-api-dto/src/main/java/org/meveo/apiv2/media/MediaFile.java
@@ -1,6 +1,9 @@
 package org.meveo.apiv2.media;
 
 import org.jboss.resteasy.annotations.providers.multipart.PartType;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 
@@ -12,14 +15,17 @@ public class MediaFile {
 
     @FormParam("uploadedFile")
     @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    @Schema(description = "The media file content en byte[]")
     private byte[] data;
 
     @FormParam("filename")
     @PartType(MediaType.TEXT_PLAIN)
+    @Schema(description = "The name of the media file")
     private String fileName;
 
     @FormParam("level")
     @PartType(MediaType.TEXT_PLAIN)
+    @Schema(description = "The level to which the quote is attached (OFFER_TEMPLATE, PRODUCT, QUOTE)")
     private LevelEnum level;
 
     public byte[] getData() {
