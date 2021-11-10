e4dd11f84eb345500b1466b1405150cc232582c5
nabilouachi <nabil.ouachi@opencellsoft.com>
2021-11-10 10:37:47 +0100

    RE # add quoteVersion to fileName generation EL context

--- opencell-admin/ejbs/src/main/java/org/meveo/service/cpq/CpqQuoteService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/cpq/CpqQuoteService.java
@@ -160,6 +160,7 @@ public class CpqQuoteService extends BusinessService<CpqQuote> {
 	         if (!StringUtils.isBlank(expression)) {
 	             Map<Object, Object> contextMap = new HashMap<Object, Object>();
 	             contextMap.put("quote",quote);
+	             contextMap.put("quoteVersion",quoteVersion);
 	             try {
 	                 String value = ValueExpressionWrapper.evaluateExpression(expression, contextMap, String.class);
 	                 if (value != null) {
@@ -201,6 +202,7 @@ public class CpqQuoteService extends BusinessService<CpqQuote> {
 	         if (!StringUtils.isBlank(expression)) {
 	             Map<Object, Object> contextMap = new HashMap<Object, Object>();
 	             contextMap.put("quote", quote);
+	             contextMap.put("quoteVersion", quoteVersion);
 	             try {
 	                 String value = ValueExpressionWrapper.evaluateExpression(expression, contextMap, String.class);
 	                 if (value != null) {
