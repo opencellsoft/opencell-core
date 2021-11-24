2d2590065079c6808f0ebc3774158668edabdd47
youssef izem <youssef.izem@opencellsoft.com>
2021-11-24 13:11:59 +0100

    Merge conflict fix

--- opencell-model/src/main/java/org/meveo/model/dunning/DunningCollectionPlanStatus.java
+++ opencell-model/src/main/java/org/meveo/model/dunning/DunningCollectionPlanStatus.java
@@ -8,6 +8,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
