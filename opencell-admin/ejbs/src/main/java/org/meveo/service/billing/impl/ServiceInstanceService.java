4292c1c68425fd6d82ede55f87f967d907756d66
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2021-11-29 17:58:22 +0100

    INTRD-3329 subscription pending job

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceInstanceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceInstanceService.java
@@ -994,6 +994,12 @@ public class ServiceInstanceService extends BusinessService<ServiceInstance> {
             .setParameter("subscriptionStatuses", Arrays.asList(SubscriptionStatusEnum.ACTIVE, SubscriptionStatusEnum.SUSPENDED)) //
             .setParameter("statuses", Arrays.asList(InstanceStatusEnum.ACTIVE, InstanceStatusEnum.SUSPENDED)) //
             .getResultList();
+        
+        ids.addAll(getEntityManager().createNamedQuery("ServiceInstance.getPendingToActivate", Long.class) //
+                .setParameter("date", new Date()) //
+                .setParameter("subscriptionStatuses", Arrays.asList(SubscriptionStatusEnum.PENDING)) //
+                .setParameter("statuses", Arrays.asList(InstanceStatusEnum.PENDING)) //
+                .getResultList());
 
         ids.addAll(getEntityManager().createNamedQuery("ServiceInstance.getToNotifyExpiration", Long.class) //
             .setParameter("date", untillDate) //
