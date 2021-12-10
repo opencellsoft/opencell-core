cd3d6a4cee3c1fd2cdbe2f0090710c0ad09b68a6
unknown <ilyas.talal@opencell.com>
2021-12-10 09:55:16 +0100

    INTRD-3563-duplicating-lines-on-quote-no-11.V1.1

--- opencell-api/src/main/java/org/meveo/apiv2/quote/service/QuoteOfferApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/quote/service/QuoteOfferApiService.java
@@ -3,6 +3,7 @@ package org.meveo.apiv2.quote.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
@@ -11,8 +12,10 @@ import org.meveo.api.exception.EntityDoesNotExistsException;
 import org.meveo.model.cpq.QuoteAttribute;
 import org.meveo.model.cpq.offer.QuoteOffer;
 import org.meveo.model.quote.QuoteArticleLine;
+import org.meveo.model.quote.QuotePrice;
 import org.meveo.model.quote.QuoteProduct;
 import org.meveo.model.quote.QuoteVersion;
+import org.meveo.service.cpq.CpqQuoteService;
 import org.meveo.service.cpq.QuoteArticleLineService;
 import org.meveo.service.cpq.QuoteAttributeService;
 import org.meveo.service.cpq.QuoteProductService;
@@ -26,11 +29,18 @@ public class QuoteOfferApiService {
 	/** Logger. */
     protected Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Inject	private QuoteOfferService quoteOfferService;
-	@Inject	private QuoteVersionService quoteVersionService;
-	@Inject	private QuoteProductService quoteProductService;
-	@Inject	private QuoteAttributeService quoteAttributeService;
-    @Inject private QuoteArticleLineService articleLineService;
+    @Inject
+	private QuoteOfferService quoteOfferService;
+	@Inject
+	private QuoteVersionService quoteVersionService;
+	@Inject
+	private QuoteProductService quoteProductService;
+	@Inject
+	private QuoteAttributeService quoteAttributeService;
+	@Inject
+	private QuoteArticleLineService articleLineService;
+	@Inject
+	private CpqQuoteService cpqQuoteService;
 	
 	public Optional<QuoteOffer> findById(Long quoteOfferId) {
 		return Optional.ofNullable(quoteOfferService.findById(quoteOfferId));
