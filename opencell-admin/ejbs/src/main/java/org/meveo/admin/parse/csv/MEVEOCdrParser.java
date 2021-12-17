bf45f564d9e796cb6a3c2e9b4cba14e481aacbae
BEN AICHA AMINE <amine.benaicha@opencellsoft.com>
2021-12-17 18:07:43 +0100

    INTRD-3706: Mediation job : The duplicated Cdr are processed in V11

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/parse/csv/MEVEOCdrParser.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/parse/csv/MEVEOCdrParser.java
@@ -350,7 +350,7 @@ public class MEVEOCdrParser implements ICdrParser {
 
     @Override
     public boolean isDuplicateCheckOn() {
-        return EdrService.isDuplicateCheckOn();
+        return edrService.isDuplicateCheckOn();
     }
 
     /**
