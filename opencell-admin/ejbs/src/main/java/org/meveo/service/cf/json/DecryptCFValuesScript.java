c01f2e01f2206df916ee7c246332191ed3a40f85
talal <ilyas.talal@opencellsoft.com>
2021-12-28 00:19:54 +0100

    RE #INTRD-3698 [Migration] ScriptingJob for decryption of cf_values in database-EncyptionException

--- opencell-admin/ejbs/src/main/java/org/meveo/service/cf/json/DecryptCFValuesScript.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/cf/json/DecryptCFValuesScript.java
@@ -153,24 +153,4 @@ public class DecryptCFValuesScript extends Script  {
 	}
 
 }
- class EncyptionException extends RuntimeException {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 5175511780611978989L;
-    
-    
-    public EncyptionException() {
-        super();
-    }
-
-    public EncyptionException(Throwable cause) {
-        super(cause);
-    }
-
-    public EncyptionException(String message) {
-        super(message);
-    }
-
-}
