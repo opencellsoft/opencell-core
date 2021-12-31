2fe317ac91d40bb8c3530be5405e3a38cdb20fdc
rachid.aityaazza <rachid.aityaazza@manaty.net>
2021-12-31 14:26:23 +0100

    INTRD-4041 initialize isExonerated

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
@@ -553,12 +553,15 @@ public class InvoiceLineService extends PersistenceService<InvoiceLine> {
 
 		if(invoiceLine.getTax()==null  && accountingArticle != null && invoiceLine.getBillingAccount()!=null)  {
 			BillingAccount  billingAccount = billingAccountService.refreshOrRetrieve(invoiceLine.getBillingAccount());
-			
+			Boolean isExonerated = billingAccount.isExoneratedFromtaxes();
+	        if (isExonerated == null) {
+	            isExonerated = billingAccountService.isExonerated(billingAccount);
+	        }
 			  TaxInfo recalculatedTaxInfo = taxMappingService.determineTax(accountingArticle.getTaxClass(), 
 					  billingAccount.getCustomerAccount().getCustomer().getSeller(), 
 					  billingAccount, null, 
 					  invoiceLine.getValueDate()!=null?invoiceLine.getValueDate():new Date(), null, 
-				      billingAccount.isExoneratedFromtaxes(), false, invoiceLine.getTax());
+				      isExonerated, false, invoiceLine.getTax());
 			  invoiceLine.setTax(recalculatedTaxInfo.tax);
 			  invoiceLine.setTaxRate(recalculatedTaxInfo.tax.getPercent());
 		}
