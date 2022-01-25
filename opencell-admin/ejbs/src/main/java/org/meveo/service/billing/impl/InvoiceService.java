f7ce0f8e819fda138c49a55a97bcb06aa4668ef9
hichamhanine <hicham.hanine@opencellsoft.com>
2022-01-25 17:45:40 +0100

    #INTRD-4487 Updated due date is not saved - 12X

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -5207,7 +5207,10 @@ public class InvoiceService extends PersistenceService<Invoice> {
                         subAggregate.setInvoiceLinesToAssociate(new ArrayList<>());
                     }
 
-                    setInvoiceDueDate(invoice, invoiceLinesGroup.getBillingCycle());
+                    if (invoice.getDueDate() == null) {
+                        setInvoiceDueDate(invoice, invoiceLinesGroup.getBillingCycle());
+                    }
+                    
                     setInitialCollectionDate(invoice, invoiceLinesGroup.getBillingCycle(), billingRun);
 
                     EntityManager em = getEntityManager();
