67993d4934c5cf41f2fb40050b157ea3ed4220e1
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2022-01-26 15:03:13 +0100

    INTRD-4676 correct transaction mode

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/TriggerCollectionPlanLevelsJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/TriggerCollectionPlanLevelsJob.java
@@ -8,7 +8,6 @@ import org.meveo.service.job.Job;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import static javax.ejb.TransactionAttributeType.NEVER;
@@ -21,7 +20,7 @@ public class TriggerCollectionPlanLevelsJob extends Job {
     private TriggerCollectionPlanLevelsJobBean collectionPlanLevelsJobBean;
 
     @Override
-    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    @TransactionAttribute(NEVER)
     protected JobExecutionResultImpl execute(JobExecutionResultImpl result, JobInstance jobInstance) throws BusinessException {
         collectionPlanLevelsJobBean.execute(result, jobInstance);
         return result;
