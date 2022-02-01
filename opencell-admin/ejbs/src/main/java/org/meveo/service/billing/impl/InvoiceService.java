d9e50cb3603aabbb6388a120fa322bb4d8f66e79
Zakaria Bariki <zakaria.bariki@opencellsoft.com>
2022-02-01 16:39:44 +0100

    INTRD-4785 : fix advance payment invoice generation issue

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -5146,7 +5146,8 @@ public class InvoiceService extends PersistenceService<Invoice> {
 
             if (invoiceLineGroupToInvoiceMap.isEmpty() && invoiceLinesGroupsPaged.isEmpty()) {
                 log.warn("Account {}/{} has no billable transactions", entityToInvoice.getClass().getSimpleName(), entityToInvoice.getId());
-                if(existingInvoice.getInvoiceLines() == null || existingInvoice.getInvoiceLines().isEmpty()) {
+                if(existingInvoice != null
+                        && (existingInvoice.getInvoiceLines() == null || existingInvoice.getInvoiceLines().isEmpty())) {
                     cleanInvoiceAggregates(existingInvoice.getId());
                     initAmounts(existingInvoice.getId());
                 }
