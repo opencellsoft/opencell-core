f70b719702d13878a9e3b868568b8025b448617d
hichamhanine <hicham.hanine@opencellsoft.com>
2022-02-02 11:08:53 +0100

    #INTRD-4654 Back update datamodel - DEV

--- opencell-model/src/main/java/org/meveo/model/securityDeposit/SecurityDepositTransaction.java
+++ opencell-model/src/main/java/org/meveo/model/securityDeposit/SecurityDepositTransaction.java
@@ -25,80 +25,82 @@ import org.meveo.model.payments.OperationCategoryEnum;
 @GenericGenerator(name = "ID_GENERATOR", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
         @Parameter(name = "sequence_name", value = "security_deposit_transaction_seq"), })
 public class SecurityDepositTransaction extends BusinessCFEntity {
-	 @ManyToOne(fetch = FetchType.LAZY)
-	    @JoinColumn(name = "security_deposit_id")
-	    private SecurityDeposit securityDeposit;
-	    
-	    @ManyToOne(fetch = FetchType.LAZY)
-	    @JoinColumn(name = "account_operation_id")
-	    private AccountOperation accountOperation;
-	    
-	    @Column(name = "security_deposit_operation")
-	    @Enumerated(value = EnumType.STRING)
-	    private SecurityDepositOperationEnum operation;
-
-        @Column(name = "amount", precision = 23, scale = 12)
-	    private BigDecimal amount;
-	    
-	    /**
-	     * Operation date
-	     */
-	    @Column(name = "transaction_date")
-	    @Temporal(TemporalType.DATE)
-	    private Date transactionDate;
-
-	    /**
-	     * Operation category Debit/Credit
-	     */
-	    @Column(name = "transaction_category")
-	    @Enumerated(value = EnumType.STRING)
-	    private OperationCategoryEnum transactionCategory;
-
-		public SecurityDeposit getSecurityDeposit() {
-			return securityDeposit;
-		}
-
-		public void setSecurityDeposit(SecurityDeposit securityDeposit) {
-			this.securityDeposit = securityDeposit;
-		}
-
-		public AccountOperation getAccountOperation() {
-			return accountOperation;
-		}
-
-		public void setAccountOperation(AccountOperation accountOperation) {
-			this.accountOperation = accountOperation;
-		}
-
-		public BigDecimal getAmount() {
-			return amount;
-		}
-
-		public void setAmount(BigDecimal amount) {
-			this.amount = amount;
-		}
-
-		public Date getTransactionDate() {
-			return transactionDate;
-		}
-
-		public void setTransactionDate(Date transactionDate) {
-			this.transactionDate = transactionDate;
-		}
-
-		public OperationCategoryEnum getTransactionCategory() {
-			return transactionCategory;
-		}
-
-		public void setTransactionCategory(OperationCategoryEnum transactionCategory) {
-			this.transactionCategory = transactionCategory;
-		}	    
-	       
-        public SecurityDepositOperationEnum getOperation() {
-            return operation;
-        }
-
-        public void setOperation(SecurityDepositOperationEnum operation) {
-            this.operation = operation;
-        }
+    private static final long serialVersionUID = 1L;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "security_deposit_id")
+    private SecurityDeposit securityDeposit;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_operation_id")
+    private AccountOperation accountOperation;
+    
+    @Column(name = "security_deposit_operation")
+    @Enumerated(value = EnumType.STRING)
+    private SecurityDepositOperationEnum operation;
+    
+    @Column(name = "amount", precision = 23, scale = 12)
+    private BigDecimal amount;
+    
+    /**
+     * Operation date
+     */
+    @Column(name = "transaction_date")
+    @Temporal(TemporalType.DATE)
+    private Date transactionDate;
+    
+    /**
+     * Operation category Debit/Credit
+     */
+    @Column(name = "transaction_category")
+    @Enumerated(value = EnumType.STRING)
+    private OperationCategoryEnum transactionCategory;
+    
+    public SecurityDeposit getSecurityDeposit() {
+    	return securityDeposit;
+    }
+    
+    public void setSecurityDeposit(SecurityDeposit securityDeposit) {
+    	this.securityDeposit = securityDeposit;
+    }
+    
+    public AccountOperation getAccountOperation() {
+    	return accountOperation;
+    }
+    
+    public void setAccountOperation(AccountOperation accountOperation) {
+    	this.accountOperation = accountOperation;
+    }
+    
+    public BigDecimal getAmount() {
+    	return amount;
+    }
+    
+    public void setAmount(BigDecimal amount) {
+    	this.amount = amount;
+    }
+    
+    public Date getTransactionDate() {
+    	return transactionDate;
+    }
+    
+    public void setTransactionDate(Date transactionDate) {
+    	this.transactionDate = transactionDate;
+    }
+    
+    public OperationCategoryEnum getTransactionCategory() {
+    	return transactionCategory;
+    }
+    
+    public void setTransactionCategory(OperationCategoryEnum transactionCategory) {
+    	this.transactionCategory = transactionCategory;
+    }	    
+       
+    public SecurityDepositOperationEnum getOperation() {
+        return operation;
+    }
+    
+    public void setOperation(SecurityDepositOperationEnum operation) {
+        this.operation = operation;
+    }
 }
