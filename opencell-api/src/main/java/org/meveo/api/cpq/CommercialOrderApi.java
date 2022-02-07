7cbd6542a9b92d3ee9a305cf310a4e6a1ddaac3e
mstitane <mohamed.stitane@opencellsoft.com>
2022-02-07 14:50:37 +0100

    INTRD-4942 fix updating user account to null

--- opencell-api/src/main/java/org/meveo/api/cpq/CommercialOrderApi.java
+++ opencell-api/src/main/java/org/meveo/api/cpq/CommercialOrderApi.java
@@ -1,5 +1,20 @@
 package org.meveo.api.cpq;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.ejb.Stateless;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+
 import org.apache.commons.collections4.map.HashedMap;
 import org.apache.logging.log4j.util.Strings;
 import org.meveo.admin.exception.BusinessException;
@@ -29,32 +44,44 @@ import org.meveo.model.cpq.Attribute;
 import org.meveo.model.cpq.CpqQuote;
 import org.meveo.model.cpq.ProductVersion;
 import org.meveo.model.cpq.ProductVersionAttribute;
-import org.meveo.model.cpq.commercial.*;
+import org.meveo.model.cpq.commercial.CommercialOrder;
+import org.meveo.model.cpq.commercial.CommercialOrderEnum;
+import org.meveo.model.cpq.commercial.InvoicingPlan;
+import org.meveo.model.cpq.commercial.OfferLineTypeEnum;
+import org.meveo.model.cpq.commercial.OrderAttribute;
+import org.meveo.model.cpq.commercial.OrderLot;
+import org.meveo.model.cpq.commercial.OrderOffer;
+import org.meveo.model.cpq.commercial.OrderProduct;
+import org.meveo.model.cpq.commercial.OrderType;
 import org.meveo.model.cpq.contract.Contract;
 import org.meveo.model.crm.custom.CustomFieldInheritanceEnum;
 import org.meveo.model.order.Order;
 import org.meveo.model.scripts.ScriptInstance;
 import org.meveo.service.admin.impl.SellerService;
-import org.meveo.service.billing.impl.*;
+import org.meveo.service.billing.impl.BillingAccountService;
+import org.meveo.service.billing.impl.InvoiceTypeService;
+import org.meveo.service.billing.impl.ServiceSingleton;
+import org.meveo.service.billing.impl.SubscriptionService;
+import org.meveo.service.billing.impl.UserAccountService;
 import org.meveo.service.catalog.impl.DiscountPlanService;
 import org.meveo.service.catalog.impl.OfferTemplateService;
 import org.meveo.service.cpq.AttributeService;
 import org.meveo.service.cpq.ContractService;
 import org.meveo.service.cpq.CpqQuoteService;
 import org.meveo.service.cpq.ProductVersionService;
-import org.meveo.service.cpq.order.*;
+import org.meveo.service.cpq.order.CommercialOrderService;
+import org.meveo.service.cpq.order.InvoicingPlanService;
+import org.meveo.service.cpq.order.OrderAttributeService;
+import org.meveo.service.cpq.order.OrderLotService;
+import org.meveo.service.cpq.order.OrderOfferService;
+import org.meveo.service.cpq.order.OrderProductService;
+import org.meveo.service.cpq.order.OrderTypeService;
 import org.meveo.service.medina.impl.AccessService;
 import org.meveo.service.order.OrderService;
 import org.meveo.service.script.Script;
 import org.meveo.service.script.ScriptInstanceService;
 import org.meveo.service.script.ScriptInterface;
 import org.tmf.dsmapi.catalog.resource.order.ProductOrder;
-
-import javax.ejb.Stateless;
-import javax.enterprise.event.Event;
-import javax.inject.Inject;
-import java.util.*;
-import java.util.stream.Collectors;
 
 
 /**
@@ -291,7 +318,8 @@ public class CommercialOrderApi extends BaseApi {
 			if(userAccount == null)
 				throw new EntityDoesNotExistsException(UserAccount.class, orderDto.getUserAccountCode());
 			order.setUserAccount(userAccount);
-		}
+		} else
+			order.setUserAccount(null);
 		if(orderDto.getAccessDto() != null) {
 			var accessDto = orderDto.getAccessDto();
 			if(Strings.isEmpty(accessDto.getCode()))
