7632a983ea2dcd0e04a43ba979db7f83eb23f1c9
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2022-02-18 17:07:38 +0100

    RE #INTRD-4551 refactoring

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/AggregationConfiguration.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/AggregationConfiguration.java
@@ -4,6 +4,9 @@ import org.meveo.admin.exception.BusinessException;
 
 import static java.util.Arrays.stream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AggregationConfiguration {
 
     /**
@@ -14,15 +17,18 @@ public class AggregationConfiguration {
     /**
      * InvoiceLine Aggregation types
      */
-    private AggregationOption aggregationOption = AggregationOption.NO_AGGREGATION;
+    private List<AggregationOption> aggregationOptions = Arrays.asList(AggregationOption.NO_AGGREGATION);
+    
+    private DateAggregationOption dateAggregationOption = DateAggregationOption.MONTH_OF_USAGE_DATE;
     
     public AggregationConfiguration(boolean enterprise) {
         this.enterprise = enterprise;
     }
 
-    public AggregationConfiguration(boolean enterprise, AggregationOption aggregationOption) {
+    public AggregationConfiguration(boolean enterprise, List<AggregationOption> aggregationOptions, DateAggregationOption dateAggregationOption) {
         this.enterprise = enterprise;
-        this.aggregationOption = aggregationOption;
+        this.aggregationOptions = aggregationOptions;
+        this.dateAggregationOption=dateAggregationOption;
     }
 
     public boolean isEnterprise() {
@@ -33,23 +39,88 @@ public class AggregationConfiguration {
         this.enterprise = enterprise;
     }
 
-    public AggregationOption getAggregationOption() {
-        return aggregationOption;
+    public List<AggregationOption> getAggregationOption() {
+        return aggregationOptions;
     }
 
-    public void setAggregationOption(AggregationOption aggregationOption) {
-        this.aggregationOption = aggregationOption;
+    public void setAggregationOption(List<AggregationOption> aggregationOptions) {
+        this.aggregationOptions = aggregationOptions;
     }
 
-    public enum AggregationOption {
-        NO_AGGREGATION, ARTICLE_LABEL, UNIT_AMOUNT, DATE;
+	/**
+	 * @return the dateAggregationOptions
+	 */
+	public DateAggregationOption getDateAggregationOption() {
+		return dateAggregationOption;
+	}
 
-        public static AggregationOption fromValue(String value) {
-            return stream(AggregationOption.values())
-                        .filter(option -> option.name().equalsIgnoreCase(value))
-                        .findFirst()
-                        .orElseThrow(() -> new BusinessException());
-        }
-    }
+	/**
+	 * @param dateAggregationOptions the dateAggregationOptions to set
+	 */
+	public void setDateAggregationOption(DateAggregationOption dateAggregationOption) {
+		this.dateAggregationOption = dateAggregationOption;
+	}
+
+	public enum AggregationOption {
+		NO_AGGREGATION("no.aggregation"), ARTICLE_LABEL("article.label"), UNIT_AMOUNT("unit.amount");
+		private String label;
+
+		/**
+		 * 
+		 */
+		private AggregationOption(String label) {
+			setLabel(label);
+		}
+
+		public static AggregationOption fromValue(String value) {
+			return stream(AggregationOption.values()).filter(option -> option.name().equalsIgnoreCase(value))
+					.findFirst().orElseThrow(() -> new BusinessException());
+		}
+
+		/**
+		 * @return the label
+		 */
+		public String getLabel() {
+			return label;
+		}
+
+		/**
+		 * @param label the label to set
+		 */
+		public void setLabel(String label) {
+			this.label = label;
+		}
+	}
+	
+	public enum DateAggregationOption {
+		NO_DATE_AGGREGATION("no.date.aggregation"), MONTH_OF_USAGE_DATE("month.of.usage.date"), WEEK_OF_USAGE_DATE("week.of.usage.date"), DAY_OF_USAGE_DATE("day.of.usage.date");
+		private String label;
+
+		/**
+		 * 
+		 */
+		private DateAggregationOption(String label) {
+			setLabel(label);
+		}
+
+		public static DateAggregationOption fromValue(String value) {
+			return stream(DateAggregationOption.values()).filter(option -> option.name().equalsIgnoreCase(value))
+					.findFirst().orElseThrow(() -> new BusinessException());
+		}
+
+		/**
+		 * @return the label
+		 */
+		public String getLabel() {
+			return label;
+		}
+
+		/**
+		 * @param label the label to set
+		 */
+		public void setLabel(String label) {
+			this.label = label;
+		}
+	}
 
 }
--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/InvoiceLinesJobBean.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/InvoiceLinesJobBean.java
@@ -3,7 +3,6 @@ package org.meveo.admin.job;
 import static java.lang.Long.valueOf;
 import static java.lang.String.format;
 import static java.util.Collections.emptyList;
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
 import static org.meveo.model.billing.BillingRunStatusEnum.INVOICE_LINES_CREATED;
@@ -16,7 +15,9 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 
 import javax.ejb.Stateless;
+import javax.ejb.Timeout;
 import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 
@@ -24,6 +25,7 @@ import org.apache.commons.collections.map.HashedMap;
 import org.meveo.admin.async.SynchronizedIterator;
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.admin.job.AggregationConfiguration.AggregationOption;
+import org.meveo.admin.job.AggregationConfiguration.DateAggregationOption;
 import org.meveo.admin.job.logging.JobLoggingInterceptor;
 import org.meveo.admin.util.pagination.PaginationConfiguration;
 import org.meveo.commons.utils.QueryBuilder;
@@ -68,16 +70,15 @@ public class InvoiceLinesJobBean extends BaseJobBean {
     @Inject
     BillingRunExtensionService billingRunExtensionService;
 
-    @JpaAmpNewTx
     @Interceptors({ JobLoggingInterceptor.class, PerformanceInterceptor.class })
-    @TransactionAttribute(REQUIRES_NEW)
     public void execute(JobExecutionResultImpl result, JobInstance jobInstance) {
         log.debug("Running for with parameter={}", jobInstance.getParametres());
         try {
             List<EntityReferenceWrapper> billingRunWrappers =
                     (List<EntityReferenceWrapper>) this.getParamOrCFValue(jobInstance, "InvoiceLinesJob_billingRun");
-            String aggregationOption = ofNullable((String) this.getParamOrCFValue(jobInstance, "InvoiceLinesJob_aggregationOption"))
-                    .orElse("NO_AGGREGATION");
+            List<AggregationOption> aggregationOptions = getTargetStatusList(jobInstance, AggregationOption.class, InvoiceLinesJob.INVOICE_LINES_IL_AGGREGATION_OPTIONS);
+            DateAggregationOption dateAggregationOptions = (DateAggregationOption) getParamOrCFValue(jobInstance, InvoiceLinesJob.INVOICE_LINES_IL_DATE_AGGREGATION_OPTIONS, DateAggregationOption.MONTH_OF_USAGE_DATE);
+
             List<Long> billingRunIds = billingRunWrappers != null ? billingRunWrappers.stream()
                     .map(br -> valueOf(br.getCode().split("/")[0]))
                     .collect(toList()) : emptyList();
@@ -89,32 +90,22 @@ public class InvoiceLinesJobBean extends BaseJobBean {
             }
             List<BillingRun> billingRuns = billingRunService.list(new PaginationConfiguration(filters));
             if(billingRuns != null && !billingRuns.isEmpty()) {
-                billingRuns.stream()
-                        .filter(billingRun -> billingRun.isExceptionalBR())
-                        .forEach(this::addExceptionalBillingRunData);
+                billingRuns.stream().filter(billingRun -> billingRun.isExceptionalBR()).forEach(this::addExceptionalBillingRunData);
                 long excludedBRCount = validateBRList(billingRuns, result);
                 result.setNbItemsProcessedWithError(excludedBRCount);
                 if (excludedBRCount == billingRuns.size()) {
                     result.registerError("No valid billing run with status = NEW found");
                 } else {
-                    AggregationConfiguration aggregationConfiguration = new AggregationConfiguration(appProvider.isEntreprise(),
-                            AggregationOption.fromValue(aggregationOption));
+                    AggregationConfiguration aggregationConfiguration = new AggregationConfiguration(appProvider.isEntreprise(), aggregationOptions, dateAggregationOptions);
                     for(BillingRun billingRun : billingRuns) {
                         List<? extends IBillableEntity> billableEntities = billingRunService.getEntitiesToInvoice(billingRun);
-                        
-                        Long nbRuns = (Long) this.getParamOrCFValue(jobInstance, "nbRuns", -1L);
-                        if (nbRuns == -1) {
-                            nbRuns = (long) Runtime.getRuntime().availableProcessors();
-                        }
+                        Long nbRuns = (Long) this.getParamOrCFValue(jobInstance, "nbRuns", (long) Runtime.getRuntime().availableProcessors());
                         Long waitingMillis = (Long) this.getParamOrCFValue(jobInstance, "waitingMillis", 0L);
                         BasicStatistics basicStatistics = new BasicStatistics();
                         BiConsumer<IBillableEntity, JobExecutionResultImpl> task = (billableEntity, jobResult) -> invoiceLinesService.createInvoiceLines(result, aggregationConfiguration, billingRun, billableEntity, basicStatistics);
-                        iteratorBasedJobProcessing.processItems(result, new SynchronizedIterator<>((Collection<IBillableEntity>) billableEntities), task, null, null, nbRuns, waitingMillis, false, jobInstance.getJobSpeed(),true);
-                        
-						updateBillingRunStatistics(billingRun, basicStatistics);
-            		    result.setNbItemsCorrectlyProcessed(billableEntities.size());
-            		    basicStatistics.setBillableEntitiesCount(billableEntities.size());
-            		    
+                        iteratorBasedJobProcessing.processItems(result, new SynchronizedIterator<>((Collection<IBillableEntity>) billableEntities), task, null, null, nbRuns, waitingMillis, true, jobInstance.getJobSpeed(),true);
+                        billingRunExtensionService.updateBillingRunStatistics(billingRun, basicStatistics, billableEntities.size(), INVOICE_LINES_CREATED);
+            		    result.setNbItemsCorrectlyProcessed(basicStatistics.getCount());
                     }
                 }
             }
@@ -138,13 +129,4 @@ public class InvoiceLinesJobBean extends BaseJobBean {
         return excludedBRs.size();
     }
 
-    private BillingRun updateBillingRunStatistics(BillingRun billingRun, BasicStatistics basicStatistics) {
-        billingRun.setBillableBillingAcountNumber(basicStatistics.getBillableEntitiesCount().intValue());
-        billingRun.setPrAmountTax(basicStatistics.getSumAmountWithTax());
-        billingRun.setPrAmountWithoutTax(basicStatistics.getSumAmountWithoutTax());
-        billingRun.setProcessDate(new Date());
-        billingRun.setStatus(INVOICE_LINES_CREATED);
-        billingRunService.update(billingRun);
-        return billingRun;
-    }
 }
