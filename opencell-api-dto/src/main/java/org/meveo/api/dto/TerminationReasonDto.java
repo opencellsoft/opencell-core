a277854125aab4ad696180c83ee82a4a354df1b6
mboukayoua <mounir.boukayoua@opencellsoft.com>
2022-02-21 17:08:09 +0000

    INTRD-4424 - Upon subscription termination, invoice all agreement charges immediately (merge V11)

--- opencell-api-dto/src/main/java/org/meveo/api/dto/TerminationReasonDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/TerminationReasonDto.java
@@ -48,6 +48,11 @@ public class TerminationReasonDto extends BusinessEntityDto {
     private boolean applyAgreement;
 
     /**
+     * The apply agreement immediately.
+     */
+    private boolean applyAgreementImmediately;
+
+    /**
      * The apply reimbursment.
      */
     private boolean applyReimbursment;
@@ -84,6 +89,7 @@ public class TerminationReasonDto extends BusinessEntityDto {
     public TerminationReasonDto(SubscriptionTerminationReason subscriptionTerminationReason) {
         super(subscriptionTerminationReason);
         applyAgreement = subscriptionTerminationReason.isApplyAgreement();
+        applyAgreementImmediately = subscriptionTerminationReason.isApplyAgreementImmediately();
         applyReimbursment = subscriptionTerminationReason.isApplyReimbursment();
         applyTerminationCharges = subscriptionTerminationReason.isApplyTerminationCharges();
         overrideProrata = subscriptionTerminationReason.getOverrideProrata();
@@ -107,6 +113,14 @@ public class TerminationReasonDto extends BusinessEntityDto {
      */
     public void setApplyAgreement(boolean applyAgreement) {
         this.applyAgreement = applyAgreement;
+    }
+
+    public boolean isApplyAgreementImmediately() {
+        return applyAgreementImmediately;
+    }
+
+    public void setApplyAgreementImmediately(boolean applyAgreementImmediately) {
+        this.applyAgreementImmediately = applyAgreementImmediately;
     }
 
     /**
@@ -183,7 +197,7 @@ public class TerminationReasonDto extends BusinessEntityDto {
 
     @Override
     public String toString() {
-        return "TerminationReasonDto [code=" + getCode() + ", description=" + getDescription() + ", applyAgreement=" + applyAgreement + ", applyReimbursment=" + applyReimbursment
+        return "TerminationReasonDto [code=" + getCode() + ", description=" + getDescription() + ", applyAgreement=" + applyAgreement + ", applyAgreementImmediately=" + applyAgreementImmediately + ", applyReimbursment=" + applyReimbursment
                 + ", applyTerminationCharges=" + applyTerminationCharges + "]";
     }
 
