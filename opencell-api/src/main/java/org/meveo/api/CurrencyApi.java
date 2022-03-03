3413bc27c3cb6ef7619cf40735398200272ba4f3
elhaloui <elhaloui@yandex.com>
2022-03-03 22:14:13 +0100

    INTRD-5505 Update /api/rest/currency/ to return the new created entity id

--- opencell-api/src/main/java/org/meveo/api/CurrencyApi.java
+++ opencell-api/src/main/java/org/meveo/api/CurrencyApi.java
@@ -164,9 +164,11 @@ public class CurrencyApi extends BaseApi {
         currency = currencyService.update(currency);
 
         tradingCurrency.setCurrency(currency);
+        tradingCurrency.setCurrencyCode(postData.getCode());
         tradingCurrency.setPrDescription(postData.getDescription());
         tradingCurrency.setPrCurrencyToThis(postData.getPrCurrencyToThis());
-        tradingCurrency.setDecimalPlaces(postData.getDecimalPlaces() == null ? 2 : postData.getDecimalPlaces());
+        tradingCurrency.setSymbol(postData.getSymbol() != null ? postData.getSymbol() : postData.getCode());
+        tradingCurrency.setDecimalPlaces(postData.getDecimalPlaces());
 
         tradingCurrencyService.update(tradingCurrency);
     }
