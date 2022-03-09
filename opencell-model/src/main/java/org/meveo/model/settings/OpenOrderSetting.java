8a9ae212a2a2ab0e868e500d01acdd577317e859
elhaloui <elhaloui@yandex.com>
2022-03-09 09:01:46 +0100

    INTRD-5419 General settings | create DataModel

--- opencell-model/src/main/java/org/meveo/model/settings/OpenOrderSetting.java
+++ opencell-model/src/main/java/org/meveo/model/settings/OpenOrderSetting.java
@@ -3,15 +3,15 @@ package org.meveo.model.settings;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Type;
-import org.meveo.model.BusinessEntity;
+import org.meveo.model.BaseEntity;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Embeddable
-public class OpenOrderSetting extends BusinessEntity {
+@Entity
+@Table(name = "open_order_setting")
+@GenericGenerator(name = "ID_GENERATOR", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+        @Parameter(name = "sequence_name", value = "open_order_setting_seq"), })
+public class OpenOrderSetting extends BaseEntity {
    @Type(type = "numeric_boolean")
     @Column(name = "use_open_orders")
     private boolean useOpenOrders = false;
@@ -23,6 +23,7 @@ public class OpenOrderSetting extends BusinessEntity {
     @Column(name = "apply_maximum_validity_value")
     private Integer applyMaximumValidityValue;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "apply_maximum_validity_unit")
     private MaximumValidityUnitEnum applyMaximumValidityUnit;
 
@@ -37,6 +38,62 @@ public class OpenOrderSetting extends BusinessEntity {
     @Column(name = "use_managment_validation_for_oo_quotation")
     private boolean useManagmentValidationForOOQuotation = false;
 
+
+ public boolean isUseOpenOrders() {
+  return useOpenOrders;
+ }
+
+ public void setUseOpenOrders(boolean useOpenOrders) {
+  this.useOpenOrders = useOpenOrders;
+ }
+
+ public boolean isApplyMaximumValidity() {
+  return applyMaximumValidity;
+ }
+
+ public void setApplyMaximumValidity(boolean applyMaximumValidity) {
+  this.applyMaximumValidity = applyMaximumValidity;
+ }
+
+ public Integer getApplyMaximumValidityValue() {
+  return applyMaximumValidityValue;
+ }
+
+ public void setApplyMaximumValidityValue(Integer applyMaximumValidityValue) {
+  this.applyMaximumValidityValue = applyMaximumValidityValue;
+ }
+
+ public MaximumValidityUnitEnum getApplyMaximumValidityUnit() {
+  return applyMaximumValidityUnit;
+ }
+
+ public void setApplyMaximumValidityUnit(MaximumValidityUnitEnum applyMaximumValidityUnit) {
+  this.applyMaximumValidityUnit = applyMaximumValidityUnit;
+ }
+
+ public boolean isDefineMaximumValidity() {
+  return defineMaximumValidity;
+ }
+
+ public void setDefineMaximumValidity(boolean defineMaximumValidity) {
+  this.defineMaximumValidity = defineMaximumValidity;
+ }
+
+ public Integer getDefineMaximumValidityValue() {
+  return defineMaximumValidityValue;
+ }
+
+ public void setDefineMaximumValidityValue(Integer defineMaximumValidityValue) {
+  this.defineMaximumValidityValue = defineMaximumValidityValue;
+ }
+
+ public boolean isUseManagmentValidationForOOQuotation() {
+  return useManagmentValidationForOOQuotation;
+ }
+
+ public void setUseManagmentValidationForOOQuotation(boolean useManagmentValidationForOOQuotation) {
+  this.useManagmentValidationForOOQuotation = useManagmentValidationForOOQuotation;
+ }
 }
 
 
