34fc3df184780381167f46931e1878e6a24f2364
BEN AICHA AMINE <amine.benaicha@opencellsoft.com>
2022-03-24 13:29:23 +0100

    INTRD-5922: [Back] Add AuditLog while updating ExchangeRate

--- opencell-api/src/main/java/org/meveo/api/CurrencyApi.java
+++ opencell-api/src/main/java/org/meveo/api/CurrencyApi.java
@@ -18,6 +18,8 @@
 
 package org.meveo.api;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -49,6 +51,7 @@ import org.meveo.model.crm.Provider;
 import org.meveo.model.shared.DateUtils;
 import org.meveo.service.admin.impl.CurrencyService;
 import org.meveo.service.admin.impl.TradingCurrencyService;
+import org.meveo.service.audit.logging.AuditLogService;
 import org.meveo.service.billing.impl.ExchangeRateService;
 import org.meveo.service.crm.impl.ProviderService;
 
@@ -72,7 +75,10 @@ public class CurrencyApi extends BaseApi {
     private ExchangeRateService exchangeRateService;
 
     @Inject
-    protected ResourceBundle resourceMessages;
+    private ResourceBundle resourceMessages;
+    
+    @Inject
+    private AuditLogService auditLogService;
 
     public CurrenciesDto list() {
         CurrenciesDto result = new CurrenciesDto();
@@ -320,6 +326,9 @@ public class CurrencyApi extends BaseApi {
             throw new EntityDoesNotExistsException(ExchangeRate.class, id);
         }
         
+        BigDecimal fromRate = exchangeRate.getExchangeRate();
+        BigDecimal toRate = postData.getExchangeRate();
+        
         // We can modify only the future rates
         if (exchangeRate.getFromDate().compareTo(DateUtils.setTimeToZero(new Date())) <= 0) {
             throw new BusinessApiException(resourceMessages.getString("error.exchangeRate.fromDate.future"));
@@ -362,5 +371,16 @@ public class CurrencyApi extends BaseApi {
         exchangeRate.setFromDate(postData.getFromDate());
         exchangeRate.setExchangeRate(postData.getExchangeRate());
         exchangeRateService.update(exchangeRate);
+        auditLogUpdateExchangeRate(exchangeRate, fromRate, toRate);
+    }
+    
+    private void auditLogUpdateExchangeRate(ExchangeRate exchangeRate, BigDecimal fromRate, BigDecimal toRate) {
+        DecimalFormat formatter = new DecimalFormat("#0.##");
+        String parameters = "User " + auditLogService.getActor() + " has changed the Exchange rate for " + exchangeRate.getTradingCurrency().getCurrencyCode();
+        
+        if (!fromRate.equals(toRate)) {
+            parameters += " from " + formatter.format(fromRate) + " to " + formatter.format(toRate);
+        }
+        auditLogService.trackOperation("UPDATE", new Date(), exchangeRate, "API", parameters);
     }
 }
