492669b8a0cf8d772caff60910611d59e6d15d65
hichamhanine <hicham.hanine@opencellsoft.com>
2022-03-25 15:46:09 +0100

    #INTRD-5893 Back update the api to return id - DEV

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ExchangeRateService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ExchangeRateService.java
@@ -1,28 +1,92 @@
-/*
- * (C) Copyright 2015-2020 Opencell SAS (https://opencellsoft.com/) and contributors.
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
- * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
- * OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS
- * IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO
- * THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE,
- * YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
- *
- * For more information on the GNU Affero General Public License, please consult
- * <https://www.gnu.org/licenses/agpl-3.0.en.html>.
- */
 package org.meveo.service.billing.impl;
 
-import javax.ejb.Stateless;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import org.meveo.admin.util.ResourceBundle;
+import org.meveo.api.dto.ExchangeRateDto;
+import org.meveo.api.exception.MeveoApiException;
 import org.meveo.model.billing.ExchangeRate;
+import org.meveo.model.billing.TradingCurrency;
+import org.meveo.model.shared.DateUtils;
+import org.meveo.service.admin.impl.TradingCurrencyService;
 import org.meveo.service.base.PersistenceService;
 
 @Stateless
 public class ExchangeRateService extends PersistenceService<ExchangeRate> {
-
+    
+    @Inject
+    protected ResourceBundle resourceMessages;
+    
+    @Inject
+    protected TradingCurrencyService tradingCurrencyService;
+    
+    public ExchangeRate createCurrentRateWithPostData(ExchangeRateDto postData, TradingCurrency tradingCurrency) {
+        if (postData.getFromDate() == null) {
+            throw new MeveoApiException(resourceMessages.getString("error.exchangeRate.fromDate.empty"));
+        } 
+        // User cannot set a rate in a paste date
+        if (postData.getFromDate().before(DateUtils.setTimeToZero(new Date()))) {
+            throw new MeveoApiException(resourceMessages.getString("error.exchangeRate.fromDate.past"));
+        }
+        
+        if (postData.getExchangeRate().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new MeveoApiException(resourceMessages.getString("error.exchangeRate.exchangeRate.incorrect"));
+        }
+        
+        ExchangeRate exchangeRate = new ExchangeRate();
+        List<ExchangeRate> listExchangeRate = tradingCurrency.getExchangeRates();        
+        for (ExchangeRate er : listExchangeRate) {
+            if (er.getFromDate().compareTo(postData.getFromDate()) == 0) {
+                throw new MeveoApiException(resourceMessages.getString("error.exchangeRate.fromDate.isAlreadyTaken"));
+            }
+        }       
+        
+        if (postData.getFromDate().compareTo(DateUtils.setTimeToZero(new Date())) == 0) {
+            exchangeRate.setCurrentRate(true);
+            for (ExchangeRate elementExchangeRate : listExchangeRate) {
+                elementExchangeRate.setCurrentRate(false);
+            }
+            tradingCurrency.setExchangeRates(listExchangeRate);
+            tradingCurrency.setCurrentRate(postData.getExchangeRate());
+            tradingCurrency.setCurrentRateFromDate(DateUtils.setTimeToZero(new Date()));
+            tradingCurrency.setCurrentRateUpdater(currentUser.getUserName());
+        }else {
+            exchangeRate.setCurrentRate(false);
+        }
+        exchangeRate.setTradingCurrency(tradingCurrency);
+        exchangeRate.setExchangeRate(postData.getExchangeRate());
+        exchangeRate.setFromDate(postData.getFromDate());
+        create(exchangeRate);
+        return exchangeRate;
+    }    
+    
+    public List<Long> getAllTradingCurrencyWithCurrentRate() {
+        return getEntityManager()
+                .createNamedQuery("ExchangeRate.getAllTradingCurrencyWithCurrentRate", Long.class)
+                .setParameter("sysDate", DateUtils.setTimeToZero(new Date()))
+                .getResultList();
+    }
+    
+    public void updateCurrentRateForTradingCurrency(Long idExchangeRate) {
+        ExchangeRate exchangeRate = findById(idExchangeRate);        
+        TradingCurrency tradingCurrency = exchangeRate.getTradingCurrency();        
+        List<ExchangeRate> listExchangeRate = tradingCurrency.getExchangeRates();
+        
+        for (ExchangeRate elementExchangeRate : listExchangeRate) {
+            elementExchangeRate.setCurrentRate(false);
+        }
+        exchangeRate.setCurrentRate(true);
+        tradingCurrency.setExchangeRates(listExchangeRate);
+        tradingCurrency.setCurrentRate(exchangeRate.getExchangeRate());
+        tradingCurrency.setCurrentRateFromDate(DateUtils.setTimeToZero(new Date()));
+        tradingCurrency.setCurrentRateUpdater(currentUser.getUserName());
+        exchangeRate.setTradingCurrency(tradingCurrency);
+        update(exchangeRate);
+    }
+    
 }
