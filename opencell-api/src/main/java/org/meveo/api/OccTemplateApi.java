fc1093c88259391e7934d16b3202f77e3a681dbd
elhaloui <elhaloui@yandex.com>
2022-03-29 08:23:18 +0000

    INTRD-5995 update payment OccTemplate schemes

--- opencell-api/src/main/java/org/meveo/api/OccTemplateApi.java
+++ opencell-api/src/main/java/org/meveo/api/OccTemplateApi.java
@@ -103,6 +103,15 @@ public class OccTemplateApi extends BaseApi {
             }
             occTemplate.setContraAccountingCode(contractAccountingCode);
         }
+
+        if(postData.getCommissionAccountingCode() != null) {
+            AccountingCode commissionAccountingCode = accountingCodeService.findByCode(postData.getCommissionAccountingCode());
+            if(commissionAccountingCode == null) {
+                throw new NotFoundException("Commission accounting code does not exist in the chart of accounts");
+            }
+            occTemplate.setContraAccountingCode(commissionAccountingCode);
+        }
+
         occTemplateService.create(occTemplate);
     }
 
@@ -153,6 +162,16 @@ public class OccTemplateApi extends BaseApi {
             occTemplate.setContraAccountingCode(contractAccountingCode);
         } else {
             occTemplate.setContraAccountingCode(null); // if no contraAccountingCode send for update, we must clear the old one.
+        }
+
+        if(postData.getCommissionAccountingCode() != null) {
+            AccountingCode commissionAccountingCode = accountingCodeService.findByCode(postData.getCommissionAccountingCode());
+            if(commissionAccountingCode == null) {
+                throw new NotFoundException("Commission accounting code does not exist in the chart of accounts");
+            }
+            occTemplate.setCommissionAccountingCode(commissionAccountingCode);
+        } else {
+            occTemplate.setCommissionAccountingCode(null); // if no contraAccountingCode send for update, we must clear the old one.
         }
         occTemplateService.update(occTemplate);
     }
