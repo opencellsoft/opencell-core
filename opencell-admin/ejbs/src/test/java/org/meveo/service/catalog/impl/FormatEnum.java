a7ba8dc05711e664694ab0a1e09850298b8b8d61
Abdelkader Bouazza <abdelkader.bouazza@opencellsoft.com>
2022-04-12 23:20:51 +0000

    INTRD-6472 : create-export-api

---
+++ opencell-admin/ejbs/src/test/java/org/meveo/service/catalog/impl/FormatEnum.java
@@ -0,0 +1,11 @@
+package org.meveo.service.catalog.impl;
+
+public enum FormatEnum {
+    CSV("csv");
+
+    private final String label;
+
+    FormatEnum(String label) {
+        this.label = label;
+    }
+}
