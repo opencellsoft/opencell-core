6362d30fbbdc725197d22912e08286c54e64bd62
MElAzzouzi <mohammed.elazzouzi@opencellsoft.com>
2022-04-27 12:13:55 +0000

    RE #INTRD-6675 incohérence des montants

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
@@ -302,7 +302,7 @@ public class InvoiceLineService extends PersistenceService<InvoiceLine> {
             invoiceLine.setBillingAccount(billingAccount);
 
         }
-        invoiceLine.setQuantity(BigDecimal.valueOf(1));
+        invoiceLine.setQuantity(quantity);
         amountWithoutTaxToBeInvoiced = (amountWithoutTaxToBeInvoiced != null) ? amountWithoutTaxToBeInvoiced : accountingArticle.getUnitPrice();
         invoiceLine.setUnitPrice(BigDecimal.ZERO.equals(quantity) ? amountWithoutTaxToBeInvoiced : amountWithoutTaxToBeInvoiced.divide(quantity));
         invoiceLine.setAmountWithoutTax(amountWithoutTaxToBeInvoiced);
