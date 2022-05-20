80eb162416df51625a7d99b45c328a2955a0ac45
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2022-05-20 14:51:09 +0000

    INTRD-6951 value initialization

--- opencell-admin/ejbs/src/main/java/org/meveo/service/settings/impl/GlobalSettingsService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/settings/impl/GlobalSettingsService.java
@@ -35,7 +35,10 @@ public class GlobalSettingsService extends PersistenceService<GlobalSettings> {
     }
 
     private void checkParameters(GlobalSettings entity) {
-        if (entity.getQuoteDefaultValidityDelay() <= 0)
+    	if (entity.getQuoteDefaultValidityDelay() == null) {
+    		entity.setQuoteDefaultValidityDelay(30);
+    	}else if (entity.getQuoteDefaultValidityDelay() <= 0) {
             throw new InvalidParameterException("QuoteDefaultValidityDelay must be greater than 0");
+    	}
     }
 }
