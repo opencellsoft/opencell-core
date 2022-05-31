9b4781262ca7cb5f82ed51f080faca1e6640fcc3
elhaloui <elhaloui@yandex.com>
2022-05-31 15:51:23 +0100

    INTRD-7541 fix the template creation throws a value instantiation exception

--- opencell-api/src/main/java/org/meveo/apiv2/ordering/resource/openOrderTemplate/OpenOrderTemplateResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/ordering/resource/openOrderTemplate/OpenOrderTemplateResourceImpl.java
@@ -2,9 +2,13 @@ package org.meveo.apiv2.ordering.resource.openOrderTemplate;
 
 import org.meveo.apiv2.ordering.resource.order.OpenOrderTemplateInput;
 import org.meveo.apiv2.ordering.services.OpenOrderTemplateApiService;
+import org.meveo.apiv2.securityDeposit.FinanceSettings;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class  OpenOrderTemplateResourceImpl  implements OpenOrderTemplateResource{
@@ -18,7 +22,7 @@ public class  OpenOrderTemplateResourceImpl  implements OpenOrderTemplateResourc
     public Response createOpenOrderTemplate(OpenOrderTemplateInput openOrderTemplateInput){
 
         openOrderTemplateApiService.create(openOrderTemplateInput);
-        return Response.ok().build();
+        return Response.ok().entity(Map.of("status", "SUCCESS", "message", "open Order template has been created with success")).build();
     }
 
 
@@ -27,7 +31,7 @@ public class  OpenOrderTemplateResourceImpl  implements OpenOrderTemplateResourc
     @Override
     public Response updateOpenOrderTemplate( String code, OpenOrderTemplateInput openOrderTemplateInput){
         openOrderTemplateApiService.update(code, openOrderTemplateInput);
-        return Response.ok().build();
+        return Response.ok().entity(Map.of("status", "SUCCESS", "message", "open Order template has been updated with success")).build();
     }
 
     @Override
@@ -35,4 +39,6 @@ public class  OpenOrderTemplateResourceImpl  implements OpenOrderTemplateResourc
         openOrderTemplateApiService.disableOpenOrderTemplate(code);
         return Response.ok().build();
     }
+
+
 }
