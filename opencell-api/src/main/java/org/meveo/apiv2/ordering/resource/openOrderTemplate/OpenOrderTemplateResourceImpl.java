38b6a3282cbd04e58b9f43cbbc545ef20ee951c6
elhaloui <elhaloui@yandex.com>
2022-06-01 16:45:31 +0100

    INTRD-7541 personalise validation messages

--- opencell-api/src/main/java/org/meveo/apiv2/ordering/resource/openOrderTemplate/OpenOrderTemplateResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/ordering/resource/openOrderTemplate/OpenOrderTemplateResourceImpl.java
@@ -1,5 +1,6 @@
 package org.meveo.apiv2.ordering.resource.openOrderTemplate;
 
+import org.meveo.api.exception.InvalidParameterException;
 import org.meveo.apiv2.ordering.resource.order.OpenOrderTemplateInput;
 import org.meveo.apiv2.ordering.services.OpenOrderTemplateApiService;
 
@@ -20,15 +21,22 @@ public class  OpenOrderTemplateResourceImpl  implements OpenOrderTemplateResourc
     @Override
     public Response createOpenOrderTemplate(OpenOrderTemplateInput openOrderTemplateInput){
 
+        checkParameters(openOrderTemplateInput);
         OpenOrderTemplateInput openOrderTemplate = openOrderTemplateApiService.create(openOrderTemplateInput);
         return buildResponse(openOrderTemplate);
     }
 
-
+    private void checkParameters(OpenOrderTemplateInput openOrderTemplateInput) {
+        if(openOrderTemplateInput.getTemplateName() == null || openOrderTemplateInput.getTemplateName().isEmpty() )
+            throw new InvalidParameterException("The following fields are required: Template name");
+        if(openOrderTemplateInput.getOpenOrderType() == null  )
+            throw new InvalidParameterException("The following fields are required: Open order type");
+    }
 
 
     @Override
     public Response updateOpenOrderTemplate( String code, OpenOrderTemplateInput openOrderTemplateInput){
+        checkParameters(openOrderTemplateInput);
         OpenOrderTemplateInput openOrderTemplate =  openOrderTemplateApiService.update(code, openOrderTemplateInput);
          return buildResponse(openOrderTemplate);
     }
