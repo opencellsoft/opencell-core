97c0f7eb50d453b740e74d4566cd95abbb335e11
aelmalki <abdelhamid.elmalki@opencellsoft.com>
2022-06-07 11:13:17 +0100

    INTRD-7400 fix Sonar code smell

--- opencell-api-dto/src/main/java/org/meveo/apiv2/payments/InstallmentAccountOperation.java
+++ opencell-api-dto/src/main/java/org/meveo/apiv2/payments/InstallmentAccountOperation.java
@@ -3,12 +3,8 @@ package org.meveo.apiv2.payments;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.immutables.value.Value;
-import org.meveo.apiv2.AcountReceivable.AccountOperationAndSequence;
-import org.meveo.apiv2.AcountReceivable.ImmutableMatchingAccountOperation;
 
 import javax.annotation.Nonnull;
-import javax.validation.constraints.NotEmpty;
-import java.util.List;
 
 @Value.Immutable
 @Value.Style(jdkOnly = true)
