960038034a25f142b73c62a83016925d84d1f536
Thang Nguyen <thang.nguyen@opencellsoft.com>
2022-06-08 10:08:48 +0200

    Revert BaseApi

--- opencell-api/src/main/java/org/meveo/api/BaseApi.java
+++ opencell-api/src/main/java/org/meveo/api/BaseApi.java
@@ -204,15 +204,8 @@ public abstract class BaseApi {
     }
 
     protected void handleMissingParameters(BaseEntityDto dto) throws MeveoApiException {
-        if (dto instanceof BusinessEntityDto) {
-            BusinessEntityDto bdto = (BusinessEntityDto) dto;
-            boolean allowEntityCodeUpdate = Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("service.allowEntityCodeUpdate", "true"));
-                if (!allowEntityCodeUpdate && !StringUtils.isBlank(bdto.getUpdatedCode()) && !currentUser.hasRole(SUPER_ADMIN_MANAGEMENT)) {
-                    missingParameters.clear(); // when exception, clear missingParameters bag to avoid inconsistency MISSING_PARAM exception for next invoke
-                    throw new org.meveo.api.exception.AccessDeniedException("Super administrator permission is required to update entity code");
-                }
-        }
         handleMissingParameters();
+        checkAllowEntityCodeUpdate(dto);
     }
 
     /**
@@ -244,6 +237,16 @@ public abstract class BaseApi {
         }
 
         handleMissingParameters();
+    }
+
+    protected void checkAllowEntityCodeUpdate(BaseEntityDto dto) throws MeveoApiException {
+        if (dto instanceof BusinessEntityDto) {
+            BusinessEntityDto bdto = (BusinessEntityDto) dto;
+            boolean allowEntityCodeUpdate = Boolean.parseBoolean(paramBeanFactory.getInstance().getProperty("service.allowEntityCodeUpdate", "true"));
+            if (!allowEntityCodeUpdate && !StringUtils.isBlank(bdto.getUpdatedCode()) && !currentUser.hasRole(SUPER_ADMIN_MANAGEMENT)) {
+                throw new org.meveo.api.exception.AccessDeniedException("Super administrator permission is required to update entity code");
+            }
+        }
     }
 
     /**
