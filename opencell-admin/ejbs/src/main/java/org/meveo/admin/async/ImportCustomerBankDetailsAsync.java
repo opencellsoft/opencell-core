36a6fc065ff62e522dee42b3561660e6a83ee434
hichamhanine <hicham.hanine@opencellsoft.com>
2022-06-14 20:05:40 +0100

    #INTRD-7339 Back create the job importcustomer - DEV

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/async/ImportCustomerBankDetailsAsync.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/async/ImportCustomerBankDetailsAsync.java
@@ -19,14 +19,6 @@ public class ImportCustomerBankDetailsAsync {
     @Inject
     private CurrentUserProvider currentUserProvider;
 
-    /**
-     * Import subscriptions
-     * 
-     * @param result Job execution result
-     * @param lastCurrentUser Current user. In case of multitenancy, when user authentication is forced as result of a fired trigger (scheduled jobs, other timed event
-     *        expirations), current user might be lost, thus there is a need to reestablish.
-     * @return Future String
-     */
     @Asynchronous
     public Future<String> launchAndForget(JobExecutionResultImpl result, MeveoUser lastCurrentUser) {
         currentUserProvider.reestablishAuthentication(lastCurrentUser);
