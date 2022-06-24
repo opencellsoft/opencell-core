bc22f71fec9cf37a45f9e988d423ea9c41df96a2
hichamhanine <hicham.hanine@opencellsoft.com>
2022-06-24 08:21:04 +0100

    #INTRD-8037 Back api create addsubtotals - DEV

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceSubTotalsService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceSubTotalsService.java
@@ -1,20 +1,13 @@
 package org.meveo.service.billing.impl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
-import org.meveo.api.dto.LanguageDescriptionDto;
 import org.meveo.api.dto.invoice.InvoiceSubTotalsDto;
 import org.meveo.api.dto.invoice.SubTotalsDto;
 import org.meveo.api.exception.BusinessApiException;
 import org.meveo.api.exception.EntityDoesNotExistsException;
-import org.meveo.api.exception.InvalidParameterException;
 import org.meveo.api.exception.MissingParameterException;
-import org.meveo.commons.utils.StringUtils;
 import org.meveo.model.billing.InvoiceSubTotals;
 import org.meveo.model.billing.InvoiceType;
 import org.meveo.service.base.PersistenceService;
@@ -24,12 +17,12 @@ public class InvoiceSubTotalsService  extends PersistenceService<InvoiceSubTotal
     @Inject
     private InvoiceTypeService invoiceTypeService;
     @Inject
-    private TradingLanguageService tradingLanguageService;
+    private BillingRunService billingRunService;
     
     public void addSubTotals(InvoiceSubTotalsDto invoiceSubTotalsDto) {        
         if (invoiceSubTotalsDto.getInvoiceType() == null || 
                 (invoiceSubTotalsDto.getInvoiceType().getId() == null 
-                    & invoiceSubTotalsDto.getInvoiceType().getCode() == null)) {
+                    && invoiceSubTotalsDto.getInvoiceType().getCode() == null)) {
             throw new MissingParameterException("following parameters are required: invoiceType");
         }
         
@@ -72,39 +65,7 @@ public class InvoiceSubTotalsService  extends PersistenceService<InvoiceSubTotal
         invoiceSubTotal.setInvoiceType(invoiceType);
         invoiceSubTotal.setSubTotalEl(subTotalEL);
         invoiceSubTotal.setLabel(subTotalLabel);
-        invoiceSubTotal.setLabelI18n(convertMultiLanguageToMapOfValues(subTotalsDto.getLanguageLabels() ,null));
-    }
-    
-    private Map<String, String> convertMultiLanguageToMapOfValues(List<LanguageDescriptionDto> translationInfos, Map<String, String> currentValues) throws InvalidParameterException {
-        if (translationInfos == null || translationInfos.isEmpty()) {
-            return null;
-        }
-
-        List<String> supportedLanguages = tradingLanguageService.listLanguageCodes();
-
-        Map<String, String> values = null;
-        if (currentValues == null) {
-            values = new HashMap<>();
-        } else {
-            values = currentValues;
-        }
-
-        for (LanguageDescriptionDto translationInfo : translationInfos) {
-            if (!supportedLanguages.contains(translationInfo.getLanguageCode())) {
-                throw new InvalidParameterException("Language " + translationInfo.getLanguageCode() + " is not supported by the provider.");
-            }
-            if (StringUtils.isBlank(translationInfo.getDescription())) {
-                values.remove(translationInfo.getLanguageCode());
-            } else {
-                values.put(translationInfo.getLanguageCode(), translationInfo.getDescription());
-            }
-        }
-
-        if (values.isEmpty()) {
-            return null;
-        } else {
-            return values;
-        }
+        invoiceSubTotal.setLabelI18n(billingRunService.convertMultiLanguageToMapOfValues(subTotalsDto.getLanguageLabels() ,null));
     }
 
 }
