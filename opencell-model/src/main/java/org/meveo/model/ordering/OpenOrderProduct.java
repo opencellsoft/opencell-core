4828e2f38876274a6772bb1d1147d5d95f380b6c
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2022-06-25 13:16:54 +0000

    INTRD-8022 open order product data model

---
+++ opencell-model/src/main/java/org/meveo/model/ordering/OpenOrderProduct.java
@@ -0,0 +1,61 @@
+package org.meveo.model.ordering;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Type;
+import org.meveo.model.BusinessEntity;
+import org.meveo.model.article.AccountingArticle;
+import org.meveo.model.cpq.Product;
+import org.meveo.model.cpq.tags.Tag;
+import org.meveo.model.tax.TaxClass;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.FetchType.LAZY;
+
+import java.util.List;
+
+@Entity
+@Table(name = "open_order_product")
+@GenericGenerator(name = "ID_GENERATOR", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+        @Parameter(name = "sequence_name", value = "open_order_product_seq"),})
+public class OpenOrderProduct extends BusinessEntity {
+
+	@OneToOne(fetch = LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+	
+	@OneToOne(fetch = LAZY)
+    @JoinColumn(name = "open_order_template_id")
+    private OpenOrderTemplate openOrderTemplate;
+	
+	@Type(type = "numeric_boolean")
+    @Column(name = "active")
+    private Boolean active;
+
+	public OpenOrderTemplate getOpenOrderTemplate() {
+		return openOrderTemplate;
+	}
+
+	public void setOpenOrderTemplate(OpenOrderTemplate openOrderTemplate) {
+		this.openOrderTemplate = openOrderTemplate;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}    
+
+}
