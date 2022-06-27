c7d907f555231816d138425046a9fe86f311b2e4
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2022-06-27 14:57:06 +0000

    INTRD-8023 apply new mapping

--- opencell-model/src/main/java/org/meveo/model/ordering/OpenOrderTemplate.java
+++ opencell-model/src/main/java/org/meveo/model/ordering/OpenOrderTemplate.java
@@ -1,15 +1,24 @@
 package org.meveo.model.ordering;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 import org.meveo.model.BusinessEntity;
-import org.meveo.model.article.AccountingArticle;
-import org.meveo.model.cpq.Product;
 import org.meveo.model.cpq.tags.Tag;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Entity
 @Table(name = "open_order_template")
