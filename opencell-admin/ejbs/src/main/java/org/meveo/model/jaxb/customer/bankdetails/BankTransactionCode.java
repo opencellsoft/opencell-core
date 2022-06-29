0eb215c9b0b00384656d25a3c200e54e2a79db15
hichamhanine <hicham.hanine@opencellsoft.com>
2022-06-29 15:27:13 +0100

    #INTRD-7339 Back create the job importcustomer - 9X

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/model/jaxb/customer/bankdetails/BankTransactionCode.java
@@ -0,0 +1,22 @@
+package org.meveo.model.jaxb.customer.bankdetails;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = { "domain" })
+@XmlRootElement(name = "BkTxCd")
+public class BankTransactionCode {    
+    @XmlElement(name = "Domn")
+    protected Domain domain;
+    
+    public Domain getDomain() {
+        return domain;
+    }    
+    public void setDomain(Domain domain) {
+        this.domain = domain;
+    }
+}
