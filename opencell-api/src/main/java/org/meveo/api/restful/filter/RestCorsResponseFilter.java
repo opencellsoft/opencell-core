0dd5e942d5f0b2e5a780e3372560ab43786b240e
Thang Nguyen <thang.nguyen@opencellsoft.com>
2022-07-07 11:21:01 +0200

    Test CORS 3

--- opencell-api/src/main/java/org/meveo/api/restful/filter/RestCorsResponseFilter.java
+++ opencell-api/src/main/java/org/meveo/api/restful/filter/RestCorsResponseFilter.java
@@ -11,7 +11,7 @@ public class RestCorsResponseFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "https://tnn.d2.opencell.work");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
