b35a48e14d36f32e9b1afaa7b5bcd4549b4065a0
Thang Nguyen <thang.nguyen@opencellsoft.com>
2022-07-07 12:34:57 +0200

    Test CORS

--- opencell-api/src/main/java/org/meveo/api/restful/filter/RestCorsResponseFilter.java
+++ opencell-api/src/main/java/org/meveo/api/restful/filter/RestCorsResponseFilter.java
@@ -1,20 +1,31 @@
 package org.meveo.api.restful.filter;
 
 
-import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import java.io.IOException;
 
 @WebFilter(filterName = "RestCorsResponseFilter", urlPatterns = { "/api/rest/v2/generic/all/*" })
-public class RestCorsResponseFilter implements Filter {
+public class RestCorsResponseFilter implements ContainerResponseFilter {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "https://wda.d2.opencell.work");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-        filterChain.doFilter(servletRequest, servletResponse);
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Headers","Content-Type, Authorization, Content-Length");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
+
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//        response.setHeader("Access-Control-Allow-Origin", "https://wda.d2.opencell.work");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+//        filterChain.doFilter(servletRequest, servletResponse);
+//    }
 }
