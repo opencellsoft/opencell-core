9e869de78e1531bb1d0b779a703250c339a2cafe
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2022-07-12 10:34:51 +0100

    fix INTRD-8514

--- opencell-admin/ejbs/src/main/java/org/meveo/service/mediation/EdrVersioningRuleService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/mediation/EdrVersioningRuleService.java
@@ -1,11 +1,31 @@
 package org.meveo.service.mediation;
 
+import java.util.ArrayList;
+
 import javax.ejb.Stateless;
 
+import org.apache.commons.lang3.StringUtils;
+import org.meveo.api.exception.MissingParameterException;
 import org.meveo.model.mediation.EdrVersioningRule;
 import org.meveo.service.base.PersistenceService;
 
 @Stateless
 public class EdrVersioningRuleService extends PersistenceService<EdrVersioningRule>{
 
+
+	public void checkField(EdrVersioningRule edrV) {
+		var fieldNames = new ArrayList<String>();
+		if(edrV.getPriority() == null)
+			fieldNames.add("priority");
+		if(StringUtils.isEmpty(edrV.getCriteriaEL()))
+			fieldNames.add("criteriaEL");
+		if(StringUtils.isEmpty(edrV.getKeyEL()))
+			fieldNames.add("KeyEL");
+		if(StringUtils.isEmpty(edrV.getIsNewVersionEL()))
+			fieldNames.add("isNewVersionEL");
+		if(fieldNames.size() > 0) {
+			throw new MissingParameterException("One of theses field is mandatory : " + fieldNames.toString());
+		}
+			
+	}
 }
