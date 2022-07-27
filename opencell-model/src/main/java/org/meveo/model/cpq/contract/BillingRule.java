f4bcaa7528d2addd78c73c04deba2df0425ed462
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2022-07-27 13:35:39 +0100

    INTRD-8753 - BillingRule - New datamodel

---
+++ opencell-model/src/main/java/org/meveo/model/cpq/contract/BillingRule.java
@@ -0,0 +1,77 @@
+package org.meveo.model.cpq.contract;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.meveo.model.EnableEntity;
+
+/**
+ * 
+ * @author Anas
+ * @since 13.0
+ *
+ */
+@Entity
+@Table(name = "cpq_billing_rule")
+@GenericGenerator(name = "ID_GENERATOR", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+        @Parameter(name = "sequence_name", value = "cpq_billing_rule_seq"), })
+public class BillingRule extends EnableEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1727135182839389638L;
+
+	@ManyToOne
+	@JoinColumn(name = "contract_id")
+	private Contract contract;
+
+	@Column(name = "priority")
+	private Integer priority;
+
+	@Column(name = "criteriaEL")
+	private String criteriaEL;
+
+	@Column(name = "invoiceBACodeEL")
+	private String invoicedBACodeEL;
+
+	public Contract getContract() {
+		return contract;
+	}
+
+	public void setContract(Contract contract) {
+		this.contract = contract;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public String getCriteriaEL() {
+		return criteriaEL;
+	}
+
+	public void setCriteriaEL(String criteriaEL) {
+		this.criteriaEL = criteriaEL;
+	}
+
+	public String getInvoicedBACodeEL() {
+		return invoicedBACodeEL;
+	}
+
+	public void setInvoicedBACodeEL(String invoicedBACodeEL) {
+		this.invoicedBACodeEL = invoicedBACodeEL;
+	}
+
+	
+
+}
