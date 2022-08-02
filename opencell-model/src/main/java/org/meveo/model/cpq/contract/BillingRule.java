7d487b47a2d8a62f5061d1cf93ae19a294766723
hichamhanine <hicham.hanine@opencellsoft.com>
2022-08-02 23:49:26 +0100

    #INTRD-8743 Back Apply model changes - DEV

--- opencell-model/src/main/java/org/meveo/model/cpq/contract/BillingRule.java
+++ opencell-model/src/main/java/org/meveo/model/cpq/contract/BillingRule.java
@@ -2,6 +2,7 @@ package org.meveo.model.cpq.contract;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -9,6 +10,7 @@ import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 import org.meveo.model.EnableEntity;
+import org.meveo.model.admin.Seller;
 
 /**
  * 
@@ -27,10 +29,10 @@ public class BillingRule extends EnableEntity {
 	 */
 	private static final long serialVersionUID = 1727135182839389638L;
 
-	@ManyToOne
-	@JoinColumn(name = "contract_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "contract_id", referencedColumnName = "id")
 	private Contract contract;
-
+	
 	@Column(name = "priority")
 	private Integer priority;
 
