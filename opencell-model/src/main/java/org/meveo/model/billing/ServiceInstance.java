d443020d7614e01c69ba3883ea5a8fce373877c4
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2022-08-13 16:52:58 +0100

    use returned value from PersistenceUtils.initializeAndUnproxy to avoid ClassCastException for Hibernate proxy objects.

--- opencell-model/src/main/java/org/meveo/model/billing/ServiceInstance.java
+++ opencell-model/src/main/java/org/meveo/model/billing/ServiceInstance.java
@@ -1262,7 +1262,7 @@ public class ServiceInstance extends BusinessCFEntity implements IWFEntity, ICou
         usageChargeInstances = new ArrayList<>();
 
         for (ChargeInstance chargeInstance : getChargeInstances()) {
-            PersistenceUtils.initializeAndUnproxy(chargeInstance);
+            chargeInstance = PersistenceUtils.initializeAndUnproxy(chargeInstance);
 
             if (chargeInstance.getChargeType().equals("R")) {
                 recurringChargeInstances.add((RecurringChargeInstance) chargeInstance);
