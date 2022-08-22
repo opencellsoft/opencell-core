e5c7af2678fc22a549e9d397efdfdc173f43e3f2
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2022-08-22 13:11:12 +0100

    INTRD-9117 Report dev fix (New entity to handle many products/articles link with many OpenOrder)

--- opencell-model/src/main/java/org/meveo/model/ordering/OpenOrder.java
+++ opencell-model/src/main/java/org/meveo/model/ordering/OpenOrder.java
@@ -86,11 +86,13 @@ public class OpenOrder extends BusinessEntity {
     @OneToMany(mappedBy = "openOrder", fetch = FetchType.LAZY)
     private List<Threshold> thresholds;
 
-    @OneToMany(mappedBy = "openOrder", fetch = FetchType.LAZY)
-    private List<Product> products;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "open_order_products", joinColumns = @JoinColumn(name = "open_order_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "open_product_id", referencedColumnName = "id"))
+    private List<OpenOrderProduct> products;
 
-    @OneToMany(mappedBy = "openOrder", fetch = FetchType.LAZY)
-    private List<AccountingArticle> articles;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "open_order_articles", joinColumns = @JoinColumn(name = "open_order_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "open_article_id", referencedColumnName = "id"))
+    private List<OpenOrderArticle> articles;
 
     @Column(name = "balance")
     private BigDecimal balance;
@@ -194,22 +196,6 @@ public class OpenOrder extends BusinessEntity {
         this.thresholds = thresholds;
     }
 
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    public List<AccountingArticle> getArticles() {
-        return articles;
-    }
-
-    public void setArticles(List<AccountingArticle> articles) {
-        this.articles = articles;
-    }
-
     public BigDecimal getBalance() {
         return balance;
     }
@@ -224,6 +210,22 @@ public class OpenOrder extends BusinessEntity {
 
     public void setTags(List<Tag> tags) {
         this.tags = tags;
+    }
+
+    public List<OpenOrderProduct> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<OpenOrderProduct> products) {
+        this.products = products;
+    }
+
+    public List<OpenOrderArticle> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<OpenOrderArticle> articles) {
+        this.articles = articles;
     }
 
     @PostPersist
