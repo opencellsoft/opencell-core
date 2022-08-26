a4ba91fe2e50376d9db8d30b7bad604b997227ac
Anas ROUAGUEBE <a.rouaguebe_ext@pixid.com>
2022-08-26 15:10:32 +0100

    INTRD-9232 - Quotation - Fix Attribute Operation validation

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/article/AccountingArticleService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/article/AccountingArticleService.java
@@ -281,6 +281,7 @@ public class AccountingArticleService extends BusinessService<AccountingArticle>
 				if (isNotOneOfOperator(givenOperator, RuleOperatorEnum.EQUAL, RuleOperatorEnum.NOT_EQUAL)) {
 					throw new BusinessException(attribute.getAttributeType() + " Atttribut type cannot have operation : " + givenOperator);
 				}
+				break;
 			case TOTAL:
 			case COUNT:
 			case NUMERIC:
@@ -292,6 +293,7 @@ public class AccountingArticleService extends BusinessService<AccountingArticle>
 						RuleOperatorEnum.LESS_THAN_OR_EQUAL, RuleOperatorEnum.LESS_THAN_OR_EQUAL)) {
 					throw new BusinessException(attribute.getAttributeType() + " Atttribut type cannot have operation : " + givenOperator);
 				}
+				break;
 			case LIST_TEXT:
 			case LIST_NUMERIC:
 			case LIST_MULTIPLE_TEXT:
@@ -299,6 +301,7 @@ public class AccountingArticleService extends BusinessService<AccountingArticle>
 				if (isNotOneOfOperator(givenOperator, RuleOperatorEnum.EQUAL, RuleOperatorEnum.NOT_EQUAL, RuleOperatorEnum.EXISTS)) {
 					throw new BusinessException(attribute.getAttributeType() + " Atttribut type cannot have operation : " + givenOperator);
 				}
+				break;
 			case EXPRESSION_LANGUAGE:
 			case INFO:
 			default:
