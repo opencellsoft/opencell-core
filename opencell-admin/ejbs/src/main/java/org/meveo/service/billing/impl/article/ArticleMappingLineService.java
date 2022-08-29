1bd2a214909e7b6944bdcac06e7292d61829b95d
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2022-08-29 18:41:54 +0100

    INTRD-9232 correct LESS_THAN in isValidOperator check

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/article/ArticleMappingLineService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/article/ArticleMappingLineService.java
@@ -73,7 +73,7 @@ public class ArticleMappingLineService extends BusinessService<ArticleMappingLin
 	        	articleMappingLineUpdated.setArticleMapping(null);
 	        }
 	        articleMappingLineUpdated.setAccountingArticle(accountingArticle);
-	        populateArticleMappingLineForUpdate(articleMappingLineUpdated,articleMappingLine);
+	        populateArticleMappingLine(articleMappingLine);
 	        
 	        articleMappingLineUpdated.setParameter1(articleMappingLine.getParameter1());
 	        articleMappingLineUpdated.setParameter2(articleMappingLine.getParameter2());
@@ -111,21 +111,6 @@ public class ArticleMappingLineService extends BusinessService<ArticleMappingLin
         if(articleMappingLine.getChargeTemplate() != null){
             ChargeTemplate chargeTemplate = (ChargeTemplate) tryToFindByEntityClassAndCodeOrId(ChargeTemplate.class, articleMappingLine.getChargeTemplate().getCode(), articleMappingLine.getChargeTemplate().getId());
             articleMappingLine.setChargeTemplate(chargeTemplate);
-        }
-    }
-	
-	private void populateArticleMappingLineForUpdate(ArticleMappingLine updatedArticleMappingLine,ArticleMappingLine articleMappingLine) {
-    	if(articleMappingLine.getOfferTemplate() != null){
-            OfferTemplate offerTemplate = (OfferTemplate) tryToFindByCodeOrId(articleMappingLine.getOfferTemplate());
-            updatedArticleMappingLine.setOfferTemplate(offerTemplate);
-        }
-        if(articleMappingLine.getProduct() != null){
-        	Product product = (Product) tryToFindByCodeOrId(articleMappingLine.getProduct());
-        	updatedArticleMappingLine.setProduct(product);
-        }
-        if(articleMappingLine.getChargeTemplate() != null){
-            ChargeTemplate chargeTemplate = (ChargeTemplate) tryToFindByEntityClassAndCodeOrId(ChargeTemplate.class, articleMappingLine.getChargeTemplate().getCode(), articleMappingLine.getChargeTemplate().getId());
-            updatedArticleMappingLine.setChargeTemplate(chargeTemplate);
         }
     }
 
@@ -177,7 +162,7 @@ public class ArticleMappingLineService extends BusinessService<ArticleMappingLin
 			case CALENDAR:
 				if (isNotOneOfOperator(givenOperator, RuleOperatorEnum.EQUAL, RuleOperatorEnum.NOT_EQUAL,
 						RuleOperatorEnum.GREATER_THAN, RuleOperatorEnum.GREATER_THAN_OR_EQUAL,
-						RuleOperatorEnum.LESS_THAN_OR_EQUAL, RuleOperatorEnum.LESS_THAN_OR_EQUAL)) {
+						RuleOperatorEnum.LESS_THAN, RuleOperatorEnum.LESS_THAN_OR_EQUAL)) {
 					throw new BusinessException(attribute.getAttributeType() + " Atttribut type cannot have operation : " + givenOperator);
 				}
 			case LIST_TEXT:
