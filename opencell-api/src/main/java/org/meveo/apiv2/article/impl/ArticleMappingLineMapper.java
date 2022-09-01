6406cdd134f1192b5964a236b2d17aea7379533b
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2022-09-01 12:10:30 +0100

    INTRD-9493 make default operator and ruleOperator in API

--- opencell-api/src/main/java/org/meveo/apiv2/article/impl/ArticleMappingLineMapper.java
+++ opencell-api/src/main/java/org/meveo/apiv2/article/impl/ArticleMappingLineMapper.java
@@ -4,6 +4,7 @@ import static org.meveo.apiv2.models.ImmutableResource.builder;
 import static org.meveo.commons.utils.EjbUtils.getServiceInterface;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.hibernate.proxy.HibernateProxy;
@@ -60,16 +61,14 @@ public class ArticleMappingLineMapper extends ResourceMapper<org.meveo.apiv2.art
         articleMappingLine.setParameter2(resource.getParameter2());
         articleMappingLine.setParameter3(resource.getParameter3());
         articleMappingLine.setMappingKelEL(resource.getMappingKeyEL());
-        articleMappingLine.setAttributeOperator(resource.getAttributeOperator());
+        articleMappingLine.setAttributeOperator(Optional.ofNullable(resource.getAttributeOperator()).orElse(OperatorEnum.AND));
         if(resource.getAttributesMapping() != null){
             List<AttributeMapping> attributesMapping = resource.getAttributesMapping()
                     .stream()
                     .map(attributeMapping -> {
                         Attribute attribute = new Attribute(attributeMapping.getAttribute().getId());
                         attribute.setCode(attributeMapping.getAttribute().getCode());
-                        // return new AttributeMapping(attribute, attributeMapping.getAttributeValue(), attributeMapping.getOperator());
-                        //  uncommend previous line and remove the following when Frontend and design technique for US INTRD-9233 are done
-                        return new AttributeMapping(attribute, attributeMapping.getAttributeValue(), RuleOperatorEnum.EQUAL);
+                        return new AttributeMapping(attribute, attributeMapping.getAttributeValue(), Optional.ofNullable(attributeMapping.getOperator()).orElse(RuleOperatorEnum.EQUAL));
                     })
                     .collect(Collectors.toList());
             articleMappingLine.setAttributesMapping(attributesMapping);
