c6086f1bca8495ca7b3f1d1b8c27dbeea50065b0
aeljaouahri <adil.eljaouhari@opencellsoft.com>
2022-10-11 10:59:50 +0200

    fix bug INTRD-10501-bug-review-po

--- opencell-api/src/main/java/org/meveo/apiv2/admin/providers/ProviderResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/admin/providers/ProviderResourceImpl.java
@@ -208,7 +208,9 @@ public class ProviderResourceImpl implements ProviderResource {
         }
         
         if (provider.getRgaaMessage() != null) {
-            providerByCode.setRgaaMessage(providerUpdateInfos.getRgaaMessage());
+        	if (provider.getRgaaMessage().length() > 500) {
+                throw new InvalidParameterException("Max size is 500 characters.");
+            } 
         }
         
         providerService.update(providerByCode);
