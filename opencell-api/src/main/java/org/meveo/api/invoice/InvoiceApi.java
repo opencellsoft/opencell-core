2f11d6fa147f3697b478b7b72a02a569d9f742ec
Zakaria EL MELIANI <zakaria.elmeliani@opencellsoft.com>
2022-10-13 10:47:22 +0100

    INTRD-10506: Increment SDTemplate Number of instanciation after creating a new SD

--- opencell-api/src/main/java/org/meveo/api/invoice/InvoiceApi.java
+++ opencell-api/src/main/java/org/meveo/api/invoice/InvoiceApi.java
@@ -585,12 +585,17 @@ public class InvoiceApi extends BaseApi {
 
         Date today = new Date();
         invoice = invoiceService.refreshOrRetrieve(invoice);
-        
+
         //Create SD
         if (invoice.getInvoiceType() != null && "SECURITY_DEPOSIT".equals(invoice.getInvoiceType().getCode()) && createSD) {
             SecurityDepositTemplate defaultSDTemplate = securityDepositTemplateService.getDefaultSDTemplate();
             Long count = securityDepositService.countPerTemplate(defaultSDTemplate);
             securityDepositService.createSD(invoice, defaultSDTemplate, count);
+
+            //Get SD Template number of instantiation and update it after creating a new SD
+            Integer numberOfInstantiation = defaultSDTemplate.getNumberOfInstantiation();
+            defaultSDTemplate.setNumberOfInstantiation(numberOfInstantiation + 1);
+            securityDepositTemplateService.update(defaultSDTemplate);
         }
         
         if(invoice.getDueDate().after(today) && invoice.getStatus() == VALIDATED){
