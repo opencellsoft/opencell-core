78fdda6ec7efb20cf821b0adc9f90c9a2b98e0ce
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2022-10-23 21:51:01 +0100

    INTRD-10695 - SecurityDeposit - Refund - Unitary Test

--- opencell-api/src/test/java/org/meveo/apiv2/securityDeposit/service/SecurityDepositApiServiceTest.java
+++ opencell-api/src/test/java/org/meveo/apiv2/securityDeposit/service/SecurityDepositApiServiceTest.java
@@ -3,27 +3,37 @@ package org.meveo.apiv2.securityDeposit.service;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.meveo.admin.exception.BusinessException;
 import org.meveo.admin.exception.ImportInvoiceException;
 import org.meveo.admin.exception.InvoiceExistException;
 import org.meveo.api.exception.BusinessApiException;
 import org.meveo.api.exception.EntityDoesNotExistsException;
+import org.meveo.api.exception.MissingParameterException;
+import org.meveo.apiv2.billing.BasicInvoice;
+import org.meveo.apiv2.billing.service.InvoiceApiService;
 import org.meveo.model.admin.Currency;
-import org.meveo.model.article.AccountingArticle;
 import org.meveo.model.billing.BillingAccount;
 import org.meveo.model.billing.Invoice;
 import org.meveo.model.billing.InvoiceLineTaxModeEnum;
 import org.meveo.model.billing.InvoiceStatusEnum;
 import org.meveo.model.billing.LinkedInvoice;
 import org.meveo.model.securityDeposit.SecurityDeposit;
+import org.meveo.model.securityDeposit.SecurityDepositOperationEnum;
 import org.meveo.model.securityDeposit.SecurityDepositStatusEnum;
 import org.meveo.model.securityDeposit.SecurityDepositTemplate;
 import org.meveo.model.securityDeposit.ValidityPeriodUnit;
-import org.meveo.service.tax.TaxMappingService.TaxInfo;
+import org.meveo.service.billing.impl.InvoiceService;
+import org.meveo.service.billing.impl.ServiceSingleton;
+import org.meveo.service.securityDeposit.impl.SecurityDepositService;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
@@ -31,10 +41,27 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
+
 @RunWith(MockitoJUnitRunner.class)
 public class SecurityDepositApiServiceTest {
     @InjectMocks
     private SecurityDepositApiService securityDepositApiService;
+
+    @Mock
+    private SecurityDepositService securityDepositServiceMock;
+
+    @Mock
+    private InvoiceService invoiceServiceMock;
+
+    @Mock
+    private EntityManager entityManagerMock;
+
+    @Mock
+    private InvoiceApiService invoiceApiServiceMock;
+
+    @Mock
+    private ServiceSingleton serviceSingletonMock;
 
     private Long sdId = 10000L;
     private BigDecimal amount = new BigDecimal(90);
@@ -180,5 +207,22 @@ public class SecurityDepositApiServiceTest {
                 }                
             }
         } catch (Exception exception) { }        
+    }
+
+    @Test
+    public void testRefund() throws MissingParameterException, EntityDoesNotExistsException, BusinessException, ImportInvoiceException, InvoiceExistException, IOException {
+    	SecurityDeposit sd = init();
+    	BillingAccount billingAccount = new BillingAccount();
+        billingAccount.setCode("OAU4494");
+        sd.setBillingAccount(billingAccount);
+        sd.setCurrentBalance(BigDecimal.ONE);
+
+        Invoice adjustmentInvoiceMock = new Invoice();
+        
+    	when(securityDepositServiceMock.refreshOrRetrieve(sd)).thenReturn(sd);
+    	when(invoiceServiceMock.createBasicInvoice(any(BasicInvoice.class))).thenReturn(adjustmentInvoiceMock);
+    	when(invoiceServiceMock.getEntityManager()).thenReturn(entityManagerMock);
+    
+    	securityDepositApiService.refund(sd, "motif", SecurityDepositOperationEnum.REFUND_SECURITY_DEPOSIT, SecurityDepositStatusEnum.REFUNDED, "REFUND");
     }
 }
