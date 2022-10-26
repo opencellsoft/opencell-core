c94d1990892aa4eaf09a3f2e2bd0ed035fc50eb9
Anas ROUAGUEBE <a.rouaguebe_ext@pixid.com>
2022-10-26 22:03:29 +0100

    INTRD-10805 - SecurityDeposit - Update Credit process

--- opencell-api/src/main/java/org/meveo/apiv2/securityDeposit/impl/SecurityDepositResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/securityDeposit/impl/SecurityDepositResourceImpl.java
@@ -14,26 +14,19 @@ import javax.ws.rs.core.Response;
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.admin.exception.ImportInvoiceException;
 import org.meveo.admin.exception.InvoiceExistException;
-import org.meveo.admin.exception.NoAllOperationUnmatchedException;
-import org.meveo.admin.exception.UnbalanceAmountException;
 import org.meveo.admin.exception.ValidationException;
-import org.meveo.api.dto.payment.PaymentDto;
 import org.meveo.api.exception.BusinessApiException;
 import org.meveo.api.exception.EntityDoesNotExistsException;
 import org.meveo.api.exception.MeveoApiException;
 import org.meveo.api.invoice.InvoiceApi;
-import org.meveo.api.payment.PaymentApi;
 import org.meveo.apiv2.securityDeposit.*;
 import org.meveo.apiv2.securityDeposit.resource.SecurityDepositResource;
 import org.meveo.apiv2.securityDeposit.service.SecurityDepositApiService;
-import org.meveo.model.payments.OperationCategoryEnum;
-import org.meveo.model.payments.Payment;
 import org.meveo.model.securityDeposit.SecurityDeposit;
 import org.meveo.model.securityDeposit.SecurityDepositOperationEnum;
 import org.meveo.model.securityDeposit.SecurityDepositStatusEnum;
 import org.meveo.service.audit.logging.AuditLogService;
 import org.meveo.service.billing.impl.BillingAccountService;
-import org.meveo.service.payments.impl.PaymentService;
 import org.meveo.service.securityDeposit.impl.SecurityDepositService;
 
 public class SecurityDepositResourceImpl implements SecurityDepositResource {
@@ -46,12 +39,6 @@ public class SecurityDepositResourceImpl implements SecurityDepositResource {
     
     @Inject
     private AuditLogService auditLogService;
-    
-    @Inject
-    private PaymentApi paymentApi;
-    
-    @Inject
-    private PaymentService paymentService;
     
     @Inject
     private InvoiceApi invoiceApi;
@@ -178,27 +165,7 @@ public class SecurityDepositResourceImpl implements SecurityDepositResource {
     
     @Override
     public Response credit(Long id, SecurityDepositCreditInput securityDepositInput) {
-        SecurityDeposit securityDepositToUpdate = securityDepositService.findById(id);
-        if(securityDepositToUpdate == null) {
-            throw new EntityDoesNotExistsException("security deposit with id " + id + " does not exist.");
-        }
-        if(SecurityDepositStatusEnum.CANCELED.equals(securityDepositToUpdate.getStatus())){
-            throw new EntityDoesNotExistsException("The Credit is not possible if the status of the security deposit is at 'Cancel'");
-        } 
-        securityDepositService.credit(securityDepositToUpdate, securityDepositInput);
-        PaymentDto paymentDto = createPaymentDto(securityDepositInput);     
-        Long idPayment = null;
-        try {
-            idPayment = paymentApi.createPayment(paymentDto);
-        } catch (BusinessException e) {
-            throw new BusinessException(e);
-        } catch (MeveoApiException | NoAllOperationUnmatchedException | UnbalanceAmountException e) {
-            throw new MeveoApiException(e);
-        }
-        Payment payment = paymentService.findById(idPayment);
-        securityDepositService.createSecurityDepositTransaction(securityDepositToUpdate, securityDepositInput.getAmountToCredit(), 
-            SecurityDepositOperationEnum.CREDIT_SECURITY_DEPOSIT, OperationCategoryEnum.CREDIT, payment);        
-        auditLogService.trackOperation("CREDIT", new Date(), securityDepositToUpdate, securityDepositToUpdate.getCode());
+        SecurityDeposit securityDepositToUpdate = securityDepositApiService.credit(id, securityDepositInput);
         securityDepositToUpdate.setBillingAccount(billingAccountService.refreshOrRetrieve(securityDepositToUpdate.getBillingAccount()));
         return Response.ok().entity(buildResponse(securityDepositMapper.toResource(securityDepositToUpdate))).build();
     }
@@ -211,32 +178,6 @@ public class SecurityDepositResourceImpl implements SecurityDepositResource {
         return Response.ok().build();
     }
 
-    private PaymentDto createPaymentDto(SecurityDepositCreditInput securityDepositInput) {
-        PaymentDto paymentDto = new PaymentDto();
-        paymentDto.setToMatching(securityDepositInput.getIsToMatching());
-        paymentDto.setCustomerAccountCode(securityDepositInput.getCustomerAccountCode());        
-        paymentDto.setPaymentMethod(securityDepositInput.getPaymentMethod());
-        paymentDto.setAmount(securityDepositInput.getAmountToCredit());
-        paymentDto.setDescription(null);
-        paymentDto.setReference(securityDepositInput.getReference());
-        paymentDto.setDueDate(new Date());
-        paymentDto.setTransactionDate(new Date());
-        paymentDto.setBankLot(securityDepositInput.getBankLot());
-        paymentDto.setPaymentInfo(securityDepositInput.getPaymentInfo());
-        paymentDto.setPaymentInfo1(securityDepositInput.getPaymentInfo1());
-        paymentDto.setPaymentInfo2(securityDepositInput.getPaymentInfo2());
-        paymentDto.setPaymentInfo3(securityDepositInput.getPaymentInfo3());
-        paymentDto.setPaymentInfo4(securityDepositInput.getPaymentInfo4());
-        paymentDto.setPaymentInfo5(securityDepositInput.getPaymentInfo5());
-        paymentDto.setOccTemplateCode(securityDepositInput.getOccTemplateCode());
-        paymentDto.setPaymentInfo6(null);
-        paymentDto.setFees(null);
-        paymentDto.setComment(null);
-        paymentDto.setPaymentOrder(null);
-        paymentDto.setCollectionDate(new Date());
-        return paymentDto;
-    }
-        
     private Map<String, Object> buildResponse(SecurityDepositInput resource) {
         Map<String, Object> response = new HashMap<>();
         response.put("actionStatus", Collections.singletonMap("status","SUCCESS"));
