6428f9e84887e183d1c61eb3ccf45d3476abab22
Andrius Karpavicius <andrius.karpavicius@opencellsoft.com>
2022-10-27 18:11:46 -0300

    Re #INTRD-10253 string_agg_long function definition

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatedTransactionService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatedTransactionService.java
@@ -1568,8 +1568,9 @@ public class RatedTransactionService extends PersistenceService<RatedTransaction
                 "(case when sum(rt.quantity)=0 then sum(rt.amountWithoutTax) else (sum(rt.amountWithoutTax) / sum(rt.quantity)) end) as unit_amount_without_tax, (case when sum(rt.quantity)=0 then sum(rt.amountWithTax) else (sum(rt.amountWithTax) / sum(rt.quantity)) end)  as unit_amount_with_tax," :
                 "rt.unitAmountWithoutTax as unit_amount_without_tax, rt.unitAmountWithTax as unit_amount_with_tax,  ";
         final String unitAmountGroupBy = aggregationConfiguration.isAggregationPerUnitAmount() ? "" : " rt.unitAmountWithoutTax, rt.unitAmountWithTax,  ";
+        
         String query =
-                "SELECT string_agg_long(concat(rt.id, ''), ',') as rated_transaction_ids, rt.billingAccount.id as billing_account__id, rt.accountingCode.id as accounting_code_id, rt.description as label, SUM(rt.quantity) AS quantity, "
+                "SELECT string_agg_long(rt.id) as rated_transaction_ids, rt.billingAccount.id as billing_account__id, rt.accountingCode.id as accounting_code_id, rt.description as label, SUM(rt.quantity) AS quantity, "
                         + unitAmount + " SUM(rt.amountWithoutTax) as sum_without_tax, SUM(rt.amountWithTax) as sum_with_tax, rt.offerTemplate.id as offer_id, "
                         + usageDateAggregation + " as usage_date, min(rt.startDate) as start_date, max(rt.endDate) as end_date, rt.orderNumber as order_number, "
                         + " rt.taxPercent as tax_percent, rt.tax.id as tax_id, "
