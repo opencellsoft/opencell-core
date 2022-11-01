87f15b3f07722b4ec6ebbbe2bfd4dae21aeb1d2e
elhaloui <elhaloui@yandex.com>
2022-11-01 10:10:11 +0100

    INTRD-8491 Filters on the Currency and Billing currency are missing

--- opencell-api/src/main/java/org/meveo/apiv2/standardReport/impl/StandardReportResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/standardReport/impl/StandardReportResourceImpl.java
@@ -34,13 +34,13 @@ public class StandardReportResourceImpl implements StandardReportResource {
                                        Date startDate, Date startDueDate, Date endDueDate, String customerAccountDescription,
                                        String sellerDescription, String sellerCode,
                                        String invoiceNumber,
-                                       Integer stepInDays, Integer numberOfPeriods, String tradingCurrency, Request request) {
+                                       Integer stepInDays, Integer numberOfPeriods, String tradingCurrency, String functionalCurrency, Request request) {
     	if (startDate == null) {
     		startDate = new Date();
 		}
     	List<Object[]> agedBalanceList =
                 standardReportApiService.list(offset, limit, sort, orderBy, customerAccountCode, startDate,
-                        startDueDate, endDueDate, customerAccountDescription, sellerDescription, sellerCode, invoiceNumber, stepInDays, numberOfPeriods, tradingCurrency);
+                        startDueDate, endDueDate, customerAccountDescription, sellerDescription, sellerCode, invoiceNumber, stepInDays, numberOfPeriods, tradingCurrency, functionalCurrency);
         agedReceivableMapper.setAppProvider(appProvider);
     	List<AgedReceivableDto> agedReceivablesList = (stepInDays == null && numberOfPeriods == null)
                 ? agedReceivableMapper.toEntityList(agedBalanceList) : agedReceivableMapper.buildDynamicResponse(agedBalanceList, numberOfPeriods);
