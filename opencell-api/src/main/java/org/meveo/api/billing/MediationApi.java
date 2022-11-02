69226d5d0be0ded468dcc617bf6a71784027433d
Tarik FAKHOURI <tarik.fakhouri@opencellsoft.com>
2022-11-02 15:44:14 +0100

    INTRD-10990 (fix sonar part 2)

--- opencell-api/src/main/java/org/meveo/api/billing/MediationApi.java
+++ opencell-api/src/main/java/org/meveo/api/billing/MediationApi.java
@@ -19,7 +19,6 @@
 package org.meveo.api.billing;
 
 import java.math.BigDecimal;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
