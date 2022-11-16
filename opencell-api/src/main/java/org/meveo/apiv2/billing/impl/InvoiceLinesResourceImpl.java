4d432f6a966d7b1fd78afa998f49dabc384524c7
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2022-11-16 11:51:53 +0100

    INTRD-11537 manage the filter

--- opencell-api/src/main/java/org/meveo/apiv2/billing/impl/InvoiceLinesResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/billing/impl/InvoiceLinesResourceImpl.java
@@ -3,16 +3,25 @@ package org.meveo.apiv2.billing.impl;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.meveo.admin.util.pagination.PaginationConfiguration;
 import org.meveo.apiv2.billing.InvoiceLinesToMarkAdjustment;
 import org.meveo.apiv2.billing.resource.InvoiceLinesResource;
 import org.meveo.apiv2.billing.service.InvoiceLinesApiService;
+import org.meveo.apiv2.generic.GenericPagingAndFiltering;
+import org.meveo.apiv2.generic.ImmutableGenericPagingAndFiltering;
+import org.meveo.apiv2.generic.core.GenericHelper;
+import org.meveo.apiv2.generic.core.GenericRequestMapper;
+import org.meveo.apiv2.generic.services.GenericApiLoadService;
+import org.meveo.apiv2.generic.services.GenericApiPersistenceDelegate;
+import org.meveo.apiv2.generic.services.PersistenceServiceHelper;
+import org.meveo.apiv2.generic.services.SearchResult;
 
 public class InvoiceLinesResourceImpl implements InvoiceLinesResource {
 
@@ -30,8 +39,9 @@ public class InvoiceLinesResourceImpl implements InvoiceLinesResource {
     @Override
     public Response markForAdjustment(@NotNull InvoiceLinesToMarkAdjustment invoiceLinesToMark) {
     	int nbInvoiceLinesMarked = 0;
-    	if(!CollectionUtils.isEmpty(invoiceLinesToMark.getInvoiceLinesIds()))
-    		nbInvoiceLinesMarked = invoiceLinesApiService.markInvoiceLinesForAdjustment(invoiceLinesToMark);
+    	invoiceLinesToMark.getFilters().get("inList id");
+    	/*if(!CollectionUtils.isEmpty(invoiceLinesToMark.getInvoiceLinesIds()))
+    		nbInvoiceLinesMarked = invoiceLinesApiService.markInvoiceLinesForAdjustment(invoiceLinesToMark);*/
     	
         Map<String, Object> response = new HashMap<>();
         response.put("actionStatus", Collections.singletonMap("status", "SUCCESS"));
@@ -41,14 +51,19 @@ public class InvoiceLinesResourceImpl implements InvoiceLinesResource {
     
     @Override
     public Response unmarkForAdjustment(@NotNull InvoiceLinesToMarkAdjustment invoiceLinesToUnmark) {
+    	
+    	invoiceLinesApiService.getInvoiceLineIds(invoiceLinesToUnmark);
+    	
+    	
     	int nbInvoiceLinesUnmarked = 0;
-    	if(!CollectionUtils.isEmpty(invoiceLinesToUnmark.getInvoiceLinesIds()))
-    		nbInvoiceLinesUnmarked = invoiceLinesApiService.unmarkInvoiceLinesForAdjustment(invoiceLinesToUnmark);
+    	/*if(!CollectionUtils.isEmpty(invoiceLinesToUnmark.getInvoiceLinesIds()))
+    		nbInvoiceLinesUnmarked = invoiceLinesApiService.unmarkInvoiceLinesForAdjustment(invoiceLinesToUnmark);*/
     	
         Map<String, Object> response = new HashMap<>();
         response.put("actionStatus", Collections.singletonMap("status", "SUCCESS"));
         response.put("message", nbInvoiceLinesUnmarked+" new invoiceLine(s) marked NOT_ADJUSTED");
         return Response.ok(response).build();
     }
+
 
 }
