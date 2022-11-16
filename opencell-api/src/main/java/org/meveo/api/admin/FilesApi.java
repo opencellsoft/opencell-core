2ff9aa8fbe4db551f179074b0e24f1d98650bcfe
Thang Nguyen <thang.nguyen@opencellsoft.com>
2022-11-16 10:14:00 +0100

    Adapt Files Api for creating a File System in S3

--- opencell-api/src/main/java/org/meveo/api/admin/FilesApi.java
+++ opencell-api/src/main/java/org/meveo/api/admin/FilesApi.java
@@ -21,6 +21,7 @@ package org.meveo.api.admin;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
+import org.meveo.admin.storage.StorageFactory;
 import org.meveo.admin.util.FlatFileValidator;
 import org.meveo.admin.util.DirectoriesConstants;
 import org.meveo.api.BaseApi;
@@ -42,12 +43,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.zip.ZipOutputStream;
 
 import static java.lang.String.format;
@@ -113,8 +119,8 @@ public class FilesApi extends BaseApi {
             subDirOUT, subDirERR, subDirWARN, catDirIN, catDirOUT, catDirKO, subDirKO, meterDirIN, meterDirOUT, meterDirKO, invoicePdfDir, invoiceXmlDir, jasperDir, priceplanVersionsDir);
         for (String custDirs : filePaths) {
             File subDir = new File(custDirs);
-            if (!subDir.exists()) {
-                subDir.mkdirs();
+            if (!StorageFactory.existsDirectory(subDir)) {
+                StorageFactory.mkdirs(subDir);
             }
         }
     }
@@ -134,11 +140,19 @@ public class FilesApi extends BaseApi {
 
         List<FileDto> result = new ArrayList<FileDto>();
 
-        if (folder.listFiles() != null && folder.listFiles().length > 0) {
-            List<File> files = Arrays.asList(folder.listFiles());
-            if (files != null) {
+        if (! StorageFactory.isS3Activated()) {
+            if (folder.listFiles() != null && Objects.requireNonNull(folder.listFiles()).length > 0) {
+                List<File> files = Arrays.asList(Objects.requireNonNull(folder.listFiles()));
                 for (File file : files) {
                     result.add(new FileDto(file));
+                }
+            }
+        }
+        else {
+            if (Objects.requireNonNull(StorageFactory.listSubFoldersAndFiles(folder)).size() > 0) {
+                Map<String, Date> map = Objects.requireNonNull(StorageFactory.listSubFoldersAndFiles(folder));
+                for (Map.Entry<String, Date> entry : map.entrySet()) {
+                    result.add(new FileDto(entry.getKey(), entry.getValue()));
                 }
             }
         }
@@ -167,8 +181,8 @@ public class FilesApi extends BaseApi {
 
     public void createDir(String dir) throws BusinessApiException {
         File file = new File(getProviderRootDir() + File.separator + normalizePath(dir));
-        if (!file.exists()) {
-            file.mkdirs();
+        if (!StorageFactory.existsDirectory(file)) {
+            StorageFactory.createDirectory(file);
         }
     }
 
@@ -219,13 +233,13 @@ public class FilesApi extends BaseApi {
      */
     public FlatFile uploadFile(byte[] data, String filename, String fileFormat) throws BusinessApiException {
         File file = new File(getProviderRootDir() + File.separator + normalizePath(filename));
-        try (FileOutputStream fop = new FileOutputStream(file)){
+        try (OutputStream fop = StorageFactory.getOutputStream(file)){
 //            if (!file.exists()) {
 //                file.createNewFile();
 //            }
 
-            ;
 
+            assert fop != null;
             fop.write(data);
             fop.close();
             
@@ -233,7 +247,7 @@ public class FilesApi extends BaseApi {
                 // unzip
                 // get parent dir
                 String parentDir = file.getParent();
-                FileUtils.unzipFile(parentDir, new FileInputStream(file));
+                FileUtils.unzipFile(parentDir, StorageFactory.getInputStream(file));
             }
 
             if (!StringUtils.isBlank(fileFormat)) {
@@ -322,9 +336,9 @@ public class FilesApi extends BaseApi {
         String filename = getProviderRootDir() + File.separator + normalizePath(filePath);
         File file = new File(filename);
 
-        if (file.exists()) {
+        if (StorageFactory.exists(file)) {
             try {
-                file.delete();
+                StorageFactory.delete(file);
             } catch (Exception e) {
                 throw new BusinessApiException("Error suppressing file: " + filename + ". " + e.getMessage());
             }
@@ -337,9 +351,9 @@ public class FilesApi extends BaseApi {
         String filename = getProviderRootDir() + File.separator + normalizePath(dir);
         File file = new File(filename);
 
-        if (file.exists()) {
+        if (StorageFactory.existsDirectory(file)) {
             try {
-                org.apache.commons.io.FileUtils.deleteDirectory(file);
+                StorageFactory.deleteDirectory(file);
             } catch (Exception e) {
                 throw new BusinessApiException("Error suppressing file: " + filename + ". " + e.getMessage());
             }
@@ -356,9 +370,9 @@ public class FilesApi extends BaseApi {
     public void downloadFile(String filePath, HttpServletResponse response) throws BusinessApiException {
         File file = checkAndGetExistingFile(filePath);
 
-        try (FileInputStream fis = new FileInputStream(file)) {
+        try (InputStream fis = StorageFactory.getInputStream(file)) {
             response.setContentType(Files.probeContentType(file.toPath()));
-            response.setContentLength((int) file.length());
+            response.setContentLength((int) StorageFactory.length(file));
             response.addHeader("Content-disposition", "attachment;filename=\"" + file.getName() + "\"");
             IOUtils.copy(fis, response.getOutputStream());
             response.flushBuffer();
@@ -374,9 +388,10 @@ public class FilesApi extends BaseApi {
      */
     private File checkAndGetExistingFile(String filePath) {
 
-        File javaXMlFormatFile = (filePath.contains(getProviderRootDir().replace("\\", "/"))) ? new File(filePath) : new File(getProviderRootDir() + File.separator + normalizePath(filePath));
+        File javaXMlFormatFile = (filePath.contains(getProviderRootDir().replace("\\", "/"))) ?
+                StorageFactory.getFile(filePath) : StorageFactory.getFile(getProviderRootDir() + File.separator + normalizePath(filePath));
 
-        if (javaXMlFormatFile.exists()) {
+        if (StorageFactory.exists(javaXMlFormatFile)) {
             return javaXMlFormatFile;
         } else {
             String[] fileNameParts = filePath.split("\\.");
