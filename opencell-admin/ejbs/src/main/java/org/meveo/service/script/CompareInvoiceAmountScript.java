9d45c0dc147ab7e72a7b01dc5589b3e9380c15b0
Anas ROUAGUEBE <a.rouaguebe_ext@pixid.com>
2022-12-01 09:21:24 +0100

    INTRD-11783 - Validate invoice amount - init script

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/script/CompareInvoiceAmountScript.java
@@ -0,0 +1,13 @@
+package org.meveo.service.script;
+
+/**
+ * 
+ * 
+ * @author are
+ *
+ */
+public class CompareInvoiceAmountScript extends Script {
+
+	private static final long serialVersionUID = -3842955629518523594L;
+	
+}
