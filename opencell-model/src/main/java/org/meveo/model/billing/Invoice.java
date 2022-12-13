223ad5fb9b45c302b12dcc9a1393871ce455dd27
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2022-12-13 14:21:36 +0100

    INTRD-12184 filter only on linked invoice that has type ADVANCEMENT_PAYMENT

--- opencell-model/src/main/java/org/meveo/model/billing/Invoice.java
+++ opencell-model/src/main/java/org/meveo/model/billing/Invoice.java
@@ -156,7 +156,7 @@ import org.meveo.model.shared.DateUtils;
         @NamedQuery(name = "Invoice.findWithFuntionalCurrencyDifferentFromOne", query = "SELECT i FROM Invoice i JOIN Provider p ON p.currency.id = i.tradingCurrency.currency.id WHERE i.lastAppliedRate <> :EXPECTED_RATE"),
 })
 @NamedNativeQueries({
-	@NamedNativeQuery(name = "Invoice.rollbackAdvance", query = "update billing_invoice set invoice_balance = invoice_balance + li.amount from (select bli.linked_invoice_id, bli.amount from billing_linked_invoices bli join billing_invoice i on i.id = bli.id where i.billing_run_id = :billingRunId) li where li.linked_invoice_id = id")
+	@NamedNativeQuery(name = "Invoice.rollbackAdvance", query = "update billing_invoice set invoice_balance = invoice_balance + li.amount from (select bli.linked_invoice_id, bli.amount from billing_linked_invoices bli join billing_invoice i on i.id = bli.id where i.billing_run_id = :billingRunId and bli.type = 'ADVANCEMENT_PAYMENT') li where li.linked_invoice_id = id")
 })
 public class Invoice extends AuditableEntity implements ICustomFieldEntity, ISearchable {
 
