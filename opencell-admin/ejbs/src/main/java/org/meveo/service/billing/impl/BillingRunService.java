95582383acca26949196427185cc84cc701438be
elhaloui <elhaloui@yandex.com>
2022-12-15 08:16:03 +0100

    INTRD-12343 Threshold priority order : SUB, ORDER, BC-BA, BA, CA, C

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunService.java
@@ -1064,23 +1064,8 @@ public class BillingRunService extends PersistenceService<BillingRun> {
 
             }
 
-            if(entity instanceof CommercialOrder
-                    && ((CommercialOrder)entity).getBillingCycle() != null
-                    && ((CommercialOrder)entity).getBillingAccount().getInvoicingThreshold() == null
-                    && ((CommercialOrder)entity).getBillingAccount().getCustomerAccount().getInvoicingThreshold() == null
-                    && ((CommercialOrder)entity).getBillingAccount().getCustomerAccount().getCustomer().getInvoicingThreshold() == null
-            ){
-                BillingCycle bc = ((CommercialOrder)entity).getBillingCycle();
-                threshold = bc.getInvoicingThreshold();
-                checkThreshold = bc.getCheckThreshold();
-                isThresholdPerEntity = bc.isThresholdPerEntity();
-            }
-            else if(entity instanceof Subscription
+            if(entity instanceof Subscription
                    && ((Subscription)entity).getBillingCycle() != null
-                    && ((Subscription)entity).getUserAccount().getBillingAccount().getInvoicingThreshold() == null
-                    && ((Subscription)entity).getUserAccount().getBillingAccount().getCustomerAccount().getInvoicingThreshold() == null
-                    && ((Subscription)entity).getUserAccount().getBillingAccount().getCustomerAccount().getCustomer().getInvoicingThreshold() == null
-
             ){
 
                 BillingCycle bc = ((Subscription)entity).getBillingCycle();
@@ -1088,6 +1073,15 @@ public class BillingRunService extends PersistenceService<BillingRun> {
                 checkThreshold = bc.getCheckThreshold();
                 isThresholdPerEntity = bc.isThresholdPerEntity();
             }
+            else if(entity instanceof CommercialOrder
+                    && ((CommercialOrder)entity).getBillingCycle() != null
+            ){
+                BillingCycle bc = ((CommercialOrder)entity).getBillingCycle();
+                threshold = bc.getInvoicingThreshold();
+                checkThreshold = bc.getCheckThreshold();
+                isThresholdPerEntity = bc.isThresholdPerEntity();
+            }
+
              else if (entity instanceof BillingAccount) {
                 BillingAccount ba = (BillingAccount) entity;
                 threshold = ba.getInvoicingThreshold();
