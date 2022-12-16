4b934f722e1310263a492f255fc99a6a83033e6b
abdellatif.bari <abdellatif.bari@opencellsoft.com>
2022-12-16 08:54:31 +0100

    INTRD-12345

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatedTransactionService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatedTransactionService.java
@@ -192,6 +192,9 @@ public class RatedTransactionService extends PersistenceService<RatedTransaction
 
     @Inject
     private BillingRulesService billingRulesService;
+
+    @Inject
+    private AccountingCodeService accountingCodeService;
     
     /**
      * Check if Billing account has any not yet billed Rated transactions
@@ -490,6 +493,7 @@ public class RatedTransactionService extends PersistenceService<RatedTransaction
             create(ratedTransaction);
             updateAggregatedWalletOperations(aggregatedWo.getWalletOperationsIds(), ratedTransaction);
         }
+        ratedTransaction.setAccountingCode(accountingCodeService.refreshOrRetrieve(aggregatedWo.getAccountingCode()));
 
         return ratedTransaction;
     }
