f6919792784165e4a38b4069709200e18bc600d5
Thang Nguyen <thang.nguyen@opencellsoft.com>
2022-12-20 16:40:12 +0100

    Adapt Get API to find a price plan matrix with a given code

--- opencell-admin/ejbs/src/main/java/org/meveo/service/catalog/impl/PricePlanMatrixVersionService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/catalog/impl/PricePlanMatrixVersionService.java
@@ -403,7 +403,7 @@ public class PricePlanMatrixVersionService extends PersistenceService<PricePlanM
 
     public PricePlanMatrixVersionDto load(Long id) {
         PricePlanMatrixVersion pricePlanMatrixVersion = findById(id);
-        return new PricePlanMatrixVersionDto(pricePlanMatrixVersion);
+        return new PricePlanMatrixVersionDto(pricePlanMatrixVersion, true);
     }
 
     @SuppressWarnings("unchecked")
