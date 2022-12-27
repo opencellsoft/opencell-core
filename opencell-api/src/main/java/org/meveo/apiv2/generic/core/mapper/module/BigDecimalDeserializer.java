33aa3a6b8cb0c266bea1d3b1996e3a1f58f981ce
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2022-12-28 00:00:38 +0100

    INTRD-12595 add deserializer for BigDecimal.ZERO only

---
+++ opencell-api/src/main/java/org/meveo/apiv2/generic/core/mapper/module/BigDecimalDeserializer.java
@@ -0,0 +1,20 @@
+package org.meveo.apiv2.generic.core.mapper.module;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class BigDecimalDeserializer extends JsonSerializer<BigDecimal> {
+
+    @Override
+    public void serialize(BigDecimal value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
+        if (value.compareTo(BigDecimal.ZERO) == 0) {
+            jsonGenerator.writeNumber(value.setScale(2, BigDecimal.ROUND_HALF_UP).toString());
+        } else {
+            jsonGenerator.writeNumber(value);
+        }
+    }
+}
