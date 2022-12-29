3d7b6b9ed2820a37e73b92952f3bb88a26f128b0
Hatim OUDAD <hatim.oudad@opencellsoft.com>
2022-12-29 11:58:00 +0100

    INTRD-12082 e invoice

--- opencell-model/src/main/java/org/meveo/model/billing/InvoiceType.java
+++ opencell-model/src/main/java/org/meveo/model/billing/InvoiceType.java
@@ -1,5 +1,5 @@
 /*
-3 * (C) Copyright 2015-2020 Opencell SAS (https://opencellsoft.com/) and contributors.
+ * (C) Copyright 2015-2020 Opencell SAS (https://opencellsoft.com/) and contributors.
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
  * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
