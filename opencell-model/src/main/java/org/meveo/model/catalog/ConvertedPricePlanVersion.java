32d0ba97f010c4ffdb60028bc4c65eae4ac0f3a9
Anas ROUAGUEBE <a.rouaguebe_ext@pixid.com>
2023-01-02 21:51:19 +0100

    INTRD-12609 - Fix sonar

--- opencell-model/src/main/java/org/meveo/model/catalog/ConvertedPricePlanVersion.java
+++ opencell-model/src/main/java/org/meveo/model/catalog/ConvertedPricePlanVersion.java
@@ -8,7 +8,6 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
