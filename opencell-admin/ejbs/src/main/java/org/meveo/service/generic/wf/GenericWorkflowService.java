68451bac339e7c2f944f4766d3b05c0beb9a9406
mboukayoua <mounir.boukayoua@opencellsoft.com>
2023-01-12 16:20:19 +0100

    INTRD-13166: GenericWF raises java.util.NoSuchElementException when currentStatus is never fromStatus of any WF's transitions and isn't toStatus of the last transition (merge V10)

--- opencell-admin/ejbs/src/main/java/org/meveo/service/generic/wf/GenericWorkflowService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/generic/wf/GenericWorkflowService.java
@@ -36,7 +36,12 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
