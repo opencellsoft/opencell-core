5633ef45805852a10435354981fb3297a72ceb9d
elhaloui <elhaloui@yandex.com>
2023-01-21 15:08:42 +0100

    INTRD-13243 fix Advanced filters are not persisted

--- opencell-api-dto/src/main/java/org/meveo/api/dto/BillingCycleDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/BillingCycleDto.java
@@ -191,7 +191,7 @@ public class BillingCycleDto extends BusinessEntityDto {
     @XmlElement
     private String billingRunValidationScriptCode;
     
-    private Map<String, String> filters;
+    private Map<String, Object> filters;
 
     public String getLastTransactionDateDelayEL() {
 		return lastTransactionDateDelayEL;
@@ -669,11 +669,11 @@ public class BillingCycleDto extends BusinessEntityDto {
         this.computeDatesAtValidation = computeDatesAtValidation;
     }
 
-	public Map<String, String> getFilters() {
+	public Map<String, Object> getFilters() {
 		return filters;
 	}
 
-	public void setFilters(Map<String, String> filters) {
+	public void setFilters(Map<String, Object> filters) {
 		this.filters = filters;
 	}
 }
