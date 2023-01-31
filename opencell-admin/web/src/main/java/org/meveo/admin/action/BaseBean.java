b5ace3683eb9ce01726cb3ca073c343c6cf1ff24
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2023-01-31 16:22:48 +0000

    fix compile

--- opencell-admin/web/src/main/java/org/meveo/admin/action/BaseBean.java
+++ opencell-admin/web/src/main/java/org/meveo/admin/action/BaseBean.java
@@ -1638,12 +1638,12 @@ public abstract class BaseBean<T extends IEntity> implements Serializable {
         return !(from != null && to != null && from.compareTo(to) > 0);
     }
     
-    public static void showDeprecatedWarning() {
+    public static void showDeprecatedWarning(String pWarningMessage) {
     	List<FacesMessage> messageList = FacesContext.getCurrentInstance().getMessageList();
-		if(messageList!=null && messageList.stream().anyMatch(x->FacesMessage.SEVERITY_WARN.equals(x.getSeverity())&& DEPRECATED_FEATURE.equals(x.getSummary()))) {
+		if(messageList!=null && messageList.stream().anyMatch(x -> FacesMessage.SEVERITY_WARN.equals(x.getSeverity()) && pWarningMessage.equals(x.getSummary()))) {
 			return;
     	}
-    	FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, DEPRECATED_FEATURE, DEPRECATED_FEATURE));
+    	FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, pWarningMessage, pWarningMessage));
     }
 
 }
