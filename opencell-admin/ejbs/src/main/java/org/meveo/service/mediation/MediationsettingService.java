6b6ff416258503a46b0e238762c0b92f48ec6d2a
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2023-02-03 10:24:24 +0000

    Fixing compile

--- opencell-admin/ejbs/src/main/java/org/meveo/service/mediation/MediationsettingService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/mediation/MediationsettingService.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
