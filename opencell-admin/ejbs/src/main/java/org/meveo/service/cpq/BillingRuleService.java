1861d826c0a5e99c89c5f14cf8f33b9ddef7f7cf
ebourras <mehdi.bourras@opencellsoft.com>
2023-02-06 16:32:36 +0100

    INTRD-13631

--- opencell-admin/ejbs/src/main/java/org/meveo/service/cpq/BillingRuleService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/cpq/BillingRuleService.java
@@ -1,11 +1,27 @@
 package org.meveo.service.cpq;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
+import org.meveo.admin.exception.BusinessException;
+import org.meveo.api.dto.cpq.ContractDto;
 import org.meveo.model.cpq.contract.BillingRule;
 import org.meveo.service.base.PersistenceService;
+import org.meveo.service.securityDeposit.impl.FinanceSettingsService;
 
 @Stateless
 public class BillingRuleService extends PersistenceService<BillingRule> {
+
+    @Inject
+    private FinanceSettingsService financeSettingsService;
+    public void checkBillingRedirectionRulesConfiguration(ContractDto contractDto) {
+        if (contractDto.getBillingRules() != null && !isBillingRedirectionRulesEnabled()) {
+            throw new BusinessException("Feature disabled in application settings");
+        }
+    }
+    public boolean isBillingRedirectionRulesEnabled() {
+        return financeSettingsService.isBillingRedirectionRulesEnabled();
+    }
+
 
 }
