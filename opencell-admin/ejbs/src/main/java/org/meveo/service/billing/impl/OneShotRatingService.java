99f96c295cc8d49daf8ab8bbe7906c1609142650
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2023-02-17 10:53:06 +0100

    INTRD-13985 Protect from possible NPE wo.serviceInstance

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/OneShotRatingService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/OneShotRatingService.java
@@ -133,7 +133,8 @@ public class OneShotRatingService extends RatingService implements Serializable 
 
                 // OSO SI Virtual : clean WalletOperation.SI if it is a SI Virtual (id==null)
                 ratingResult.getWalletOperations().forEach(walletOperation -> walletOperation.setServiceInstance(
-                        walletOperation.getServiceInstance().getId() == null ? null : walletOperation.getServiceInstance()));
+                        walletOperation.getServiceInstance() != null && walletOperation.getServiceInstance().getId() == null
+                                ? null : walletOperation.getServiceInstance()));
 
             }
 
