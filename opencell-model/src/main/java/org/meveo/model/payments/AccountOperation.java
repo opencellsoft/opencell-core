adcd061eb2aa7d0f164727d74da819bf3723ca20
BOT <opencell-bot@opencellsoft.com>
2023-03-02 08:45:49 +0000

    Revert "fixing build"
    This reverts commit 40a2a548d8deb9b592da4859e21e7a302881357e.

--- opencell-model/src/main/java/org/meveo/model/payments/AccountOperation.java
+++ opencell-model/src/main/java/org/meveo/model/payments/AccountOperation.java
@@ -1088,6 +1088,14 @@ public class AccountOperation extends BusinessEntity implements ICustomFieldEnti
         auditable.setUpdated(new Date());
     }
 
+    public BigDecimal getAmountForUnmatching() {
+        return amountForUnmatching;
+    }
+
+    public void setAmountForUnmatching(BigDecimal amountForUnmatching) {
+        this.amountForUnmatching = amountForUnmatching;
+    }
+
     public BigDecimal getConvertedAmount() {
         return convertedAmount;
     }
