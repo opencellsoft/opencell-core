b476acb5a999f129433ed7b43ee7e9522fd0951d
hichamhanine <hicham.hanine@opencellsoft.com>
2023-03-03 09:49:26 +0100

    #INTRD-14342 back validate acceptance - DEV

--- opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/RecordedInvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/RecordedInvoiceService.java
@@ -474,7 +474,8 @@ public class RecordedInvoiceService extends PersistenceService<RecordedInvoice> 
         if (isRecordedInvoice) {
             recordedInvoice = (T) new RecordedInvoice();
             recordedInvoice.setNetToPay(netToPay);
-
+            recordedInvoice.setConvertedNetToPay(netConvertedToPay);
+            
             List<String> orderNums = new ArrayList<>();
             if (invoice.getOrders() != null) {
                 for (Order order : invoice.getOrders()) {
@@ -526,6 +527,13 @@ public class RecordedInvoiceService extends PersistenceService<RecordedInvoice> 
 
         recordedInvoice.setAmountWithoutTax(amountWithoutTax);
         recordedInvoice.setTaxAmount(amountTax);
+        
+        recordedInvoice.setConvertedAmount(amountConvertedWithTax);
+        recordedInvoice.setConvertedAmountWithoutTax(amountConvertedWithoutTax);
+        recordedInvoice.setConvertedTaxAmount(amountConvertedTax);        
+        recordedInvoice.setConvertedUnMatchingAmount(amountConvertedWithTax);
+        recordedInvoice.setConvertedMatchingAmount(BigDecimal.ZERO);
+        
         recordedInvoice.setSeller(invoice.getSeller());
         recordedInvoice.setCollectionDate(invoice.getInitialCollectionDate());
         try {
