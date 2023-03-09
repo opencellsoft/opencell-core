871ccecdd2a1f193131dbea02f27ff70e2d194ce
Mohamed El Youssoufi <mohamed-el-youssoufi@outlook.com>
2023-03-09 17:33:26 +0100

    Named parameter not bound : date

--- opencell-admin/ejbs/src/main/java/org/meveo/service/notification/InboundRequestService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/notification/InboundRequestService.java
@@ -32,38 +32,36 @@ import org.meveo.service.base.BusinessService;
 @Stateless
 public class InboundRequestService extends BusinessService<InboundRequest> {
 
-    /**
-     * Count Inbound requests which date is older then a given date.
-     * 
-     * @param date Date to check
-     * @return A number of Inbound requests which date is older then a given date
-     */
-    public long countRequestsToDelete(Date date) {
-        long result = getEntityManager().createNamedQuery("InboundRequest.countRequestsToPurgeByDate", Long.class).setParameter("date", date).getSingleResult();
-        return result;
-    }
+	/**
+	 * Count Inbound requests which date is older then a given date.
+	 * 
+	 * @param date Date to check
+	 * @return A number of Inbound requests which date is older then a given date
+	 */
+	public long countRequestsToDelete(Date date) {
+		long result = getEntityManager().createNamedQuery("InboundRequest.countRequestsToPurgeByDate", Long.class).setParameter("date", date).getSingleResult();
+		return result;
+	}
 
-    /**
-     * Remove Inbound requests which date is older than a given date.
-     * 
-     * @param date Date to check
-     * @return A number of Inbound requests that were removed
-     */
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public long deleteRequests(Date date) {
-        log.debug("Removing Inbound requests which date is older then a {} date", date);
-
-        // Can not delete with a single bulk query as need to remove associated NotificationHistory records and header, cookies records
-        long itemsDeleted = 0;
-        EntityManager em = getEntityManager();
-        List<InboundRequest> requests = em.createNamedQuery("InboundRequest.getRequestsToPurgeByDate", InboundRequest.class).getResultList();
-        for (InboundRequest request : requests) {
-            em.remove(request);
-            itemsDeleted++;
-        }
-
-        log.debug("Removed {} Inbound requests which date is older then a {} date", itemsDeleted, date);
-
-        return itemsDeleted;
-    }
+	/**
+	 * Remove Inbound requests which date is older than a given date.
+	 * 
+	 * @param date Date to check
+	 * @return A number of Inbound requests that were removed
+	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	public long deleteRequests(Date date) {
+		log.debug("Removing Inbound requests which date is older then a {} date", date);
+		// Can not delete with a single bulk query as need to remove associated
+		// NotificationHistory records and header, cookies records
+		long itemsDeleted = 0;
+		EntityManager em = getEntityManager();
+		List<InboundRequest> requests = em.createNamedQuery("InboundRequest.getRequestsToPurgeByDate", InboundRequest.class).setParameter("date", date).getResultList();
+		for (InboundRequest request : requests) {
+			em.remove(request);
+			itemsDeleted++;
+		}
+		log.debug("Removed {} Inbound requests which date is older then a {} date", itemsDeleted, date);
+		return itemsDeleted;
+	}
 }
