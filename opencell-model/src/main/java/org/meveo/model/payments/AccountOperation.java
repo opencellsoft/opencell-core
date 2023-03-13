20ec0a0d1f4cfc008f1d01b04b039dc686739ef2
ebourras <mehdi.bourras@opencellsoft.com>
2023-03-13 13:04:18 +0100

    INTRD-14787

--- opencell-model/src/main/java/org/meveo/model/payments/AccountOperation.java
+++ opencell-model/src/main/java/org/meveo/model/payments/AccountOperation.java
@@ -1094,7 +1094,7 @@ public class AccountOperation extends BusinessEntity implements ICustomFieldEnti
     }
 
     public BigDecimal getConvertedAmount() {
-        return convertedAmount;
+        return convertedAmount != null ? amount : convertedAmount;
     }
 
     public void setConvertedAmount(BigDecimal convertedAmount) {
@@ -1123,14 +1123,14 @@ public class AccountOperation extends BusinessEntity implements ICustomFieldEnti
 
     public void setConvertedMatchingAmount(BigDecimal convertedMatchingAmount) {
         if(convertedMatchingAmount == null) {
-            this.convertedMatchingAmount =BigDecimal.ZERO;
+            this.convertedMatchingAmount = matchingAmount;
         }else {
             this.convertedMatchingAmount = convertedMatchingAmount;
         }
     }
 
     public BigDecimal getConvertedUnMatchingAmount() {
-        return convertedUnMatchingAmount;
+        return convertedUnMatchingAmount != null ? convertedUnMatchingAmount : unMatchingAmount;
     }
 
     public void setConvertedUnMatchingAmount(BigDecimal convertedUnMatchingAmount) {      
