89884aa213bbbee89729abdfcd06bee6ec6ad47a
Andrius Karpavicius <andrius.karpavicius@opencellsoft.com>
2023-03-14 03:42:09 -0300

    Re #INTRD-14743 Added caching of URL per session for API authorization check in KC

--- opencell-admin/web/src/main/java/org/meveo/security/filters/AuthorizationFilter.java
+++ opencell-admin/web/src/main/java/org/meveo/security/filters/AuthorizationFilter.java
@@ -2,6 +2,7 @@ package org.meveo.security.filters;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
@@ -12,25 +13,27 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.meveo.security.keycloak.CurrentUserProvider;
+import org.meveo.util.view.PageAccessHandler;
 
 @WebFilter(urlPatterns = "/*")
 public class AuthorizationFilter implements Filter {
 
+    @Inject
+    private PageAccessHandler pageAccessHandler;
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        boolean isPermited = pageAccessHandler.isCurrentURLAccesible();
 
-        String url = httpRequest.getRequestURL().toString();
-        String contextPath = httpRequest.getContextPath();
-
-        url = url.substring(url.indexOf(contextPath) + contextPath.length());
-
-        boolean[] isPermited = CurrentUserProvider.isLinkAccesible(httpRequest, httpRequest.getMethod(), url);
-
-        if (!isPermited[0]) {
+        if (!isPermited) {
             ((HttpServletResponse) response).setStatus(403);
+
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String url = httpRequest.getRequestURL().toString();
+            String contextPath = httpRequest.getContextPath();
+            url = url.substring(url.indexOf(contextPath) + contextPath.length());
+
             if (!url.startsWith("/api")) {
                 String page = "/errors/403.jsf";
                 RequestDispatcher dispatcher = httpRequest.getRequestDispatcher(page);
