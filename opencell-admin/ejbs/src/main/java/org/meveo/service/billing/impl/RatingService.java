64bdba4fe73cb0493e1944d87b9d2370e31002a8
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2023-03-20 18:31:32 +0000

    INTRD-15092

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatingService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatingService.java
@@ -1540,7 +1540,7 @@ public abstract class RatingService extends PersistenceService<WalletOperation> 
     				isDiscountPlanInstanceUpdated = true;
     			}
     			
-    			if(isDiscountPlanInstanceUpdated) {
+    			if(isDiscountPlanInstanceUpdated && !isVirtual) {
 	   				 discountPlanInstanceService.update(discountPlanInstance);
     			}
     		
