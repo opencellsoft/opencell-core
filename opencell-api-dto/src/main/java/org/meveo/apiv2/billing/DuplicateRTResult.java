cba4889007e294f55c8c3be7db8e8895e0a065db
Tarik FAKHOURI <tarik.fakhouri@opencellsoft.com>
2023-03-22 14:53:11 +0000

    INTRD-15076

--- opencell-api-dto/src/main/java/org/meveo/apiv2/billing/DuplicateRTResult.java
+++ opencell-api-dto/src/main/java/org/meveo/apiv2/billing/DuplicateRTResult.java
@@ -12,6 +12,7 @@ public class DuplicateRTResult {
 	private Statistics statistics;
 	private ActionStatus actionStatus;
 	private List<Long> failIds = new ArrayList<>();
+	private List<Long> createdRts = new ArrayList<>();
 	
 	
 	public DuplicateRTResult(Statistics statistics, ActionStatus actionStatus) {
@@ -63,6 +64,20 @@ public class DuplicateRTResult {
 	 */
 	public void setFailIds(List<Long> failIds) {
 		this.failIds = failIds;
+	}
+
+	/**
+	 * @return the createdRts
+	 */
+	public List<Long> getCreatedRts() {
+		return createdRts;
+	}
+
+	/**
+	 * @param createdRts the createdRts to set
+	 */
+	public void setCreatedRts(List<Long> createdRts) {
+		this.createdRts = createdRts;
 	}
 	
 	
