faa696f963858efb766dc04bd47d64290057bf7e
mboukayoua <mounir.boukayoua@opencellsoft.com>
2023-03-28 03:51:06 +0100

    INTRD-15293: Update Detailed Invoice - Validating Rejected invoice should only pass it to status DRAFT

--- opencell-admin/web/src/main/java/org/meveo/admin/action/billing/CreationInvoiceBean.java
+++ opencell-admin/web/src/main/java/org/meveo/admin/action/billing/CreationInvoiceBean.java
@@ -1439,12 +1439,12 @@ public class CreationInvoiceBean extends CustomFieldBean<Invoice> {
     
     public String cancelInvoice() throws BusinessException {
         invoiceService.cancelInvoice(entity, false);
-        return saveOrUpdate(false);
+        return getListViewName();
     }
 
     public String validateInvoice() throws BusinessException {
         invoiceService.validateInvoice(entity, true);
-        return saveOrUpdate(false);
+        return getListViewName();
     }
     
     public String rebuildInvoice() throws BusinessException {
