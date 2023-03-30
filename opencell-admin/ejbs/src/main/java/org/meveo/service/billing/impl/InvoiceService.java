ebcb1f761fd9a3456c15df696f2fe321ea83584c
hichamhanine <hicham.hanine@opencellsoft.com>
2023-03-30 06:04:55 +0000

    #INTRD-15222 i can make two adjustment - DEV

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -6952,7 +6952,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
         duplicatedInvoice.setInvoiceDate(new Date());
         duplicatedInvoice.setInvoiceType(invoiceTypeService.getDefaultAdjustement());
         duplicatedInvoice.setStatus(InvoiceStatusEnum.DRAFT);
-        LinkedInvoice linkedInvoice = new LinkedInvoice(duplicatedInvoice, invoice, invoice.getAmountWithTax(), invoice.getTransactionalAmountWithTax(), InvoiceTypeEnum.ADJUSTMENT);
+        LinkedInvoice linkedInvoice = new LinkedInvoice(duplicatedInvoice, invoice, duplicatedInvoice.getAmountWithTax(), duplicatedInvoice.getTransactionalAmountWithTax(), InvoiceTypeEnum.ADJUSTMENT);
         duplicatedInvoice.setLinkedInvoices(of(linkedInvoice));
         duplicatedInvoice.setOpenOrderNumber(StringUtils.EMPTY);
         getEntityManager().flush();
