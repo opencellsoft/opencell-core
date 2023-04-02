5533156b1fe10624164941d00ca2fa82392b6110
Zakaria EL MELIANI <zakaria.elmeliani@opencellsoft.com>
2023-04-02 15:28:48 +0000

    INTRD-15186: Fix merge issue

--- opencell-api/src/main/java/org/meveo/apiv2/GenericOpencellRestful.java
+++ opencell-api/src/main/java/org/meveo/apiv2/GenericOpencellRestful.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
