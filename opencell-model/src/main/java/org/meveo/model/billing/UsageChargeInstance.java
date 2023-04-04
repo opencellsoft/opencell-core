0873ac0c341e6dd48bb1d4c2ccd421f67c7b0e4d
Thang Nguyen <thang.nguyen@opencellsoft.com>
2023-04-04 10:05:32 +0200

    Optimize request usage charge instance once

--- opencell-model/src/main/java/org/meveo/model/billing/UsageChargeInstance.java
+++ opencell-model/src/main/java/org/meveo/model/billing/UsageChargeInstance.java
@@ -46,13 +46,7 @@ import org.meveo.model.catalog.ChargeTemplate.ChargeMainTypeEnum;
         @NamedQuery(name = "UsageChargeInstance.getActiveUsageChargesBySubscriptionId", query = "SELECT c FROM UsageChargeInstance c where c.status='ACTIVE' and c.subscription.id=:subscriptionId order by c.priority ASC", hints = {
                 @QueryHint(name = "org.hibernate.cacheable", value = "true") }),
         @NamedQuery(name = "UsageChargeInstance.getActiveUsageCharges", query = "SELECT c FROM UsageChargeInstance c where c.status='ACTIVE'  order by c.priority ASC", hints = {
-                @QueryHint(name = "org.hibernate.cacheable", value = "true") }),
-        @NamedQuery(name = "UsageChargeInstance.getUsageChargesValidesForDateBySubscription",
-                query = "SELECT c FROM UsageChargeInstance c left join fetch c.chargeTemplate where (c.status='ACTIVE' OR ((c.status='TERMINATED' OR c.status='SUSPENDED') AND c.terminationDate>:terminationDate)) and c.subscription.id=:subscriptionId order by c.priority ASC"),
-        @NamedQuery(name = "UsageChargeInstance.getByIds",
-                query = "select c from UsageChargeInstance c " +
-                        "where id in :ids and c.chargeTemplate.id in :ctIds and c.serviceInstance.id in :siIds and c.userAccount.id in :uaIds " +
-                        "and c.userAccount.wallet.id in :waIds order by c.priority ASC")})
+                @QueryHint(name = "org.hibernate.cacheable", value = "true") })})
 public class UsageChargeInstance extends ChargeInstance {
 
     private static final long serialVersionUID = 1L;
