73223fe18eef5b25962ac9c3c6315ae7b8890ba9
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2023-04-10 14:09:26 +0000

    INTRD-14967 - Search Price Plan Matrix Line - Clean

--- opencell-api/src/main/java/org/meveo/apiv2/GenericOpencellRestful.java
+++ opencell-api/src/main/java/org/meveo/apiv2/GenericOpencellRestful.java
@@ -90,7 +90,6 @@ import org.meveo.apiv2.ordering.resource.order.OrderResourceImpl;
 import org.meveo.apiv2.ordering.resource.orderitem.OrderItemResourceImpl;
 import org.meveo.apiv2.ordering.resource.product.ProductResourceImpl;
 import org.meveo.apiv2.payments.resource.PaymentPlanResourceImpl;
-import org.meveo.apiv2.price.search.SearchPriceLineByAttributeResourceImpl;
 import org.meveo.apiv2.quote.impl.QuoteOfferResourceImpl;
 import org.meveo.apiv2.rating.impl.WalletOperationResourceImpl;
 import org.meveo.apiv2.refund.RefundResourceImpl;
@@ -153,7 +152,7 @@ public class GenericOpencellRestful extends Application {
                 OpenOrderSettingResourceImpl.class, GlobalSettingsResourceImpl.class, Apiv2SwaggerGeneration.class,
                 OpenOrderTemplateResourceImpl.class, AccountingResourceImpl.class, PaymentPlanResourceImpl.class, MediationSettingResourceImpl.class,
                 OpenOrderQuoteResourceImpl.class, CpqQuoteResourceImpl.class, CommercialOrderResourceImpl.class,
-                SearchPriceLineByAttributeResourceImpl.class, InvoiceLinesResourceImpl.class, CpqContractResourceImpl.class, OpenOrderResourceImpl.class,
+                InvoiceLinesResourceImpl.class, CpqContractResourceImpl.class, OpenOrderResourceImpl.class,
                 ContactCategoryResourceImpl.class, WalletOperationResourceImpl.class, InvoiceValidationRulesResourceImpl.class, InternationalSettingsResourceImpl.class, 
                 CustomTableResourceImpl.class).collect(Collectors.toSet());
         if (GENERIC_API_REQUEST_LOGGING_CONFIG.equalsIgnoreCase("true")) {
