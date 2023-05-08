d3ae58d37d21caaa2943d5043d1fc752f9647ed1
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2023-05-08 13:50:17 +0000

    Revert "re INTRD-11719 (pull request #7680)"

--- opencell-api/src/main/java/org/meveo/apiv2/GenericOpencellRestful.java
+++ opencell-api/src/main/java/org/meveo/apiv2/GenericOpencellRestful.java
@@ -90,7 +90,6 @@ import org.meveo.apiv2.ordering.resource.order.OrderResourceImpl;
 import org.meveo.apiv2.ordering.resource.orderitem.OrderItemResourceImpl;
 import org.meveo.apiv2.ordering.resource.product.ProductResourceImpl;
 import org.meveo.apiv2.payments.resource.PaymentPlanResourceImpl;
-import org.meveo.apiv2.payments.resource.PaymentResourceImpl;
 import org.meveo.apiv2.price.search.SearchPriceLineByAttributeResourceImpl;
 import org.meveo.apiv2.quote.impl.QuoteOfferResourceImpl;
 import org.meveo.apiv2.rating.impl.WalletOperationResourceImpl;
@@ -159,7 +158,7 @@ public class GenericOpencellRestful extends Application {
                 OpenOrderQuoteResourceImpl.class, CpqQuoteResourceImpl.class, CommercialOrderResourceImpl.class,
                 SearchPriceLineByAttributeResourceImpl.class, InvoiceLinesResourceImpl.class, CpqContractResourceImpl.class, OpenOrderResourceImpl.class,
                 ContactCategoryResourceImpl.class, WalletOperationResourceImpl.class, InvoiceValidationRulesResourceImpl.class, InternationalSettingsResourceImpl.class, 
-                CustomTableResourceImpl.class,PaymentResourceImpl.class).collect(Collectors.toSet());
+                CustomTableResourceImpl.class).collect(Collectors.toSet());
         if (GENERIC_API_REQUEST_LOGGING_CONFIG.equalsIgnoreCase("true")) {
             resources.add(GenericApiLoggingFilter.class);
             log.info(
