d8df332068a5fcaffa3616352830434764cf097b
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2023-05-08 21:28:09 +0000

    Revert "Re #INTRD-15694 Added a opencell-authorization-cache under opencell cache container to store authorization check results. (pull request #7662)"

--- opencell-admin/ejbs/src/main/java/org/meveo/cache/TenantCacheContainerProvider.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/cache/TenantCacheContainerProvider.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.ejb.Asynchronous;
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import org.apache.commons.beanutils.BeanUtils;
@@ -48,6 +49,7 @@ import org.slf4j.Logger;
  * @author Abdellatif BARI
  * @lastModifiedVersion 7.0
  */
+@Stateless
 public class TenantCacheContainerProvider implements Serializable { // CacheContainerProvider, Serializable {
 
     private static final long serialVersionUID = 180156064688145292L;
