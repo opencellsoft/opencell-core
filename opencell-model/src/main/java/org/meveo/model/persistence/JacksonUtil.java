b08fefe75d57139c2e183f178d097ef0998351f1
Andrius Karpavicius <andrius.karpavicius@opencellsoft.com>
2023-05-12 02:48:14 -0300

    Re #INTRD-16206 In JacksonUtils replace static ObjectMapper with a ThreadLocal

--- opencell-model/src/main/java/org/meveo/model/persistence/JacksonUtil.java
+++ opencell-model/src/main/java/org/meveo/model/persistence/JacksonUtil.java
@@ -32,27 +32,28 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class JacksonUtil {
 
-    static {
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(om.getVisibilityChecker().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
-        om.setVisibility(om.getVisibilityChecker().withGetterVisibility(JsonAutoDetect.Visibility.NONE));
-        om.setVisibility(om.getVisibilityChecker().withIsGetterVisibility(Visibility.NONE));
-        om.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
-        om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    private static final ThreadLocal<ObjectMapper> OBJECT_MAPPER = new ThreadLocal<ObjectMapper>() {
+        @Override
+        protected ObjectMapper initialValue() {
+            ObjectMapper om = new ObjectMapper();
+            om.setVisibility(om.getVisibilityChecker().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+            om.setVisibility(om.getVisibilityChecker().withGetterVisibility(JsonAutoDetect.Visibility.NONE));
+            om.setVisibility(om.getVisibilityChecker().withIsGetterVisibility(Visibility.NONE));
+            om.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+            om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
-        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        om.setSerializationInclusion(Include.NON_NULL);
-        OBJECT_MAPPER = om;
+            om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            om.setSerializationInclusion(Include.NON_NULL);
 
-    }
-
-    public static ObjectMapper OBJECT_MAPPER;
+            return om;
+        }
+    };
 
     public static <T> T fromString(String string, Class<T> clazz) {
         try {
-        	synchronized (OBJECT_MAPPER) {
-        		return OBJECT_MAPPER.readValue(string, clazz);
-			}
+            ObjectMapper om = OBJECT_MAPPER.get();
+            return om.readValue(string, clazz);
+
         } catch (IOException e) {
             throw new IllegalArgumentException("The given string value: " + string + " cannot be transformed to Json object", e);
         }
@@ -60,9 +61,9 @@ public class JacksonUtil {
 
     public static <T> T fromString(String string, TypeReference<T> typeReference) {
         try {
-        	synchronized (OBJECT_MAPPER) {
-        		return OBJECT_MAPPER.readValue(string, typeReference);
-        	}
+            ObjectMapper om = OBJECT_MAPPER.get();
+            return om.readValue(string, typeReference);
+
         } catch (IOException e) {
             throw new IllegalArgumentException("The given string value: " + string + " cannot be transformed to Json object", e);
         }
@@ -70,9 +71,9 @@ public class JacksonUtil {
 
     public static String toString(Object value) {
         try {
-        	synchronized (OBJECT_MAPPER) {
-        		return OBJECT_MAPPER.writeValueAsString(value);
-        	}
+            ObjectMapper om = OBJECT_MAPPER.get();
+            return om.writeValueAsString(value);
+
         } catch (JsonProcessingException e) {
             throw new IllegalArgumentException("The given Json object value: " + value + " cannot be transformed to a String", e);
         }
@@ -80,18 +81,17 @@ public class JacksonUtil {
 
     public static JsonNode toJsonNode(String value) {
         try {
-        	synchronized (OBJECT_MAPPER) {
-        		return OBJECT_MAPPER.readTree(value);
-        	}
+            ObjectMapper om = OBJECT_MAPPER.get();
+            return om.readTree(value);
+
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
         }
     }
-    
+
     public static JsonNode toJsonNode(Object value) {
-    	synchronized (OBJECT_MAPPER) {
-    		return OBJECT_MAPPER.valueToTree(value);
-    	}
+        ObjectMapper om = OBJECT_MAPPER.get();
+        return om.valueToTree(value);
     }
 
     @SuppressWarnings("unchecked")
