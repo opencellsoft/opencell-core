8179df3a577ae4f6904440fbac02ba91c88e3c95
Anas ROUAGUEBE <a.rouaguebe_ext@pixid.com>
2023-05-14 14:21:51 +0100

    INTRD-16097 - PriceList - New endpoint - update status

--- opencell-api/src/main/java/org/meveo/apiv2/catalog/resource/pricelist/PriceListResource.java
+++ opencell-api/src/main/java/org/meveo/apiv2/catalog/resource/pricelist/PriceListResource.java
@@ -4,11 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.meveo.api.exception.BusinessApiException;
 import org.meveo.api.exception.EntityDoesNotExistsException;
 import org.meveo.model.pricelist.PriceListStatusEnum;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,7 +28,7 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public interface PriceListResource {
 
-    @POST
+    @PUT
     @Path("/{priceListCode}/status/{newStatus}")
     @Operation(summary = "Update PriceList status",
             tags = {"Catalog", "Price List"},
@@ -38,6 +39,21 @@ public interface PriceListResource {
                             responseCode = "404",
                             description = "The price list with 'priceListCode' has not been found",
                             content = @Content(schema = @Schema(implementation = EntityDoesNotExistsException.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "PriceList without lines cannot be activated",
+                            content = @Content(schema = @Schema(implementation = BusinessApiException.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Cannot activate PriceList without lines having a price or active PricePlan",
+                            content = @Content(schema = @Schema(implementation = BusinessApiException.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Cannot activate PriceList without application rules",
+                            content = @Content(schema = @Schema(implementation = BusinessApiException.class))
                     )
             })
     Response updateStatus(@PathParam("priceListCode") String priceListCode, @PathParam("newStatus") PriceListStatusEnum newStatus);
