878f3b2c63ecc99b908fecf4cc8fd21fbdbe90fd
abdellatif.bari <abdellatif.bari@opencellsoft.com>
2023-05-18 09:46:24 +0100

    INTRD-16291

--- opencell-admin/ejbs/src/main/java/org/meveo/commons/utils/QueryBuilder.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/commons/utils/QueryBuilder.java
@@ -1612,7 +1612,7 @@ public class QueryBuilder {
         String query = toStringQuery();
 
         for (Map.Entry<String, Object> e : params.entrySet()) {
-            query = query.replaceAll(":" + e.getKey(), paramToString(e.getValue()));
+            query = query.replaceAll(":" + e.getKey() + "\\b", paramToString(e.getValue()));
         }
         return query;
     }
