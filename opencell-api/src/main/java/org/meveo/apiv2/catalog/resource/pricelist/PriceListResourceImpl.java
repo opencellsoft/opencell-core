a38ca190a0cfbb198b2be8f4dcec8519dc8bb597
Zakaria EL MELIANI <zakaria.elmeliani@opencellsoft.com>
2023-05-19 09:42:38 +0100

    INTRD-16037: Implement CRUD APIs for PriceList

--- opencell-api/src/main/java/org/meveo/apiv2/catalog/resource/pricelist/PriceListResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/catalog/resource/pricelist/PriceListResourceImpl.java
@@ -1,16 +1,49 @@
 package org.meveo.apiv2.catalog.resource.pricelist;
 
-import org.meveo.apiv2.catalog.service.PriceListApiService;
-import org.meveo.model.pricelist.PriceListStatusEnum;
+import static javax.ws.rs.core.Response.ok;
+
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
-import java.util.Map;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.meveo.apiv2.catalog.service.PriceListApiService;
+import org.meveo.apiv2.dunning.resource.DunningLevelResource;
+import org.meveo.apiv2.generic.common.LinkGenerator;
+import org.meveo.model.pricelist.PriceList;
+import org.meveo.model.pricelist.PriceListStatusEnum;
 
 public class PriceListResourceImpl implements PriceListResource {
 
     @Inject
     private PriceListApiService priceListApiService;
+    private PriceListMapper mapper = new PriceListMapper();
+    
+    @Override
+	public Response create(org.meveo.apiv2.catalog.PriceList priceList) {
+    	PriceList entity = mapper.toEntity(priceList);
+    	PriceList savedPriceList = priceListApiService.create(entity, priceList.getPaymentMethods());
+		return ok(LinkGenerator.getUriBuilderFromResource(PriceListResource.class, savedPriceList.getId()).build())
+				.entity(mapper.toResource(savedPriceList))
+				.build();
+	}
+    
+    @Override
+	public Response update(org.meveo.apiv2.catalog.PriceList priceList, Long priceListId) {
+    	PriceList entity = mapper.toEntity(priceList);
+    	PriceList updated = priceListApiService.update(entity, priceListId, priceList.getPaymentMethods()).get();
+		return ok(LinkGenerator.getUriBuilderFromResource(DunningLevelResource.class, updated.getId()).build())
+				.entity(mapper.toResource(updated))
+				.build();
+	}
+    
+    @Override
+	public Response delete(Long priceListId) {
+		priceListApiService.delete(priceListId);
+		ResponseBuilder response = ok();
+		return response.build();
+	}
 
     @Override
     public Response updateStatus(String priceListCode, PriceListStatusEnum newStatus) {
