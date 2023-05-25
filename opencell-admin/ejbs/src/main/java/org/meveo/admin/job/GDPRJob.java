6fd950ad8b3505029e57ea1f50f3a134262eb68e
anasseh <abdelhadi.nasseh@opencellsoft.com>
2023-05-25 16:23:10 +0100

    re first part INTRD-13210

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/GDPRJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/GDPRJob.java
@@ -18,10 +18,15 @@
 
 package org.meveo.admin.job;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.model.crm.CustomFieldTemplate;
+import org.meveo.model.crm.custom.CustomFieldTypeEnum;
 import org.meveo.model.jobs.JobCategoryEnum;
 import org.meveo.model.jobs.JobExecutionResultImpl;
 import org.meveo.model.jobs.JobInstance;
@@ -50,5 +55,22 @@ public class GDPRJob extends Job {
 	public JobCategoryEnum getJobCategory() {
 		return MeveoJobCategoryEnum.DWH;
 	}
+    
+	@Override
+    public Map<String, CustomFieldTemplate> getCustomFields() {
+        Map<String, CustomFieldTemplate> result = new HashMap<>();
 
+        CustomFieldTemplate customQueryListInvoice = new CustomFieldTemplate();
+        customQueryListInvoice.setCode("GDPRcustomQueryListInvoice");
+        customQueryListInvoice.setAppliesTo("JobInstance_GDPRJob");
+        customQueryListInvoice.setActive(true);
+        customQueryListInvoice.setDescription(resourceMessages.getString("GDPRJob.queryListInvoiceIDs"));
+        customQueryListInvoice.setFieldType(CustomFieldTypeEnum.TEXT_AREA);
+        customQueryListInvoice.setValueRequired(false);
+        customQueryListInvoice.setDefaultValue("");
+        customQueryListInvoice.setGuiPosition("tab:Custom fields:0;fieldGroup:Configuration:0;field:0");
+        result.put("GDPRcustomQueryListInvoice", customQueryListInvoice);
+
+        return result;
+    }
 }
