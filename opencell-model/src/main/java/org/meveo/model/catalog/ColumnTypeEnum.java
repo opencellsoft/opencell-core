8bbb2b15b64abcf15b6a9bff228c77deada0bf6e
abdellatif.bari <abdellatif.bari@opencellsoft.com>
2023-05-30 09:44:41 +0100

    INTRD-16518

--- opencell-model/src/main/java/org/meveo/model/catalog/ColumnTypeEnum.java
+++ opencell-model/src/main/java/org/meveo/model/catalog/ColumnTypeEnum.java
@@ -14,7 +14,7 @@ public enum ColumnTypeEnum {
         @Override
         public boolean valueMatch(PricePlanMatrixValue pricePlanMatrixValue, AttributeValue attributeValue) {
         	String multiValuesAttributeSeparator = ParamBean.getInstance().getProperty("attribute.multivalues.separator", ";");
-            if (matchWithAllValues(pricePlanMatrixValue)) {
+            if (StringUtils.isEmpty(pricePlanMatrixValue.getStringValue())) {
                 return true;
             } else if (attributeValue.getStringValue() == null && attributeValue.getAttribute().getAttributeType() != AttributeTypeEnum.EXPRESSION_LANGUAGE) {
                 return false;
@@ -58,7 +58,7 @@ public enum ColumnTypeEnum {
         @Override
         public boolean valueMatch(PricePlanMatrixValue pricePlanMatrixValue, AttributeValue attributeValue) {
         	String multiValuesAttributeSeparator = ParamBean.getInstance().getProperty("attribute.multivalues.separator", ";");
-            if (matchWithAllValues(pricePlanMatrixValue) || attributeValue.getDoubleValue() == null) {
+            if (pricePlanMatrixValue.getLongValue() == null || attributeValue.getDoubleValue() == null) {
                 return true;
             }
             switch (attributeValue.getAttribute().getAttributeType()) {
@@ -89,7 +89,7 @@ public enum ColumnTypeEnum {
         @Override
         public boolean valueMatch(PricePlanMatrixValue pricePlanMatrixValue, AttributeValue attributeValue) {
         	String multiValuesAttributeSeparator = ParamBean.getInstance().getProperty("attribute.multivalues.separator", ";");
-            if (matchWithAllValues(pricePlanMatrixValue) || attributeValue.getDoubleValue() == null) {
+            if (pricePlanMatrixValue.getDoubleValue() == null || attributeValue.getDoubleValue() == null) {
                 return true;
             }
             Object passedAttributeValue =  attributeValue.getAttribute().getAttributeType().getValue(attributeValue);
@@ -132,7 +132,7 @@ public enum ColumnTypeEnum {
     Range_Date {
         @Override
         public boolean valueMatch(PricePlanMatrixValue pricePlanMatrixValue, AttributeValue attributeValue) {
-            if (matchWithAllValues(pricePlanMatrixValue) || attributeValue.getDateValue() == null) {
+            if ((pricePlanMatrixValue.getFromDateValue() == null && pricePlanMatrixValue.getToDateValue() == null) || attributeValue.getDateValue() == null) {
                 return true;
             } else if (pricePlanMatrixValue.getFromDateValue() != null && pricePlanMatrixValue.getToDateValue() == null) {
                 return attributeValue.getDateValue().equals(pricePlanMatrixValue.getFromDateValue()) || attributeValue.getDateValue().after(pricePlanMatrixValue.getFromDateValue());
@@ -200,7 +200,7 @@ public enum ColumnTypeEnum {
     Boolean {
         @Override
         public boolean valueMatch(PricePlanMatrixValue pricePlanMatrixValue, AttributeValue attributeValue) {
-            if (matchWithAllValues(pricePlanMatrixValue) || attributeValue.getBooleanValue() == null) {
+            if (pricePlanMatrixValue.getBooleanValue() == null || attributeValue.getBooleanValue() == null) {
                 return false;
             }
             return attributeValue.getStringValue().equalsIgnoreCase(pricePlanMatrixValue.getStringValue());
