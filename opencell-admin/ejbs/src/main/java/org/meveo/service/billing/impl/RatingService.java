e1d3cf7f9a4117cb2d6f19594dd5c5188928befc
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2023-06-06 23:37:26 +0100

    INTRD-16739 Since we have a WO parameter, we should use it to recover Article by WO parametres and/or expression language

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatingService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatingService.java
@@ -1608,7 +1608,7 @@ public abstract class RatingService extends PersistenceService<WalletOperation> 
         if (walletOperation.getSubscription() != null) {
             discountPlanInstances.addAll(walletOperation.getSubscription().getUserAccount().getBillingAccount().getAllDiscountPlanInstances());
         }
-        var accountingArticle = walletOperation.getAccountingArticle()!=null?walletOperation.getAccountingArticle():accountingArticleService.getAccountingArticleByChargeInstance(chargeInstance);
+        var accountingArticle = walletOperation.getAccountingArticle()!=null?walletOperation.getAccountingArticle():accountingArticleService.getAccountingArticleByChargeInstance(chargeInstance, walletOperation);
         List<DiscountPlanItem>  applicableDiscountPlanItems = new ArrayList<>();
         List<DiscountPlanItem>  fixedDiscountPlanItems = new ArrayList<>();
         if(!discountPlanInstances.isEmpty()) {
