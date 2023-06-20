9a28c36311566091ecacd279b184e233c6ff76bc
abdellatif.bari <abdellatif.bari@opencellsoft.com>
2023-06-20 09:27:43 +0100

    INTRD-16436

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
@@ -751,7 +751,7 @@ public class InvoiceLineService extends PersistenceService<InvoiceLine> {
             if (resource.getUnitPriceCurrency().equals(tradingCurrency)) {
 				BigDecimal appliedRate = invoiceLine.getInvoice() != null ? invoiceLine.getInvoice().getAppliedRate() : ONE;
 				invoiceLine.setUnitPrice(resource.getUnitPrice().divide(appliedRate, BaseEntity.NB_DECIMALS, RoundingMode.HALF_UP));
-				invoiceLine.setTransactionalUnitPrice(resource.getUnitPrice());
+				//invoiceLine.setTransactionalUnitPrice(resource.getUnitPrice());
 				invoiceLine.setConversionFromBillingCurrency(true);
 			}
         }
