f3ad37188a3519705b8725cc56fda2a98cee6b3d
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2023-06-27 17:50:32 +0100

    INTRD-16596 adding callback part 1

--- opencell-api/src/main/java/org/meveo/apiv2/esignature/impl/SignatureRequestResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/esignature/impl/SignatureRequestResourceImpl.java
@@ -1,6 +1,13 @@
 package org.meveo.apiv2.esignature.impl;
 
+import org.apache.commons.lang3.StringUtils;
+import org.meveo.api.dto.ActionStatus;
+import org.meveo.api.dto.ActionStatusEnum;
+import org.meveo.api.dto.document.sign.YousignEventEnum;
+import org.meveo.api.rest.impl.BaseRs;
 import org.meveo.apiv2.esignature.SigantureRequest;
+import org.meveo.apiv2.esignature.SignatureRequestWebHookPayload;
+import org.meveo.apiv2.esignature.SignatureRequestWebhook;
 import org.meveo.apiv2.esignature.resource.SignatureRequestResource;
 import org.meveo.apiv2.esignature.service.SignatureRequestApiService;
 import org.meveo.model.esignature.Operator;
@@ -8,7 +15,7 @@ import org.meveo.model.esignature.Operator;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 
-public class SignatureRequestResourceImpl implements SignatureRequestResource {
+public class SignatureRequestResourceImpl extends BaseRs implements SignatureRequestResource {
 	
 	@Inject
 	private SignatureRequestApiService signatureRequestApiService;
@@ -25,5 +32,30 @@ public class SignatureRequestResourceImpl implements SignatureRequestResource {
 	@Override
 	public Response download(Operator operator, String signatureRequestId) {
 		return Response.ok(signatureRequestApiService.download(operator, signatureRequestId)).build();
+	}
+	
+	@Override
+	public ActionStatus signatureRequestDone(Operator operator, SignatureRequestWebHookPayload signatureRequestWebHookPayload) {
+		log.info("callback from yousign : " + signatureRequestWebHookPayload);
+		ActionStatus result = new ActionStatus(ActionStatusEnum.SUCCESS, "");
+		try {
+			String eventName = signatureRequestWebHookPayload.getEventName();
+			if(StringUtils.isEmpty(eventName) || !"signature_request.done".equalsIgnoreCase(eventName)) {
+				return new ActionStatus(ActionStatusEnum.FAIL, " Event not supported : " + eventName);
+			}
+			if(signatureRequestWebHookPayload.getData() != null &&
+					signatureRequestWebHookPayload.getData().getSignatureRequestWebhook() != null &&
+					signatureRequestWebHookPayload.getData().getSignatureRequestWebhook().getDocuments() != null){
+				String signatureRequestId = signatureRequestWebHookPayload.getData().getSignatureRequestWebhook().getId();
+				for(String key: signatureRequestWebHookPayload.getData().getSignatureRequestWebhook().getDocuments().keySet()) {
+					Object value = signatureRequestWebHookPayload.getData().getSignatureRequestWebhook().getDocuments().get(key);
+					log.info("key : " + key  + " : value : " + value);
+					signatureRequestApiService.download(operator, signatureRequestId);
+				}
+			}
+		} catch (Exception e) {
+			processException(e, new ActionStatus(ActionStatusEnum.FAIL, e.getMessage()));
+		}
+		return result;
 	}
 }
