92d83e39ebaf72756376fc83ed643c398f3cd679
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2023-07-04 10:00:52 +0100

    fixing build

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/utils/CustomFieldTemplateUtils.java
@@ -0,0 +1,47 @@
+package org.meveo.admin.job.utils;
+
+import org.meveo.model.crm.CustomFieldTemplate;
+import org.meveo.model.crm.custom.CustomFieldStorageTypeEnum;
+import org.meveo.model.crm.custom.CustomFieldTypeEnum;
+
+/**
+ * Custom field template related utilities
+ */
+public class CustomFieldTemplateUtils {
+
+    /**
+     * Build a custom field definition
+     * 
+     * @param code Custom field code
+     * @param description Description
+     * @param fieldType Field type
+     * @param guiPosition GUI position
+     * @param defaultValue Default value
+     * @param valueRequire Is value required
+     * @param storageType Storage type
+     * @param entityClazz Entity class
+     * @param appliesTo Applies to value
+     * @return A custom field template
+     */
+    public static CustomFieldTemplate buildCF(String code, String description, CustomFieldTypeEnum fieldType, String guiPosition, String defaultValue, boolean valueRequire, CustomFieldStorageTypeEnum storageType,
+            String entityClazz, String appliesTo) {
+        CustomFieldTemplate cft = new CustomFieldTemplate();
+        cft.setCode(code);
+        cft.setAppliesTo(appliesTo);
+        cft.setActive(true);
+        cft.setDescription(description);
+        cft.setFieldType(fieldType);
+        cft.setValueRequired(valueRequire);
+        cft.setGuiPosition(guiPosition);
+        if (defaultValue != null) {
+            cft.setDefaultValue(defaultValue);
+        }
+        if (storageType != null) {
+            cft.setStorageType(storageType);
+        }
+        if (entityClazz != null) {
+            cft.setEntityClazz(entityClazz);
+        }
+        return cft;
+    }
+}
