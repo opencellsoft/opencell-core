689b57f58ff894145433f717d5a1cea3376ae435
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2023-08-03 18:02:12 +0100

    INTRD-17063 [API] unable to import all objects and fields

--- opencell-scripts/src/main/java/org/meveo/service/script/CdrJsonImportScript.java
+++ opencell-scripts/src/main/java/org/meveo/service/script/CdrJsonImportScript.java
@@ -11,7 +11,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Objects;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONArray;
