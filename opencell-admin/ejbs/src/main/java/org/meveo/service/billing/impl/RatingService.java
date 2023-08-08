c9d834a598b3541c910373280f62850d82358c81
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2023-08-08 08:09:21 +0100

    INTRD-17763 - Rating / ProductService - Fix pricePlanVersion Date

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatingService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/RatingService.java
@@ -1110,7 +1110,9 @@ public abstract class RatingService extends PersistenceService<WalletOperation> 
 		    priceWithTax = wo.getUnitAmountWithTax()!=null?wo.getUnitAmountWithTax():BigDecimal.ZERO;
 		    return new Amounts(priceWithoutTax, priceWithTax);
 	    }
-        PricePlanMatrixVersion ppmVersion = pricePlanSelectionService.getPublishedVersionValidForDate(pricePlan.getId(), serviceInstance, wo.getOperationDate());
+
+        Date ppmvDate = Optional.ofNullable(serviceInstance.getPriceVersionDate()).orElse(wo.getOperationDate());
+        PricePlanMatrixVersion ppmVersion = pricePlanSelectionService.getPublishedVersionValidForDate(pricePlan.getId(), serviceInstance, ppmvDate);
         if (ppmVersion != null) {
             wo.setPricePlanMatrixVersion(ppmVersion);
             if (!ppmVersion.isMatrix()) {
