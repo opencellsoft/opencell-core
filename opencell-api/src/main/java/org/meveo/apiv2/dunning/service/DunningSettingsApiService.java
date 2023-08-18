1974558ff53490db6370d69881ab6ca6b226c058
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2023-08-18 08:27:57 +0100

    INTRD-17912 - DunningSetings - Don't block CUSTOMER_LEVEL anymore

--- opencell-api/src/main/java/org/meveo/apiv2/dunning/service/DunningSettingsApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/dunning/service/DunningSettingsApiService.java
@@ -50,7 +50,6 @@ public class DunningSettingsApiService implements ApiService<DunningSettings> {
 	private static final String CUSTOMER_BALANCE_IS_MANDATORY = "Customer balance is mandatory to create a dunning settings with mode INVOICE_LEVEL";
 	private static final String MANY_DEFAULT_CUSTOMER_BALANCE_FOUND = "Many Customer Balance are configured as default";
 	private static final String ACTIVE_OR_PAUSED_DUNNING_COLLECTION_PLAN_FOUND = "One or many Active/Paused Dunning Collection Plan was found";
-	private static final String AUTHORIZED_DUNNING_MODE = "Only dunning mode INVOICE_LEVEL is authorized for the first version of dunning";
 
 	@Override
 	public List<DunningSettings> list(Long offset, Long limit, String sort, String orderBy, String filter) {
@@ -79,8 +78,6 @@ public class DunningSettingsApiService implements ApiService<DunningSettings> {
 		//If the dunning mode is set to CUSTOMER_LEVEL for the first setting then return a functional exception
 		if(baseEntity.getDunningMode() == null) {
 			baseEntity.setDunningMode(DunningModeEnum.INVOICE_LEVEL);
-		} else if(baseEntity.getDunningMode().equals(DunningModeEnum.CUSTOMER_LEVEL) && dunningSettingsService.count() == 0) {
-			throw new BadRequestException(AUTHORIZED_DUNNING_MODE);
 		}
 
 		if(baseEntity.getMaxDunningLevels() == null)
@@ -103,11 +100,6 @@ public class DunningSettingsApiService implements ApiService<DunningSettings> {
 	public Optional<DunningSettings> update(Long id, DunningSettings dunningSettings) {
 		globalSettingsVerifier.checkActivateDunning();
 		var dunningSettingsUpdate = findById(id).orElseThrow(() -> new BadRequestException(NO_DUNNING_FOUND + id));
-
-		//If the dunning mode is set to CUSTOMER_LEVEL for the first setting then return a functional exception
-		if(dunningSettings.getDunningMode().equals(DunningModeEnum.CUSTOMER_LEVEL) && dunningSettingsService.count() == 1) {
-			throw new BadRequestException(AUTHORIZED_DUNNING_MODE);
-		}
 
 		if(dunningSettings.getAccountingArticle() != null) {
 			var accountingArticle = accountingArticleService.findById(dunningSettings.getAccountingArticle().getId());
