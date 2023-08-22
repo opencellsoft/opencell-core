7bc7b0e2065e0b6289c99e5381646ddaa5bf2071
aeljaouhari <adil.eljaouhari@opencellsoft.com>
2023-08-22 20:14:51 +0200

    Feature INTRD-17955 INTRD-17956 : API enable disable Billing Run

--- opencell-api/src/main/java/org/meveo/apiv2/billing/resource/InvoicingResource.java
+++ opencell-api/src/main/java/org/meveo/apiv2/billing/resource/InvoicingResource.java
@@ -77,4 +77,34 @@ public interface InvoicingResource {
                                 @PathParam("billingRunId") Long billingRunId,
                                @Parameter(description = "True to generate invoice immediately, false otherwise") @DefaultValue("false")
                                @QueryParam("executeInvoicingJob") boolean executeInvoicingJob);
+    
+    @POST
+    @Path("/{billingRunId}/enableBillingRun")
+    @Operation(summary = "enable the billing run",
+            tags = {"Invoicing"},
+            description = "enable the billing run",
+            responses = {
+            		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",
+                            description = "The billing enabled successfully"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404",
+                            description = "The billing run does not exists"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "409",
+                            description = "The billing run cannot be enabled")
+            })
+    Response enableBillingRun(@PathParam("billingRunId") Long billingRunId);
+    
+    @POST
+    @Path("/{billingRunId}/disableBillingRun")
+    @Operation(summary = "disable the billing run",
+            tags = {"Invoicing"},
+            description = "Disable the billing run",
+            responses = {
+            		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",
+                            description = "The billing disabled successfully"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404",
+                            description = "The billing run does not exists"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "409",
+                            description = "The billing run cannot be disabled")
+            })
+    Response disableBillingRun(@PathParam("billingRunId") Long billingRunId);
 }
