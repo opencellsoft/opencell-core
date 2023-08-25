45ab349000e2e5576251540acfa1e352501310ac
ZBariki <zakaria.bariki@opencellsoft.com>
2023-08-25 01:20:41 +0100

    INTRD-17742 : should cancell old wo

--- opencell-model/src/main/java/org/meveo/model/billing/WalletOperation.java
+++ opencell-model/src/main/java/org/meveo/model/billing/WalletOperation.java
@@ -170,7 +170,7 @@ import org.meveo.model.tax.TaxClass;
 		@NamedQuery(name = "WalletOperation.discountWalletOperation", query = "SELECT o FROM WalletOperation o WHERE discountedWalletOperation is not null and o.id IN (:woIds)"),
 		@NamedQuery(name = "WalletOperation.findByTriggerdEdr", query = "SELECT o FROM WalletOperation o left join o.edr edr where o.edr in (select e.id FROM EDR e where e.walletOperation.id =:rerateWalletOperationIds)"),
         @NamedQuery(name = "WalletOperation.cancelTriggerEdr", query = "UPDATE WalletOperation o SET o.status='TO_RERATE' where o.id in (ids)"),
-        @NamedQuery(name = "WalletOperation.findWalletOperationByChargeInstance", query = "SELECT wo.id FROM WalletOperation wo WHERE wo.chargeInstance.id = :chargeInstanceId AND wo.status IN ('OPEN', 'TREATED') AND wo.ratedTransaction IS NOT NULL AND wo.ratedTransaction.status = 'OPEN' AND wo.ratedTransaction.invoiceLine IS NOT NULL AND wo.ratedTransaction.invoiceLine.status = 'OPEN' AND wo.subscription.id = :subscriptionId")
+        @NamedQuery(name = "WalletOperation.findWalletOperationByChargeInstance", query = "SELECT wo.id FROM WalletOperation wo LEFT JOIN wo.ratedTransaction rt WHERE wo.subscription.id = :subscriptionId AND wo.chargeInstance.id = :chargeInstanceId AND wo.status IN ('OPEN', 'TREATED') AND (wo.ratedTransaction.id IN (SELECT rt.id FROM RatedTransaction rt WHERE rt.status = 'OPEN' AND rt.subscription.id = :subscriptionId AND rt.chargeInstance.id = :chargeInstanceId) OR wo.ratedTransaction.id IS NULL)"),
 })
 
 @NamedNativeQueries({
