b9f1f4c1a354a3edf659eb9858af5f249235812d
anasseh <abdelhadi.nasseh@opencellsoft.com>
2023-09-05 13:58:48 +0100

    re INTRD-18167

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceSingleton.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/ServiceSingleton.java
@@ -59,6 +59,7 @@ import org.meveo.service.securityDeposit.impl.FinanceSettingsService;
 import org.meveo.util.ApplicationProvider;
 import org.slf4j.Logger;
 
+import javax.ejb.AccessTimeout;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
@@ -322,7 +323,7 @@ public class ServiceSingleton {
         String occTemplateCode = null;
         try {
             occTemplateCode = (String) customFieldInstanceService.getOrCreateCFValueFromParamValue(occCode, occCodeDefaultValue, appProvider, true);
-            log.debug("occTemplateCode:" + occTemplateCode);
+            log.debug("occTemplateCode:{}" , occTemplateCode);
             occTemplate = oCCTemplateService.findByCode(occTemplateCode);
         } catch (Exception e) {
             log.error("error while getting occ template ", e);
@@ -415,6 +416,7 @@ public class ServiceSingleton {
      * @throws BusinessException business exception
      */
     @Lock(LockType.WRITE)
+    @AccessTimeout(value=30000)
     @JpaAmpNewTx
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public Invoice assignInvoiceNumberVirtual(Invoice invoice) throws BusinessException {
@@ -428,6 +430,7 @@ public class ServiceSingleton {
      * @throws BusinessException business exception
      */
     @Lock(LockType.WRITE)
+    @AccessTimeout(value=30000)    
     @JpaAmpNewTx
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public Invoice assignInvoiceNumber(Invoice invoice) throws BusinessException {
