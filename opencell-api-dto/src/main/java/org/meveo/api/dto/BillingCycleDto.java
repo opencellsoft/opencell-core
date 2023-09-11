fd4e3104bcd89af6a78e83627a221b4d2b563573
abdelhamid.elmalki <abdelhamid.elmalki@opencellsoft.com>
2023-09-11 12:53:15 +0100

    INTRD-18256 add applicationEl field

--- opencell-api-dto/src/main/java/org/meveo/api/dto/BillingCycleDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/BillingCycleDto.java
@@ -213,6 +213,9 @@ public class BillingCycleDto extends BusinessEntityDto {
     @Schema(description = "Use incremental mode in invoice lines or not", nullable = true)
     private Boolean incrementalInvoiceLines;
 
+    @Size(max = 2000)
+    private String applicationEl;
+
     public String getLastTransactionDateDelayEL() {
 		return lastTransactionDateDelayEL;
 	}
@@ -780,5 +783,13 @@ public class BillingCycleDto extends BusinessEntityDto {
      */
     public Boolean getIncrementalInvoiceLines() {
         return incrementalInvoiceLines;
+    }
+
+    public String getApplicationEl() {
+        return applicationEl;
+    }
+
+    public void setApplicationEl(String applicationEl) {
+        this.applicationEl = applicationEl;
     }
 }
