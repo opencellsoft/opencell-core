c4a725dc5bb92edd7536a0166c6917183a049054
ZBariki <zakaria.bariki@opencellsoft.com>
2023-09-28 16:54:05 +0100

    INTRD-17698 ;  apply JOB changes

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunReportService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/BillingRunReportService.java
@@ -2,20 +2,31 @@ package org.meveo.service.billing.impl;
 
 import static java.math.BigDecimal.ZERO;
 import static java.math.BigDecimal.valueOf;
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static org.meveo.model.billing.BillingRunReportTypeEnum.OPEN_RATED_TRANSACTIONS;
 import static org.meveo.model.jobs.JobLauncherEnum.API;
 
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.model.article.AccountingArticle;
+import org.meveo.model.billing.AccountingArticleAmount;
 import org.meveo.model.billing.BillingAccountAmount;
+import org.meveo.model.billing.BillingCycle;
 import org.meveo.model.billing.BillingRun;
 import org.meveo.model.billing.BillingRunReport;
 import org.meveo.model.billing.BillingRunReportTypeEnum;
 import org.meveo.model.billing.OfferAmount;
+import org.meveo.model.billing.ProductAmount;
 import org.meveo.model.billing.RatedTransaction;
+import org.meveo.model.billing.ServiceInstance;
+import org.meveo.model.billing.SubscriptionAmount;
+import org.meveo.model.cpq.Product;
 import org.meveo.model.jobs.JobInstance;
 import org.meveo.service.base.PersistenceService;
+import org.meveo.service.billing.impl.article.AccountingArticleService;
 import org.meveo.service.catalog.impl.OfferTemplateService;
+import org.meveo.service.cpq.ProductService;
 import org.meveo.service.job.JobExecutionService;
 import org.meveo.service.job.JobInstanceService;
 
@@ -27,6 +38,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Stateless
 public class BillingRunReportService extends PersistenceService<BillingRunReport> {
@@ -55,7 +67,31 @@ public class BillingRunReportService extends PersistenceService<BillingRunReport
     @Inject
     private JobExecutionService jobExecutionService;
 
+    @Inject
+    private SubscriptionAmountService subscriptionAmountService;
+
+    @Inject
+    private SubscriptionService subscriptionService;
+
+    @Inject
+    private ProductAmountService productAmountService;
+
+    @Inject
+    private ProductService productService;
+
+    @Inject
+    private ServiceInstanceService serviceInstanceService;
+
+    @Inject
+    private AccountingArticleAmountService accountingArticleAmountService;
+
+    @Inject
+    private AccountingArticleService accountingArticleService;
+
     private static final String BILLING_RUN_REPORT_JOB_CODE = "BILLING_RUN_REPORT_JOB";
+
+    private static final int DEFAULT_BLOCK_SIZE = 10;
+
 
     /**
      * Create billing run reports from billing run
@@ -75,6 +111,9 @@ public class BillingRunReportService extends PersistenceService<BillingRunReport
             billingRunReport = prepareBillingRunReport(reportDetails, reportType);
             createBillingAccountAmounts(billingRun, rtIds, billingRunReport, filters);
             createOfferAmounts(billingRun, rtIds, billingRunReport, filters);
+            createSubscriptionAmounts(billingRun, rtIds, billingRunReport, filters);
+            createProductAmounts(billingRun, rtIds, billingRunReport, filters);
+            createArticleAmounts(billingRun, rtIds, billingRunReport, filters);
         }
         billingRunReport.setBillingRun(billingRun);
         create(billingRunReport);
@@ -122,36 +161,133 @@ public class BillingRunReportService extends PersistenceService<BillingRunReport
 
     private List<BillingAccountAmount> createBillingAccountAmounts(BillingRun billingRun, List<Long> rtIds,
                                                                    BillingRunReport billingRunReport, Map<String, Object> filters) {
-        List<BillingAccountAmount> billingAccountAmounts = new ArrayList<>();
-        List<Object[]> amountsPerBA = ratedTransactionService.getBillingAccountStatisticsDetails(billingRun, rtIds, filters);
-        for (Object[] amount : amountsPerBA) {
-            if(amount[0] != null) {
-                BillingAccountAmount billingAccountAmount = new BillingAccountAmount();
-                billingAccountAmount.setBillingAccount(billingAccountService.findById((Long) amount[0]));
-                billingAccountAmount.setAmount((BigDecimal) amount[1]);
-                billingAccountAmountService.create(billingAccountAmount);
-                billingAccountAmount.setBillingRunReport(billingRunReport);
-                billingAccountAmounts.add(billingAccountAmount);
+        if(billingRun.getBillingCycle() == null
+                || (billingRun.getBillingCycle() != null && billingRun.getBillingCycle().getReportConfigDisplayBillingAccounts())) {
+            List<BillingAccountAmount> billingAccountAmounts = new ArrayList<>();
+            final int blockSize = ofNullable(billingRun.getBillingCycle())
+                    .map(BillingCycle::getReportConfigBlockSizeBillingAccounts)
+                    .orElse(DEFAULT_BLOCK_SIZE);
+            List<Object[]> amountsPerBA =
+                    ratedTransactionService.getBillingAccountStatisticsDetails(billingRun, rtIds, filters, blockSize);
+            for (Object[] amount : amountsPerBA) {
+                if (amount[0] != null) {
+                    BillingAccountAmount billingAccountAmount = new BillingAccountAmount();
+                    billingAccountAmount.setBillingAccount(billingAccountService.findById((Long) amount[0]));
+                    billingAccountAmount.setAmount((BigDecimal) amount[1]);
+                    billingAccountAmountService.create(billingAccountAmount);
+                    billingAccountAmount.setBillingRunReport(billingRunReport);
+                    billingAccountAmounts.add(billingAccountAmount);
+                }
             }
+            return billingAccountAmounts;
         }
-        return billingAccountAmounts;
+        return emptyList();
     }
 
     private List<OfferAmount> createOfferAmounts(BillingRun billingRun, List<Long> rtIds,
                                                  BillingRunReport billingRunReport, Map<String, Object> filters) {
-        List<OfferAmount> offerAmounts = new ArrayList<>();
-        List<Object[]> amountsPerOffer = ratedTransactionService.getOfferStatisticsDetails(billingRun, rtIds, filters);
-        for (Object[] amount : amountsPerOffer) {
-            if(amount[0] != null) {
-                OfferAmount offerAmount = new OfferAmount();
-                offerAmount.setOffer(offerTemplateService.findById((Long) amount[0]));
-                offerAmount.setAmount((BigDecimal) amount[1]);
-                offerAmount.setBillingRunReport(billingRunReport);
-                offerAmountService.create(offerAmount);
-                offerAmounts.add(offerAmount);
+        if(billingRun.getBillingCycle() == null
+                || (billingRun.getBillingCycle() != null && billingRun.getBillingCycle().getReportConfigDisplayOffers())) {
+            List<OfferAmount> offerAmounts = new ArrayList<>();
+            final int OfferBlockSize = ofNullable(billingRun.getBillingCycle())
+                    .map(BillingCycle::getReportConfigBlockSizeBillingAccounts)
+                    .orElse(DEFAULT_BLOCK_SIZE);
+            List<Object[]> amountsPerOffer =
+                    ratedTransactionService.getOfferStatisticsDetails(billingRun, rtIds, filters, OfferBlockSize);
+            for (Object[] amount : amountsPerOffer) {
+                if (amount[0] != null) {
+                    OfferAmount offerAmount = new OfferAmount();
+                    offerAmount.setOffer(offerTemplateService.findById((Long) amount[0]));
+                    offerAmount.setAmount((BigDecimal) amount[1]);
+                    offerAmount.setBillingRunReport(billingRunReport);
+                    offerAmountService.create(offerAmount);
+                    offerAmounts.add(offerAmount);
+                }
             }
+            return offerAmounts;
         }
-        return offerAmounts;
+        return emptyList();
+    }
+
+
+    private List<SubscriptionAmount> createSubscriptionAmounts(BillingRun billingRun, List<Long> rtIds,
+                                                               BillingRunReport billingRunReport, Map<String, Object> filters) {
+        if(billingRun.getBillingCycle() == null
+                || (billingRun.getBillingCycle() != null && billingRun.getBillingCycle().getReportConfigDisplaySubscriptions())) {
+            List<SubscriptionAmount> subscriptionAmounts = new ArrayList<>();
+            final int subscriptionBlockSize = ofNullable(billingRun.getBillingCycle())
+                    .map(BillingCycle::getReportConfigBlockSizeSubscriptions)
+                    .orElse(DEFAULT_BLOCK_SIZE);
+            List<Object[]> amountsPerSubscription =
+                    ratedTransactionService.getSubscriptionStatisticsDetails(billingRun, rtIds, filters, subscriptionBlockSize);
+            for (Object[] amount : amountsPerSubscription) {
+                if (amount[0] != null) {
+                    SubscriptionAmount subscriptionAmount = new SubscriptionAmount();
+                    subscriptionAmount.setSubscription(subscriptionService.findById((Long) amount[0]));
+                    subscriptionAmount.setAmount((BigDecimal) amount[1]);
+                    subscriptionAmount.setBillingRunReport(billingRunReport);
+                    subscriptionAmountService.create(subscriptionAmount);
+                    subscriptionAmounts.add(subscriptionAmount);
+                }
+            }
+            return subscriptionAmounts;
+        }
+        return emptyList();
+    }
+
+    private List<ProductAmount> createProductAmounts(BillingRun billingRun, List<Long> rtIds,
+                                                               BillingRunReport billingRunReport, Map<String, Object> filters) {
+        if(billingRun.getBillingCycle() == null
+                || (billingRun.getBillingCycle() != null && billingRun.getBillingCycle().getReportConfigDisplayProducts())) {
+            List<ProductAmount> productAmounts = new ArrayList<>();
+            final int productsBlockSize = ofNullable(billingRun.getBillingCycle())
+                    .map(BillingCycle::getReportConfigBlockSizeProducts)
+                    .orElse(DEFAULT_BLOCK_SIZE);
+            List<Object[]> amountsPerProduct =
+                    ratedTransactionService.getProductStatisticsDetails(billingRun, rtIds, filters, productsBlockSize);
+            for (Object[] amount : amountsPerProduct) {
+                if (amount[0] != null) {
+                    ProductAmount productAmount = new ProductAmount();
+                    ServiceInstance serviceInstance = serviceInstanceService.findById((Long) amount[0]);
+                    Product product = serviceInstance != null && serviceInstance.getProductVersion() != null
+                            ? serviceInstance.getProductVersion().getProduct() : null;
+                    if(product != null) {
+                        productAmount.setProduct(product);
+                        productAmount.setAmount((BigDecimal) amount[1]);
+                        productAmount.setBillingRunReport(billingRunReport);
+                        productAmountService.create(productAmount);
+                        productAmounts.add(productAmount);
+                    }
+                }
+            }
+            return productAmounts;
+        }
+        return emptyList();
+    }
+
+    private List<AccountingArticleAmount> createArticleAmounts(BillingRun billingRun, List<Long> rtIds,
+                                                     BillingRunReport billingRunReport, Map<String, Object> filters) {
+        if(billingRun.getBillingCycle() == null
+                || (billingRun.getBillingCycle() != null && billingRun.getBillingCycle().getReportConfigDisplayProducts())) {
+            List<AccountingArticleAmount> accountingArticleAmounts = new ArrayList<>();
+            final int productsBlockSize = ofNullable(billingRun.getBillingCycle())
+                    .map(BillingCycle::getReportConfigBlockSizeProducts)
+                    .orElse(DEFAULT_BLOCK_SIZE);
+            List<Object[]> amountsPerArticle =
+                    ratedTransactionService.getArticleStatisticsDetails(billingRun, rtIds, filters, productsBlockSize);
+            for (Object[] amount : amountsPerArticle) {
+                if (amount[0] != null) {
+                    AccountingArticleAmount accountingArticleAmount = new AccountingArticleAmount();
+                    accountingArticleAmount.setAccountingArticle(accountingArticleService.findById((Long) amount[0]));
+                    accountingArticleAmount.setAmount((BigDecimal) amount[1]);
+                    accountingArticleAmount.setBillingRunReport(billingRunReport);
+                    accountingArticleAmountService.create(accountingArticleAmount);
+                    accountingArticleAmounts.add(accountingArticleAmount);
+                }
+            }
+            return accountingArticleAmounts;
+        }
+        return emptyList();
     }
 
     /**
