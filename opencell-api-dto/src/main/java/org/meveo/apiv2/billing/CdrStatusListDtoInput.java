c86c1fc8b2c3e3791e55fb33dd3b78d2720ab346
aeljaouhari <adil.eljaouhari@opencellsoft.com>
2023-10-02 16:12:48 +0200

    Feature INTRD-18491 - [Back] Create a new bulk status change API

---
+++ opencell-api-dto/src/main/java/org/meveo/apiv2/billing/CdrStatusListDtoInput.java
@@ -0,0 +1,32 @@
+package org.meveo.apiv2.billing;
+
+import java.util.List;
+
+import org.immutables.value.Value;
+import org.immutables.value.Value.Default;
+import org.meveo.apiv2.models.Resource;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Value.Immutable
+@Value.Style(jdkOnly = true)
+@JsonDeserialize(as = ImmutableCdrStatusListDtoInput.class)
+public interface CdrStatusListDtoInput extends Resource {
+
+    @Default
+    @Schema(description = "How the CDR list is processed : STOP_ON_FIRST_FAIL, PROCESS_ALL, ROLLBACK_ON_ERROR")
+    default ProcessingModeEnum getMode() {
+        return ProcessingModeEnum.STOP_ON_FIRST_FAIL;
+    }
+    
+    @Default
+    default boolean getReturnCDRs() { return false;}
+    
+    @Default
+    default boolean getReturnErrors() { return true;}
+    
+    List<CdrStatusDtoInput> getCdrs();
+    
+}
