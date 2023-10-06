6a40dd9a92224a1ace7acc581e9acdff7b4c12df
rachidAityaazza <rachid.aityaazza@opencellsoft.com>
2023-10-06 20:52:03 +0100

    RE #INTRD-18874 : fix the discount IL AAR

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceLineService.java
@@ -265,7 +265,12 @@ public class InvoiceLineService extends PersistenceService<InvoiceLine> {
         BigDecimal invoiceLineDiscountAmount = BigDecimal.ZERO;
         int rounding = appProvider.getRounding();
         RoundingModeEnum roundingMode = appProvider.getRoundingMode();
+        AccountingArticle accountingArticle=null;
         for (DiscountPlanItem discountPlanItem : discountItems) {
+        	accountingArticle=discountPlanItem.getAccountingArticle();
+        	if(accountingArticle==null) {
+        		accountingArticle=invoiceLine.getAccountingArticle();
+        	}
         	InvoiceLine discountInvoice = new InvoiceLine(invoiceLine, invoice);
         	discountInvoice.setStatus(invoiceLine.getStatus());
             if(discountPlanItem.getDiscountPlanItemType() == DiscountPlanItemTypeEnum.FIXED) {
@@ -273,8 +278,8 @@ public class InvoiceLineService extends PersistenceService<InvoiceLine> {
             } else {
                 //invoiceLineDiscountAmount = invoiceLineDiscountAmount.add((discountPlanItem.getDiscountValue().divide(hundred)).multiply(entity.getAmountWithoutTax()));
                 BigDecimal taxPercent = invoiceLine.getTaxRate();
-                if(invoiceLine.getAccountingArticle() != null) {
-                	TaxInfo taxInfo = taxMappingService.determineTax(invoiceLine.getAccountingArticle().getTaxClass(), seller, billingAccount, null, invoice.getInvoiceDate(),  false, false);
+                if(accountingArticle != null) {
+                	TaxInfo taxInfo = taxMappingService.determineTax(accountingArticle.getTaxClass(), seller, billingAccount, null, invoice.getInvoiceDate(),  false, false);
                         taxPercent = taxInfo.tax.getPercent();
                 }
                 BigDecimal discountAmount = discountPlanItemService.getDiscountAmount(invoiceLine.getUnitPrice(), discountPlanItem,null,invoice,invoiceLine, Collections.emptyList());
@@ -284,14 +289,15 @@ public class InvoiceLineService extends PersistenceService<InvoiceLine> {
                 BigDecimal[] amounts = NumberUtils.computeDerivedAmounts(invoiceLineDiscountAmount, invoiceLineDiscountAmount, taxPercent, appProvider.isEntreprise(),  rounding,
                         roundingMode.getRoundingMode());
                 var quantity = invoiceLine.getQuantity();
-                discountInvoice.setUnitPrice(invoiceLineDiscountAmount);
+                discountInvoice.setUnitPrice(discountAmount);
                 discountInvoice.setAmountWithoutTax(quantity.compareTo(BigDecimal.ZERO)>0?quantity.multiply(amounts[0]):BigDecimal.ZERO);
                 discountInvoice.setAmountWithTax(quantity.multiply(amounts[1]));
                 discountInvoice.setDiscountPlan(null);
                 discountInvoice.setDiscountedInvoiceLine(invoiceLine);
                 discountInvoice.setAmountTax(quantity.multiply(amounts[2]));
                 discountInvoice.setTaxRate(taxPercent);
-                discountInvoice.setRawAmount(invoiceLineDiscountAmount);
+                discountInvoice.setRawAmount(discountAmount);
+                discountInvoice.setAccountingArticle(accountingArticle);
             	super.create(discountInvoice);
             }
         }
