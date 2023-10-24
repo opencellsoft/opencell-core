e6384ec6c7654c1f4d505f163ce9350dc20fd577
Mbarek-Ay <mbarek.aityaazza@opencellsoft.com>
2023-10-24 18:26:20 +0100

    INTRD-19165 - Regression in counter

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/CounterInstanceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/CounterInstanceService.java
@@ -195,8 +195,9 @@ public class CounterInstanceService extends PersistenceService<CounterInstance> 
             } else if (entity instanceof ServiceInstance) {
                 counterInstance.setServiceInstance((ServiceInstance) entity);
             }
-
+            if(counterTemplate.getAccumulator() != null && counterTemplate.getAccumulator()) {
             counterInstance.getChargeInstances().add(chargeInstance);
+            }
 
             if (!isVirtual) {
                 create(counterInstance);
