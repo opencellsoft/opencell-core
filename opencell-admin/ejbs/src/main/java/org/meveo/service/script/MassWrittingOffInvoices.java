e9ee20694a86b8c58c3235ce0e81187c82c8334f
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2023-11-15 14:16:13 +0100

    INTRD-19464 [Back] Update data.xml (Job configuration)

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/script/MassWrittingOffInvoices.java
@@ -0,0 +1,13 @@
+package org.meveo.service.script;
+
+import org.meveo.admin.exception.BusinessException;
+
+import java.util.Map;
+
+public class MassWrittingOffInvoices extends GenericMassImportScript {
+	
+	@Override
+	public void execute(Map<String, Object> methodContext) throws BusinessException {
+		super.execute(methodContext);
+	}
+}
