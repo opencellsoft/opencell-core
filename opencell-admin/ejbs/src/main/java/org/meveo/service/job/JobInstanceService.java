6632a4e9cd4c407c3b0bece91bb6d93c824c759c
Andrius Karpavicius <andrius.karpavicius@opencellsoft.com>
2023-11-22 14:33:18 +0200

    Re #INTRD-17222 Added a check if all nodes are finished after job execution returns COMPLETE_MORE status and new job cycle is started

--- opencell-admin/ejbs/src/main/java/org/meveo/service/job/JobInstanceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/job/JobInstanceService.java
@@ -195,7 +195,7 @@ public class JobInstanceService extends BusinessService<JobInstance> {
     @Override
     public void create(JobInstance jobInstance) throws BusinessException {
         super.create(jobInstance);
-        jobCacheContainerProvider.addUpdateJobInstance(jobInstance);
+        jobCacheContainerProvider.addUpdateJobInstance(jobInstance, true);
         scheduleJob(jobInstance, null);
 
         clusterEventPublisher.publishEvent(jobInstance, CrudActionEnum.create);
@@ -204,7 +204,7 @@ public class JobInstanceService extends BusinessService<JobInstance> {
     @Override
     public JobInstance update(JobInstance jobInstance) throws BusinessException {
         super.update(jobInstance);
-        jobCacheContainerProvider.addUpdateJobInstance(jobInstance);
+        jobCacheContainerProvider.addUpdateJobInstance(jobInstance, true);
         scheduleUnscheduleJob(jobInstance);
 
         clusterEventPublisher.publishEvent(jobInstance, CrudActionEnum.update);
