305cfe04c921f27b9502078f8d637387f3cdf859
aeljaouhari <adil.eljaouhari@opencellsoft.com>
2023-12-04 14:18:14 +0100

    Fix INTRD-19901 - framework agreement does not apply to Oneshot charge

--- opencell-model/src/main/java/org/meveo/model/cpq/contract/ContractItem.java
+++ opencell-model/src/main/java/org/meveo/model/cpq/contract/ContractItem.java
@@ -46,7 +46,7 @@ import java.util.Set;
 @NamedQueries({
 	@NamedQuery(name = "ContractItem.getApplicableContracts", query = "select c from ContractItem c left join c.targetAccountingArticles article where  c.contract.id=:contractId "
 			+ " and (c.offerTemplate is null or c.offerTemplate.id=:offerId) "
-			+ " and (c.product is null or c.product.id=:productId) "
+			+ " and (:productId is null or c.product is null or c.product.id=:productId) "
 			+ " and (c.chargeTemplate is null or c.chargeTemplate.id=:chargeTemplateId) "
 			+ " and ((c.targetAccountingArticles is empty and c.chargeTemplate is not null) or article.id =:accountingArticleId)" )})
 	
