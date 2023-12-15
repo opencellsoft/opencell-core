47f42a9dd5a703ca8b1a9e1ab9dbde54acb66a92
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2023-12-15 08:53:42 +0100

    INTRD-19649 - Partition Purge Job

---
+++ opencell-model/src/main/java/org/meveo/model/admin/partitioning/WoPartitionLog.java
@@ -0,0 +1,9 @@
+package org.meveo.model.admin.partitioning;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tech_wo_partition_log")
+public class WoPartitionLog extends AbstractPartitionLog {
+}
