d5e17ff9aed8ea808f2b334ec3b2555cb596ab0d
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2023-12-18 13:14:08 +0100

    default value for manualCreationEnabled = true

--- opencell-model/src/main/java/org/meveo/model/payments/OCCTemplate.java
+++ opencell-model/src/main/java/org/meveo/model/payments/OCCTemplate.java
@@ -99,7 +99,7 @@ public class OCCTemplate extends BusinessEntity {
 
     @Type(type = "numeric_boolean")
     @Column(name = "manual_creation_enabled")
-    private boolean manualCreationEnabled;
+    private boolean manualCreationEnabled = true;
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "ar_customer_balance_templates",
