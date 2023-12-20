5cb8b3b57cf97ff294debf30f5036370341793e6
aeljaouhari <adil.eljaouhari@opencellsoft.com>
2023-12-20 17:53:16 +0100

    Feature INTRD-20217 INTRD-20218 mass Disable/enable offer template

--- opencell-api-dto/src/main/java/org/meveo/api/dto/catalog/EnableOfferTemplateDto.java
+++ opencell-api-dto/src/main/java/org/meveo/api/dto/catalog/EnableOfferTemplateDto.java
@@ -4,19 +4,24 @@ import java.util.Map;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import org.meveo.api.dto.BusinessEntityDto;
+import org.meveo.api.dto.BaseEntityDto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 
 @XmlRootElement(name = "EnableOfferTemplateDto")
 @XmlType(name = "EnableOfferTemplateDto")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class EnableOfferTemplateDto extends BusinessEntityDto{
+public class EnableOfferTemplateDto extends BaseEntityDto{
 
 	private static final long serialVersionUID = -5213351345129753071L;
 	
+	@XmlElement
+	@Schema(description = "Provided filters to filter Offer Template")
     private Map<String, Object> filters;
 
 	public Map<String, Object> getFilters() {
